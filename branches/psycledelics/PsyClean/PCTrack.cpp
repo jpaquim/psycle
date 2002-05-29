9b764526e7267f6ac6a154c9f798cd21b8f8864d
nobody <nobody@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2002-05-29 18:29:32 +0000

    This commit was manufactured by cvs2svn to create branch 'psycledelics'.

---
+++ branches/psycledelics/PsyClean/PCTrack.cpp
@@ -0,0 +1,27 @@
+// PCTrack.cpp: implementation of the PCTrack class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "PsyClean.h"
+#include "PCTrack.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+PCTrack::PCTrack()
+{
+
+}
+
+PCTrack::~PCTrack()
+{
+
+}
