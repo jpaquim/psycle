258c6ad7c8b0345ec7aba35c3b80690baf651c95
bernadat <bernadat@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2002-10-06 11:55:32 +0000

    cvs test

--- trunk/PsyClean/Song.h
+++ trunk/PsyClean/Song.h
@@ -1,3 +1,13 @@
-// The Song class stores:
+// Song class
+// The Song class shall store:
 // - Patterns
-// - Sequences, as [determine how sequences are stored in current psycle sources]
+// - Sequences
+// 
+
+
+
+// [JAZ]: the next step is defining the Song Class, and each one of its members's classes
+// Guillaume: ok, I make it my current task
+// [JAZ]: good. Don't worry if you do it right or not. you can check psycle's sourcecode for guideline, but I advise you that it is not very object oriented.
+// [JAZ]: Patterns should be defined as one, sequence surely too (since a sequence is no longer an array of numbers)
+
