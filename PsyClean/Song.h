1281c9df6b7705c0e202fcb3b52f1319c7d4ba27
bernadat <bernadat@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2002-10-06 11:57:24 +0000

    *** empty log message ***

--- PsyClean/Song.h
+++ PsyClean/Song.h
@@ -1,13 +1,5 @@
 // Song class
 // The Song class shall store:
 // - Patterns
-// - Sequences
-// 
-
-
-
-// [JAZ]: the next step is defining the Song Class, and each one of its members's classes
-// Guillaume: ok, I make it my current task
-// [JAZ]: good. Don't worry if you do it right or not. you can check psycle's sourcecode for guideline, but I advise you that it is not very object oriented.
-// [JAZ]: Patterns should be defined as one, sequence surely too (since a sequence is no longer an array of numbers)
-
+// - Sequences (array of references to patterns)
+// This class will probably have to interact with the document management classes of wxWindows
