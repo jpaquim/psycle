4fd83a2b0f06af62330d4c4efcd58ac5eee7db99
pooplog <pooplog@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2002-10-15 20:34:22 +0000

    Moved the hex to int function to a public place so everyone can use it

---
+++ trunk/Psycle/Helpers.h
@@ -0,0 +1,6 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+int _httoi(const TCHAR *value);
+
+#endif
--- trunk/Psycle/MidiInputDlg.cpp
+++ trunk/Psycle/MidiInputDlg.cpp
@@ -7,6 +7,7 @@
 #include "MidiInput.h"
 #include "Configuration.h"
 #include "MidiInputDlg.h"
+#include "Helpers.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -393,52 +394,6 @@ BOOL CMidiInputDlg::OnInitDialog()
 	return TRUE;  // return TRUE unless you set the focus to a control
 	              // EXCEPTION: OCX Property Pages should return FALSE
 }
-
-int _httoi(const TCHAR *value)
-{
-  struct CHexMap
-  {
-    TCHAR chr;
-    int value;
-  };
-  const int HexMapL = 16;
-  CHexMap HexMap[HexMapL] =
-  {
-    {'0', 0}, {'1', 1},
-    {'2', 2}, {'3', 3},
-    {'4', 4}, {'5', 5},
-    {'6', 6}, {'7', 7},
-    {'8', 8}, {'9', 9},
-    {'A', 10}, {'B', 11},
-    {'C', 12}, {'D', 13},
-    {'E', 14}, {'F', 15}
-  };
-  TCHAR *mstr = _tcsupr(_tcsdup(value));
-  TCHAR *s = mstr;
-  int result = 0;
-  if (*s == '0' && *(s + 1) == 'X') s += 2;
-  bool firsttime = true;
-  while (*s != '\0')
-  {
-    bool found = false;
-    for (int i = 0; i < HexMapL; i++)
-    {
-      if (*s == HexMap[i].chr)
-      {
-        if (!firsttime) result <<= 4;
-        result |= HexMap[i].value;
-        found = true;
-        break;
-      }
-    }
-    if (!found) break;
-    s++;
-    firsttime = false;
-  }
-  free(mstr);
-  return result;
-}
-
 
 void CMidiInputDlg::OnOK() 
 {
