4bfc461cd93a9f9a297d4a4681aff0848b1fe8f6
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2002-11-28 12:57:57 +0000

    file got wrongly overwritten. now it is correct.

--- trunk/Psycle/ChildView.cpp
+++ trunk/Psycle/ChildView.cpp
@@ -669,18 +669,20 @@ BOOL CChildView::OnFileSave(UINT id)
 				MessageBox("Error saving file", "Error", MB_OK);
 				bResult = FALSE;
 			}
-			else _pSong->_saved=true;
+			else 
+			{
+				_pSong->_saved=true;
+				if (pUndoList)
+				{
+					UndoSaved = pUndoList->counter;
+				}
+				else
+				{
+					UndoSaved = 0;
+				}
+				SetTitleBarText();
+			}				
 			file.Close();
-
-			if (pUndoList)
-			{
-				UndoSaved = pUndoList->counter;
-			}
-			else
-			{
-				UndoSaved = 0;
-			}
-			SetTitleBarText();
 		}
 		else 
 		{
@@ -764,21 +766,20 @@ BOOL CChildView::OnFileSavesong(UINT id)
 			else 
 			{
 				_pSong->_saved=true;
+				AppendToRecent(str.GetBuffer(1));
+				
+				if (pUndoList)
+				{
+					UndoSaved = pUndoList->counter;
+				}
+				else
+				{
+					UndoSaved = 0;
+				}
+				SetTitleBarText();
 			}
 			file.Close();
-
-			AppendToRecent(str.GetBuffer(1));
-			
-			if (pUndoList)
-			{
-				UndoSaved = pUndoList->counter;
-			}
-			else
-			{
-				UndoSaved = 0;
-			}
 		}
-		SetTitleBarText();
 	}
 	else
 	{
@@ -1974,87 +1975,87 @@ void CChildView::LoadMachineSkin()
 		MachineCoords.sMaster.x = 0;
 		MachineCoords.sMaster.y = 0;
 		MachineCoords.sMaster.width = 148;
-		MachineCoords.sMaster.height = 47;//48;
+		MachineCoords.sMaster.height = 48;
 
 		MachineCoords.sGenerator.x = 0;
-		MachineCoords.sGenerator.y = 47;//48;
+		MachineCoords.sGenerator.y = 48;
 		MachineCoords.sGenerator.width = 148;
-		MachineCoords.sGenerator.height = 47;//48;
+		MachineCoords.sGenerator.height = 48;
 		MachineCoords.sGeneratorVu0.x = 0;
-		MachineCoords.sGeneratorVu0.y = 141;//144;
-		MachineCoords.sGeneratorVu0.width = 2;//6;
-		MachineCoords.sGeneratorVu0.height = 4;//5;
-		MachineCoords.sGeneratorVuPeak.x = 128;//96;
-		MachineCoords.sGeneratorVuPeak.y = 141;//144;
-		MachineCoords.sGeneratorVuPeak.width = 2;//6;
-		MachineCoords.sGeneratorVuPeak.height = 4;//5;
-		MachineCoords.sGeneratorPan.x = 45;//102;
-		MachineCoords.sGeneratorPan.y = 145;//144;
-		MachineCoords.sGeneratorPan.width = 16;//24;
-		MachineCoords.sGeneratorPan.height = 5;//9;
-		MachineCoords.sGeneratorMute.x = 0;//133;
-		MachineCoords.sGeneratorMute.y = 145;//144;
-		MachineCoords.sGeneratorMute.width = 15;//7;
-		MachineCoords.sGeneratorMute.height = 14;//7;
-		MachineCoords.sGeneratorSolo.x = 15;//140;
-		MachineCoords.sGeneratorSolo.y = 145;//144;
-		MachineCoords.sGeneratorSolo.width = 15;//7;
-		MachineCoords.sGeneratorSolo.height = 14;//7;
+		MachineCoords.sGeneratorVu0.y = 144;
+		MachineCoords.sGeneratorVu0.width = 6;
+		MachineCoords.sGeneratorVu0.height = 5;
+		MachineCoords.sGeneratorVuPeak.x = 96;
+		MachineCoords.sGeneratorVuPeak.y = 144;
+		MachineCoords.sGeneratorVuPeak.width = 6;
+		MachineCoords.sGeneratorVuPeak.height = 5;
+		MachineCoords.sGeneratorPan.x = 21;
+		MachineCoords.sGeneratorPan.y = 149;
+		MachineCoords.sGeneratorPan.width = 24;
+		MachineCoords.sGeneratorPan.height = 9;
+		MachineCoords.sGeneratorMute.x = 7;
+		MachineCoords.sGeneratorMute.y = 149;
+		MachineCoords.sGeneratorMute.width = 7;
+		MachineCoords.sGeneratorMute.height = 7;
+		MachineCoords.sGeneratorSolo.x = 14;
+		MachineCoords.sGeneratorSolo.y = 149;
+		MachineCoords.sGeneratorSolo.width = 7;
+		MachineCoords.sGeneratorSolo.height = 7;
 
 		MachineCoords.sEffect.x = 0;
-		MachineCoords.sEffect.y = 94;//96;
+		MachineCoords.sEffect.y = 96;
 		MachineCoords.sEffect.width = 148;
-		MachineCoords.sEffect.height = 47;//48;
+		MachineCoords.sEffect.height = 48;
 		MachineCoords.sEffectVu0.x = 0;
-		MachineCoords.sEffectVu0.y = 141;//144;
-		MachineCoords.sEffectVu0.width = 2;//6;
-		MachineCoords.sEffectVu0.height = 4;//5;
-		MachineCoords.sEffectVuPeak.x = 128;//96;
-		MachineCoords.sEffectVuPeak.y = 141;//144;
-		MachineCoords.sEffectVuPeak.width = 2;//6;
-		MachineCoords.sEffectVuPeak.height = 4;//5;
-		MachineCoords.sEffectPan.x = 45;//102;
-		MachineCoords.sEffectPan.y = 145;//144;
-		MachineCoords.sEffectPan.width = 16;//24;
-		MachineCoords.sEffectPan.height = 5;//9;
-		MachineCoords.sEffectMute.x = 0;//133;
-		MachineCoords.sEffectMute.y = 145;//144;
-		MachineCoords.sEffectMute.width = 15;//7;
-		MachineCoords.sEffectMute.height = 14;//7;
-		MachineCoords.sEffectBypass.x = 30;//126;
-		MachineCoords.sEffectBypass.y = 145;//144;
-		MachineCoords.sEffectBypass.width = 15;//7;
-		MachineCoords.sEffectBypass.height = 14;//13;
+		MachineCoords.sEffectVu0.y = 144;
+		MachineCoords.sEffectVu0.width = 6;
+		MachineCoords.sEffectVu0.height = 5;
+		MachineCoords.sEffectVuPeak.x = 96;
+		MachineCoords.sEffectVuPeak.y = 144;
+		MachineCoords.sEffectVuPeak.width = 6;
+		MachineCoords.sEffectVuPeak.height = 5;
+		MachineCoords.sEffectPan.x = 21;
+		MachineCoords.sEffectPan.y = 149;
+		MachineCoords.sEffectPan.width = 24;
+		MachineCoords.sEffectPan.height = 9;
+		MachineCoords.sEffectMute.x = 7;
+		MachineCoords.sEffectMute.y = 149;
+		MachineCoords.sEffectMute.width = 7;
+		MachineCoords.sEffectMute.height = 7;
+		MachineCoords.sEffectBypass.x = 0;
+		MachineCoords.sEffectBypass.y = 149;
+		MachineCoords.sEffectBypass.width = 7;
+		MachineCoords.sEffectBypass.height = 12;
 
-		MachineCoords.dGeneratorVu.x = 10;//8;
-		MachineCoords.dGeneratorVu.y = 35;//3;
-		MachineCoords.dGeneratorVu.width = 130;//96;
+		MachineCoords.dGeneratorVu.x = 8;
+		MachineCoords.dGeneratorVu.y = 3;
+		MachineCoords.dGeneratorVu.width = 96;
 		MachineCoords.dGeneratorVu.height = 0;
-		MachineCoords.dGeneratorPan.x = 39;//3;
-		MachineCoords.dGeneratorPan.y = 26;//35;
-		MachineCoords.dGeneratorPan.width = 91;//117;
+		MachineCoords.dGeneratorPan.x = 3;
+		MachineCoords.dGeneratorPan.y = 35;
+		MachineCoords.dGeneratorPan.width = 117;
 		MachineCoords.dGeneratorPan.height = 0;
-		MachineCoords.dGeneratorMute.x = 11;//137;
-		MachineCoords.dGeneratorMute.y = 5;//4;
-		MachineCoords.dGeneratorSolo.x = 26;//137;
-		MachineCoords.dGeneratorSolo.y = 5;//17;
-		MachineCoords.dGeneratorName.x = 49;//10;
-		MachineCoords.dGeneratorName.y = 7;//12;
+		MachineCoords.dGeneratorMute.x = 137;
+		MachineCoords.dGeneratorMute.y = 4;
+		MachineCoords.dGeneratorSolo.x = 137;
+		MachineCoords.dGeneratorSolo.y = 17;
+		MachineCoords.dGeneratorName.x = 10;
+		MachineCoords.dGeneratorName.y = 12;
 
-		MachineCoords.dEffectVu.x = 10;//8;
-		MachineCoords.dEffectVu.y = 35;//3;
-		MachineCoords.dEffectVu.width = 130;//96;
+		MachineCoords.dEffectVu.x = 8;
+		MachineCoords.dEffectVu.y = 3;
+		MachineCoords.dEffectVu.width = 96;
 		MachineCoords.dEffectVu.height = 0;
-		MachineCoords.dEffectPan.x = 39;//3;
-		MachineCoords.dEffectPan.y = 26;//35;
-		MachineCoords.dEffectPan.width = 91;//117;
+		MachineCoords.dEffectPan.x = 3;
+		MachineCoords.dEffectPan.y = 35;
+		MachineCoords.dEffectPan.width = 117;
 		MachineCoords.dEffectPan.height = 0;
-		MachineCoords.dEffectMute.x = 11;//137;
-		MachineCoords.dEffectMute.y = 5;//4;
-		MachineCoords.dEffectBypass.x = 26;//137;
-		MachineCoords.dEffectBypass.y = 5;//15;
-		MachineCoords.dEffectName.x = 49;//10;
-		MachineCoords.dEffectName.y = 7;//12;
+		MachineCoords.dEffectMute.x = 137;
+		MachineCoords.dEffectMute.y = 4;
+		MachineCoords.dEffectBypass.x = 137;
+		MachineCoords.dEffectBypass.y = 15;
+		MachineCoords.dEffectName.x = 10;
+		MachineCoords.dEffectName.y = 12;
 		MachineCoords.bHasTransparency = FALSE;
 
 		machineskin.DeleteObject();
@@ -2627,33 +2628,33 @@ void CChildView::LoadPatternHeaderSkin()
 		PatHeaderCoords.sBackground.x=0;
 		PatHeaderCoords.sBackground.y=0;
 		PatHeaderCoords.sBackground.width=109;
-		PatHeaderCoords.sBackground.height=18;//16
+		PatHeaderCoords.sBackground.height=16;
 		PatHeaderCoords.sNumber0.x = 0;
-		PatHeaderCoords.sNumber0.y = 18;//16
+		PatHeaderCoords.sNumber0.y = 16;
 		PatHeaderCoords.sNumber0.width = 7;
 		PatHeaderCoords.sNumber0.height = 12;
 		PatHeaderCoords.sRecordOn.x = 70;
-		PatHeaderCoords.sRecordOn.y = 18;//16
-		PatHeaderCoords.sRecordOn.width = 11;//7;
-		PatHeaderCoords.sRecordOn.height = 11;//7;
-		PatHeaderCoords.sMuteOn.x = 81;//77;
-		PatHeaderCoords.sMuteOn.y = 18;//16;
-		PatHeaderCoords.sMuteOn.width = 11;//7;
-		PatHeaderCoords.sMuteOn.height = 11;//7;
-		PatHeaderCoords.sSoloOn.x = 92;//84;
-		PatHeaderCoords.sSoloOn.y = 18;//16;
-		PatHeaderCoords.sSoloOn.width = 11;//7;
-		PatHeaderCoords.sSoloOn.height = 11;//7;
-		PatHeaderCoords.dDigitX0.x = 24;//22;
-		PatHeaderCoords.dDigitX0.y = 3;//2;
-		PatHeaderCoords.dDigit0X.x = 31;//29;
-		PatHeaderCoords.dDigit0X.y = 3;//2;
+		PatHeaderCoords.sRecordOn.y = 16;
+		PatHeaderCoords.sRecordOn.width = 7;
+		PatHeaderCoords.sRecordOn.height = 7;
+		PatHeaderCoords.sMuteOn.x = 77;
+		PatHeaderCoords.sMuteOn.y = 16;
+		PatHeaderCoords.sMuteOn.width = 7;
+		PatHeaderCoords.sMuteOn.height = 7;
+		PatHeaderCoords.sSoloOn.x = 84;
+		PatHeaderCoords.sSoloOn.y = 16;
+		PatHeaderCoords.sSoloOn.width = 7;
+		PatHeaderCoords.sSoloOn.height = 7;
+		PatHeaderCoords.dDigitX0.x = 23;
+		PatHeaderCoords.dDigitX0.y = 2;
+		PatHeaderCoords.dDigit0X.x = 30;
+		PatHeaderCoords.dDigit0X.y = 2;
 		PatHeaderCoords.dRecordOn.x = 52;
-		PatHeaderCoords.dRecordOn.y = 3;//5;
+		PatHeaderCoords.dRecordOn.y = 5;
 		PatHeaderCoords.dMuteOn.x = 75;
-		PatHeaderCoords.dMuteOn.y = 3;//5;
-		PatHeaderCoords.dSoloOn.x = 97;//96;
-		PatHeaderCoords.dSoloOn.y = 3;//5;
+		PatHeaderCoords.dMuteOn.y = 5;
+		PatHeaderCoords.dSoloOn.x = 96;
+		PatHeaderCoords.dSoloOn.y = 5;
 		PatHeaderCoords.bHasTransparency = FALSE;
 
 		patternheader.DeleteObject();
