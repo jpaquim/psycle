8df633b6eb9f90f956220948db177954fe00996b
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2003-04-07 19:17:27 +0000

    added sources of Yannis midi plugin.

--- Psycle/Constants.h
+++ Psycle/Constants.h
@@ -1,7 +1,7 @@
 #ifndef __CONSTANTS_H
 #define __CONSTANTS_H
 
-#define VERSION_NUMBER "1.7.1"
+#define VERSION_NUMBER "1.7.2"
 
 #define TWEAK_SLIDE_SAMPLES		64 // number of samples per tweak slide update
 #define MAX_TWS					16 // # of tws commands that can be active on one machine
