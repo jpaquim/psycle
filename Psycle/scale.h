36eec774103498fda06ac8522c86ec8e9d56176f
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2003-12-17 04:38:46 +0000

    moved content of math.h into scale.h and deleted math.h

--- Psycle/scale.h
+++ Psycle/scale.h
@@ -1,11 +1,107 @@
 #ifndef PSYCLE__SCALE
 #define PSYCLE__SCALE
 #pragma once
-#include "math.h"
+#include <limits>
+#include <cmath>
+#include <vector>
 /// \file
 /// \brief scale facilities for machine parameters
 namespace psycle
 {
+	typedef double Real;
+	typedef float Sample;
+
+	namespace math
+	{
+		const Real pi(3.14159265358979323846);
+		namespace sample { const Sample pi(static_cast<Sample>(pi)); }
+
+		template<typename Real> inline const Real remainder(const Real & numerator, const Real & denominator)
+		{
+			Real result(numerator);
+			if(result >= denominator) result -= denominator;
+			if(result >= denominator) result = ::fmod(result, denominator);
+			return result;
+		}
+
+		template<typename Real> inline const Real clipped(const Real & minimum, const Real & sample, const Real & maximum)
+		{
+			if(minimum > sample) return minimum;
+			else if(sample > maximum) return maximum;
+			else return sample;
+		}
+
+		template<typename Real> inline const int rounded(const Real & x)
+		{
+			if(x > 0) return static_cast<int>(x + 0.5);
+			else return static_cast<int>(x - 0.5);
+		}
+
+		inline const int truncated(const Real & x)
+		{
+			const Real result(x - 0.5 + 6755399441055744); // 2^51 + 2^52
+			return *reinterpret_cast<const int *>(&result);
+		};
+
+		inline const int truncated(const Sample & x) 
+		{ 
+			const double half(0.5); 
+			int i; 
+			__asm 
+			{ 
+				fld x
+				fsub half
+				fistp i
+			}
+			return i;
+		}
+
+		inline const Sample log(const Sample & x) 
+		{ 
+			const int i(*reinterpret_cast<const int *>(&x));
+			return
+				(( i & 0x7f800000) >> 23)
+				+ (i & 0x007fffff) / Sample(0x800000)
+				-0x7f;
+		} 
+
+		template<typename Real> inline const bool denormal(const Real & x)
+		{
+			//return ::fabs(x) < std::numeric_limits<Real>::denorm_min();
+			return Real(::fabs(x)) < std::numeric_limits<Real>::epsilon();
+		}
+
+		inline const bool denormal(const Sample & x)
+		{
+			#if PSYCLE__DEBUG_LEVEL > 1
+			assert(sizeof x == sizeof(int)); 
+			assert(sizeof x == 4);
+			#endif
+			return !(*reinterpret_cast<const unsigned int *>(&x) & 0x7f800000);
+		}
+
+		template<typename Real> inline const Real renormalized(const Real & x)
+		{
+			return denormal(x) ? 0 : x;
+			
+		}
+
+		template<typename Real> inline const Real renormalize(Real & x)
+		{
+			x = renormalized(x);
+		}
+
+		inline void renormalize(Sample samples [], const int & sample_count)
+		{
+			float normal_dither(Sample(1e-20));
+			for(int sample = 0 ; sample < sample_count ; ++sample)
+			{
+				samples[sample] += normal_dither;
+				normal_dither = -normal_dither;
+			}
+		}
+	}
+
 	class Scale
 	{
 	public:
