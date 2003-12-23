84218a6d7056ace9c7753f895a39945bce3a27db
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2003-12-23 12:43:08 +0000

    no message

--- trunk/portable_psycle/src/pre_compiled_headers.h
+++ trunk/portable_psycle/src/pre_compiled_headers.h
@@ -57,7 +57,7 @@
 #include <cstring> // memset, memcpy, memmove
 #include <cassert>
 #include <csignal>
-#include <ctime> // clock_t clock() / CLOCKS_PER_SEC (\todo i don't know if this is as accurate as the performance counter)
+#include <ctime> // clock_t clock() / CLOCKS_PER_SEC (\todo not as accurate as the performance counter, CLOCKS_PER_SEC is only 1000)
 #include <exception>
 #include <typeinfo>
 #include <limits>
@@ -82,6 +82,27 @@
 #	include <sys/unistd.h>
 #endif
 
+///////////////
+// os-specific
+///////////////
+
+#if defined OPERATING_SYSTEM__MICROSOFT
+	// excludes rarely used stuff from windows headers:
+#	if !defined WIN32_LEAN_AND_MEAN
+#		define WIN32_LEAN_AND_MEAN
+#	endif
+#	if !defined VC_EXTRA_LEAN
+#		define VC_EXTRA_LEAN
+#	endif
+#	if !defined _AFXDLL
+		// when mfc is used we must not do the following ("explained" below)
+#		if !defined WIN32_EXTRA_LEAN
+#			define WIN32_EXTRA_LEAN // for mfc apps, we would get unresolved symbols
+#		endif
+#		include <windows.h> // for mfc apps,  we would get a fatal error C1189: #error :  WINDOWS.H already included.  MFC apps must not #include <windows.h>
+#	endif
+#endif
+
 //////////////////////////
 // boost http://boost.org
 //////////////////////////
@@ -93,13 +114,13 @@
 #	include <boost/thread/condition.hpp>
 #	include <boost/spirit.hpp>
 #	if defined COMPILER__MICROSOFT
-#		if !defined BOOST__PATH
-#			define BOOST__PATH "/usr/boost/libs/"
+#		if !defined LIBRARY__BOOST__PATH
+#			define LIBRARY__BOOST__PATH "/usr/lib/boost/libs/"
 #		endif
 #		if defined NDEBUG
-#			pragma comment(lib, BOOST__PATH "thread/build/bin-stage/boost_thread")
+#			pragma comment(lib, LIBRARY__BOOST__PATH "thread/build/bin-stage/boost_thread")
 #		else
-#			pragma comment(lib, BOOST__PATH "thread/build/bin-stage/boost_threadd")
+#			pragma comment(lib, LIBRARY__BOOST__PATH "thread/build/bin-stage/boost_threadd")
 #		endif
 #	endif
 #endif
@@ -109,34 +130,32 @@
 ////////////////////////
 
 #if defined OPERATING_SYSTEM__CROSSPLATFORM
-#	define FL_SHARED
+	// when compiling fltk dynamic shared library
+//#		define USE_CONF
+//#		define FL_DLL
+//#		define FL_LIBRARY
+//#		define FL_GL_LIBRARY
+//#		pragma comment(lib, "OpenGL32")
+//#		define FL_IMAGES_LIBRARY
+//#		define _MSC_DLL
+//#		define WIN32
+//#		define _WINDOWS
+//#		pragma comment(lib, "wsock32")
+#	if !defined FL_SHARED
+#		define FL_SHARED // when using fltk dynamic shared library
+#	endif
 #	if !defined _WIN32_WINNT
 #		define _WIN32_WINNT 0x0500
 #	endif
-#	if 0 // disabled since i did my own customized version of fltk
-#	if !defined FLTK__PATH
-#		define FLTK__PATH "/home/bohan/projects/cvs.sourceforge.net/fltk-2/fltk/lib/"
+#	if 0 // disabled since i've put fltk in the directory tree
+#	if !defined LIBRARY__FLTK__PATH
+#		define LIBRARY__FLTK__PATH "/home/bohan/projects/cvs.sourceforge.net/fltk/fltk/lib/"
 #	endif
 #	if defined NDEBUG
-#		pragma comment(lib, FLTK__PATH "fltkdll")
+#		pragma comment(lib, LIBRARY__FLTK__PATH "fltkdll")
 #	else
-#		pragma comment(lib, FLTK__PATH "fltkdlld")
+#		pragma comment(lib, LIBRARY__FLTK__PATH "fltkdlld")
 #	endif
-#	endif
-#endif
-
-///////////////
-// os-specific
-///////////////
-
-#if defined OPERATING_SYSTEM__MICROSOFT
-	// excludes rarely used stuff from windows headers:
-#	define WIN32_LEAN_AND_MEAN
-#	define VC_EXTRA_LEAN
-#	if !defined _AFXDLL
-		// when mfc is used we must not do the following
-#		define WIN32_EXTRA_LEAN // for mfc apps, we would get unresolved symbols
-#		include <windows.h> // for mfc apps,  we would get a fatal error C1189: #error :  WINDOWS.H already included.  MFC apps must not #include <windows.h>
 #	endif
 #endif
 
