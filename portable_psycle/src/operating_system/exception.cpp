9a4a47d5de09afab208c984a14738c5ae8da842b
zealmange <zealmange@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2004-05-26 02:04:24 +0000

    got formatmessage working

--- portable_psycle/src/operating_system/exception.cpp
+++ portable_psycle/src/operating_system/exception.cpp
@@ -25,50 +25,76 @@ namespace operating_system
 				s << "microsoft nt structured exception (external cpu/os exception): 0x" << std::hex << code << ": ";
 				switch(code)
 				{
-				case EXCEPTION_ACCESS_VIOLATION: s << "access violation: The thread tried to read from or write to a virtual address for which it does not have the appropriate access."; break;
-				case EXCEPTION_ARRAY_BOUNDS_EXCEEDED: s << "array bounds exceeded: The thread tried to access an array element that is out of bounds and the underlying hardware supports bounds checking."; break;
-				case EXCEPTION_BREAKPOINT: s << "breakpoint: A breakpoint was encountered."; break;
-				case EXCEPTION_DATATYPE_MISALIGNMENT: s << "datatype misalignment: The thread tried to read or write data that is misaligned on hardware that does not provide alignment. For example, 16-bit values must be aligned on 2-byte boundaries; 32-bit values on 4-byte boundaries, and so on."; break;
-				case EXCEPTION_FLT_DENORMAL_OPERAND: s << "denormal operand: One of the operands in a floating-point operation is denormal. A denormal value is one that is too small to represent as a standard floating-point value."; break;
-				case EXCEPTION_FLT_DIVIDE_BY_ZERO: s << "divive by zero: The thread tried to divide a floating-point value by a floating-point divisor of zero."; break;
-				case EXCEPTION_FLT_INEXACT_RESULT: s << "inexact result: The result of a floating-point operation cannot be represented exactly as a decimal fraction."; break;
-				case EXCEPTION_FLT_INVALID_OPERATION: s << "invalid operation: This exception represents any floating-point exception not included in this list."; break;
-				case EXCEPTION_FLT_OVERFLOW: s << "overflow: The exponent of a floating-point operation is greater than the magnitude allowed by the corresponding type."; break;
-				case EXCEPTION_FLT_STACK_CHECK: s << "stack check: The stack overflowed or underflowed as the result of a floating-point operation."; break;
-				case EXCEPTION_FLT_UNDERFLOW: s << "underflow: The exponent of a floating-point operation is less than the magnitude allowed by the corresponding type."; break;
-				case EXCEPTION_ILLEGAL_INSTRUCTION: s << "illegal instruction: The thread tried to execute an invalid instruction."; break;
-				case EXCEPTION_IN_PAGE_ERROR: s << "page error: The thread tried to access a page that was not present, and the system was unable to load the page. For example, this exception might occur if a network connection is lost while running a program over the network."; break;
-				case EXCEPTION_INT_DIVIDE_BY_ZERO: s << "int divide by zero: The thread tried to divide an integer value by an integer divisor of zero."; break;
-				case EXCEPTION_INT_OVERFLOW: s << "int overflow: The result of an integer operation caused a carry out of the most significant bit of the result."; break;
-				case EXCEPTION_INVALID_DISPOSITION: s << "invalid disposition: An exception handler returned an invalid disposition to the exception dispatcher. Programmers using a high-level language such as C should never encounter this exception."; break;
-				case EXCEPTION_NONCONTINUABLE_EXCEPTION: s << "noncontinuable exception: The thread tried to continue execution after a noncontinuable exception occurred."; break;
-				case EXCEPTION_PRIV_INSTRUCTION: s << "priv instruction: The thread tried to execute an instruction whose operation is not allowed in the current machine mode."; break;
-				case EXCEPTION_SINGLE_STEP: s << "single step: A trace trap or other single-instruction mechanism signaled that one instruction has been executed."; break;
-				case EXCEPTION_STACK_OVERFLOW: s << "stack overflow: The thread used up its stack."; break;
+				#ifdef C
+					#error macro clash
+				#endif
+				#define C(exception,description) case exception: s << #exception ": " description; break;
+
+				// see http://www.iseran.com/Win32/FAQ/except.html
+/*
+				// warnings
+				C(EXCEPTION_GUARD_PAGE,"")
+				C(EXCEPTION_DATATYPE_MISALIGNMENT, "The thread tried to read or write data that is misaligned on hardware that does not provide alignment. For example, 16-bit values must be aligned on 2-byte boundaries; 32-bit values on 4-byte boundaries, and so on.")
+				C(EXCEPTION_BREAKPOINT, "A breakpoint was encountered.")
+				C(EXCEPTION_SINGLE_STEP, "A trace trap or other single-instruction mechanism signaled that one instruction has been executed.")
+*/
+				// errors
+				C(EXCEPTION_ACCESS_VIOLATION, "The thread tried to read from or write to a virtual address for which it does not have the appropriate access.")
+/*				C(EXCEPTION_IN_PAGE_ERROR, "The thread tried to access a page that was not present, and the system was unable to load the page. For example, this exception might occur if a network connection is lost while running a program over the network.")
+				C(STATUS_INVALID_HANDLE,"An invalid handle was used in an api function call.")
+				C(STATUS_NO_MEMORY,"")
+				C(EXCEPTION_ILLEGAL_INSTRUCTION, "The thread tried to execute an invalid instruction.")
+				C(EXCEPTION_NONCONTINUABLE_EXCEPTION, "The thread tried to continue execution after a noncontinuable exception occurred.")
+				C(EXCEPTION_INVALID_DISPOSITION, "An exception handler returned an invalid disposition to the exception dispatcher. Programmers using a high-level language such as C should never encounter this exception.")
+				C(EXCEPTION_ARRAY_BOUNDS_EXCEEDED, "The thread tried to access an array element that is out of bounds and the underlying hardware supports bounds checking.")
+
+				C(EXCEPTION_FLT_DENORMAL_OPERAND, "One of the operands in a floating-point operation is denormal. A denormal value is one that is too small to represent as a standard floating-point value.")
+				C(EXCEPTION_FLT_DIVIDE_BY_ZERO, "The thread tried to divide a floating-point value by a floating-point divisor of zero.")
+				C(EXCEPTION_FLT_INEXACT_RESULT, "The result of a floating-point operation cannot be represented exactly as a decimal fraction.")
+				C(EXCEPTION_FLT_INVALID_OPERATION, "This exception represents any floating-point exception not included in this list.")
+				C(EXCEPTION_FLT_OVERFLOW, "The exponent of a floating-point operation is greater than the magnitude allowed by the corresponding type.")
+				C(EXCEPTION_FLT_STACK_CHECK, "The stack overflowed or underflowed as the result of a floating-point operation.")
+				C(EXCEPTION_FLT_UNDERFLOW, "The exponent of a floating-point operation is less than the magnitude allowed by the corresponding type.")
+				C(EXCEPTION_INT_DIVIDE_BY_ZERO, "The thread tried to divide an integer value by an integer divisor of zero.")
+				C(EXCEPTION_INT_OVERFLOW, "The result of an integer operation caused a carry out of the most significant bit of the result.")
+				C(EXCEPTION_PRIV_INSTRUCTION, "The thread tried to execute an instruction whose operation is not allowed in the current machine mode.")
+				C(EXCEPTION_STACK_OVERFLOW, "The thread used up its stack.")
+				C(STATUS_CONTROL_C_EXIT,"")
+
+				C(0xE06D7363,"Visual C++ exception: The SEH exception used by the c++ try/catch/throw mechanism in Visual C++")
+*/
+#undef C
 				default:
 					// <magnus> FormatMessage doesn't really seem to succeed but it couldn't hurt, could it?
 					// is there any other system routine that can format a nice message?
-					LPVOID lpMsgBuf;
-					if (!FormatMessage( 
-						FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-						FORMAT_MESSAGE_FROM_SYSTEM | 
-						FORMAT_MESSAGE_IGNORE_INSERTS,
-						NULL,
+					DWORD flags=FORMAT_MESSAGE_ALLOCATE_BUFFER
+						| FORMAT_MESSAGE_FROM_SYSTEM
+						| FORMAT_MESSAGE_IGNORE_INSERTS;
+					LPVOID lpMsgBuf=NULL;
+					HMODULE hModule=LoadLibraryExA("NTDLL.DLL",NULL,LOAD_LIBRARY_AS_DATAFILE);
+					if(hModule != NULL)
+						flags |= FORMAT_MESSAGE_FROM_HMODULE;
+
+					if (!FormatMessageA(
+						flags,
+						hModule,
 						code,
 						MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
-						(LPTSTR) &lpMsgBuf,
+						(LPSTR) &lpMsgBuf,
 						0,
 						NULL ))
 					{
 						s << "unkown exception code: 0x" << std::hex << code << " !!!";
 					}
 					else {
-						s << lpMsgBuf;
+						s << (const char *)lpMsgBuf;
 						// Free the buffer.
-						LocalFree( lpMsgBuf );
+						
 					}
-
-
+					if(lpMsgBuf != NULL)
+						LocalFree( lpMsgBuf );
+					if(hModule != NULL)
+						FreeLibrary(hModule);
 				}
 				return s.str();
 			}
