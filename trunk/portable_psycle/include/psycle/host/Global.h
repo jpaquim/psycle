4438e834398cb50bab0bb288aeec9b6a65230be5
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2004-07-22 00:01:59 +0000

    no message

--- trunk/portable_psycle/include/psycle/host/Global.h
+++ trunk/portable_psycle/include/psycle/host/Global.h
@@ -68,7 +68,7 @@ namespace psycle
 	/// Safer version of delete that clears the pointer automatically. Don't use it for arrays!
 	/// \param pointer pointer to single object to be deleted.
 	/// \param new_value the new value pointer will be set to. By default it is null.
-	inline template<typename single_object> single_object * zapObject(single_object *& pointer, single_object * const new_value = 0)
+	template<typename single_object> inline single_object * zapObject(single_object *& pointer, single_object * const new_value = 0)
 	{
 		if(pointer) delete pointer;
 		return pointer = new_value;
@@ -77,7 +77,7 @@ namespace psycle
 	/// Safer version of delete[] that clears the pointer automatically. Only use it for arrays!
 	/// \param pointer pointer to array to be deleted.
 	/// \param new_value the new value pointer will be set to. By default it is null.
-	inline template<typename object_array> object_array * zapArray(object_array *& pointer, object_array * const new_value = 0)
+	template<typename object_array> inline object_array * zapArray(object_array *& pointer, object_array * const new_value = 0)
 	{
 		if(pointer) delete [] pointer;
 		return pointer = new_value;
