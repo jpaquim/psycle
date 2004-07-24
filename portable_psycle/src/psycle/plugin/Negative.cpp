a4ebfb16c0899acb9f118652a551361afbce07a5
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2004-07-24 17:27:10 +0000

    no message

--- portable_psycle/src/psycle/plugin/Negative.cpp
+++ portable_psycle/src/psycle/plugin/Negative.cpp
@@ -3,7 +3,6 @@
 /// \file
 /// \brief just a multiplier
 namespace psycle { namespace plugin {
-
 class Negative : public Plugin
 {
 public:
@@ -11,21 +10,17 @@ public:
 	{
 		out << "just a Negative (out = -in)" << std::endl;
 	}
-
 	static const Information & information() throw()
 	{
 		static const Information information(Information::Type::effect, "Negative", "Negative", "who cares", 1, 0, 0);
 		return information;
 	}
-
 	Negative() : Plugin(information()) {}
 	virtual void process(Sample l[], Sample r[], int samples, int);
 protected:
 	inline void process(Sample &);
 };
-
 PSYCLE__PLUGIN__INSTANCIATOR(Negative);
-
 void Negative::process(Sample l[], Sample r[], int sample, int)
 {
 	while(sample--)
@@ -34,10 +29,8 @@ void Negative::process(Sample l[], Sample r[], int sample, int)
 		process(r[sample]);
 	}
 }
-
 inline void Negative::process(Sample & sample)
 {
 	sample = -sample;
 }
-
 }}
