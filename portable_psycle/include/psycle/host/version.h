0f6c12ec53c258de4b01134afacdc011ea75eb29
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-01-19 23:16:55 +0000

    no more .rar binary distro, it's 7-zip now :-)

--- portable_psycle/include/psycle/host/version.h
+++ portable_psycle/include/psycle/host/version.h
@@ -3,6 +3,9 @@
 #if 0
 /*
 $Log$
+Revision 1.9  2005/01/19 23:16:55  johan-boule
+no more .rar binary distro, it's 7-zip now :-)
+
 Revision 1.8  2005/01/19 23:11:11  johan-boule
 no message
 
@@ -48,7 +51,7 @@ fix closing bug [ 1087782 ] psycle MFC's version number is spread in several pla
 #define PSYCLE__VERSION__CODEBASE 1
 #define PSYCLE__VERSION__MAJOR 7
 #define PSYCLE__VERSION__MINOR 7
-#define PSYCLE__VERSION__PATCH 21 /* $Revision$ $Date$ */
+#define PSYCLE__VERSION__PATCH 22 /* $Revision$ $Date$ */
 #define PSYCLE__VERSION__QUALITY "alpha"
 
 #define PSYCLE__VERSION \
