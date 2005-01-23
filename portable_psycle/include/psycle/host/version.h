a6949d642659915c12b0c6c12694fbefba8a9d01
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-01-23 10:36:06 +0000

    version numbering again, huhu.

--- portable_psycle/include/psycle/host/version.h
+++ portable_psycle/include/psycle/host/version.h
@@ -3,6 +3,9 @@
 #if 0
 /*
 $Log$
+Revision 1.17  2005/01/23 10:36:05  johan-boule
+version numbering again, huhu.
+
 Revision 1.16  2005/01/23 10:07:17  johan-boule
 typo
 
@@ -59,14 +62,12 @@ fix closing bug [ 1087782 ] psycle MFC's version number is spread in several pla
 /// Versions are composed this way:
 /// C.M.m.p , where:
 /// - C = codebase generation.
-/// - M = major version number:
-///			- if even, then it's a stable release.
-///			- if odd, then it's a beta or release candidate for next major version number.
+/// - M = major version number.
 /// - m = minor version number:
-///			- if the major version number is even (stable release), then this minor version number must be 0.
-///			- if the major version number is odd (beta or RC), then this minor version number can be incremented at will to extol important changes that might be worth upgrading (contrary to patches).
+///			- if even, then it's a stable release.
+///			- if odd, then it's a beta or release candidate for next major version number, can be incremented at will to extol important changes that might be worth upgrading (contrary to patches).
 /// - p = patch number:
-///			- if the major version number is even (stable release), then the minor version number is always 0, and this patch number is used for bug fixes.
+///			- if the major version number is even (stable release), then this patch number is used for bug fixes, so, it's hopefully rarely incremented.
 ///			- if the major version number is odd (beta or RC), then this patch number is incremented very often, each cvs commit.
 
 /// Other files that need to be updated accordingly:
