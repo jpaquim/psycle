3c24d3498ce534488dbc2fe49522e06d363f4248
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-01-29 19:08:32 +0000

    modernified strongstructs.hpp

--- trunk/portable_psycle/src/psycle/host/SongStructs.hpp
+++ trunk/portable_psycle/src/psycle/host/SongStructs.hpp
@@ -1,76 +1,88 @@
-#ifndef _SONGSTRUCTS_H
-#define _SONGSTRUCTS_H
-
-#pragma pack(push, 1)
-typedef struct PatternEntry
+#pragma once
+#include <project.hpp>
+namespace psycle
 {
-	PatternEntry()
-	{ _note=0;_inst=0;_mach=0;_cmd=0;_parameter=0;/*_volcmd=0;_volume=0;*/
+	namespace host
+	{
+		#pragma pack(push, 1)
+
+		class PatternEntry
+		{
+			public:
+				inline PatternEntry()
+				:
+					_note(0),
+					_inst(0),
+					_mach(0),
+					_cmd(0),
+					_parameter(0)
+					//_volcmd(0),
+					//_volume(0)
+				{
+				}
+				compiler::uint8 _note;
+				compiler::uint8 _inst;
+				compiler::uint8 _mach;
+				compiler::uint8 _cmd;
+				compiler::uint8 _parameter;
+				//compiler::uint8 _volcmd;	// Unimplemented for now. Used by XMSampler
+				//compiler::uint8 _volume;	// Unimplemented for now. Used by XMSampler
+		};
+
+		// Patterns are organized in lines by rows,
+		// i.e. the first TRACK*sizeof(Entry) bytes
+		// belong to the first line.
+		#pragma warning(disable:4200) // warning C4200: nonstandard extension used : zero-sized array in struct/union; Cannot generate copy-ctor or copy-assignment operator when UDT contains a zero-sized array
+		class Pattern
+		{
+			public:
+				PatternEntry _data[];
+		};
+		#pragma warning(default:4200)
+
+		enum MachineType
+		{
+			MACH_UNDEFINED = -1,
+			MACH_MASTER = 0,
+				MACH_SINE = 1,
+				MACH_DIST = 2,
+				MACH_SAMPLER = 3,
+				MACH_DELAY = 4,
+				MACH_2PFILTER = 5,
+				MACH_GAIN = 6,
+				MACH_FLANGER = 7,
+				MACH_PLUGIN = 8,
+				MACH_VST = 9,
+				MACH_VSTFX = 10,
+				MACH_SCOPE = 11,
+				MACH_XMSAMPLER = 12,
+				MACH_DUMMY = 255
+		};
+
+		enum MachineMode
+		{
+			MACHMODE_UNDEFINED = -1,
+			MACHMODE_GENERATOR = 0,
+			MACHMODE_FX = 1,
+			MACHMODE_MASTER = 2,
+		};
+
+		class PatternCmd
+		{
+			public:
+				static const compiler::uint8 PCMD_VELOCITY = 0xc;
+				static const compiler::uint8 PCMD_PATTERNBREAK = 0xd;
+				static const compiler::uint8 PCMD_OLD_MIDI = 0x10;
+				static const compiler::uint8 PCMD_NOTECUT = 0xe;
+				static const compiler::uint8 PCMD_PAN = 0xf8; 
+				static const compiler::uint8 PCMD_RETRIG_CONTINUE = 0xfa; 
+				static const compiler::uint8 PCMD_RETRIG = 0xfb; 
+				static const compiler::uint8 PCMD_VOLCHG = 0xfc;
+				static const compiler::uint8 PCMD_DELAY = 0xfd;
+				static const compiler::uint8 PCMD_CHGTPB = 0xfe;
+				static const compiler::uint8 PCMD_CHGBPM = 0xff;
+		};
+
+		#pragma pack(pop)
 	}
-	UCHAR _note;
-	UCHAR _inst;
-	UCHAR _mach;
-	UCHAR _cmd;
-	UCHAR _parameter;
-//	UCHAR _volcmd;	// Unimplemented for now. Used by XMSampler
-//	UCHAR _volume;	// Unimplemented for now. Used by XMSampler
 }
-PatternEntry;
-
-// Patterns are organized in lines by rows,
-// i.e. the first TRACK*sizeof(Entry) bytes
-// belong to the first line.
-#pragma warning(disable : 4200)
-typedef struct
-{
-	PatternEntry _data[];
-}
-Pattern;
-
-typedef enum
-{
-	MACH_UNDEFINED = -1,
-	MACH_MASTER = 0,
-		MACH_SINE = 1,
-		MACH_DIST = 2,
-		MACH_SAMPLER = 3,
-		MACH_DELAY = 4,
-		MACH_2PFILTER = 5,
-		MACH_GAIN = 6,
-		MACH_FLANGER = 7,
-		MACH_PLUGIN = 8,
-		MACH_VST = 9,
-		MACH_VSTFX = 10,
-		MACH_SCOPE = 11,
-		MACH_XMSAMPLER = 12,
-		MACH_DUMMY = 255
-}
-MachineType;
-
-typedef enum
-{
-	MACHMODE_UNDEFINED = -1,
-	MACHMODE_GENERATOR = 0,
-	MACHMODE_FX = 1,
-	MACHMODE_MASTER = 2,
-}
-MachineMode;
-
-struct PatternCmd {
-	static const UCHAR PCMD_VELOCITY = 0xc;
-	static const UCHAR PCMD_PATTERNBREAK = 0xd;
-	static const UCHAR PCMD_OLD_MIDI = 0x10;
-	static const UCHAR PCMD_NOTECUT = 0xe;
-	static const UCHAR PCMD_PAN = 0xf8; 
-	static const UCHAR PCMD_RETRIG_CONTINUE = 0xfa; 
-	static const UCHAR PCMD_RETRIG = 0xfb; 
-	static const UCHAR PCMD_VOLCHG = 0xfc;
-	static const UCHAR PCMD_DELAY = 0xfd;
-	static const UCHAR PCMD_CHGTPB = 0xfe;
-	static const UCHAR PCMD_CHGBPM = 0xff;
-};
-
-#pragma warning(default : 4200)
-#pragma pack(pop)
-
-#endif
