aacf706d63ba8aef2439b30f0929bb226c03d9e3
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-01-31 10:01:31 +0000

    MFC crap

--- trunk/portable_psycle/src/pre_compiled_headers.mfc.private.hpp
+++ trunk/portable_psycle/src/pre_compiled_headers.mfc.private.hpp
@@ -22,9 +22,9 @@
 // mfc (aka afx)
 
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
-#define _AFX_ALL_WARNINGS // turns off MFC's hiding of some common and often safely ignored warning messages
+//#define _AFX_ALL_WARNINGS // turns off MFC's hiding of some common and often safely ignored warning messages
 #include <afxwin.h> // MFC core and standard components
-//#include <afxext.h> // MFC extensions
+#include <afxext.h> // MFC extensions
 //#include <afxdisp.h> // MFC Automation classes
 #include <afxdtctl.h> // MFC support for Internet Explorer 4 Common Controls
 #if !defined _AFX_NO_AFXCMN_SUPPORT
