64d38d1fa44399ea5b10c30c51c8978414858c0d
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-03-04 03:39:00 +0000

    added .cvsignore files

---
+++ trunk/portable_psycle/create-cvsignore-files.cmd
@@ -0,0 +1,12 @@
+echo .cvsignore> make\msvc_7.1\.cvsignore
+echo *.suo>> make\msvc_7.1\.cvsignore
+echo *.ncb>> make\msvc_7.1\.cvsignore
+echo output>> make\msvc_7.1\.cvsignore
+
+echo .cvsignore> include\.cvsignore
+echo boost>> include\.cvsignore
+
+echo .cvsignore> doc/.cvsignore
+echo doxygen.mfc>> doc/.cvsignore
+
+pause
