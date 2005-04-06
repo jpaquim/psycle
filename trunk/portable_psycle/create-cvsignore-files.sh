fd29df75e33ac1c83c45a82239142433c9024795
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-04-06 00:12:20 +0000

    *** empty log message ***

--- trunk/portable_psycle/create-cvsignore-files.sh
+++ trunk/portable_psycle/create-cvsignore-files.sh
@@ -2,22 +2,21 @@
 
 function main
 {
+	cd $(dirname $0) &&
 	(
 		echo .cvsignore
 		echo \*.suo
 		echo \*.ncb
 		echo output
-	) > make\msvc_7.1\.cvsignore
-	
+	) > make\msvc_7.1\.cvsignore &&
 	(
 		echo .cvsignore
 		echo boost
-	) > include\.cvsignore
-	
+	) > include\.cvsignore &&
 	(
 		echo .cvsignore
 		echo doxygen.mfc
 	) > doc/.cvsignore
-}
+} &&
 
 main "$@"
