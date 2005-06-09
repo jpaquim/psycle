d546ba78f8bc4356b8ab5594d0fd2f57a485a26b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-06-09 22:49:00 +0000

    just increasing the version number to reflect the presence of a bug fix concerning loading of skins

--- portable_psycle/src/psycle/host/version.hpp
+++ portable_psycle/src/psycle/host/version.hpp
@@ -3,6 +3,9 @@
 #if 0
 /*
 $Log$
+Revision 1.37  2005/06/09 22:49:00  johan-boule
+just increasing the version number to reflect the presence of a bug fix concerning loading of skins
+
 Revision 1.36  2005/06/07 18:16:55  johan-boule
 version 1.7.54
 oops, fix for bug introduced in 1.7.53 concerning midi device selection in the configuration
@@ -184,7 +187,7 @@ fix closing bug [ 1087782 ] psycle MFC's version number is spread in several pla
 #define PSYCLE__LICENSE "none, public domain"
 #define PSYCLE__VERSION__MAJOR 1
 #define PSYCLE__VERSION__MINOR 7
-#define PSYCLE__VERSION__PATCH 54 /* $Revision$ $Date$ */
+#define PSYCLE__VERSION__PATCH 55 /* $Revision$ $Date$ */
 #define PSYCLE__VERSION__QUALITY "alpha"
 
 /// identifies what sources the build comes from.
