add7667b5f0f40089ef628c0505cbaba0c345e57
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-06-20 22:31:40 +0000

    *** empty log message ***

--- trunk/portable_psycle/src/universalis/compiler/detail/pragmatic/pragmas.hpp
+++
@@ -1,8 +0,0 @@
-// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright (C) 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
-
-///\file
-#pragma once
-#include <universalis/detail/project.hpp>
-
-// arch-tag: ba0ca2d8-2d3c-4368-90ca-e370523a04e9
