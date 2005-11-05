b50e97091c04cebc805178dd8d2d334e47497d7d
nobody <nobody@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-11-05 15:36:16 +0000

    This commit was manufactured by cvs2svn to create tag 'release_1_8_0_stable'.

---
+++ tags/release_1_8_0_stable/portable_psycle/src/psycle/host/ConfigDlg.hpp
@@ -0,0 +1,50 @@
+///\file
+///\brief interface file for psycle::host::CConfigDlg.
+#pragma once
+#include "DirectoryDlg.hpp"
+#include "SkinDlg.hpp"
+#include "OutputDlg.hpp"
+#include "MidiInputDlg.hpp"
+#include "Configuration.hpp"
+#include "KeyConfigDlg.hpp"
+NAMESPACE__BEGIN(psycle)
+	NAMESPACE__BEGIN(host)
+		/// config window.
+		class CConfigDlg : public CPropertySheet
+		{
+			DECLARE_DYNAMIC(CConfigDlg)
+		public:
+			CConfigDlg(UINT nIDCaption, CWnd* pParentWnd = NULL, UINT iSelectPage = 0);
+			CConfigDlg(LPCTSTR pszCaption, CWnd* pParentWnd = NULL, UINT iSelectPage = 0);
+		// Attributes
+		public:
+			CDirectoryDlg _dirDlg;
+			CSkinDlg _skinDlg;
+			COutputDlg _outputDlg;
+			CMidiInputDlg _midiDlg;
+			CKeyConfigDlg _keyDlg;
+		// Operations
+		public:
+			void Init(Configuration* pConfig,int dlgnum=0);
+		// Overrides
+			// ClassWizard generated virtual function overrides
+			//{{AFX_VIRTUAL(CConfigDlg)
+			public:
+			virtual int DoModal();
+			//}}AFX_VIRTUAL
+		// Implementation
+		public:
+			virtual ~CConfigDlg();
+			// Generated message map functions
+		protected:
+			Configuration* _pConfig;
+			//{{AFX_MSG(CConfigDlg)
+ 				// NOTE - the ClassWizard will add and remove member functions here.
+			//}}AFX_MSG
+			DECLARE_MESSAGE_MAP()
+		};
+
+		//{{AFX_INSERT_LOCATION}}
+		// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+	NAMESPACE__END
+NAMESPACE__END
---
+++ tags/release_1_8_0_stable/portable_psycle/src/psycle/host/DSoundConfig.hpp
@@ -0,0 +1,52 @@
+///\file
+///\interface psycle::host::CDSoundConfig
+#pragma once
+#include <afxwin.h>
+#include <afxcmn.h> // CSpinButtonCtrl
+#include "resources/resources.hpp"
+NAMESPACE__BEGIN(psycle)
+	NAMESPACE__BEGIN(host)
+		/// direct sound config window.
+		class CDSoundConfig : public CDialog
+		{
+				DECLARE_MESSAGE_MAP()
+			public:
+				CDSoundConfig(CWnd * pParent = 0);
+				int const static IDD = IDD_DSOUNDCONFIG;
+				CButton         m_exclusiveCheck;
+				CStatic         m_latency;
+				CSpinButtonCtrl m_numBuffersSpin;
+				CSpinButtonCtrl m_bufferSizeSpin;
+				CButton         m_ditherCheck;
+				CEdit           m_bufferSizeEdit;
+				CEdit           m_numBuffersEdit;
+				CComboBox       m_sampleRateCombo;
+				CComboBox       m_deviceComboBox;
+			protected:
+				virtual void DoDataExchange(CDataExchange* pDX);
+			private:
+				virtual BOOL OnInitDialog();
+				virtual void OnOK();
+				afx_msg void OnSelendokSamplerate();
+				afx_msg void OnChangeBufnumEdit();
+				afx_msg void OnChangeBufsizeEdit();
+				afx_msg void OnSelchangeDevice();
+				afx_msg void OnDestroy();
+				afx_msg void OnExclusive();
+				afx_msg void OnDither();
+			public:
+				GUID              device_guid;
+				int               sample_rate;
+				int               buffer_count;
+				int  const static buffer_count_min = 2;
+				int  const static buffer_count_max = 16;
+				int               buffer_size;
+				int  const static buffer_size_min = 512;
+				int  const static buffer_size_max = 32768;
+				bool              dither;
+				bool              exclusive;
+			private:
+				void RecalcLatency();
+		};
+	NAMESPACE__END
+NAMESPACE__END
