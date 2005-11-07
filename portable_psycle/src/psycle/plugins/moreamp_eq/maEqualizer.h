421771bf445116adaa87329ce967b2362ccdf1fd
sartorius <sartorius@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-11-07 08:35:59 +0000

    *** empty log message ***

--- portable_psycle/src/psycle/plugins/moreamp_eq/maEqualizer.h
+++ portable_psycle/src/psycle/plugins/moreamp_eq/maEqualizer.h
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////
 // Modified for PSYCLE by Sartorius
 //
+// you can try to use eq-xmms's coeffs instead of MoreAmp's.
 
 // maEqualizer.h
 
@@ -30,12 +31,8 @@
 #define EQBANDS31 31					// larger number of pcm equalizer bands (sliders)
 #define EQBANDS10 10					// lesser number of pcm equalizer bands
 
-#define EQSLIDERFIRST 0					// first pcm equalizer slider index
-#define EQSLIDERLAST (EQBANDS31 - 1)	// last  pcm equalizer slider index
-#define EQNAMELEN 32					// max length of name of equalizer preset
 #define EQSLIDERMAX 64					// pcm equalizer slider limits
 #define EQSLIDERMIN 0
-#define EQNUMPRESETS 16					// sets of pcm equalizer slider values
 
 #define EQ_MAX_BANDS EQBANDS31
 #define EQ_CHANNELS 2
@@ -47,12 +44,27 @@ typedef struct
 	float gamma;
 }sIIRCoefficients;
 
+/*
+typedef struct 
+{
+	double beta;
+	double alpha; 
+	double gamma;
+}sIIRCoefficients;
+*/
 /* Coefficient history for the IIR filter */
 typedef struct
 {
 	float x[3]; /* x[n], x[n-1], x[n-2] */
 	float y[3]; /* y[n], y[n-1], y[n-2] */
 }sXYData;
+
+//typedef struct
+//{
+//	double x[3]; /* x[n], x[n-1], x[n-2] */
+//	double y[3]; /* y[n], y[n-1], y[n-2] */
+//}sXYData;
+
 
 static sIIRCoefficients iir_cf10[]
 = {
@@ -65,8 +77,23 @@ static sIIRCoefficients iir_cf10[]
 	{ (float)(8.1778971701e-01), (float)(9.1105141497e-02), (float)(1.7444877599e+00) },	//  2000.0 Hz
 	{ (float)(6.6857185264e-01), (float)(1.6571407368e-01), (float)(1.4048592171e+00) },	//  4000.0 Hz
 	{ (float)(4.4861333678e-01), (float)(2.7569333161e-01), (float)(6.0518718075e-01) },	//  8000.0 Hz
-	{ (float)(2.4201241845e-01), (float)(3.7899379077e-01), (float)(-8.0847117831e-01) },	// 16000.0 Hz
+	{ (float)(2.4201241845e-01), (float)(3.7899379077e-01), (float)(-8.0847117831e-01) }	// 16000.0 Hz
 };
+
+/* //from eq-xmms
+static sIIRCoefficients iir_cf10[] = {
+{ 9.9688176273e-01, 1.5591186337e-03, 1.9968622855e+00 },
+{ 9.9377323686e-01, 3.1133815717e-03, 1.9936954495e+00 },
+{ 9.8748575691e-01, 6.2571215431e-03, 1.9871705722e+00 },
+{ 9.7512812040e-01, 1.2435939802e-02, 1.9738753198e+00 },
+{ 9.5087485437e-01, 2.4562572817e-02, 1.9459267562e+00 },
+{ 9.0416308662e-01, 4.7918456688e-02, 1.8848691023e+00 },
+{ 8.1751373987e-01, 9.1243130064e-02, 1.7442229115e+00 },
+{ 6.6840529852e-01, 1.6579735074e-01, 1.4047189863e+00 },
+{ 4.4858358977e-01, 2.7570820511e-01, 6.0517475334e-01 },
+{ 2.4198119087e-01, 3.7900940457e-01, -8.0845085113e-01 },
+};
+*/
 
 static sIIRCoefficients iir_cf31[]
 = {
@@ -100,9 +127,45 @@ static sIIRCoefficients iir_cf31[]
 	{ (float)(7.2627335006e-01), (float)(1.3686332497e-01), (float)(2.5120594308e-01) },	// 10000.0 Hz
 	{ (float)(6.7690084582e-01), (float)(1.6154957709e-01), (float)(-3.4981568641e-01) },	// 12500.0 Hz
 	{ (float)(6.2492532874e-01), (float)(1.8753733563e-01), (float)(-1.0577231561e+00) },	// 16000.0 Hz
-	{ (float)(6.1776462189e-01), (float)(1.9111768906e-01), (float)(-1.5492495659e+00) },	// 20000.0 Hz
+	{ (float)(6.1776462189e-01), (float)(1.9111768906e-01), (float)(-1.5492495659e+00) }	// 20000.0 Hz
 };
 
+/* // from eq-xmms
+static sIIRCoefficients iir_cf31[]  = {
+
+{ 9.9934037157e-01, 3.2981421662e-04, 1.9993322545e+00 },
+{ 9.9917555233e-01, 4.1222383516e-04, 1.9991628705e+00 },
+{ 9.9896129025e-01, 5.1935487310e-04, 1.9989411587e+00 },
+{ 9.9868118265e-01, 6.5940867495e-04, 1.9986487252e+00 },
+{ 9.9835175161e-01, 8.2412419683e-04, 1.9983010452e+00 },
+{ 9.9792365217e-01, 1.0381739160e-03, 1.9978431682e+00 },
+{ 9.9736411067e-01, 1.3179446674e-03, 1.9972343673e+00 },
+{ 9.9670622662e-01, 1.6468866919e-03, 1.9965035707e+00 },
+{ 9.9588448566e-01, 2.0577571681e-03, 1.9955679690e+00 },
+{ 9.9473519326e-01, 2.6324033689e-03, 1.9942169198e+00 },
+{ 9.9342335280e-01, 3.2883236020e-03, 1.9926141028e+00 },
+{ 9.9178600786e-01, 4.1069960678e-03, 1.9905226414e+00 },
+{ 9.8966154150e-01, 5.1692292513e-03, 1.9876580847e+00 },
+{ 9.8689036168e-01, 6.5548191616e-03, 1.9836646251e+00 },
+{ 9.8364027156e-01, 8.1798642207e-03, 1.9786090689e+00 },
+{ 9.7943153305e-01, 1.0284233476e-02, 1.9714629236e+00 },
+{ 9.7395577681e-01, 1.3022111597e-02, 1.9611472340e+00 },
+{ 9.6755437936e-01, 1.6222810321e-02, 1.9476180811e+00 },
+{ 9.5961458750e-01, 2.0192706249e-02, 1.9286193446e+00 },
+{ 9.4861481164e-01, 2.5692594182e-02, 1.8982024567e+00 },
+{ 9.3620971896e-01, 3.1895140519e-02, 1.8581325022e+00 },
+{ 9.2095325455e-01, 3.9523372724e-02, 1.8003794694e+00 },
+{ 9.0153642498e-01, 4.9231787512e-02, 1.7132251201e+00 },
+{ 8.7685876255e-01, 6.1570618727e-02, 1.5802270232e+00 },
+{ 8.4886734822e-01, 7.5566325889e-02, 1.3992391376e+00 },
+{ 8.1417575446e-01, 9.2912122771e-02, 1.1311200817e+00 },
+{ 7.7175298860e-01, 1.1412350570e-01, 7.4018523020e-01 },
+{ 7.2627049462e-01, 1.3686475269e-01, 2.5120552756e-01 },
+{ 6.7674787974e-01, 1.6162606013e-01, -3.4978377639e-01 },
+{ 6.2482197550e-01, 1.8758901225e-01, -1.0576558797e+00 },
+{ 6.1776148240e-01, 1.9111925880e-01, -1.5492465594e+00 },
+};
+*/
 /* History for two filters */
 sXYData data_history[EQ_MAX_BANDS][EQ_CHANNELS];
 sXYData data_history2[EQ_MAX_BANDS][EQ_CHANNELS];
@@ -118,8 +181,6 @@ static float slidertodb[] = {
  16.0
 };
 
-//double DBfromScaleGain(int value);
-
 inline float DBfromScaleGain(int value)
 {
 	if(value >= EQSLIDERMIN && value <= EQSLIDERMAX)
@@ -130,7 +191,8 @@ inline float DBfromScaleGain(int value)
 /* Round function provided by Frank Klemm which saves around 100K
  * CPU cycles in my PIII for each call to the IIR function
  */
-inline int round_trick(float floatvalue_to_round)
+/*
+inline static int round_trick(float floatvalue_to_round)
 {
 	float	floattmp ;
 	int  rounded_value ;
@@ -142,6 +204,7 @@ inline int round_trick(float floatvalue_to_round)
             rounded_value = ( rounded_value >> 31 ) ^ 0x7FFF;
 		return rounded_value;
 }
+*/
 
 #endif // __MAEQUALIZER_H__
 
