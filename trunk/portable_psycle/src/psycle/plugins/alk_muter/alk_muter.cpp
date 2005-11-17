09b854d0c5cf9fe641374f2e3b9ec601422bb28b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-11-17 20:16:06 +0000

    gobby edit

--- trunk/portable_psycle/src/psycle/plugins/alk_muter/alk_muter.cpp
+++ trunk/portable_psycle/src/psycle/plugins/alk_muter/alk_muter.cpp
@@ -1,20 +1,20 @@
 #include <project.private.hpp>
 #include <psycle/plugin_interface.hpp>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <math.h>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+#include <cmath>
 
 //////////////////////////////////////////////////////////////////////
-// Arguru simple distortion/saturator plugin for PSYCLE
+// Alk muter plugin for PSYCLE
 
 CMachineParameter const paraType = 
 { 
 	"Mute",
 	"Mute off/on",						// description
-	0,												// MinValue	
-	1,											// MaxValue
-	MPF_STATE,										// Flags
+	0,							// MinValue	
+	1,							// MaxValue
+	MPF_STATE,						// Flags
 	0,
 };
 
@@ -27,17 +27,17 @@ CMachineParameter const *pParameters[] =
 CMachineInfo const MacInfo = 
 {
 	MI_VERSION,	
-	0,										// flags
-	1,										// numParameters
-	pParameters,							// Pointer to parameters
-#ifdef _DEBUG
-	"Alk Muter (Debug build)",		// name
+	0,							// flags
+	1,							// numParameters
+	pParameters,						// Pointer to parameters
+#ifndef NDEBUG
+	"Alk Muter (Debug build)",				// name
 #else
-	"Alk Muter",					// name
+	"Alk Muter",						// name
 #endif
-	"Muter",							// short name
+	"Muter",						// short name
 	"Alk",							// author
-	"About",								// A command, that could be use for open an editor, etc...
+	"About",						// A command, that could be use for open an editor, etc...
 	1
 };
 
@@ -47,34 +47,28 @@ class mi : public CMachineInterface
 public:
 	mi();
 	virtual ~mi();
-
 	virtual void Init();
 	virtual void SequencerTick();
 	virtual void Work(float *psamplesleft, float *psamplesright , int numsamples, int tracks);
 	virtual bool DescribeValue(char* txt,int const param, int const value);
 	virtual void Command();
 	virtual void ParameterTweak(int par, int val);
-
-private:
 };
 
 PSYCLE__PLUGIN__INSTANCIATOR(mi, MacInfo)
 
 mi::mi()
 {
-	// The constructor zone
-	Vals = new int[1];
+	Vals = new int[sizeof pParameters];
 }
 
 mi::~mi()
 {
 	delete Vals;
-// Destroy dinamically allocated objects/memory here
 }
 
 void mi::Init()
 {
-// Initialize your stuff here
 }
 
 void mi::SequencerTick()
@@ -85,8 +79,6 @@ void mi::SequencerTick()
 void mi::Command()
 {
 // Called when user presses editor button
-// Probably you want to show your custom window here
-// or an about button
 pCB->MessBox("Made by Alkz0r","Alk's Muter",0);
 }
 
@@ -95,7 +87,6 @@ void mi::ParameterTweak(int par, int val)
 	Vals[par]=val;
 }
 
-// Work... where all is cooked 
 void mi::Work(float *psamplesleft, float *psamplesright , int numsamples, int tracks)
 {
 	if(Vals[0]==1)
@@ -110,7 +101,6 @@ void mi::Work(float *psamplesleft, float *psamplesright , int numsamples, int tr
 	} 
 }
 
-// Function that describes value on client's displaying
 bool mi::DescribeValue(char* txt,int const param, int const value)
 {
 	switch(value)
@@ -118,6 +108,5 @@ bool mi::DescribeValue(char* txt,int const param, int const value)
 		case 0:sprintf(txt,"off");	break;
 		case 1:sprintf(txt,"on");	break;
 	}
-
 	return false;
 }
