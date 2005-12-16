526240a4ecf1a474a8012588df6560598b410795
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2005-12-16 21:43:35 +0000

    removed old files and dirs (everything can be found in the attic or by retrieveing old revisions)

--- trunk/PsyClean/Song.h
+++
@@ -1,5 +0,0 @@
-// Song class
-// The Song class shall store:
-// - Patterns
-// - Sequences (array of references to patterns)
-// This class will probably have to interact with the document management classes of wxWindows
