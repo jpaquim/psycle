a2284cac85f6b2648953c275514b3703def63c69
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-01-08 17:59:09 +0000

    config setting for boost 1.33.1

--- portable_psycle/src/configuration.private.hpp
+++ portable_psycle/src/configuration.private.hpp
@@ -4,3 +4,6 @@
 #pragma once
 #include "configuration.hpp"
 #define CONFIGURATION__OPTION__ENABLE__PRE_COMPILATION
+
+/// [bohan] this is needed with boost version 1.33.1, but i think it's a bug in boost's autolinking.
+#define BOOST_THREAD_USE_DLL
