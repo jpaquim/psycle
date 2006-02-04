3b6140c25c8548c28b6c75094016bf14c45ab397
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-02-04 18:40:10 +0000

    copyrights

--- portable_psycle/src/diversalis/compiler.hpp
+++ portable_psycle/src/diversalis/compiler.hpp
@@ -1,5 +1,6 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright (C) 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
+// Copyright (C) 1999-2006 Johan Boule <bohan@jabber.org>
+// Copyright (C) 2004-2006 Psycledelics http://psycle.pastnotecut.org
 
 ///\file
 ///\brief project-wide compiler-specific tweaks.
@@ -230,4 +231,3 @@
 	//#endnamespace
 //#endnamespace
 #endif
-// arch-tag: d86cb65e-f60a-4940-afc7-cd3ea553cb21
