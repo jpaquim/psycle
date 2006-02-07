3764b9f0f01587b279f0401bbe6b6c8461525676
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-02-07 12:23:10 +0000

    *** empty log message ***

--- portable_psycle/src/universalis/operating_system/exception.hpp
+++ portable_psycle/src/universalis/operating_system/exception.hpp
@@ -1,7 +1,7 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright (C) 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
+// Copyright (C) 1999-2006 Johan Boule <bohan@jabber.org>
+// Copyright (C) 2004-2006 Psycledelics http://psycle.pastnotecut.org
 
-///\file
 ///\interface universalis::operating_system::exception
 #pragma once
 #include <universalis/exception.hpp>
--- portable_psycle/src/universalis/operating_system/exceptions/code_description.hpp
+++ portable_psycle/src/universalis/operating_system/exceptions/code_description.hpp
@@ -1,5 +1,6 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright (C) 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
+// Copyright (C) 1999-2006 Johan Boule <bohan@jabber.org>
+// Copyright (C) 2004-2006 Psycledelics http://psycle.pastnotecut.org
 
 ///\file
 ///\brief
@@ -48,4 +49,3 @@ namespace universalis
 	}
 }
 #include <universalis/compiler/dynamic_link/end.hpp>
-// arch-tag: 3bafc500-fe01-4cde-9a14-8002180e84f1
