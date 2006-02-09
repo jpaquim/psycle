a31b3482b64c75503d2b66d051c6e169d3719193
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-02-09 23:06:14 +0000

    Added some bits of psycoal

---
+++ portable_psycle/src/psycle/coal/host.hpp
@@ -0,0 +1,17 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// Copyright (C) 2006 Psycledelics http://psycle.pastnotecut.org
+
+#pragma once
+
+namespace psycle
+{
+	namespace coal
+	{
+		class host
+		{
+		public:
+			host(void);
+			virtual ~host(void);
+		};
+	}
+}
