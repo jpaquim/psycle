15eed1646eb5d38f00185197795f3e8ae707af8c
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-02-10 00:54:49 +0000

    commit for psycoal - added handwritten hardcoded "build configuration" files for msvc 7.1 (the stuff autoconf's configure would normally produce) - added boilerplate code for "transparent" dll im/ex-port handling - removed dummy hello.{c,h}pp

--- trunk/portable_psycle/src/psycle/coal/arranger.cpp
+++ trunk/portable_psycle/src/psycle/coal/arranger.cpp
@@ -1,10 +1,12 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // Copyright (C) 2006 Psycledelics http://psycle.pastnotecut.org
 
+///\implementation psycle::coal::arranger
 #include PACKAGENERIC__PRE_COMPILED
 #include PACKAGENERIC
-#include <psycle/coal/detail/project.private.hpp>
-#include ".\gear.hpp"
+#include "detail/project.private.hpp"
+#include "arranger.hpp"
+
 namespace psycle
 {
 	namespace coal
@@ -13,7 +15,7 @@ namespace psycle
 		{
 		}
 
-		arranger::~arranger(void)
+		arranger::~arranger(void) throw()
 		{
 		}
 	}
