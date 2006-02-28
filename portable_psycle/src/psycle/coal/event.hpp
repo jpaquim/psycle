6a4729898f1133e3c8cfe7f69901db2a5fe60ba3
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-02-28 03:45:06 +0000

    compilation fixes for psycoal on msvc7.1

--- portable_psycle/src/psycle/coal/event.hpp
+++ portable_psycle/src/psycle/coal/event.hpp
@@ -1,11 +1,11 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // Copyright (C) 2006 Psycledelics http://psycle.pastnotecut.org
 
-///\interface psycle::coal::arranger
+///\interface psycle::coal::event
 #pragma once
 #include "detail/project.hpp"
 
-#define UNIVERSALIS__COMPILER__DYNAMIC_LINK  PSYCLE__COAL__ARRANGER
+#define UNIVERSALIS__COMPILER__DYNAMIC_LINK  PSYCLE__COAL__EVENT
 #include <universalis/compiler/dynamic_link/begin.hpp>
 
 namespace psycle
