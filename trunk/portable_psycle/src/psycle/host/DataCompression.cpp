62bb6fb5675e92d60300e9c3901de72aef489623
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-03-01 02:43:56 +0000

    some more problems

--- trunk/portable_psycle/src/psycle/host/DataCompression.cpp
+++ trunk/portable_psycle/src/psycle/host/DataCompression.cpp
@@ -510,6 +510,10 @@ namespace psycle
 
 					// get file size
 					int FileSize = *(DWORD*)pSourcePos;
+					#include <diversalis/processor.hpp>
+					#if defined DIVERSALIS__PROCESSOR__ENDIAN__BIG
+						#error ":("
+					#endif
 
 					pSourcePos+=4;
 					//ok, now we can start decompressing
