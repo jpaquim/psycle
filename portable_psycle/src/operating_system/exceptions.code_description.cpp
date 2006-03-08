0f07085feb47da70211a4d2df80c0e063c923f44
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-03-08 04:30:58 +0000

    cleanup of the tags (part 1)

---
+++ portable_psycle/src/operating_system/exceptions.code_description.cpp
@@ -0,0 +1,33 @@
+#include <stdafx.h>
+//#include <project.h>
+#define OPERATING_SYSTEM__EXCEPTIONS__CODE_DESCRIPTION
+#include <operating_system/exceptions/code_description.h>
+#include <string>
+#include <sstream>
+#include <windows.h>
+///\file
+///\brief
+namespace operating_system
+{
+	namespace exceptions
+	{
+		const std::string code_description(const unsigned long int & code) throw()
+		{
+			char * error_message_pointer;
+			::FormatMessage // ouch! plain old c api style, this is ugly...
+			(
+				FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+				0, // module to get message from. 0 means system.
+				code,
+				MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL),
+				reinterpret_cast<char*>(&error_message_pointer), // we *must* hard-cast! this seems to be a hack to extend an originally badly designed api... there is no other way to do it
+				0,
+				0
+			);
+			std::ostringstream s;
+			s << "microsoft api error: " << code << " 0x" << std::hex << code << ": " << error_message_pointer;
+			::LocalFree(error_message_pointer);
+			return s.str();
+		}
+	}
+}
