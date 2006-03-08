264a5e568888a2b129f80d4020a25d182096457f
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-03-08 04:39:56 +0000

    cleanup of the tags (part 2)

---
+++ release_1_7_4/Psycle/GearDelay.h
@@ -0,0 +1,87 @@
+#if !defined(AFX_GEARDELAY_H__50335B20_0E81_11D4_8913_FACF4EED7F7C__INCLUDED_)
+#define AFX_GEARDELAY_H__50335B20_0E81_11D4_8913_FACF4EED7F7C__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// GearDelay.h : header file
+//
+
+#include "Machine.h"
+#include "constants.h"
+
+class CChildView;
+
+/////////////////////////////////////////////////////////////////////////////
+// CGearDelay dialog
+
+class CGearDelay : public CDialog
+{
+// Construction
+public:
+	CGearDelay(CChildView* pParent = NULL);   // standard constructor
+	Delay* _pMachine;
+	int* SPT;
+	BOOL Create();
+	afx_msg void OnCancel();
+
+// Dialog Data
+	//{{AFX_DATA(CGearDelay)
+	enum { IDD = IDD_GEAR_DELAY };
+	CStatic	m_drylabel;
+	CStatic	m_wetlabel;
+	CSliderCtrl	m_wetslider;
+	CSliderCtrl	m_dryslider;
+	CStatic	m_msr;
+	CStatic	m_msl;
+	CStatic	m_flabel_r;
+	CStatic	m_flabel_l;
+	CSliderCtrl	m_sliderfeedback_r;
+	CSliderCtrl	m_slidertime_r;
+	CStatic	m_label_timer;
+	CSliderCtrl	m_sliderfeedback_l;
+	CStatic	m_label_timel;
+	CSliderCtrl	m_slidertime_l;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CGearDelay)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	CChildView* m_pParent;
+
+	// Generated message map functions
+	//{{AFX_MSG(CGearDelay)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnCustomdrawSliderTime(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnCustomdrawSliderTime2(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnCustomdrawSlider2(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnCustomdrawSlider3(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnButton1();
+	afx_msg void OnButton3();
+	afx_msg void OnButton14();
+	afx_msg void OnButton15();
+	afx_msg void OnButton16();
+	afx_msg void OnButton17();
+	afx_msg void OnButton11();
+	afx_msg void OnButton18();
+	afx_msg void OnButton19();
+	afx_msg void OnButton20();
+	afx_msg void OnButton21();
+	afx_msg void OnButton22();
+	afx_msg void OnCustomdrawDryslider(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnCustomdrawWetslider(NMHDR* pNMHDR, LRESULT* pResult);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_GEARDELAY_H__50335B20_0E81_11D4_8913_FACF4EED7F7C__INCLUDED_)
---
+++ release_1_7_4/portable_psycle/src/psycle/host/SwingFillDlg.cpp
@@ -0,0 +1,103 @@
+#include "stdafx.h"
+
+// SwingFillDlg.cpp : implementation file
+//
+
+#include "psycle2.h"
+#include "SwingFillDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CSwingFillDlg dialog
+
+
+CSwingFillDlg::CSwingFillDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CSwingFillDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CSwingFillDlg)
+		// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+
+void CSwingFillDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CSwingFillDlg)
+		// NOTE: the ClassWizard will add DDX and DDV calls here
+	DDX_Control(pDX, IDC_CENTER_TEMPO, m_Tempo);
+	DDX_Control(pDX, IDC_WIDTH, m_Width);
+	DDX_Control(pDX, IDC_VARIANCE, m_Variance);
+	DDX_Control(pDX, IDC_PHASE, m_Phase);
+	DDX_Control(pDX, IDC_OFFSET, m_Offset);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CSwingFillDlg, CDialog)
+	//{{AFX_MSG_MAP(CSwingFillDlg)
+		// NOTE: the ClassWizard will add message map macros here
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CSwingFillDlg message handlers
+
+BOOL CSwingFillDlg::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+	bGo = FALSE;
+	
+	char buf[32];
+	sprintf(buf,"%d",tempo);
+	m_Tempo.SetWindowText(buf);
+	m_Tempo.SetSel(-1,-1,false);
+
+	sprintf(buf,"%d",width);
+	m_Width.SetWindowText(buf);
+	m_Width.SetSel(-1,-1,false);
+
+	sprintf(buf,"%.2f",variance);
+	m_Variance.SetWindowText(buf);
+	m_Variance.SetSel(-1,-1,false);
+	
+	sprintf(buf,"%.2f",phase);
+	m_Phase.SetWindowText(buf);
+	m_Phase.SetSel(-1,-1,false);
+
+	m_Offset.SetCheck(offset?1:0);
+
+	return FALSE;
+}
+
+void CSwingFillDlg::OnOK() 
+{
+	bGo = TRUE;
+	char buf[32];
+	m_Tempo.GetWindowText(buf,32);
+	tempo=atoi(buf);
+	if (tempo < 33)
+		tempo = 33;
+	else if (tempo > 999)
+		tempo = 999;
+
+	m_Width.GetWindowText(buf,32);
+	width=atoi(buf);
+	if (width < 1)
+		width = 1;
+
+	m_Variance.GetWindowText(buf,32);
+	variance=float(atof(buf));
+
+	m_Phase.GetWindowText(buf,32);
+	phase=float(atof(buf));
+
+	offset = m_Offset.GetCheck()?true:false;
+
+	CDialog::OnOK();
+}
