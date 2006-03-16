45e58830cb0b8f674588ccaa563fc99d0f68b1c9
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-03-16 11:24:36 +0000

    removed msvc6 stuff

--- psycle/src/psycle/host/ProgressDialog.hpp
+++ psycle/src/psycle/host/ProgressDialog.hpp
@@ -1,39 +1,24 @@
-///\file
-///\brief interface file for psycle::host::CProgressDialog.
+///\interface psycle::host::CProgressDialog
 #pragma once
 #include "detail/project.hpp"
 UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 	UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(host)
+
 		/// progress meter window.
 		class CProgressDialog : public CDialog
 		{
-		public:
-			CProgressDialog(CWnd* pParent = 0);
-			BOOL Create();
-			afx_msg void OnCancel();
-			CWnd* m_pParent;
-		// Dialog Data
-			//{{AFX_DATA(CProgressDialog)
-			enum { IDD = IDD_PROGRESS_DIALOG };
-			CProgressCtrl	m_Progress;
-			//}}AFX_DATA
-		// Overrides
-			// ClassWizard generated virtual function overrides
-			//{{AFX_VIRTUAL(CProgressDialog)
+			public:
+				CProgressDialog(CWnd* pParent = 0);
+				BOOL Create();
+				afx_msg void OnCancel();
+				CWnd* m_pParent;
+				enum { IDD = IDD_PROGRESS_DIALOG };
+				CProgressCtrl m_Progress;
 			protected:
-			virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-			//}}AFX_VIRTUAL
-		// Implementation
-		protected:
-			// Generated message map functions
-			//{{AFX_MSG(CProgressDialog)
-			virtual BOOL OnInitDialog();
-				// NOTE: the ClassWizard will add member functions here
-			//}}AFX_MSG
+				virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+				virtual BOOL OnInitDialog();
 			DECLARE_MESSAGE_MAP()
 		};
 
-		//{{AFX_INSERT_LOCATION}}
-		// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 	UNIVERSALIS__COMPILER__NAMESPACE__END
 UNIVERSALIS__COMPILER__NAMESPACE__END
