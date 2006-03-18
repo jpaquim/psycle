f41fa141a3febb69ae33b9ba2bbc91b91b1d5a8f
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-03-18 23:53:15 +0000

    pattern class

---
+++ psycle/src/psycle/host/pattern.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include "detail/project.hpp"
+#include <cstdint>
+namespace psycle
+{
+	namespace host
+	{
+		///\todo this class is unfortunatly not used
+		template<typename Entry>
+		class pattern : protected std::vector< std::vector<Entries> > // anything more direct for matrices?
+		{
+			public:
+				typedef Entry entry;
+
+			public:
+				pattern(unsigned int const & lines, unsigned int const & columns)
+				:
+					columns_(columns)
+				{
+					this->lines(lines);
+				}
+
+			public:
+				void inline name(std::string const & value) throw() { name_ = value; }
+				std::string const inline & name() const throw() { return name_; }
+			private:
+				std::string name_;
+
+			public:
+				unsigned int const inline & lines() const throw() { return entries.size(); }
+				void lines(unsigned int const & value)
+				{
+					resize(lines);
+					for(iterator i(begin()) ; i != end() ; ++i) *i.resize(columns);
+				}
+
+			public:
+				unsigned int const inline & columns() const throw() { return column_; }
+				void columns(unsigned int const & value)
+				{
+					columns_ = value;
+					for(iterator i(begin()) ; i != end() ; ++i) *i.resize(columns);
+				}
+			private:
+				unsigned int columns_;
+		};
+	}
+}
