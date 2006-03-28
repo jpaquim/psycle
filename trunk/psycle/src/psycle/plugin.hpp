77f7d593b44a6923ed439e2ea4117c6f8fd4cf83
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-03-28 03:34:55 +0000

    some documentation for src/psycle/plugin.hpp

--- trunk/psycle/src/psycle/plugin.hpp
+++ trunk/psycle/src/psycle/plugin.hpp
@@ -1,6 +1,10 @@
 /// \file
 /// \brief yet another psycle plugin interface api by bohan
 /// This one is more object-oriented than the original plugin_interface.hpp one.
+/// The benefits of it are:
+/// - it can apply a scale to parameter values, transparently, so you only deal with the meaningful real scaled value.
+/// - it provides a bugfree initialization of the plugin.
+/// - it provides the events samples_per_second_changed and sequencer_ticks_per_second_changed, which were not part of the original interface (i don't know if it's been fixed in the host nowadays).
 #pragma once
 #include "scale.hpp"
 #include <universalis/compiler.hpp>
@@ -13,26 +17,43 @@ namespace psycle
 	namespace plugin
 	{
 		/// the plugin class used by the host hence to which all plugins must conform.
+		/// You don't normally derive your plugin class directly from this class, but rather from the Plugin class below
+		///\see Plugin
+		///\todo nested definition is hard to read ; make it flat.
 		class Host_Plugin
 		{
 			public:
+				/// information describing a plugin
 				class Information
 				{
 					public:
+						/// version of the binary interface of the plugin.
 						int const interface_version;
+						/// type of the plugin, either generator or effect.
 						struct Types { enum Type { effect = 0, generator = 3 }; };
+						/// type of the plugin, either generator or effect.
+						/// declared as int rather than enum for binary compatibility.
 						int /*Types::Type*/ const type;
+						/// number of parameters the plugin has.
 						int const parameter_count;
-						class Parameter;
+						class Parameter; // forward declaration
 					private:
+						///\internal
 						Parameter const * * const parameters;
 					public:
+						///\return the Parameter at the index parameter.
 						Parameter const inline & parameter(int const & parameter) const throw() { return *parameters[parameter]; }
+						/// description of the plugin.
 						char const * const description;
+						/// name of the plugin.
 						char const * const name;
+						/// author of the plugin.
 						char const * const author;
+						/// caption to be displated in the help message.
 						char const * const help_caption;
+						/// number of columns that the parameter grid should be displayed with.
 						int const columns;
+						/// creates the information object.
 						Information
 						(
 							Types::Type const & type,
@@ -57,18 +78,29 @@ namespace psycle
 							for(int i(0) ; i < parameter_count ; ++i) this->parameters[i] = &parameters[i];
 						}
 					public:
+						/// information describing a parameter
 						class Parameter
 						{
 							public:
+								/// name of the parameter.
 								char const * const name;
+								/// useless field, set to the same value as name.
 								char const * const unused_name;
+								/// type of parameter, either null (label), or state.
 								struct Types { enum Type { null = 0, state = 2 }; };
+								/// type of parameter, either null (label), or state.
+								/// declared as int rather than enum for binary compatibility.
 								int /*Types::Type*/ const type;
+								/// the lower bound the parameter's range.
+								/// If using a non-discrete scale (i.e. real), you can't chose this, it's always the minimum value possible.
 								int const minimum_value;
+								/// the upper bound of the parameter's range.
+								/// If using a non-discrete scale (i.e. real), you can't chose this, it's always the maximum value possible.
 								int const maximum_value;
 								int const default_value;
 								Scale const & scale;
 							public:
+								/// creates a separator with an optional label.
 								Parameter(char const name[] = "")
 								:
 									name(name),
@@ -80,9 +112,12 @@ namespace psycle
 									scale(* new scale::Discrete(0))
 								{}
 							public:
+							/// minimum lower bound authorized by the binary interface. (unsigned 16-bit integer)
 							int const static input_minimum_value = 0;
+							/// maximum upper bound authorized by the binary interface. (unsigned 16-bit integer)
 							int const static input_maximum_value = 0xffff;
 							private:
+								/// creates a scaled parameter ; you don't use this directly, but rather the public static creation functions.
 								Parameter(char const name[], Scale const & scale, Real const & default_value, int const & input_maximum_value = Parameter::input_maximum_value)
 								:
 									name(name),
@@ -105,18 +140,22 @@ namespace psycle
 									scale(scale)
 								{}
 							public:
+								/// creates a discrete scale, i.e. integers.
 								static const Parameter & discrete(char const name[], int const & default_value, int const & maximum_value)
 								{
 									return * new Parameter(name, * new scale::Discrete(static_cast<Real>(maximum_value)), static_cast<Real>(default_value), maximum_value);
 								}
+								/// creates a linear real scale.
 								static const Parameter & linear(char const name[], double const & minimum_value, double const & default_value, double const & maximum_value)
 								{
 									return * new Parameter(name, * new scale::Linear(static_cast<Real>(input_maximum_value), static_cast<Real>(minimum_value), static_cast<Real>(maximum_value)), static_cast<Real>(default_value));
 								}
+								/// creates an exponential scale.
 								static const Parameter & exponential(char const name[], double const & minimum_value, double const & default_value, double const & maximum_value)
 								{
 									return * new Parameter(name, * new scale::Exponential(static_cast<Real>(input_maximum_value), static_cast<Real>(minimum_value), static_cast<Real>(maximum_value)), static_cast<Real>(default_value));
 								}
+								/// creates a logarithmic scale.
 								static const Parameter & logarithmic(char const name[], double const & minimum_value, double const & default_value, double const & maximum_value)
 								{
 									return * new Parameter(name, * new scale::Logarithmic(static_cast<Real>(input_maximum_value), static_cast<Real>(minimum_value), static_cast<Real>(maximum_value)), static_cast<Real>(default_value));
@@ -126,47 +165,79 @@ namespace psycle
 			public:
 				virtual ~Host_Plugin() throw() {}
 			protected:
+				/// event called by the host to ask the plugin to initialize.
 				virtual void host() {}
 			public:
+				/// event called by the host every tick.
 				virtual void sequencer_tick_event() {}
+				/// event called by the host when it wants a parameter value to be changed.
 				virtual void parameter(int const parameter, int const value) {}
+				/// event called by the host when it wants the plugin to process an audio buffer.
 				virtual void process(Sample stereo_left_channel_samples[], Sample stereo_right_channel_samples[], int samples, int tracks) {}
+				/// event called by the host when it, hmm ... dunno :)
+				///\todo what is this?
 				virtual void stop() {}
+				/// event called by the host when it wants to serialize the plugin's state.
 				virtual void write_data(unsigned char data[]) const {}
+				/// event called by the host when it wants to deserialize the plugin's state.
 				virtual void read_data(unsigned char const data[]) {}
+				/// event called by the host in relation with read_data and write_data.
+				/// must be overridden in your plugin class to return the size of the data that read_data and write_data should use.
 				virtual int const data_size() const { return 0; }
+				/// event called by the host when the user queries information about your plugin.
 				virtual void help() const throw() {}
 			private:
+				/// unused virtual table slot kept for binary compatibility.
 				virtual void unused__mute(int const track) {}
+				/// unused virtual table slot kept for binary compatibility.
 				virtual bool unused__muted(int const track) const { return false; }
+				/// unused virtual table slot kept for binary compatibility.
 				virtual void unused__midi_note(int const channel, int const note, int const velocity) {}
+				/// unused virtual table slot kept for binary compatibility.
 				virtual void unused__event(unsigned long int const) {}
 			protected:
+				/// event called by the host went it wants a string description of a parameter's current value.
 				virtual bool describe(char out[], int const parameter, int const value) const { return false; }
 			private:
+				/// unused virtual table slot kept for binary compatibility.
 				virtual bool unused__play_wave(int const, int const, Sample const) { return false; }
 			public:
+				/// event called by the host
 				virtual void sequencer_note_event(int const channel, int const note, int const instrument, int const command, int const value) {}
 			private:
+				/// unused virtual table slot kept for binary compatibility.
 				virtual void unused__stop_wave() {}
 			protected:
+				/// You don't deal directly with this array. It's part of the binary interface.
 				int * parameters_;
 			protected:
+				/// Interface that the host provides to plugins, i.e. what plugins can query the host for.
 				class Host
 				{
 					public:
 						virtual inline ~Host() throw() {}
+						/// you can ask the host to display a message.
 						virtual void message(char const message[], char const caption[], unsigned int type = 0) const {}
+					private:
+						/// unused virtual table slot kept for binary compatibility.
 						virtual int const unused__raw(int const, int const, int const, int const) const { return 0; }
+						/// unused virtual table slot kept for binary compatibility.
 						virtual float const * const unused__wave_data_left(int, int) const { return 0; }
+						/// unused virtual table slot kept for binary compatibility.
 						virtual float const * const unused__wave_data_right(int, int) const { return 0; }
+					public:
+						/// asks the host to return this value.
 						virtual int const samples_per_tick() const { return 0; }
+						/// asks the host to return this value.
 						virtual int const samples_per_second() const { return 0; }
+						/// asks the host to return this value.
 						virtual int const sequencer_beats_per_minute() const { return 0; }
+						/// asks the host to return this value.
 						virtual int const sequencer_ticks_per_sequencer_beat() const { return 0; }
 				} const * host_;
 		}; // class Host_Plugin
 
+		/// This class is the one your plugin class should derive from.
 		class Plugin : protected Host_Plugin
 		{
 			protected:
@@ -175,6 +246,8 @@ namespace psycle
 			public:
 				virtual ~Plugin() throw() { delete [] parameters_; delete [] scaled_parameters_; }
 			protected:
+				/// creates an instance of your plugin.
+				/// Note: functions that queries the host should only be called after the init() event.
 				Plugin(Information const & information)
 				:
 					information(information),
@@ -188,27 +261,39 @@ namespace psycle
 					for(int parameter(0) ; parameter < information.parameter_count ; ++parameter)
 						parameter_internal(parameter, information.parameter(parameter).default_value);
 				}
+				///\returns the integral internal value (i.e. non-scaled) of a parameter ; use this for discrete scales.
 				inline int const & operator[](int const & parameter) const throw() { return parameters_[parameter]; }
+				///\returns the real scaled value of a parameter.
 				inline Real const & operator()(int const & parameter) const throw() { return scaled_parameters_[parameter]; }
 			private:
+				///\internal
 				void parameter_internal(int const parameter, int const value)
 				{
 					this->parameters_[parameter] = value;
 					this->scaled_parameters_[parameter] = information.parameter(parameter).scale.apply(static_cast<Real>(value));
 				}
+				///\internal
 				void parameter(int const parameter, int const value)
 				{
 					parameter_internal(parameter, value);
 					this->parameter(parameter);
 				}
 			protected:
+				/// event called by the host when it wants a parameter value to be changed.
+				/// override this virtual function in your plugin to take any action in response to this event.
+				/// You don't need to update the parameter value itself when this event occurs,
+				/// but you might want to update other data dependent on the parameter's value.
 				virtual void parameter(int const &) {}
 			protected:
+				/// event called by the host went it wants a string description of a parameter's current value.
+				/// override this virtual function in your plugin to return a specialized text instead of just the value of the number,
+				/// i.e. you can append the unit (Hz, seconds, etc), or show the value in deciBell, etc.
 				virtual void describe(std::ostream & out, int const & parameter) const
 				{
 					out << (*this)(parameter);
 				}
 			private:
+				///\internal
 				bool describe(char out[], int const parameter, int const) const
 				{
 					std::stringstream s;
@@ -217,18 +302,29 @@ namespace psycle
 					return out[0];
 				}
 			protected:
+				/// event called by the host to ask the plugin to initialize.
+				/// You should rather override the virtual init() function.
+				///\see init()
 				virtual void host()
 				{
 					Plugin::sequencer_tick_event();
 					init();
 					initialized_ = true;
 				}
+				/// event called by the host to ask the plugin to initialize.
+				/// override this virtual function in your plugin to take any action in response to this event.
+				/// Note: functions that queries the host should only be called after this event.
 				virtual void init() {}
 			private:
+				///\internal
 				bool initialized_;
+				///\internal
 				Real samples_per_second_, seconds_per_sample_, sequencer_ticks_per_seconds_;
+				///\internal
 				int sequencer_ticks_per_sequencer_beat_, sequencer_beats_per_minute_;
 			protected:
+				/// event called by the host every tick.
+				/// override this virtual function in your plugin to take any action in response to this event.
 				virtual void sequencer_tick_event()
 				{
 					if(samples_per_second_ != static_cast<Real>(host_->samples_per_second()))
@@ -249,15 +345,25 @@ namespace psycle
 						if(initialized_) sequencer_ticks_per_second_changed();
 					}
 				}
+				/// event called when the sample rate changed.
+				/// override this virtual function in your plugin to take any action in response to this event.
 				virtual void samples_per_second_changed() {}
+				/// event called when the duration of a tick changed.
+				/// override this virtual function in your plugin to take any action in response to this event.
 				virtual void sequencer_ticks_per_second_changed() {}
+				/// asks the host to return this value.
 				inline Real const samples_per_second() const { return samples_per_second_; }
+				/// asks the host to return this value.
 				inline Real const seconds_per_sample() const { return 1 / samples_per_second(); }
+				/// asks the host to return this value.
 				inline Real const sequencer_ticks_per_seconds() const { return sequencer_ticks_per_seconds_; }
+				/// asks the host to return this value.
 				inline Real const samples_per_sequencer_tick() const { return samples_per_second() / sequencer_ticks_per_seconds(); }
 			protected:
+				/// you can ask the host to display a message.
 				inline void message(std::string const & message) const throw() { this->message("message", message); }
 			private:
+				///\internal
 				inline void message(std::string const & caption, std::string const & message) const
 				{
 					std::stringstream s;
@@ -265,8 +371,11 @@ namespace psycle
 					host_->message(message.c_str(), s.str().c_str());
 				}
 			protected:
+				/// event called by the host when the user queries information about your plugin.
+				/// override this virtual function in your plugin to output a help text describing what your plugin does, what tracker commands are available etc.
 				virtual void help(std::ostream & out) const throw() { out << "no help available"; }
 			private:
+				///\internal
 				void help() const throw()
 				{
 					std::ostringstream message;
@@ -289,6 +398,7 @@ namespace psycle
 				}; // class Exception
 		}; // class Plugin
 
+		/// call this from your plugin's source file to export the necessary function from the dynamically linked library.
 		#define PSYCLE__PLUGIN__INSTANCIATOR(typename) \
 			extern "C" \
 			{ \
