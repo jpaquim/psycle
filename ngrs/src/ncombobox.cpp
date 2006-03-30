46efbb1abf62f099fd6a615c2ef55c03611cf8ba
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-03-30 16:51:36 +0000

    ...

--- ngrs/src/ncombobox.cpp
+++ ngrs/src/ncombobox.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Stefan   *
+ *   Copyright (C) 2005 by Stefan Nattkemper Nattkemper  *
  *   natti@linux   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
