794849ff8a8904e84052f6240756872152ea1247
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-04-22 22:19:24 +0000

    freepcycle: work-in-progress on gsteamer

--- freepsycle/src/psycle/plugins/outputs/gstreamer.cpp
+++ freepsycle/src/psycle/plugins/outputs/gstreamer.cpp
@@ -7,7 +7,7 @@
 #include "gstreamer.hpp"
 #include <universalis/exception.hpp>
 #include <gst/gst.h>
-#include <cstddefs>
+#include <cstddef>
 #include <limits>
 #include <cmath>
 namespace psycle
@@ -49,42 +49,45 @@ namespace psycle
 				if(sink_) ::g_object_set(G_OBJECT(sink_), "name", (name + "-sink").c_str(), 0);
 			}
 
-			::GstElement & instanciate(std::string const & type, std::string const & name) throw(std::exception)
+			namespace
 			{
-				try
+				::GstElement & instanciate(std::string const & type, std::string const & name) throw(universalis::exception)
 				{
-					if(loggers::information()())
+					try
 					{
-						std::ostringstream s; s << "instanciating " << type << " " << name;
-						loggers::information()(s.str(), UNIVERSALIS__COMPILER__LOCATION);
+						if(loggers::information()())
+						{
+							std::ostringstream s; s << "instanciating " << type << " " << name;
+							loggers::information()(s.str(), UNIVERSALIS__COMPILER__LOCATION);
+						}
+						::GstElementFactory * factory;
+						if(!(factory = ::gst_element_factory_find(type.c_str())))
+						{
+							std::ostringstream s;
+							s << "could not find element type: " << type;
+							throw engine::exceptions::runtime_error(s.str(), UNIVERSALIS__COMPILER__LOCATION);
+						}
+						if(loggers::information()())
+						{
+							std::ostringstream s;
+							s
+								<< "The element type " << ::gst_plugin_feature_get_name(GST_PLUGIN_FEATURE(factory))
+								<< " is a member of the group " << ::gst_element_factory_get_klass(factory) << "." << std::endl
+								<< "Description:" << std::endl
+								<< ::gst_element_factory_get_description(factory);
+							loggers::information()(s.str(), UNIVERSALIS__COMPILER__LOCATION);
+						}
+						::GstElement * element;
+						if(!(element = ::gst_element_factory_create(factory, name.c_str())))
+						{
+							std::ostringstream s;
+							s << "found element type: " << ::gst_plugin_feature_get_name(GST_PLUGIN_FEATURE(factory)) << ", but could not create element instance: " << name;
+							throw engine::exceptions::runtime_error(s.str(), UNIVERSALIS__COMPILER__LOCATION);
+						}
+						return *element;
 					}
-					::GstElementFactory * factory;
-					if(!(factory = ::gst_element_factory_find(type.c_str())))
-					{
-						std::ostringstream s;
-						s << "could not find element type: " << type;
-						throw engine::exceptions::runtime_error(s.str(), UNIVERSALIS__COMPILER__LOCATION);
-					}
-					if(loggers::information()())
-					{
-						std::ostringstream s;
-						s
-							<< "The element type " << ::gst_plugin_feature_get_name(GST_PLUGIN_FEATURE(factory))
-							<< " is a member of the group " << ::gst_element_factory_get_klass(factory) << "." << std::endl
-							<< "Description:" << std::endl
-							<< ::gst_element_factory_get_description(factory);
-						loggers::information()(s.str(), UNIVERSALIS__COMPILER__LOCATION);
-					}
-					::GstElement * element;
-					if(!(element = ::gst_element_factory_create(factory, name.c_str())))
-					{
-						std::ostringstream s;
-						s << "found element type: " << ::gst_plugin_feature_get_name(GST_PLUGIN_FEATURE(factory)) << ", but could not create element instance: " << name;
-						throw engine::exceptions::runtime_error(s.str(), UNIVERSALIS__COMPILER__LOCATION);
-					}
-					return *element;
+					UNIVERSALIS__EXCEPTIONS__CATCH_ALL_AND_CONVERT_TO_STANDARD_AND_RETHROW__NO_CLASS
 				}
-				UNIVERSALIS__EXCEPTIONS__CATCH_ALL_AND_CONVERT_TO_STANDARD_AND_RETHROW__NO_CLASS
 			}
 
 			void gstreamer::do_open() throw(engine::exception)
@@ -100,12 +103,12 @@ namespace psycle
 					{
 						once = true;
 						int * argument_count(0);
-                        char ** arguments[](0);
+                        char *** arguments = 0;
 						::GError * error(0);
 						if(!::gst_init_check(argument_count, arguments, &error))
 						{
-							std::ostringstream s; s << "could not initialize gstreamer: " << error.;
-							::g_clear_error(error);
+							std::ostringstream s; s << "could not initialize gstreamer: " << error->code << ": " << error->message;
+							::g_clear_error(&error);
 							throw engine::exceptions::runtime_error(s.str(), UNIVERSALIS__COMPILER__LOCATION);
 						}
 					}
@@ -122,8 +125,8 @@ namespace psycle
 						"audio/x-raw-int",
 						"rate"      , G_TYPE_INT    , samples_per_second,
 						"channels"  , G_TYPE_INT    , channels,
-						"width"     , G_TYPE_INT    , bits_per_samples,
-						"depth"     , G_TYPE_INT    , bits_per_samples,
+						"width"     , G_TYPE_INT    , bits_per_sample,
+						"depth"     , G_TYPE_INT    , bits_per_sample,
 						"signed"    , G_TYPE_BOOLEAN, samples_signed,
 						"endianness", G_TYPE_INT    , G_BYTE_ORDER,
 						0
@@ -162,7 +165,7 @@ namespace psycle
 						if(!::g_signal_connect(G_OBJECT(source_), "handoff", G_CALLBACK(handoff_static), this)) throw engine::exceptions::runtime_error("could not connect handoff signal", UNIVERSALIS__COMPILER__LOCATION);
 
 						// get the source element's src pad
-						::GstPad * source_pad(::gst_element_get_pad(source_, "src" );
+						::GstPad * source_pad(::gst_element_get_pad(source_, "src" ));
 						if(!source_pad) throw engine::exceptions::runtime_error("no src pad found on source element", UNIVERSALIS__COMPILER__LOCATION);
 						try
 						{
@@ -175,11 +178,16 @@ namespace psycle
 							{
 								sink_ = &instanciate("gconfaudiosink", name() + "-sink");
 							}
-							catch(...)
+							catch(std::exception const & e)
 							{
 								// maybe the user didn't configure his default audio-sink, falling back to several possibilities
 								#if 0 // jacksink needs to be put in a jackbin, because it's pulling audio data.
-								loggers::information()("exception: caught while trying to instanciate audio sink ; trying next type ...", UNIVERSALIS__COMPILER__LOCATION);
+								if(loggers:information())
+								{
+									std::ostringstream s;
+									s
+										<< "exception: caught while trying to instanciate audio sink: " <<
+									loggers::information()("exception: caught while trying to instanciate audio sink ; trying next type ...", UNIVERSALIS__COMPILER__LOCATION);
 								try
 								{
 									sink_ = &instanciate("jacksink", name() + "-sink");
@@ -234,7 +242,7 @@ namespace psycle
 							}
 
 							// get the sink element's sink pad
-							::GstPad * sink_pad(::gst_element_get_pad(source_, "sink" );
+							::GstPad * sink_pad(::gst_element_get_pad(source_, "sink" ));
 							if(!sink_pad) throw engine::exceptions::runtime_error("no sink pad found on sink element", UNIVERSALIS__COMPILER__LOCATION);
 							try
 							{
@@ -243,7 +251,8 @@ namespace psycle
 
 								// connect the source element's src pad to the sink element's sink pad
 								{
-									::GstPadLinkReturn const result(::gst_pad_link_filtered(source_pad, sink_pad, caps_));
+									//::GstPadLinkReturn const result(::gst_pad_link_filtered(source_pad, sink_pad, caps_));
+									::GstPadLinkReturn const result(::gst_pad_link(source_pad, sink_pad));
 									if(GST_PAD_LINK_FAILED(result))
 									{
 										std::ostringstream s; s << "could not link source and sink: code: " << result;
@@ -295,7 +304,7 @@ namespace psycle
 				return ::gst_element_get_state(pipeline_) == ::GST_STATE_PLAYING;
 			}
 		
-			void static gstreamer::handoff_static(::GstElement * source, ::GstBuffer * buffer, GstPad * pad, gstreamer * instance)
+			void gstreamer::handoff_static(::GstElement * source, ::GstBuffer * buffer, GstPad * pad, gstreamer * instance)
 			{
 				instance->handoff(*buffer);
 			}
@@ -309,11 +318,10 @@ namespace psycle
 						
 			void gstreamer::handoff(::GstBuffer & buffer)
 			{
-				if(!caps_set_)
+				if(true)///\todo
 				{
 					if(loggers::trace())
 					{
-						std::ostringstream s; s << "handoff: buffer size: " << size;
 						loggers::trace()("caps not set on buffer ; setting them", UNIVERSALIS__COMPILER__LOCATION);
 					}
 					::gst_buffer_set_caps(&buffer, caps_);
