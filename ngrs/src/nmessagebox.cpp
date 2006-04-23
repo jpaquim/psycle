0f6673089ae0802b4e7621254ec27050614b7a61
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-04-23 19:59:16 +0000

    msgbox added

---
+++ ngrs/src/nmessagebox.cpp
@@ -0,0 +1,114 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Stefan Nattkemper   *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "nmessagebox.h"
+#include "nbutton.h"
+#include "nflowlayout.h"
+#include "nlabel.h"
+
+NMessageBox::NMessageBox()
+ : NDialog()
+{
+  init();
+}
+
+NMessageBox::NMessageBox( const std::string & msgText )
+{
+  init();
+  textLbl->setText(msgText);
+}
+
+
+void NMessageBox::init( )
+{
+  NPanel* btnPnl = new NPanel();
+    btnPnl->setLayout(new NFlowLayout(nAlRight,5,5),true);
+    okBtn = new NButton("ok");
+      okBtn->setFlat(false);
+      okBtn->clicked.connect(this,&NMessageBox::onBtnPress);
+    btnPnl->add(okBtn);
+    useBtn = new NButton("use");
+      useBtn->setFlat(false);
+      useBtn->clicked.connect(this,&NMessageBox::onBtnPress);
+    btnPnl->add(useBtn);
+    cancelBtn = new NButton("cancel");
+      cancelBtn->setFlat(false);
+      cancelBtn->clicked.connect(this,&NMessageBox::onBtnPress);
+    btnPnl->add(cancelBtn);
+  pane()->add(btnPnl,nAlBottom);
+
+  textLbl = new NLabel();
+  pane()->add(textLbl,nAlClient);
+  textLbl->setWordbreak(true);
+
+  setPosition(10,10,300,150);
+  setTitle("Message");
+}
+
+
+NMessageBox::~NMessageBox()
+{
+}
+
+void NMessageBox::setButtons( int btnMask )
+{
+  okBtn->setVisible(false);
+  useBtn->setVisible(false);
+  cancelBtn->setVisible(false);
+
+  switch (btnMask) {
+    case nMsgOkBtn :
+       okBtn->setVisible(true);
+    break;
+    case nMsgCancelBtn :
+       cancelBtn->setVisible(true);
+    break;
+    case nMsgUseBtn :
+       useBtn->setVisible(true);
+    break;
+  }
+}
+
+void NMessageBox::setButtonText( const std::string & ok, const std::string & use, const std::string & cancel )
+{
+  okBtn->setText(ok);
+  cancelBtn->setText(cancel);
+  useBtn->setText(use);
+}
+
+void NMessageBox::setText( const std::string & msgText )
+{
+  textLbl->setText(msgText);
+}
+
+void NMessageBox::onBtnPress( NButtonEvent * ev )
+{
+  if (ev->sender()==okBtn) {
+     doClose(true);
+  } else
+  if (ev->sender()==useBtn) {
+     use.emit(this);
+  } else
+  if (ev->sender()==cancelBtn) {
+    doClose(false);
+  }
+}
+
+
+
---
+++ ngrs/src/nmessagebox.h
@@ -0,0 +1,64 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Stefan Nattkemper   *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef NMESSAGEBOX_H
+#define NMESSAGEBOX_H
+
+#include <ndialog.h>
+
+class NButton;
+class NLabel;
+
+
+/**
+@author Stefan Nattkemper
+*/
+
+const int nMsgOkBtn = 0;
+const int nMsgCancelBtn = 1;
+const int nMsgUseBtn = 2;
+
+class NMessageBox : public NDialog
+{
+public:
+    NMessageBox();
+    NMessageBox(const std::string & msgText);
+
+    ~NMessageBox();
+
+    void setButtons(int btnMask);
+    void setButtonText(const std::string & ok,const std::string & use, const std::string & cancel);
+    void setText(const std::string & msgText);
+
+    signal1<NObject*> use;
+
+private:
+
+    NButton* okBtn;
+    NButton* cancelBtn;
+    NButton* useBtn;
+
+    NLabel* textLbl;
+
+    void onBtnPress(NButtonEvent* ev);
+
+    void init();
+};
+
+#endif
