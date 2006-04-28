6849e8e4f545fa5ece93f52e6f0289e2c3647e1d
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-04-28 19:28:29 +0000

    global fix on copyright years

--- psycle/src/universalis/compiler/detail/microsoft/property.hpp
+++ psycle/src/universalis/compiler/detail/microsoft/property.hpp
@@ -1,6 +1,6 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright (C) 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
-// Copyright (C) 2005 Johan Boule: rewritting of the property emulation: using two template-based tricks, one to get the class holding the member functions, and another to support all the operators (thanks to SFINAE).
+// copyright 2000-2006 psycledelics http://psycle.pastnotecut.org : Johan Boule
+// copyright 2005-2006 johan boule: rewritting of the property emulation: using two template-based tricks, one to get the class holding the member functions, and another to support all the operators (thanks to SFINAE).
 // Copyright (C) 2004 Boaz Harrosh: original idea for property emulation submited to wine: http://www.winehq.org/hypermail/wine-devel/2004/03/0180.html.
 
 ///\file
