ebb69b819139e6d25ae827931507a60cda7970d7
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-04-30 17:20:47 +0000

    xpsycle: output_plugins: xpsycle master -> freepsycle node input port connection

--- trunk/xpsycle/src/output_plugins/output_plugin.hpp
+++ trunk/xpsycle/src/output_plugins/output_plugin.hpp
@@ -19,29 +19,13 @@
  ***************************************************************************/
 
 #pragma once
-#include "../interface.hpp"
-#include <psycle/host/scheduler/single_threaded.hpp>
+#include "interface.hpp"
 
 namespace psycle
 {
-	namespace output_plugins
+	class output_plugin : public output_plugins::interface
 	{
-		/// connect psycle's master machine to an input port of a freepsycle node
-		class freepsycle_sink : public output_plugin
-		{
-			public:
-				///\param sink_plugin the freepsycle node plugin to instanciate
-				///\param sink_input_port the input port to connect to
-				freepsycle_sink(std::string const & sink_plugin = "output.default", std::string const & sink_input_port = "in");
-			private:
-				/// freepsycle graph connecting psycle with gstreamer
-				engine::graph & graph_;
-				/// freepsycle source node fed with the audio data from psycle
-				engine::node & source_;
-				/// freepsycle sink node where the audio data from psycle's master machine is forwarded to
-				engine::node & sink_;
-				/// scheduler for processing the graph
-				host::schedulers::single_threaded scheduler_;
-		};
-	}
+		public:
+			void output_plugin(AUDIODRIVERWORKFN callback, void *);
+	};
 }
