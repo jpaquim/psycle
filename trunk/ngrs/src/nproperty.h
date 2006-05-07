21c1a1ce1b4854886d6cffee82c8e7c62b46f369
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-05-07 22:16:40 +0000

    property base

---
+++ trunk/ngrs/src/nproperty.h
@@ -0,0 +1,64 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Stefan Nattkemper   *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef NPROPERTY_H
+#define NPROPERTY_H
+
+#include <map>
+#include <string>
+#include "sigslot.h"
+#include <vector>
+
+/**
+@author Stefan Nattkemper
+*/
+
+
+
+template<class SetType, class GetType> 
+class SetGet {
+  public:
+    sigslot::signal1<SetType>  set;
+    sigslot::signal1<GetType>  get;
+    std::string methodName;
+};
+
+class NProperty{
+public:
+    NProperty();
+
+    ~NProperty();
+
+    template<class SetType, class GetType> inline void registrate(const std::string &  methodName, SetGet<SetType, GetType>* setGet) {
+        setGetMap[methodName] = (void*) setGet;
+    }
+
+    void write(const std::string & name, const void* value);
+    void read(const std::string & name, const void* value);
+
+    //SetGet* registerProperty(std::string propertyName, RuntimeType setType, RuntimeType getType);
+
+private:
+
+   std::map<std::string,void*> setGetMap;
+    //std::map<std::string,SetGet>  setGetMap;
+    //SetGetSignal setGet;
+};
+
+#endif
