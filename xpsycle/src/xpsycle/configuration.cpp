368e8b5cfd1c0720e4278ba01a0261c8d8587066
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-05-18 19:00:22 +0000

    xpsycle: moved warning about lack of autoconf configuration to configuration's ctor

--- xpsycle/src/xpsycle/configuration.cpp
+++ xpsycle/src/xpsycle/configuration.cpp
@@ -29,10 +29,11 @@
 
 Configuration::Configuration()
 {
-  setSkinDefaults();
-
-  defaultPatLines = 64;
-
+	#if !defined XPSYCLE__CONFIGURATION
+		std::cout << "xpsycle: warning: built without configuration" << std::endl;
+	#endif
+	setSkinDefaults();
+	defaultPatLines = 64;
 }
 
 
