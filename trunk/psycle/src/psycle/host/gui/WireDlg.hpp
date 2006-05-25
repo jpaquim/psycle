ed556940ac1d8b6964f1b8f73d646cc8ef7a7419
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-05-25 20:34:39 +0000

    WireDlg Source reordering. Changes come next.

--- trunk/psycle/src/psycle/host/gui/WireDlg.hpp
+++ trunk/psycle/src/psycle/host/gui/WireDlg.hpp
@@ -8,9 +8,9 @@ UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 	UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(host)
 		class CChildView;
 
-		#define MAX_SCOPE_BANDS 128
 		#define SCOPE_BUF_SIZE 4096
 		#define SCOPE_SPEC_SAMPLES	256
+		#define MAX_SCOPE_BANDS 128
 
 		class Song;
 
@@ -20,59 +20,66 @@ UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 		public:
 			CWireDlg(CChildView* pParent);
 			BOOL Create();
-			afx_msg void OnCancel();
-			UINT this_index;
-			int wireIndex;
-			int isrcMac;
-			bool Inval;
-			Machine* _pSrcMachine;
-			Machine* _pDstMachine;
-			int _dstWireIndex;
-			float invol;
-			float mult;
 
-			int scope_mode;
-			int scope_peak_rate;
-			int scope_osc_freq;
-			int scope_osc_rate;
-			int scope_spec_bands;
-			int scope_spec_rate;
-			int scope_phase_rate;
+			virtual BOOL OnInitDialog();
+			virtual void OnCancel();
 
-			float peakL,peakR;
-			float peak2L,peak2R;
-			int peakLifeL,peakLifeR;
+			virtual BOOL PreTranslateMessage(MSG* pMsg);
+		protected:
+			void SetMode();
+			inline int GetY(float f);
+			void DrawPoints();
+			RECT GetNodeRect(int node, bool bDirection);
+			void GraphClicked (bool bSelType, CPoint point);
 
-			float o_mvc, o_mvpc, o_mvl, o_mvdl, o_mvpl, o_mvdpl, o_mvr, o_mvdr, o_mvpr, o_mvdpr;
+			virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
+			afx_msg void OnTimer(UINT nIDEvent);
+			afx_msg void OnPaint();
+			afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
+			afx_msg void OnCustomdrawSlsize(NMHDR* pNMHDR, LRESULT* pResult);
+			afx_msg void OnCustomdrawSlspeed(NMHDR* pNMHDR, LRESULT* pResult);
+			afx_msg void OnMode();
+			afx_msg void OnHold();
+			afx_msg void OnDelete();
+			afx_msg void OnCustomdrawSlvolume(NMHDR* pNMHDR, LRESULT* pResult);
+			afx_msg void OnVolumeDb();
+			afx_msg void OnVolumePer();
+			afx_msg void OnBnClickedViewScope();
+			afx_msg void OnBnClickedViewConnections();
+			afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+			afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+			DECLARE_MESSAGE_MAP()
+
+		public:
+		// Dialog Data
+			enum { IDD = IDD_WIREDIALOG };
+			CSliderCtrl	m_slspeed;
+			CSliderCtrl	m_slsize;
+			CStatic m_lblspeed;
+			CStatic m_lblsize;
+			CButton m_mode;
+			CButton	m_btnper;
+			CButton	m_btndb;
+			CSliderCtrl	m_slvolume;
+		protected:
+			CChildView* m_pParent;
+			int numIns, numOuts;
+
+		public:
 			float pSamplesL[SCOPE_BUF_SIZE];
 			float pSamplesR[SCOPE_BUF_SIZE];
 
-			bool bcurrentview;
+			UINT this_index;
+			int wireIndex;
+			int isrcMac;
+			Machine* _pSrcMachine;
+			Machine* _pDstMachine;
+			bool Inval;
+			int _dstWireIndex;
+			float invol;
 
-		// Dialog Data
-			//{{AFX_DATA(CWireDlg)
-			enum { IDD = IDD_WIREDIALOG };
-			CSliderCtrl	m_slider;
-			CSliderCtrl	m_slider2;
-			CStatic	m_volabel_per;
-			CStatic	m_volabel_db;
-			CButton m_mode;
-			CSliderCtrl	m_volslider;
-			//}}AFX_DATA
-		// Overrides
-			// ClassWizard generated virtual function overrides
-			//{{AFX_VIRTUAL(CWireDlg)
-			public:
-			virtual BOOL PreTranslateMessage(MSG* pMsg);
-			protected:
-			virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-			//}}AFX_VIRTUAL
-		// Implementation
 		protected:
-			inline int GetY(float f);
-			void SetMode();
-			CChildView* m_pParent;
 			CBitmap* bufBM;
 			CBitmap* clearBM;
 			CPen linepenL;
@@ -80,42 +87,39 @@ UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 			CPen linepenbL;
 			CPen linepenbR;
 			CRect rc;
+			RECT conn_grapharea;
+			bool bcurrentview;
 			CFont font;
 			CFont* oldFont;
+
 			BOOL hold;
+			int scope_mode;
+			int scope_offset;
+			int scope_peak_rate;
+			int scope_osc_freq;
+			int scope_osc_rate;
+			int scope_spec_bands;
+			int scope_spec_rate;
+			int scope_phase_rate;
+
+			float mult;
 			BOOL clip;
-			int pos;
+			float peakL,peakR;
+			float peak2L,peak2R;
+			int peakLifeL,peakLifeR;
 			int bar_heightsl[MAX_SCOPE_BANDS];
 			int bar_heightsr[MAX_SCOPE_BANDS];
-			// Generated message map functions
-			//{{AFX_MSG(CWireDlg)
-			virtual BOOL OnInitDialog();
-			afx_msg void OnCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult);
-			afx_msg void OnButton1();
-			afx_msg void OnTimer(UINT nIDEvent);
-			afx_msg void OnCustomdrawSlider(NMHDR* pNMHDR, LRESULT* pResult);
-			afx_msg void OnCustomdrawSlider2(NMHDR* pNMHDR, LRESULT* pResult);
-			afx_msg void OnMode();
-			afx_msg void OnHold();
-			afx_msg void OnVolumeDb();
-			afx_msg void OnVolumePer();
-			//}}AFX_MSG
-			DECLARE_MESSAGE_MAP()
+
+			float o_mvc, o_mvpc, o_mvl, o_mvdl, o_mvpl, o_mvdpl, o_mvr, o_mvdr, o_mvpr, o_mvdpr;
+
+
 		public:
-			CStatic m_param1;
-			CStatic m_param2;
-			afx_msg void OnBnClickedViewScope();
-			afx_msg void OnBnClickedViewConnections();
-
-			void DrawPoints();
-			int numIns, numOuts;
-			RECT conn_grapharea;
-
 			int conn_ins_spacing;
 			int conn_outs_spacing;
-			bool bOneSelected;
 			bool bSelectionType;
 			bool bLastSelectionType;
+			bool bOneSelected;
+			int conn_sel_in, conn_sel_out;
 
 			std::vector <bool> ins_node_used;
 			std::vector <bool> outs_node_used;
@@ -123,17 +127,8 @@ UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 			std::vector <std::string> ins_node_names;
 			std::vector <std::string> outs_node_names;
 
-			int conn_sel_in, conn_sel_out;
-
 			int connections[64][64]; //hopefully this is enough, shouldn't be more than 32 pairs of channels
 
-			RECT GetNodeRect(int node, bool bDirection);
-
-			void GraphClicked (bool bSelType, CPoint point);
-			afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
-			afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
-			afx_msg void OnPaint();
-			afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
 };
 
 		//{{AFX_INSERT_LOCATION}}
