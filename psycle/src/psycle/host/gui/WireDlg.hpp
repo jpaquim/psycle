884c590fa39d520872168dd272a8df72f5831cf6
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-05-26 21:56:27 +0000

    WireDlg Reconstruction phase 2.

--- psycle/src/psycle/host/gui/WireDlg.hpp
+++ psycle/src/psycle/host/gui/WireDlg.hpp
@@ -3,16 +3,109 @@
 #pragma once
 #include <psycle/host/engine/Machine.hpp>
 #include <psycle/host/engine/constants.hpp>
-#include "afxwin.h"
+#include <afxwin.h>
 UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 	UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(host)
+
 		class CChildView;
 
 		#define SCOPE_BUF_SIZE 4096
 		#define SCOPE_SPEC_SAMPLES	256
 		#define MAX_SCOPE_BANDS 128
+		#define LOG2	0.301029995663981f
 
-		class Song;
+		class CMultiScopeCtrl : public CStatic
+		{
+		public:
+			enum
+			{
+				mode_vu=0,
+				mode_scope,
+				mode_spectrum,
+				mode_phase,
+				num_osci_modes,
+				mode_viewcons,
+				num_modes
+			} scope_modes;
+
+			CMultiScopeCtrl();
+			virtual ~CMultiScopeCtrl();
+
+			void Initialize();
+			void InitSpectrum();
+			void PrepareView();
+			inline int GetY(float f);
+			RECT GetNodeRect(int node, bool bDirection);
+			virtual void DrawItem( LPDRAWITEMSTRUCT lpDrawItemStruct );
+			void PaintVu(CDC* bufDC);
+			void PaintOsci(CDC* bufDC);
+			void PaintSpectrum(CDC* bufDC);
+			void PaintPhase(CDC* bufDC);
+			void PaintConnections(CDC* bufDC);
+
+		protected:
+			CBitmap* bufBM;
+			CBitmap* clearBM;
+			CPen linepenL;
+			CPen linepenR;
+			CPen linepenbL;
+			CPen linepenbR;
+			CRect rcscope;
+			CRect rccons;
+
+			CFont font;
+
+			float rangecorrection;
+			float vucorrection;
+			float scopecorrection;
+			float spectrumcorrection;
+			float cth[SCOPE_BUF_SIZE][MAX_SCOPE_BANDS];
+			float sth[SCOPE_BUF_SIZE][MAX_SCOPE_BANDS];
+			float heightcompensation[MAX_SCOPE_BANDS];
+
+			int bar_heightsl[MAX_SCOPE_BANDS];
+			int bar_heightsr[MAX_SCOPE_BANDS];
+
+		public:
+			float *pSamplesL;
+			float *pSamplesR;
+			Machine* _pSrcMachine;
+			Machine* _pDstMachine;
+
+
+			bool hold;
+			int scope_mode;
+			int scope_offset;
+			int scope_peak_rate;
+			int scope_osc_freq;
+			int scope_osc_rate;
+			int scope_spec_bands;
+			int scope_spec_rate;
+			int scope_phase_rate;
+
+			bool clip;
+			float peakL,peakR;
+			float peak2L,peak2R;
+			int peakLifeL,peakLifeR;
+			float o_mvc, o_mvpc, o_mvl, o_mvdl, o_mvpl, o_mvdpl, o_mvr, o_mvdr, o_mvpr, o_mvdpr;
+
+			int numIns, numOuts;
+
+			int conn_ins_spacing;
+			int conn_outs_spacing;
+			bool bSelectionType;
+			bool bLastSelectionType;
+			bool bOneSelected;
+			int conn_sel_in, conn_sel_out;
+
+			std::vector <bool> ins_node_used;
+			std::vector <bool> outs_node_used;
+			int connections[64][64]; //hopefully this is enough, shouldn't be more than 32 pairs of channels
+
+
+		};
+
+
 
 		/// wire monitor window.
 		class CWireDlg : public CDialog
@@ -27,15 +120,11 @@ UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 			virtual BOOL PreTranslateMessage(MSG* pMsg);
 		protected:
 			void SetMode();
-			inline int GetY(float f);
-			void DrawPoints();
-			RECT GetNodeRect(int node, bool bDirection);
 			void GraphClicked (bool bSelType, CPoint point);
 
 			virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 			afx_msg void OnTimer(UINT nIDEvent);
-			afx_msg void OnPaint();
 			afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
 			afx_msg void OnCustomdrawSlsize(NMHDR* pNMHDR, LRESULT* pResult);
 			afx_msg void OnCustomdrawSlspeed(NMHDR* pNMHDR, LRESULT* pResult);
@@ -62,9 +151,10 @@ UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 			CButton	m_btnper;
 			CButton	m_btndb;
 			CSliderCtrl	m_slvolume;
+			CMultiScopeCtrl m_multiscope;
+
 		protected:
 			CChildView* m_pParent;
-			int numIns, numOuts;
 
 		public:
 			float pSamplesL[SCOPE_BUF_SIZE];
@@ -75,63 +165,12 @@ UNIVERSALIS__COMPILER__NAMESPACE__BEGIN(psycle)
 			int isrcMac;
 			Machine* _pSrcMachine;
 			Machine* _pDstMachine;
-			bool Inval;
 			int _dstWireIndex;
 			float invol;
 
 		protected:
-			CBitmap* bufBM;
-			CBitmap* clearBM;
-			CPen linepenL;
-			CPen linepenR;
-			CPen linepenbL;
-			CPen linepenbR;
-			CRect rc;
-			RECT conn_grapharea;
-			bool bcurrentview;
-			CFont font;
-			CFont* oldFont;
 
-			BOOL hold;
-			int scope_mode;
-			int scope_offset;
-			int scope_peak_rate;
-			int scope_osc_freq;
-			int scope_osc_rate;
-			int scope_spec_bands;
-			int scope_spec_rate;
-			int scope_phase_rate;
+		};
 
-			float mult;
-			BOOL clip;
-			float peakL,peakR;
-			float peak2L,peak2R;
-			int peakLifeL,peakLifeR;
-			int bar_heightsl[MAX_SCOPE_BANDS];
-			int bar_heightsr[MAX_SCOPE_BANDS];
-
-			float o_mvc, o_mvpc, o_mvl, o_mvdl, o_mvpl, o_mvdpl, o_mvr, o_mvdr, o_mvpr, o_mvdpr;
-
-
-		public:
-			int conn_ins_spacing;
-			int conn_outs_spacing;
-			bool bSelectionType;
-			bool bLastSelectionType;
-			bool bOneSelected;
-			int conn_sel_in, conn_sel_out;
-
-			std::vector <bool> ins_node_used;
-			std::vector <bool> outs_node_used;
-
-			std::vector <std::string> ins_node_names;
-			std::vector <std::string> outs_node_names;
-
-			int connections[64][64]; //hopefully this is enough, shouldn't be more than 32 pairs of channels
-
-};
-
-		//{{AFX_INSERT_LOCATION}}
-		// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 	UNIVERSALIS__COMPILER__NAMESPACE__END
 UNIVERSALIS__COMPILER__NAMESPACE__END
