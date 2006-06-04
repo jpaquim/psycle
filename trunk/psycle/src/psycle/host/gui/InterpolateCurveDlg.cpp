e395d47ec0d6ed192e5d57dbe3a8671a42857655
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-06-04 01:35:26 +0000

    unused #include

--- trunk/psycle/src/psycle/host/gui/InterpolateCurveDlg.cpp
+++ trunk/psycle/src/psycle/host/gui/InterpolateCurveDlg.cpp
@@ -1,7 +1,6 @@
 ///\file
 ///\brief implementation file for psycle::host::CInterpolateCurve.
 #include <packageneric/pre-compiled.private.hpp>
-#include PACKAGENERIC
 #include "psycle.hpp"
 #include "InterpolateCurveDlg.hpp"
 
