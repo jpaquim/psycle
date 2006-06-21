94370ad6011ab3830f56a365d28f3c8c618ec771
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-06-21 22:13:29 +0000

    atoms

---
+++ ngrs/src/ngrs/natoms.cpp
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Stefan Nattkemper   *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "natoms.h"
+
+NAtoms::NAtoms(Display* dpy)
+{
+  wm_delete_window_ = XInternAtom(dpy, "WM_DELETE_WINDOW\0", False);;
+  wm_motif_hint_ = XInternAtom(dpy,"_MOTIF_WM_HINTS\0",False);;
+  net_wm_state_ = XInternAtom(dpy,"_NET_WM_STATE\0",False);
+  net_wm_state_above_ = XInternAtom(dpy,"_NET_WM_STATE_ABOVE\0",False);
+  net_wm_state_stays_on_top_ = XInternAtom(dpy,"_NET_WM_STATE_STAYS_ON_TOP\0",False);
+  net_wm_state_modal_ = XInternAtom(dpy,"_NET_WM_STATE_MODAL\0",False);
+}
+
+
+NAtoms::~NAtoms()
+{
+}
+
+Atom NAtoms::wm_delete_window( ) const
+{
+  return wm_delete_window_;
+}
+
+Atom NAtoms::wm_motif_hint( ) const
+{
+  return wm_motif_hint_;
+}
+
+Atom NAtoms::net_wm_state( ) const
+{
+  return net_wm_state_;
+}
+
+Atom NAtoms::net_wm_state_above( ) const
+{
+  return net_wm_state_above_;
+}
+
+Atom NAtoms::net_wm_state_stays_on_top( ) const
+{
+  return net_wm_state_stays_on_top_;
+}
+
+Atom NAtoms::net_wm_state_modal( ) const
+{
+  return net_wm_state_modal_;
+}
+
---
+++ ngrs/src/ngrs/natoms.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Stefan Nattkemper   *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef NATOMS_H
+#define NATOMS_H
+
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+
+
+/**
+@author Stefan Nattkemper
+*/
+class NAtoms {
+public:
+    NAtoms(Display* dpy);
+
+    ~NAtoms();
+
+	 Atom wm_delete_window() const;
+   Atom wm_motif_hint() const;
+   Atom net_wm_state() const;
+   Atom net_wm_state_above() const;
+   Atom net_wm_state_stays_on_top() const;
+   Atom net_wm_state_modal() const;
+
+private:
+
+   Atom wm_delete_window_;
+   Atom wm_motif_hint_;
+   Atom net_wm_state_;
+   Atom net_wm_state_above_;
+   Atom net_wm_state_stays_on_top_;
+   Atom net_wm_state_modal_;
+
+};
+
+#endif
