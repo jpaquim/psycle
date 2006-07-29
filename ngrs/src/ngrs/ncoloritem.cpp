ce407dccbfcdf5ac000a7c5fb2063031405b4026
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-07-29 18:53:49 +0000

    ...

---
+++ ngrs/src/ngrs/ncoloritem.cpp
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Stefan Nattkemper   *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "ncoloritem.h"
+#include "nfontmetrics.h"
+
+NColorItem::NColorItem()
+ : NCustomItem()
+{
+}
+
+NColorItem::NColorItem( const NColor & color, const std::string & text )
+ : NCustomItem(), color_(color), text_(text)
+{
+}
+
+NColorItem::~NColorItem()
+{
+}
+
+void NColorItem::paint( NGraphics * g )
+{
+  int cw = clientWidth();
+  int ch = clientHeight();
+  int ident = 4;
+
+  g->setForeground(color_);
+  g->fillRect(ident,ident,20,ch-2*ident);
+
+  g->setForeground(NColor(220,220,220));
+  g->drawRect(ident,ident,20,ch-2*ident);
+
+  int yp = (clientHeight() + g->textHeight() /2 ) / 2;
+  g->drawText(2*ident+20,yp , text_);
+}
+
+void NColorItem::setColor( const NColor & color )
+{
+  color_ = color;
+}
+
+const NColor & NColorItem::color( ) const
+{
+  return color_;
+}
+
+int NColorItem::preferredHeight( ) const
+{
+  NFontMetrics metrics( font() );
+  return metrics.textHeight() + spacing().top()+spacing().bottom() +borderTop()+borderBottom();
+}
+
+
+
+
