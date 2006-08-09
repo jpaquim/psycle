09b01fc35f401cfb95692d154f0287d060dad182
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-08-09 09:38:57 +0000

    start work on fileout driver

---
+++ xpsycle/src/xpsycle/wavefileout.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Stefan Nattkemper   *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef WAVEFILEOUT_H
+#define WAVEFILEOUT_H
+
+#include <audiodriver.h>
+#include "riff.h"
+
+namespace psycle
+{
+	namespace host
+	{
+
+		/**
+		@author Stefan Nattkemper
+		*/
+
+		class WaveFileOut : public AudioDriver
+		{
+			public:
+				WaveFileOut();
+
+				~WaveFileOut();
+
+				// starts stops file writing
+				virtual bool Enable(bool e);
+
+				void setFileName( const std::string & fileName);
+				const std::string & fileName() const;
+
+			private:
+
+				std::string fileName_;
+
+				int iret1;
+				pthread_t threadid;
+
+				int kill_thread;
+
+				static int audioOutThread(void * ptr);
+
+				WaveFile _outputWaveFile;
+
+		};
+
+	}
+}
+
+#endif
