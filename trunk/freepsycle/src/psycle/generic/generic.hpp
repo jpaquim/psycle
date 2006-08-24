8b6677ab413a0ac6ab8751d8836c57b291dde136
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-08-24 22:30:10 +0000

    buildbot: test

--- trunk/freepsycle/src/psycle/generic/generic.hpp
+++ trunk/freepsycle/src/psycle/generic/generic.hpp
@@ -1,3 +1,4 @@
+#error
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 2003-2006 johan boule <bohan@jabber.org>
 // copyright 2003-2006 psycledelics http://psycle.sourceforge.net
