ac8bcc91ada02a702d203e0679ea34cc4b98a8a3
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-08-29 22:03:43 +0000

    patcursor

--- xpsycle/src/xpsycle/custompatternview.h
+++ xpsycle/src/xpsycle/custompatternview.h
@@ -45,6 +45,33 @@ namespace psycle {
 				int type_;
 		};
 
+		class PatCursor {
+		public:
+
+			PatCursor();
+			PatCursor( int track, int line, int eventNr, int col );
+			~PatCursor();
+
+			void setPosition( int track, int line, int eventNr, int col );
+
+			void setTrack( int x);
+			int track() const;
+			void setLine( int y);
+			int line() const;
+			void setEventNr( int event );
+			int eventNr() const;
+			void setCol( int col);
+			int col() const;
+
+		private:
+
+			int track_;
+			int line_;
+			int eventNr_;
+			int col_;
+			
+		};
+
 		class CustomPatternView : public NPanel
 		{
 			public:
