e78c4d377c06c4068c150b11b12702a001a21a36
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-09-25 12:03:22 +0000

    nfile xml entities

--- ngrs/src/ngrs/nfile.h
+++ ngrs/src/ngrs/nfile.h
@@ -45,6 +45,15 @@ public:
     static std::string env(const std::string & envName);
     static std::string extractFileNameFromPath(const std::string & fileName);
 
+		// replaces entitys`s for xml writing
+		// There are 5 predefined entity references in XML:
+ 		// &lt; 	< 	less than 
+		// &gt; 	> 	greater than
+    // &amp; 	& 	ampersand 
+    // &apos; 	' 	apostrophe
+    // &quot; 	" 	quotation mark
+    // Only the characters "<" and "&" are strictly illegal in XML. Apostrophes, quotation marks and greater than signs are legal. strict = true  replaces all.
+		static std::string replaceIllegalXmlChr( const std::string & text, bool strict = true );
 };
 
 #endif
