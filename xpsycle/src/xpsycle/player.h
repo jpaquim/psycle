fe00cd3f9df18692ccd205a87cfe5ccec9650587
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2006-09-27 17:18:55 +0000

    player code clearing

--- xpsycle/src/xpsycle/player.h
+++ xpsycle/src/xpsycle/player.h
@@ -45,13 +45,25 @@ namespace psycle
 				song_ = song;
 			}
 
-			/// starts to play.
-			void Start(double pos);
+			// starts to play.
+			void start( double pos = 0.0 );
 
-			/// stops playing.
-			void Stop();
+			// stops playing.
+			void stop();
 
+			// is the player in playmode.
+			bool playing() const;
+
+			// set the actualPlaypos
+			void setPlayPos( double pos );
+
+			// the current playPos
+			double playPos() const;
+
+			// for wave render set the filename
 			void setFileName( const std::string & fileName);
+			
+			// gets the wave to render filename
 			const std::string fileName() const;
 
 			void setAutoRecording( bool on );
@@ -67,6 +79,16 @@ namespace psycle
 			void SampleRate(const int sampleRate);
 
 			bool autoStopMachines;
+
+			// work function. (Entrance for the callback function (audiodriver)
+			static float * Work(void* context, int& nsamples);
+			
+			// used by the plugins to indicate that they need redraw.
+			bool Tweaker;
+
+			void lock();
+			void unlock();
+
 			
 
 		private:
@@ -77,97 +99,41 @@ namespace psycle
 			std::string fileName_;
 			AudioDriver* driver_;
 
+			bool _playing;
 			bool autoRecord_;
 			bool recording_;
 
-			/// Final Loop. Read new line for notes to send to the Machines
+			// Final Loop. Read new line for notes to send to the Machines
 			void ExecuteNotes( double beatOffset , PatternLine & line );
 			void ProcessGlobalEvent(const GlobalEvent & event);
 			void Process(int nsamples);
 
-
-//		PSYCLE__PRIVATE:
-		public:
-			/// Indicates if the playback has moved to a new line. Used for GUI updating.
-			bool _lineChanged;
-			/// elapsed time since playing started. Units is seconds and the float type allows for storing milliseconds.
-			float _playTime;
-			/// elapsed time since playing started in minutes.It just serves to complement the previous variable
-			///\todo There is no need for two vars.
-			int _playTimem;
-			
-
-			/// wether this player has been started.
-			bool _playing;
-			/// wether this player should only play the selected block in the sequence.
-			bool _playBlock;
-			/// wheter this player should play the song/block in loop.
-			bool _loopSong;
-
-		public:
-			
-			/// work function. (Entrance for the callback function (audiodriver)
 			float * Work(int nsamples);
-		//private:
-			static float * Work(void* context, int& nsamples);
 
-			void setPlayPos( double pos );
-			double PlayPos() const { return timeInfo_.playBeatPos(); }
-
-			void lock();
-			void unlock();
-
-		public:
-
-			/// used by the plugins to indicate that they need redraw.
-			bool Tweaker;
-
-		private:
-			/// Stores which machine played last in each track. this allows you to not specify the machine number everytime in the pattern.
+			// Stores which machine played last in each track. this allows you to not specify the machine number everytime in the pattern.
 			Machine::id_type prevMachines[MAX_TRACKS];
-			/// Stores the samplerate of playback when recording to wave offline (non-realtime), since it can be changed.
+			// Stores the samplerate of playback when recording to wave offline (non-realtime), since it can be changed.
 			int backup_rate;
-			/// Stores the bitdepth of playback when recording to wave offline (non-realtime), since it can be changed.
+			// Stores the bitdepth of playback when recording to wave offline (non-realtime), since it can be changed.
 			int backup_bits;
-			/// Stores the channel mode (mono/stereo) of playback when recording to wave offline (non-realtime), since it can be changed.
+			// Stores the channel mode (mono/stereo) of playback when recording to wave offline (non-realtime), since it can be changed.
 			int backup_channelmode;
-			/// Temporary buffer to get all the audio from Master (which work in small chunks), and send it to the soundcard after converting it to float.
+			// Temporary buffer to get all the audio from Master (which work in small chunks), and send it to the soundcard after converting it to float.
 			float _pBuffer[MAX_DELAY_BUFFER];
-			/// file to which to output signal.
+			// file to which to output signal.
 			WaveFile _outputWaveFile;
-			/// dither handler
+			// dither handler
 			dsp::Dither dither;
 
 			void writeSamplesToFile( int amount );
 			bool _doDither;
 
-		///\name deprecated by multiseq
-		///\{
-		public:
-				
-				/// Reports the LinesPerBeat (used for machines that require "ticks" between commands).
-				int LinesPerBeat() { return timeInfo_.linesPerBeat(); }
-			/// Used to indicate that the SamplesPerRow has been manually changed ( right now, in effects "pattern delay" and "fine delay" )
-			bool _SPRChanged;
-			/// the line currently being played in the current pattern
-			int _lineCounter;			
-			/// the pattern currently being played.
-			/// Moves the cursor one line forward, changing the pattern if needed.
-			//void ExecuteGlobalCommands( std::list<PatternLine*> & tempPlayLines );
-			/// Contains the number of samples until a line change comes in.
-			int _samplesRemaining;
-
-
-		private:
-			
-			short _patternjump;
-			short _linejump;
-			short _loop_count;
-			short _loop_line;
-
+			// here we need some real mutexes			
 			bool lock_;
 			bool inWork_;
-		///\}
+			// Contains the number of samples until a line change comes in.
+			int _samplesRemaining;
+		
 		};
 	}
 }
