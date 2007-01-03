c9808a102ec854926858d44777f8a0862ac597f7
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-01-03 19:22:25 +0000

    Happy new year psycle!

--- trunk/psycle/src/psycle/host/detail/project.private.hpp
+++ trunk/psycle/src/psycle/host/detail/project.private.hpp
@@ -1,5 +1,5 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright (C) 2006 Psycledelics http://psycle.pastnotecut.org
+// copyright 2006-2007 psycledelics http://psycle.pastnotecut.org
 
 ///\file
 ///\brief global private include for source files of the psycle::host project
