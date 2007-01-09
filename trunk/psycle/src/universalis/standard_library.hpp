cc993e739718ddb9d67d211965b44939b20f01c0
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-01-09 20:18:18 +0000

    svn:externals, with an s

--- trunk/psycle/src/universalis/standard_library.hpp
+++
@@ -1,8 +0,0 @@
-// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2007 johan boule <bohan@jabber.org>
-// copyright 2004-2007 psycledelics http://psycle.pastnotecut.org
-
-///\file
-///\brief meta header
-#pragma once
-#include "standard_library/standard_library.hpp"
