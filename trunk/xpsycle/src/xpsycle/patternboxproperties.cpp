5a826f5880d28e66b731eb62eed89a1a9d11f502
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-02-10 20:22:57 +0000

    another addendum

--- trunk/xpsycle/src/xpsycle/patternboxproperties.cpp
+++ trunk/xpsycle/src/xpsycle/patternboxproperties.cpp
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include "patternboxproperties.h"
-#include "sequencerbar.h"
+#include "patternbox.h"
 #include "patterndata.h"
 #include <ngrs/frameborder.h>
 #include <ngrs/tablelayout.h>
