3303872dcc1ae4107c178124c931aff7a5e29f42
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-02-10 12:46:44 +0000

    Add a couple of older ngrs files back to accomodate current xpsycle changes.  All the others are untouched.

---
+++ ngrs/src/ngrs/ntreenode.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *   Copyright (C) 2005, 2006, 2007 by Stefan Nattkemper  *
+ *   natti@linux   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef NTREENODE_H
+#define NTREENODE_H
+
+#include "flipbox.h"
+
+/**
+@author Stefan Nattkemper
+*/
+
+namespace ngrs {
+
+  class CustomItem;
+
+  class NTreeNode : public NFlipBox
+  {
+
+
+  public:
+    NTreeNode();
+
+    ~NTreeNode();
+
+    signal2<NTreeNode*, CustomItem*> itemSelected;
+
+    virtual void setHeader(CustomItem* entry);
+    virtual void addEntry(CustomItem* entry);
+    virtual void addNode(NTreeNode* node);
+
+
+    CustomItem* headerItem();
+
+    virtual void paint( Graphics& g );
+
+  private:
+
+    Panel* entries_;
+    Panel* subNodes;
+
+    CustomItem* headerItem_;
+
+    void onItemPress(ButtonEvent * ev);
+
+  };
+
+}
+
+#endif
