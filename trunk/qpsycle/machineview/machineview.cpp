1f62b953f69a251d79b97dd85617cbca85508299
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-02-28 23:17:48 +0000

    qpsycle: added copyright notices to wiregui files.

--- trunk/qpsycle/machineview/machineview.cpp
+++ trunk/qpsycle/machineview/machineview.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-*   Copyright (C) 2006 by  Neil Mather   *
+*   Copyright (C) 2007 by  Neil Mather   *
 *   nmather@sourceforge   *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
