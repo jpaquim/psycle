30f6ae4933b5ba3899f83a70f32720198fe9f033
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-03-02 01:01:46 +0000

    qpsycle: made new connection line start from centre of machinegui.

--- qpsycle/machineview/machineview.cpp
+++ qpsycle/machineview/machineview.cpp
@@ -47,6 +47,10 @@
      machGui2->setName("Baz");
      connect( machGui0, SIGNAL(wiringNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)), this, SLOT(newConnection(MachineGui*, QGraphicsSceneMouseEvent*)) );
      connect( machGui0, SIGNAL(closeNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)), this, SLOT(closeNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)) );
+     connect( machGui1, SIGNAL(wiringNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)), this, SLOT(newConnection(MachineGui*, QGraphicsSceneMouseEvent*)) );
+     connect( machGui1, SIGNAL(closeNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)), this, SLOT(closeNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)) );
+     connect( machGui2, SIGNAL(wiringNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)), this, SLOT(newConnection(MachineGui*, QGraphicsSceneMouseEvent*)) );
+     connect( machGui2, SIGNAL(closeNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)), this, SLOT(closeNewConnection(MachineGui*, QGraphicsSceneMouseEvent*)) );
 
      scene_->addItem(machGui0);
      scene_->addItem(machGui1);
@@ -61,7 +65,7 @@
 
      // A temporary line to display when user is making a new connection.
      tempLine_ = new QGraphicsLineItem(0, 0, 0, 0);
-     tempLine_->setPen(QPen(Qt::white));
+     tempLine_->setPen(QPen(Qt::gray,2,Qt::DashLine));
      tempLine_->setVisible(false);// We don't want it to be visible yet.
      scene_->addItem(tempLine_);
  }
@@ -102,7 +106,7 @@
  void MachineView::newConnection(MachineGui *macGui, QGraphicsSceneMouseEvent *event)
  {
      qDebug("machineview: new con");
-     tempLine_->setLine(QLineF(macGui->x(), macGui->y(), event->scenePos().x(), event->scenePos().y()));
+     tempLine_->setLine( QLineF( macGui->centrePointInSceneCoords(), event->scenePos() ) );
      tempLine_->setVisible(true);
  }
 
