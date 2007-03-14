9effa27967a105b388083f3e9d52abd31ea23628
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-03-14 19:06:44 +0000

    qpsycle: moving multiple items at once in the sequencer.

--- trunk/qpsycle/moc_mainwindow.cpp
+++ trunk/qpsycle/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Wed Mar 14 18:34:32 2007
+** Created: Wed Mar 14 18:55:59 2007
 **      by: The Qt Meta Object Compiler version 59 (Qt 4.2.1)
 **
 ** WARNING! All changes made in this file will be lost!
