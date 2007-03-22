c839130f467be1f57b4794eec289ba8ed1882989
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-03-22 18:05:16 +0000

    qpsycle: tidy up patternview code.

--- qpsycle/patternview/patternview.h
+++ qpsycle/patternview/patternview.h
@@ -17,76 +17,75 @@
 *   Free Software Foundation, Inc.,                                       *
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
- #ifndef PATTERNVIEW_H
- #define PATTERNVIEW_H
+#ifndef PATTERNVIEW_H
+#define PATTERNVIEW_H
 
- #include <QVBoxLayout>
+#include <QVBoxLayout>
 
- #include "linenumbercolumn.h"
+#include "linenumbercolumn.h"
 #include "patterndraw.h"
 #include "patterngrid.h"
- #include "psycore/song.h"
- #include "psycore/singlepattern.h"
+#include "psycore/song.h"
+#include "psycore/singlepattern.h"
 
- class QToolBar;
- class QComboBox;
- class QGridLayout;
- class QAction;
+class QToolBar;
+class QComboBox;
+class QAction;
 
- class LineNumberColumn;
- class PatternDraw;
- class PatternGrid;
- class PatCursor;
+class LineNumberColumn;
+class PatternDraw;
+class PatternGrid;
+class PatCursor;
 
- class PatternView : public QWidget
- {
-     Q_OBJECT
+class PatternView : public QWidget
+{
+    Q_OBJECT
 
- public:
-     PatternView( psy::core::Song *song );
+public:
+    PatternView( psy::core::Song *song );
 
-     int rowHeight() const;
-     int numberOfLines() const;
-     int numberOfTracks() const;
-     void setNumberOfTracks( int numTracks );
-     int trackWidth() const;
+    void enterNote( const PatCursor & cursor, int note );
+    void onTick( double sequenceStart );
 
-     void setPattern( psy::core::SinglePattern *pattern );
-     psy::core::SinglePattern *pattern();
 
-     void enterNote( const PatCursor & cursor, int note );
-    void keyPressEvent( QKeyEvent *event );
-
-    void setSelectedMachineIndex( int idx );
-    int selectedMachineIndex() const;
-
+    // Getters.
+    psy::core::Song *song() { return song_; }
+    psy::core::SinglePattern *pattern() const { return pattern_; }
     PatternDraw* patDraw() { return patDraw_; }
+    PatternGrid* patternGrid(); 
+    int rowHeight() const;
+    int numberOfLines() const;
+    int numberOfTracks() const;
+    int trackWidth() const;
+    int selectedMachineIndex() const;
+    int playPos() { return playPos_; }
     int beatZoom() const;
 
-    void onTick( double sequenceStart );
-    int playPos() { return playPos_; }
+    // Setters.
+    void setSelectedMachineIndex( int idx );
+    void setPattern( psy::core::SinglePattern *pattern );
+    void setNumberOfTracks( int numTracks );
 
+    // GUI events.
+    void keyPressEvent( QKeyEvent *event );
 
- private:
+private:
     void createToolBar();
-    PatternDraw *patDraw_;
-    psy::core::SinglePattern* pattern_;
 
     psy::core::Song *song_;
+    psy::core::SinglePattern* pattern_;
 
     int numberOfTracks_;
-    QVBoxLayout *layout;
+    int selectedMacIdx_;
+    int playPos_;
 
+    PatternDraw *patDraw_;
+    QVBoxLayout *layout;
     QToolBar *toolBar_;
     QComboBox *meterCbx_;
     QComboBox *patternCbx_;
-
     QAction *delBarAct_;
 
-    int selectedMacIdx_;
+};
 
-    int playPos_;
-
- };
-
- #endif
+#endif
