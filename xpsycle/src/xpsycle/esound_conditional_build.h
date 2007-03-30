a967699e5ff645fbc902627a9279fc9f23ddadab
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-03-30 20:33:30 +0000

    unmaintained branch

--- xpsycle/src/xpsycle/esound_conditional_build.h
+++
@@ -1,5 +0,0 @@
-/* ./src/xpsycle/esound_conditional_build.h.  Generated from esound_conditional_build.h.in by configure.  */
-#ifndef XPSYCLE__ESOUND_CONDITIONAL_BUILD
-#define XPSYCLE__ESOUND_CONDITIONAL_BUILD
-	#define XPSYCLE__NO_ESOUND 
-#endif
