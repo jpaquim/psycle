59f2a1d36bd9f1b3032ffbfa946db7f4d5815bda
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-05-01 15:11:33 +0000

    added results for amd64 on old ms compiler

--- speed-tests/double_to_int/msvc.cmd
+++ speed-tests/double_to_int/msvc.cmd
@@ -1,1 +1,3 @@
-cl doubletoint-msvc71.cpp -c -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -Ob2 -Oi -Ot -Oy -fp:fast -arch:SSE2 /link doubletoint-msvc71.obj /OUT:a.exe /LTCG winmm.lib
+cl doubletoint-msvc71.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast              -link -LTCG winmm.lib -out:x87.exe
+cl doubletoint-msvc71.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -QIfist -link -LTCG winmm.lib -out:x87-QIfist.exe
+cl doubletoint-msvc71.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -arch:SSE2   -link -LTCG winmm.lib -out:sse2.exe
