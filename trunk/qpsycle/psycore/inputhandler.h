81457b389f2625cfb9edd5676b02f811c34a806d
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-05-20 18:52:13 +0000

    indentation

--- trunk/qpsycle/psycore/inputhandler.h
+++ trunk/qpsycle/psycore/inputhandler.h
@@ -261,7 +261,7 @@ namespace psy {
 		{
 			public:
 				Key() : shift_(), vkey_() {}
-				Key( int shift, int key ) : shift(shift), key(key) {}
+				Key( int shift, int key ) : shift_(shift), key_(key) {}
 
 				bool operator<(const Key & key) const;
 
