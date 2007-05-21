2c6e341cb0341632e432149274be15f48cba04bd
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-05-21 01:39:27 +0000

    implemented a tiny player, using psycore. i don't know why i can't hear sound.

--- trunk/psycle-player/src/psycle/player/configuration.cpp
+++ trunk/psycle-player/src/psycle/player/configuration.cpp
@@ -52,9 +52,6 @@
 #include <QDomDocument> // for reading XML file
 
 Configuration::Configuration()
-:
-	_RecordTweaks(),
-	_RecordUnarmed()
 {
 	//setXmlDefaults();
 
@@ -90,157 +87,8 @@ Configuration::Configuration()
 	#if defined PSYCLE__NET_AUDIO_AVAILABLE
 		addAudioDriver(new psy::core::NetAudioOut);
 	#endif
-	configureKeyBindings();
 	loadConfig();			
 }
-
-#if 0
-void Configuration::setXmlDefaults() {     
-	std::string xml_mem_;   
-
-	xml_mem_ =  " <!-- xpsycle configuration file --> ";
-	xml_mem_ += " <xpsycle> ";
-	xml_mem_ += " <path id='plugindir'";
-	xml_mem_ += " src='c:\\Programme\\Psycle\\PsyclePlugins' />";
-
-	//			xml_mem_ += " <audio enable='1' />";
-	//			xml_mem_ += " <driver name='mswaveout' />"; 
-
-	/*  xml_mem_ += " <!-- help configuration -->";
-	// bohan i don't know where to close that comment, so i close it here: */
-	//  xml_mem_ += " <path id='hlpdir' src='~/xpsycle/doc/' />";
-
-	xml_mem_ += " <!-- audio configuration -->";
-
-
-	xml_mem_ += " <!-- gui configuration -->";
-
-	//  xml_mem_ += " <path id='icondir' src='~/xpsycle/icons/' />";
-
-	//  xml_mem_ += " <path id='prsdir' src='~/.xpsycle/prs/'></path>";
-
-	xml_mem_ += " <!-- keyHandler configuration -->";
-	xml_mem_ += " <!-- general keys -->";
-
-	xml_mem_ += " <key id='current_machine-1' mod='ctrl' keychar='NK_Left' />";
-	xml_mem_ += " <key id='current_machine+1' mod='ctrl' keychar='NK_Right' />";
-	xml_mem_ += " <key id='current_instrument-1' mod='ctrl' keychar='NK_Up' />";
-	xml_mem_ += " <key id='current_instrument+1' mod='ctrl' keychar='NK_Down' />";
-
-	xml_mem_ += " <key id='screen_machines' keychar='F2' />";
-	xml_mem_ += " <key id='screen_patterns' keychar='F3' />";
-	xml_mem_ += " <key id='edit_instrument' keychar='F4' />";
-	xml_mem_ += " <key id='screen_sequencer' keychar='F5' />";
-	xml_mem_ += " <key id='play_song_start' keychar='F6' />";
-	xml_mem_ += " <key id='play_song_normal' keychar='F7' />";
-	xml_mem_ += " <key id='stop_playback' keychar='F8' />";
-	xml_mem_ += " <key id='add_new_machine' keychar='F9' />";
-
-	xml_mem_ += " <!-- patternview octave 0  -->";
-
-	xml_mem_ += " <key id='oct_C_0' keychar='Y'/>";
-	xml_mem_ += " <key id='oct_CS0' keychar='S'/>";
-	xml_mem_ += " <key id='oct_D_0' keychar='X'/>";
-	xml_mem_ += " <key id='oct_DS0' keychar='D'/>";
-	xml_mem_ += " <key id='oct_E_0' keychar='C'/>";
-	xml_mem_ += " <key id='oct_F_0' keychar='V'/>";
-	xml_mem_ += " <key id='oct_FS0' keychar='G'/>";
-	xml_mem_ += " <key id='oct_G_0' keychar='B'/>";
-	xml_mem_ += " <key id='oct_GS0' keychar='H'/>";
-	xml_mem_ += " <key id='oct_A_0' keychar='N'/>";
-	xml_mem_ += " <key id='oct_AS0' keychar='J'/>";
-	xml_mem_ += " <key id='oct_B_0' keychar='M'/>";
-
-	xml_mem_ += " <!-- patternview octave 1  -->";
-
-	xml_mem_ += " <key id='oct_C_1' keychar='Q'/>";
-	xml_mem_ += " <key id='oct_CS1' keychar='2'/>";
-	xml_mem_ += " <key id='oct_D_1' keychar='W'/>";
-	xml_mem_ += " <key id='oct_DS1' keychar='3'/>";
-	xml_mem_ += " <key id='oct_E_1' keychar='E'/>";
-	xml_mem_ += " <key id='oct_F_1' keychar='R'/>";
-	xml_mem_ += " <key id='oct_FS1' keychar='5'/>";
-	xml_mem_ += " <key id='oct_G_1' keychar='T'/>";
-	+1)xml_mem_ += " <key id='oct_GS1' keychar='6'/>";
-	xml_mem_ += " <key id='oct_A_1' keychar='Z'/>";
-	xml_mem_ += " <key id='oct_AS1' keychar='7'/>";
-	xml_mem_ += " <key id='oct_B_1' keychar='U'/>";
-
-	xml_mem_ += " <!-- patternview octave 2  -->";
-
-	xml_mem_ += " <key id='oct_C_2' keychar='I' />";
-	xml_mem_ += " <key id='oct_CS2' keychar='9' />";
-	xml_mem_ += " <key id='oct_D_2' keychar='O' />";
-	xml_mem_ += " <key id='oct_DS2' keychar='0' />";
-
-	xml_mem_ += " <!-- patternview misc key options -->";
-
-	xml_mem_ += " <key id='edit_toggle' mod='ctrl' keychar=' ' />";
-	xml_mem_ += " <key id='key_stop' keychar='1' />";
-	xml_mem_ += " <key id='current_octave-1' mod='ctrl' keychar='+' />";
-	xml_mem_ += " <key id='current_octave+1' mod='ctrl' keychar='#' />";
-	xml_mem_ += " <key id='patternstep_dec' keychar='[' />";
-	xml_mem_ += " <key id='patternstep_inc' keychar=']' />";
-
-	xml_mem_ += " <!-- patternview navigation key options -->";
-	xml_mem_ += " <key id='nav_left' keychar='NK_Left' />";
-	xml_mem_ += " <key id='nav_right' keychar='NK_Right' />";
-	xml_mem_ += " <key id='nav_up' keychar='NK_Up' />";
-	xml_mem_ += " <key id='nav_down' keychar='NK_Down' />";
-	xml_mem_ += " <key id='nav_up_16' keychar='NK_Page_Up' />";
-	xml_mem_ += " <key id='nav_down_16' keychar='NK_Page_Down' />";
-	xml_mem_ += " <key id='nav_top' keychar='NK_Home' />";
-	xml_mem_ += " <key id='nav_bottom' keychar='NK_End' />";
-	xml_mem_ += " <key id='next_column' keychar='NK_Tab' />";
-	xml_mem_ += " <key id='prev_column' keychar='XK_ISO_Left_Tab' />";
-	xml_mem_ += " <key id='delete_row' keychar='NK_Delete' />";
-	xml_mem_ += " <key id='insert_row' keychar='NK_Insert' />";
-	xml_mem_ += " <key id='clear_row' keychar='NK_BackSpace' />";
-
-	xml_mem_ += " <!-- patternview block key options -->";
-
-	xml_mem_ += " <key id='block_copy' mod='ctrl' keychar='C' />";
-	xml_mem_ += " <key id='block_cut' mod='ctrl' keychar='X' />";
-	xml_mem_ += " <key id='block_delete' mod='ctrl' keychar='Y' />";
-	xml_mem_ += " <key id='block_double' mod='ctrl' keychar='D' />";
-	xml_mem_ += " <key id='block_halve' mod='ctrl' keychar='H' />";
-	xml_mem_ += " <key id='block_interpolate' mod='ctrl' keychar='I' />";
-	xml_mem_ += " <key id='block_mix' mod='ctrl' keychar='M' />";
-	xml_mem_ += " <key id='block_paste' mod='ctrl' keychar='V' />";
-	xml_mem_ += " <key id='block_select_all' mod='ctrl' keychar='A' />";
-	xml_mem_ += " <key id='block_select_bar' mod='ctrl' keychar='E' />";
-	xml_mem_ += " <key id='block_select_column' mod='ctrl' keychar='R' />";
-	xml_mem_ += " <key id='block_select_up' mod='shift' keychar='NK_Up' />";
-	xml_mem_ += " <key id='block_select_down' mod='shift' keychar='NK_Down' />";
-	xml_mem_ += " <key id='block_select_left' mod='shift' keychar='NK_Left' />";
-	xml_mem_ += " <key id='block_select_right' mod='shift' keychar='NK_Right' />";
-	xml_mem_ += " <key id='block_select_top' mod='shift' keychar='NK_Home' />";
-	xml_mem_ += " <key id='block_select_bottom' mod='shift' keychar='NK_End' />";
-	xml_mem_ += " <key id='block_set_instrument' mod='ctrl' keychar='T' />";
-	xml_mem_ += " <key id='block_set_machine' mod='ctrl' keychar='G' />";
-	xml_mem_ += " <key id='block_start' mod='ctrl' keychar='B' />";
-	xml_mem_ += " <key id='block_start' mod='ctrl' keychar='K' />";
-	xml_mem_ += " <key id='block_unmark' mod='ctrl' keychar='U' />";
-
-	xml_mem_ += " </xpsycle> ";
-
-	const char* pcLADSPAPath = std::getenv("LADSPA_PATH");
-	if ( !pcLADSPAPath) {
-		#ifdef __unix__
-			pcLADSPAPath = "/usr/lib/ladspa/";
-		#else
-			///\todo hardcoded path. if env isn't set, maybe look in the registry db.
-			pcLADSPAPath = "I:\\Archivos de programa\\Multimedia\\Audacity\\Plug-Ins";
-		#endif
-	}
-	ladspaPath_ = pcLADSPAPath;
-
-	///\todo uses ngrs xml parser
-	ngrs::XmlParser parser;
-	parser.tagParse.connect(this,&Configuration::onConfigTagParse);
-	parser.parseString ( xml_mem_ );
-}
-#endif
 
 void Configuration::addAudioDriver(AudioDriver* driver)
 {
@@ -254,12 +102,13 @@ void Configuration::setDriverByName( const std::string & driverName )
 	if ( ( it = driverMap_.find( driverName ) ) != driverMap_.end() ) {
 		// driver found
 		_pOutputDriver = it->second;
+		std::cout << "psycle: configuration: audio driver set to: " << driverName << "\n";
 	}
 	else {
+		std::cerr << "psycle: configuration: audio driver not found: " << driverName << ", setting fallback: " << _pOutputDriver->info().name() << "\n";
 		// driver not found,  set silent default driver
 		_pOutputDriver = _pSilentDriver;
 	}
-	std::cout << "psycle: configuration: audio driver not found, setting fallback: " << _pOutputDriver->info().name() << std::endl;
 }
 
 void Configuration::loadConfig()
@@ -302,17 +151,8 @@ void Configuration::loadConfig( const std::string & path )
 			QDomElement path = paths.item( i ).toElement();
 			std::string id = path.attribute("id").toStdString();
 			std::string src = path.attribute("src").toStdString();
-			if ( id == "icondir" )   iconPath_   = src;
-			else  
-				if ( id == "plugindir" ) pluginPath_ = src;
-				else
-					if ( id == "prsdir" ) prsPath_ = src;
-					else
-						if ( id == "hlpdir" ) hlpPath_ = src; 
-						else
-							if ( id == "ladspadir" ) ladspaPath_ = src;
-							else  
-								if ( id == "songdir" ) songPath_ = src;
+			if ( id == "plugindir" ) pluginPath_ = src;
+			else if ( id == "ladspadir" ) ladspaPath_ = src;
 		}
 
 		// Audio.
@@ -344,118 +184,7 @@ void Configuration::loadConfig( const std::string & path )
 				it->second->setSettings( settings );
 			}		
 		}
-
-		// Options.
-		QDomNodeList options = root.elementsByTagName( "option" );
-		for ( int i = 0; i < options.count(); i++ )
-		{
-			QDomElement option = options.item( i ).toElement();
-			QString id = option.attribute("id");
-			QString value = option.attribute("value");
-			if ( id == "ft2-home-end-behaviour" )
-				ft2HomeEndBehaviour_ = value.toInt();     
-			if ( id == "shift-arrow-for-select" )
-				shiftArrowForSelect_ = value.toInt();     
-			if ( id == "wrap-around" )
-				wrapAround_ = value.toInt();     
-			if ( id == "center-cursor" )
-				centerCursor_ = value.toInt();     
-		}
 	}
 
 	doEnableSound = true;
-}
-
-void Configuration::configureKeyBindings() // FIXME: Key bindings are host specific, should be moved?
-{
-	int modifiers = Qt::NoModifier; // Shouldn't have Qt stuff in psycore in the future.
-	inputHandler_.changeKeyCode(commands::key_C_0, Key( modifiers, Qt::Key_Z ) );
-	inputHandler_.changeKeyCode(commands::key_CS0, Key( modifiers, Qt::Key_S ) );
-	inputHandler_.changeKeyCode(commands::key_D_0,Key(modifiers,Qt::Key_X));
-	inputHandler_.changeKeyCode(commands::key_DS0,Key(modifiers,Qt::Key_D));
-	inputHandler_.changeKeyCode(commands::key_E_0,Key(modifiers,Qt::Key_C));
-	inputHandler_.changeKeyCode(commands::key_F_0,Key(modifiers,Qt::Key_V));
-	inputHandler_.changeKeyCode(commands::key_FS0,Key(modifiers,Qt::Key_G));
-	inputHandler_.changeKeyCode(commands::key_G_0,Key(modifiers,Qt::Key_B));
-	inputHandler_.changeKeyCode(commands::key_GS0,Key(modifiers,Qt::Key_H));
-	inputHandler_.changeKeyCode(commands::key_A_0,Key(modifiers,Qt::Key_N));
-	inputHandler_.changeKeyCode(commands::key_AS0,Key(modifiers,Qt::Key_J));
-	inputHandler_.changeKeyCode(commands::key_B_0,Key(modifiers,Qt::Key_M));
-	inputHandler_.changeKeyCode(commands::key_C_1,Key(modifiers,Qt::Key_Q));
-	inputHandler_.changeKeyCode(commands::key_CS1,Key(modifiers,Qt::Key_2));
-	inputHandler_.changeKeyCode(commands::key_D_1,Key(modifiers,Qt::Key_W));
-	inputHandler_.changeKeyCode(commands::key_DS1,Key(modifiers,Qt::Key_3));
-	inputHandler_.changeKeyCode(commands::key_E_1,Key(modifiers,Qt::Key_E));
-	inputHandler_.changeKeyCode(commands::key_F_1,Key(modifiers,Qt::Key_R));
-	inputHandler_.changeKeyCode(commands::key_FS1,Key(modifiers,Qt::Key_5));
-	inputHandler_.changeKeyCode(commands::key_G_1,Key(modifiers,Qt::Key_T));
-	inputHandler_.changeKeyCode(commands::key_GS1,Key(modifiers,Qt::Key_6));
-	inputHandler_.changeKeyCode(commands::key_A_1,Key(modifiers,Qt::Key_Y));
-	inputHandler_.changeKeyCode(commands::key_AS1,Key(modifiers,Qt::Key_7));
-	inputHandler_.changeKeyCode(commands::key_B_1,Key(modifiers,Qt::Key_U));
-	inputHandler_.changeKeyCode(commands::key_C_2,Key(modifiers,Qt::Key_I));
-	inputHandler_.changeKeyCode(commands::key_CS2,Key(modifiers,Qt::Key_9));
-	inputHandler_.changeKeyCode(commands::key_D_2,Key(modifiers,Qt::Key_O));
-	inputHandler_.changeKeyCode(commands::key_DS2,Key(modifiers,Qt::Key_0));
-	inputHandler_.changeKeyCode(commands::key_E_2,Key(modifiers,Qt::Key_P));
-	/*inputHandler_.changeKeyCode(commands::key_F_2,Key(modifiers,Qt::Key_X)); // no bindings for now.
-	inputHandler_.changeKeyCode(commands::key_FS2,Key(modifiers,Qt::Key_X));
-	inputHandler_.changeKeyCode(commands::key_G_2,Key(modifiers,Qt::Key_X));
-	inputHandler_.changeKeyCode(commands::key_GS2,Key(modifiers,Qt::Key_X));
-	inputHandler_.changeKeyCode(commands::key_A_2,Key(modifiers,Qt::Key_X));*/
-
-	inputHandler_.changeKeyCode( commands::show_pattern_box, Key( modifiers, Qt::Key_F1 ) );
-	inputHandler_.changeKeyCode( commands::show_machine_view, Key( modifiers, Qt::Key_F2 ) );
-	inputHandler_.changeKeyCode( commands::show_pattern_view, Key( modifiers, Qt::Key_F3 ) );
-	inputHandler_.changeKeyCode( commands::show_wave_editor, Key( modifiers, Qt::Key_F4 ) );
-	inputHandler_.changeKeyCode( commands::show_sequencer_view, Key( modifiers, Qt::Key_F5 ) );
-
-	/** Play control commands. **/
-	inputHandler_.changeKeyCode( commands::play_from_position, Key( Qt::NoModifier, Qt::Key_F6 ) );
-	inputHandler_.changeKeyCode( commands::play_start, Key( Qt::ShiftModifier, Qt::Key_F6 ) );
-	inputHandler_.changeKeyCode( commands::play_loop_entry, Key( Qt::NoModifier, Qt::Key_F7 ) );
-	inputHandler_.changeKeyCode( commands::play_stop, Key( Qt::NoModifier, Qt::Key_F8 ) );
-
-	inputHandler_.changeKeyCode( commands::instrument_dec, Key( Qt::ControlModifier, Qt::Key_Minus ) );
-	inputHandler_.changeKeyCode( commands::instrument_inc, Key( Qt::ControlModifier, Qt::Key_Equal ) );
-
-	inputHandler_.changeKeyCode( commands::octave_down, Key( Qt::KeypadModifier, Qt::Key_Slash ) );
-	inputHandler_.changeKeyCode( commands::octave_up, Key( Qt::KeypadModifier, Qt::Key_Asterisk ) );
-
-	/** Machine View. **/
-	inputHandler_.changeKeyCode( commands::mute_machine, Key( Qt::ShiftModifier, Qt::Key_M ) );
-	inputHandler_.changeKeyCode( commands::solo_machine, Key( Qt::ShiftModifier, Qt::Key_S ) );
-	inputHandler_.changeKeyCode( commands::bypass_machine, Key( Qt::ShiftModifier, Qt::Key_B ) );
-
-	/** Pattern View. **/
-	inputHandler_.changeKeyCode( commands::navigate_up, Key( Qt::NoModifier, Qt::Key_Up ) );
-	inputHandler_.changeKeyCode( commands::navigate_down, Key( Qt::NoModifier, Qt::Key_Down ) );
-	inputHandler_.changeKeyCode( commands::navigate_left, Key( Qt::NoModifier, Qt::Key_Left ) );
-	inputHandler_.changeKeyCode( commands::navigate_right, Key( Qt::NoModifier, Qt::Key_Right ) );
-	inputHandler_.changeKeyCode( commands::navigate_page_down, Key( Qt::NoModifier, Qt::Key_PageDown ) );
-	inputHandler_.changeKeyCode( commands::navigate_page_up, Key( Qt::NoModifier, Qt::Key_PageUp ) );
-	inputHandler_.changeKeyCode( commands::track_next, Key( Qt::NoModifier, Qt::Key_Tab ) );
-	inputHandler_.changeKeyCode( commands::track_prev, Key( Qt::ShiftModifier, Qt::Key_Backtab ) );
-	inputHandler_.changeKeyCode( commands::navigate_top, Key( Qt::NoModifier, Qt::Key_Home ) );
-	inputHandler_.changeKeyCode( commands::navigate_bottom, Key( Qt::NoModifier, Qt::Key_End ) );
-
-	inputHandler_.changeKeyCode( commands::pattern_step_dec, Key( Qt::NoModifier, Qt::Key_BracketLeft ) );
-	inputHandler_.changeKeyCode( commands::pattern_step_inc, Key( Qt::NoModifier, Qt::Key_BracketRight ) );
-
-	inputHandler_.changeKeyCode( commands::select_up, Key( Qt::ShiftModifier, Qt::Key_Up ) );
-	inputHandler_.changeKeyCode( commands::select_down, Key( Qt::ShiftModifier, Qt::Key_Down ) );
-	inputHandler_.changeKeyCode( commands::select_left, Key( Qt::ShiftModifier, Qt::Key_Left ) );
-	inputHandler_.changeKeyCode( commands::select_right, Key( Qt::ShiftModifier, Qt::Key_Right ) );
-	inputHandler_.changeKeyCode( commands::select_track, Key( Qt::ControlModifier, Qt::Key_R ) );
-	inputHandler_.changeKeyCode( commands::select_all, Key( Qt::ControlModifier, Qt::Key_A ) );
-	inputHandler_.changeKeyCode( commands::block_start, Key( Qt::ControlModifier, Qt::Key_B ) );
-	inputHandler_.changeKeyCode( commands::block_end, Key( Qt::ControlModifier, Qt::Key_E ) );
-	inputHandler_.changeKeyCode( commands::block_unmark, Key( Qt::ControlModifier, Qt::Key_U ) );
-
-	inputHandler_.changeKeyCode( commands::block_copy, Key( Qt::ControlModifier, Qt::Key_C ) );
-	inputHandler_.changeKeyCode( commands::block_cut, Key( Qt::ControlModifier, Qt::Key_X ) );
-	inputHandler_.changeKeyCode( commands::block_paste, Key( Qt::ControlModifier, Qt::Key_V ) );
-	inputHandler_.changeKeyCode( commands::block_delete, Key( Qt::ControlModifier | Qt::ShiftModifier, Qt::Key_X ) );
-
-	inputHandler_.changeKeyCode( commands::row_clear, Key(Qt::NoModifier, Qt::Key_Delete) );
 }
