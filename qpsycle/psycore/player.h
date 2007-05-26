f3ef49352cb3ce293fe924da695bd85499a432c3
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-05-26 22:33:25 +0000

    documentation from psycle 1.9. grouped the many declarations by feature instead of access

--- qpsycle/psycore/player.h
+++ qpsycle/psycore/player.h
@@ -1,167 +1,221 @@
 ///\file
 ///\brief interface file for psy::core::Player.
-/// schedule the processing of machines, sends signal buffers and sequence events to them, ...
 
 /***************************************************************************
-	*   Copyright (C) 2007 Psycledelics  , Josep Maria Antolín Segura, D.W. Aley			
-	*   psycle.sf.net   *
-	*                                                                         *
-	*   This program is free software; you can redistribute it and/or modify  *
-	*   it under the terms of the GNU General Public License as published by  *
-	*   the Free Software Foundation; either version 2 of the License, or     *
-	*   (at your option) any later version.                                   *
-	*                                                                         *
-	*   This program is distributed in the hope that it will be useful,       *
-	*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-	*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-	*   GNU General Public License for more details.                          *
-	*                                                                         *
-	*   You should have received a copy of the GNU General Public License     *
-	*   along with this program; if not, write to the                         *
-	*   Free Software Foundation, Inc.,                                       *
-	*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-	***************************************************************************/
+*   Copyright (C) 2007 Psycledelics  , Josep Maria Antolín Segura, D.W. Aley			
+*   psycle.sf.net   *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
 
 #pragma once
 #include "song.h"
 #include "dither.h"
 #include "machine.h"
 #include "riff.h"
-#include "../audiodrivers/audiodriver.h" // FIXME: doesn't belong in psycore
+#include "../audiodrivers/audiodriver.h" ///\todo doesn't belong in psycore
 
 namespace psy
 {
 	namespace core
 	{
-
+		/// schedules the processing of machines, sends signal buffers and sequence events to them, ...
 		class Player : public MachineCallbacks
 		{
 			public:
 				Player();
 				~Player();
 
-			public:
 				static Player* Instance() {
-					// note keep sure a player instance is created from the gui
+					// note: keep sure a player instance is created from the gui
 					// before starting audiothread
 					// or use single threaded only
 					static Player s;
 					return &s; 
 				}
 
-      // FIXME: player should not need to know about the audio driver.
-      // Player should export a callback to generate audio instead.
-      void setDriver(  const AudioDriver & driver );
-			AudioDriver & driver();
+			public:
+				/// used by the plugins to indicate that they need redraw.
+				///\todo private access
+				bool Tweaker;
 
-			Song inline & song() {
-				return *song_;
-			}
+			///\name audio driver
+			///\todo player should not need to know about the audio driver, it should export a callback to generate audio instead.
+			///\{
+				public:
+					///\todo player should not need to know about the audio driver, it should export a callback to generate audio instead.
+					void setDriver(  const AudioDriver & driver );
+					///\todo player should not need to know about the audio driver, it should export a callback to generate audio instead.
+					AudioDriver & driver() { return *driver_; }
+					///\todo player should not need to know about the audio driver, it should export a callback to generate audio instead.
+					AudioDriver const & driver() const { return *driver_; }
+				private:
+					///\todo player should not need to know about the audio driver, it should export a callback to generate audio instead.
+					AudioDriver* driver_;
+			///\}
 
-			void inline song(Song * song) {
-				song_ = song;
-			}
+			public:
+				void SampleRate(const int sampleRate);
 
-			// starts to play.
-			void start( double pos = 0.0 );
+			///\name recording
+			///\todo missplaced?
+			///\{
+				public:
+					void setAutoRecording( bool on );
+				private:
+					bool autoRecord_;
+			///\}
 
-			// stops playing.
-			void stop();
+			///\name callback
+			///\{
+				public:
+					/// entrance for the callback function (audiodriver)
+					static float * Work(void* context, int& nsamples);
+				private:
+					/// entrance for the callback function (audiodriver)
+					float * Work(int nsamples);
+			///\}
+				
+			///\name song
+			///\{
+				public:
+					Song const & song() const { return *song_; }
+					Song & song() { return *song_; }
+					void song(Song * song) { song_ = song; }
+				private:
+					Song * song_;
+			///\}
 
-			// is the player in playmode.
-			bool playing() const;
+			///\name secondary output device, write to a file
+			///\{
+				public:
+					/// starts the recording output device.
+					void startRecording( );
+					/// stops the recording output device.
+					void stopRecording( );
+					/// wether the recording device has been started.
+					bool recording() const;
+					/// for wave render set the filename
+					void setFileName( const std::string & fileName); // FIXME: maybe this shouldn't be in player either.
+					/// gets the wave to render filename
+					const std::string fileName() const; // FIXME: maybe this shouldn't be in player either.
+				private:
+					/// wether the recording device has been started.
+					bool recording_;
+					/// whether to apply dither to recording
+					bool _doDither;
+					/// wave render filename
+					std::string fileName_;
+					/// file to which to output signal.
+					WaveFile _outputWaveFile;
+					void writeSamplesToFile( int amount );
+			///\}
 
-			// set the actualPlaypos
-			void setPlayPos( double pos );
-
-			// the current playPos
-			double playPos() const;
-
-			// for wave render set the filename
-			void setFileName( const std::string & fileName); // FIXME: maybe this shouldn't be in player either.
+			///\name bpm
+			///\{
+				public:
+					void setBpm( double bpm ) { timeInfo_.setBpm( bpm ); }
+					double bpm() const { return timeInfo_.bpm(); }
+			///\}
 			
-			// gets the wave to render filename
-			const std::string fileName() const; // FIXME: maybe this shouldn't be in player either.
+			///\name time info
+			///\{
+				public:
+					const PlayerTimeInfo & timeInfo() const { return timeInfo_; }
+				private:
+					PlayerTimeInfo timeInfo_;
+			///\}
 
-			void setAutoRecording( bool on );
-			void startRecording( );
-			void stopRecording( );
-			bool recording() const;
-
-            void setLoopSong( bool setit );
-			bool loopSong() const;
-
-            void setLoopSequenceEntry( SequenceEntry *seqEntry );
-			SequenceEntry *loopSequenceEntry() const;
-
-			const PlayerTimeInfo & timeInfo() const;
-
-			void setBpm( double bpm );
-			double bpm() const;
-
-			void SampleRate(const int sampleRate);
-
-      bool autoStopMachines() const;
-    private:
-			bool autoStopMachines_;
-    public:
-
-			// work function. (Entrance for the callback function (audiodriver)
-			static float * Work(void* context, int& nsamples);
+			///\name start/stop
+			///\{
+				public:
+					/// starts to play.
+					void start( double pos = 0.0 );
+					/// stops playing.
+					void stop();
+					/// is the player in playmode.
+					bool playing() const;
+				private:
+					bool _playing;
+			///\}
 			
-			// used by the plugins to indicate that they need redraw.
-			bool Tweaker;
-
-			void lock();
-			void unlock();
-
+			///\name play position
+			///\{
+				public:
+					/// sets the current play position
+					void setPlayPos( double pos );
+					/// the current play position
+					double playPos() const;
+			///\}
 			
+			///\name loop
+			///\{
+				public:			
+					void setLoopSong( bool setit );
+					bool loopSong() const;
 
-		private:
+					void setLoopSequenceEntry( SequenceEntry *seqEntry );
+					SequenceEntry *loopSequenceEntry() const;
+				private:
+					bool loopSong_;
+					SequenceEntry *loopSequenceEntry_;
+			///\}
 
-			PlayerTimeInfo timeInfo_;
+			///\name auto stop
+			///\{
+				public:
+					bool autoStopMachines() const;
+				private:
+					bool autoStopMachines_;
+			///\}
+			
+			///\name multithreading locking
+			///\{
+				public:
+					void lock();
+					void unlock();
+				private:
+					///\todo here we need some real mutexes
+					bool lock_;
+					///\todo here we need some real mutexes
+					bool inWork_;
+			///\}
 
-			Song * song_;
-			std::string fileName_;
-			AudioDriver* driver_;
+			private:
+				/// Final Loop. Read new line for notes to send to the Machines
+				void ExecuteNotes( double beatOffset , PatternLine & line );
+				void ProcessGlobalEvent(const GlobalEvent & event);
+				void Process(int nsamples);
 
-			bool _playing;
-			bool autoRecord_;
-			bool recording_;
-			bool loopSong_;
-            SequenceEntry *loopSequenceEntry_;
+				/// Stores which machine played last in each track. this allows you to not specify the machine number everytime in the pattern.
+				Machine::id_type prevMachines[MAX_TRACKS];
+				/// Stores the samplerate of playback when recording to wave offline (non-realtime), since it can be changed.
+				int backup_rate;
+				/// Stores the bitdepth of playback when recording to wave offline (non-realtime), since it can be changed.
+				int backup_bits;
+				/// Stores the channel mode (mono/stereo) of playback when recording to wave offline (non-realtime), since it can be changed.
+				int backup_channelmode;
+				/// Temporary buffer to get all the audio from Master (which work in small chunks), and send it to the soundcard after converting it to float.
+				float _pBuffer[MAX_DELAY_BUFFER];
 
-			// Final Loop. Read new line for notes to send to the Machines
-			void ExecuteNotes( double beatOffset , PatternLine & line );
-			void ProcessGlobalEvent(const GlobalEvent & event);
-			void Process(int nsamples);
+				/// dither handler
+				dsp::Dither dither;
 
-			float * Work(int nsamples);
-
-			// Stores which machine played last in each track. this allows you to not specify the machine number everytime in the pattern.
-			Machine::id_type prevMachines[MAX_TRACKS];
-			// Stores the samplerate of playback when recording to wave offline (non-realtime), since it can be changed.
-			int backup_rate;
-			// Stores the bitdepth of playback when recording to wave offline (non-realtime), since it can be changed.
-			int backup_bits;
-			// Stores the channel mode (mono/stereo) of playback when recording to wave offline (non-realtime), since it can be changed.
-			int backup_channelmode;
-			// Temporary buffer to get all the audio from Master (which work in small chunks), and send it to the soundcard after converting it to float.
-			float _pBuffer[MAX_DELAY_BUFFER];
-			// file to which to output signal.
-			WaveFile _outputWaveFile;
-			// dither handler
-			dsp::Dither dither;
-
-			void writeSamplesToFile( int amount );
-			bool _doDither;
-
-			// here we need some real mutexes			
-			bool lock_;
-			bool inWork_;
-			// Contains the number of samples until a line change comes in.
-			int _samplesRemaining;
-		
+				/// Contains the number of samples until a line change comes in.
+				int _samplesRemaining;
 		};
 	}
 }
