c2d264b1bcba5e0d5f66e513918dc399d89ba35d
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-05-27 12:50:40 +0000

    Added new class WaveFormView which will be the new real WaveForm Visualizer

---
+++ qpsycle/waveview/waveformview.cpp
@@ -0,0 +1,35 @@
+/***************************************************************************
+*   Copyright (C) 2007 Psycledelics Community   *
+*   psycle.sourceforge.net   *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+#include "waveformview.h"
+
+WaveFormView::WaveFormView(bool *mini, QWidget *parent); //inizialization file for WaveFormView.
+{
+	if (mini = true)
+	{
+		//if it is a miniwaveview set fixed sizes and dimension
+		this.setSizePolicies( QSizePolicy::Fixed , QSizePolicy::Preferred);
+	}
+	else
+	{
+		//if it is not mini set size policies only
+		this.setSizePolicies( QSizePolicy::Preferred, QSizePolicy::Preferred);
+	}
+}
+
