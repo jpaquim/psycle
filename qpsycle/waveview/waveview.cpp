daef882793d8f6b936b2580afcae274fc72b5530
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-05-29 19:15:08 +0000

    Get default sample directory from the config file.

--- qpsycle/waveview/waveview.cpp
+++ qpsycle/waveview/waveview.cpp
@@ -17,113 +17,117 @@
 *   Free Software Foundation, Inc.,                                       *
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
-class QVBoxLayout;
+
+#include "../global.h"
+#include "../configuration.h"
 #include "waveview.h"
 
 #include <QtGui>
 #include <iostream>
 #include <QtGui/QGraphicsView> //temp... just for testing...
 #include <QGraphicsScene>
- #include <QGraphicsRectItem>
+#include <QGraphicsRectItem>
 
- WaveView::WaveView( psy::core::Song *song, QWidget *parent) 
-    : QWidget(parent)
- {
 
-     song_ = song;
-     layout_ = new QVBoxLayout();
-     setLayout(layout_);
-	 
-	 //TODO: this should be in the waveview.h and waveformview should be an object of WaveFormView
-	 QGraphicsView *waveformview = new QGraphicsView(this);
-	 QGraphicsScene *scene = new QGraphicsScene(this);
-	 QGraphicsRectItem *separator = new QGraphicsRectItem(0, scene);
-	 waveformview->setScene(scene);
-	 waveformview->setBackgroundBrush( Qt::black );
-	 waveformview->adjustSize();
+WaveView::WaveView( psy::core::Song *song, QWidget *parent) 
+	: QWidget(parent)
+{
 
-	 toolBar_ = new QToolBar();
-	 toolBar_->setSizePolicy( QSizePolicy::Preferred, QSizePolicy::Fixed );
+	song_ = song;
+	layout_ = new QVBoxLayout();
+	setLayout(layout_);
 	 
-	 //sample loading
-	 QLabel *playLabel = new QLabel("Play: ");
-	 
-	 loadSmp_ = new QAction( QIcon(":images/wave-open.png"), "Load Sample", this );
-	 connect( loadSmp_, SIGNAL( triggered() ), this, SLOT( onLoadButtonClicked() ) );
-	 
-	 saveSmp_ = new QAction( QIcon(":images/wave-save.png"), "Save Sample", this);
-	 connect( saveSmp_, SIGNAL( triggered() ), this, SLOT( onSaveButtonClicked() ) );
-	 
-	 playsSmp_ = new QAction(QIcon(":/images/wave_playstart.png"), tr("Play From Start"), this);
-	 playSmp_ = new QAction(QIcon(":/images/wave_play.png"), tr("Play"), this);
-	 stopSmp_ = new QAction(QIcon(":/images/wave_stop.png"), tr("Stop"), this);
-	 
-	 //editing
-	 QLabel *editLabel = new QLabel("Edit: ", this);
-	 cutAct_ = new QAction( QIcon(":images/wave-cut.png"), "Cut", this);
-	 copyAct_ = new QAction( QIcon(":images/wave-copy.png"), "Copy", this);
-	 pasteAct_ = new QAction( QIcon(":images/wave-paste.png"), "Paste", this);
-	 selAct_ = new QAction("Select All", this);
-	 
-	 //effects
-	 QLabel *efxLabel = new QLabel("Effects: ");
-	 ampEfx_ = new QAction("Amplify", this);
-	 //scrollbar for the WaveFormView
-	 
-	 waveScroll_ = new QScrollBar(Qt::Horizontal, this);
-	 
-	 
-	 //zoom and various
-	 zoomBar_ = new QToolBar();
-	 zoomBar_->setSizePolicy( QSizePolicy::Preferred, QSizePolicy::Fixed );
-	 QLabel *zoomLabel = new QLabel("Zoom: ");
-	 zoomMore_ = new QAction(QIcon(":/images/plus.png"), tr("Zoom More"), this);
-	 connect( zoomMore_, SIGNAL( triggered() ), this, SLOT( onPlusButtonClicked() ));
-	 
-	 zoomLess_ = new QAction(QIcon(":/images/minus.png"), tr("Zoom Less"), this);
-	 connect( zoomLess_, SIGNAL( triggered() ), this, SLOT( onMinusButtonClicked() ));
+	//TODO: this should be in the waveview.h and waveformview should be an object of WaveFormView
+	QGraphicsView *waveformview = new QGraphicsView(this);
+	QGraphicsScene *scene = new QGraphicsScene(this);
+	QGraphicsRectItem *separator = new QGraphicsRectItem(0, scene);
+	waveformview->setScene(scene);
+	waveformview->setBackgroundBrush( Qt::black );
+	waveformview->adjustSize();
 
-	 zoomSlide_ = new QSlider(Qt::Horizontal, this);
-	 zoomSlide_->setMinimum(0);
-	 zoomSlide_->setMaximum(100);
-	 zoomSlide_->setPageStep(10);
+	toolBar_ = new QToolBar();
+	toolBar_->setSizePolicy( QSizePolicy::Preferred, QSizePolicy::Fixed );
 	 
-	 zoomBar_->addWidget( zoomLabel );
-	 zoomBar_->addAction( zoomLess_ );
-	 zoomBar_->addWidget( zoomSlide_ );
-	 zoomBar_->addAction( zoomMore_ );
+	//sample loading
+	QLabel *playLabel = new QLabel("Play: ");
+	 
+	loadSmp_ = new QAction( QIcon(":images/wave-open.png"), "Load Sample", this );
+	connect( loadSmp_, SIGNAL( triggered() ), this, SLOT( onLoadButtonClicked() ) );
+	 
+	saveSmp_ = new QAction( QIcon(":images/wave-save.png"), "Save Sample", this);
+	connect( saveSmp_, SIGNAL( triggered() ), this, SLOT( onSaveButtonClicked() ) );
+	 
+	playsSmp_ = new QAction(QIcon(":/images/wave_playstart.png"), tr("Play From Start"), this);
+	playSmp_ = new QAction(QIcon(":/images/wave_play.png"), tr("Play"), this);
+	stopSmp_ = new QAction(QIcon(":/images/wave_stop.png"), tr("Stop"), this);
+	 
+	//editing
+	QLabel *editLabel = new QLabel("Edit: ", this);
+	cutAct_ = new QAction( QIcon(":images/wave-cut.png"), "Cut", this);
+	copyAct_ = new QAction( QIcon(":images/wave-copy.png"), "Copy", this);
+	pasteAct_ = new QAction( QIcon(":images/wave-paste.png"), "Paste", this);
+	selAct_ = new QAction("Select All", this);
+	 
+	//effects
+	QLabel *efxLabel = new QLabel("Effects: ");
+	ampEfx_ = new QAction("Amplify", this);
+	//scrollbar for the WaveFormView
+	 
+	waveScroll_ = new QScrollBar(Qt::Horizontal, this);
+	 
+	 
+	//zoom and various
+	zoomBar_ = new QToolBar();
+	zoomBar_->setSizePolicy( QSizePolicy::Preferred, QSizePolicy::Fixed );
+	QLabel *zoomLabel = new QLabel("Zoom: ");
+	zoomMore_ = new QAction(QIcon(":/images/plus.png"), tr("Zoom More"), this);
+	connect( zoomMore_, SIGNAL( triggered() ), this, SLOT( onPlusButtonClicked() ));
+	 
+	zoomLess_ = new QAction(QIcon(":/images/minus.png"), tr("Zoom Less"), this);
+	connect( zoomLess_, SIGNAL( triggered() ), this, SLOT( onMinusButtonClicked() ));
+
+	zoomSlide_ = new QSlider(Qt::Horizontal, this);
+	zoomSlide_->setMinimum(0);
+	zoomSlide_->setMaximum(100);
+	zoomSlide_->setPageStep(10);
+	 
+	zoomBar_->addWidget( zoomLabel );
+	zoomBar_->addAction( zoomLess_ );
+	zoomBar_->addWidget( zoomSlide_ );
+	zoomBar_->addAction( zoomMore_ );
 	 
 			 
-	 toolBar_->addAction( loadSmp_ );
-	 toolBar_->addAction( saveSmp_ );
-	 toolBar_->addSeparator();
-	 toolBar_->addWidget( playLabel );
-	 toolBar_->addAction( playsSmp_ );
-	 toolBar_->addAction( playSmp_ );
-	 toolBar_->addAction( stopSmp_ );
-	 toolBar_->addSeparator();
-	 toolBar_->addWidget( editLabel );
-	 toolBar_->addAction( cutAct_ );
-	 toolBar_->addAction( copyAct_ );
-	 toolBar_->addAction( pasteAct_ );
-	 toolBar_->addAction( selAct_);
-	 toolBar_->addSeparator();
-	 toolBar_->addWidget( efxLabel );
-	 toolBar_->addAction( ampEfx_ );
+	toolBar_->addAction( loadSmp_ );
+	toolBar_->addAction( saveSmp_ );
+	toolBar_->addSeparator();
+	toolBar_->addWidget( playLabel );
+	toolBar_->addAction( playsSmp_ );
+	toolBar_->addAction( playSmp_ );
+	toolBar_->addAction( stopSmp_ );
+	toolBar_->addSeparator();
+	toolBar_->addWidget( editLabel );
+	toolBar_->addAction( cutAct_ );
+	toolBar_->addAction( copyAct_ );
+	toolBar_->addAction( pasteAct_ );
+	toolBar_->addAction( selAct_);
+	toolBar_->addSeparator();
+	toolBar_->addWidget( efxLabel );
+	toolBar_->addAction( ampEfx_ );
 	 
 
-     layout_->addWidget( toolBar_ );
-	 layout_->addWidget( waveformview );
-	 layout_->addWidget( waveScroll_);
-	 layout_->addWidget( zoomBar_ );
-	 waveformview->show();
- }
+	layout_->addWidget( toolBar_ );
+	layout_->addWidget( waveformview );
+	layout_->addWidget( waveScroll_);
+	layout_->addWidget( zoomBar_ );
+	waveformview->show();
+}
 
 void WaveView::onLoadButtonClicked()
 {
 	// FIXME: unhardcode the default sample directory.
+	QString samplePath = QString::fromStdString( Global::configuration().samplePath() );
 	QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
-							"/home/neil/mymusic/samples/",
+							samplePath,
 							tr("Wave files (*.wav)"));
 	int si = song()->instSelected;
 	std::cout << "inst sel: " << si << std::endl;
@@ -132,19 +136,17 @@ void WaveView::onLoadButtonClicked()
 	{
 		//if (MessageBox("Overwrite current sample on the slot?","A sample is already loaded here",MB_YESNO) == IDNO)  return;
 	}
-
 	if ( song()->WavAlloc( si, fileName.toStdString().c_str() ) )
 	{
-		std::cout << "1" << fileName.toStdString() << std::endl;
 		emit sampleAdded();
 	}
-	std::cout << "2" << fileName.toStdString().c_str() << std::endl;
 }
 
 void WaveView::onSaveButtonClicked()
 {
+	QString samplePath = QString::fromStdString( Global::configuration().samplePath() );
 	QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"),
-							"/home/neil/mymusic/samples/",
+							samplePath,
 							tr("Wave files (*.wav)"));
 }
 
@@ -166,5 +168,5 @@ void WaveView::onMinusButtonClicked()
 
 psy::core::Song* WaveView::song()
 {
-    return song_;
+	return song_;
 }
