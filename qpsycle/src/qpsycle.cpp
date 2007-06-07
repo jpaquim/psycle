62f5aff6b8fa2ee5d6ef7113ee7161bc3cfe9dc9
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-06-07 16:37:37 +0000

    added default psycle icon ;)

--- qpsycle/src/qpsycle.cpp
+++ qpsycle/src/qpsycle.cpp
@@ -6,7 +6,7 @@
  {
      QApplication app(argc, argv);
      Q_INIT_RESOURCE(qpsycle);
-
+	 app.setWindowIcon ( QIcon(":images/psycle.png") );
      MainWindow mainWin;
      mainWin.show();
      return app.exec();
