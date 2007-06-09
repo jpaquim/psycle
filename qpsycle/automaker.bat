0d9778d4aba3a73cbea71a6a9f56e73aeced72c2
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-06-09 11:48:31 +0000

    Added Gravity_0's Automaker Script

---
+++ qpsycle/automaker.bat
@@ -0,0 +1,15 @@
+@REM Created By Graity_0... Released Under GPL Licence
+@echo off
+title Automaker QT 1.0
+COLOR 0%3
+echo Welcome to Gravity_0 Automaker for QT
+echo Qt is a registered trademaerk of Trolltech
+pause
+echo Running QMake
+qmake
+echo QMake Complete
+make
+echo Make Complete
+echo
+echo Created By Gravity_0
+pause
