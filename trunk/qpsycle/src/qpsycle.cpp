37ef3cc1285e0d9ecd134fecd483df0c5479d3e8
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-06-10 19:00:39 +0000

    added plastique style as default as base for the windows color themes

--- trunk/qpsycle/src/qpsycle.cpp
+++ trunk/qpsycle/src/qpsycle.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
   Q_INIT_RESOURCE(qpsycle);
   app.setWindowIcon ( QIcon(":images/psycle.png") );
+  app.setStyle("plastique");
   MainWindow mainWin;
   mainWin.show();
   return app.exec();
