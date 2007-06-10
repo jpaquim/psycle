2344a080055b4aae4deff0eba9fc1e531f7d7b6a
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-06-10 19:12:41 +0000

    reverted to normal

--- qpsycle/src/qpsycle.cpp
+++ qpsycle/src/qpsycle.cpp
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
   Q_INIT_RESOURCE(qpsycle);
   app.setWindowIcon ( QIcon(":images/psycle.png") );
-  app.setStyle("plastique");
   MainWindow mainWin;
   mainWin.show();
   return app.exec();
