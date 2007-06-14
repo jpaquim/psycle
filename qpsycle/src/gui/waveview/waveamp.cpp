0145c6c3643bbbbeef37aea81210923677a46ac0
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-06-14 18:15:18 +0000

    added missing files

---
+++ qpsycle/src/gui/waveview/waveamp.cpp
@@ -0,0 +1,40 @@
+/***************************************************************************
+*   Copyright (C) 2007 Psycledelics Community   *
+*   psycle.sourceforge.net   *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include "waveamp.h"
+
+#include <QHBoxLayout>
+
+WaveAmp::WaveAmp()
+{
+	setWindowTitle("Amplify");
+	
+	QHBoxLayout *layout = new QHBoxLayout();
+	gain = new QLabel("Gain", this);
+	gainSli = new QSlider(-300, 300, 20, 0, Qt::Horizontal, 0);
+	ok = new QPushButton("Ok", this);
+	cancel = new QPushButton("Cancel", this);
+	
+	layout->addWidget(gain);
+	layout->addWidget(gainSli);
+	layout->addWidget(ok);
+	layout->addWidget(cancel);
+	
+}
