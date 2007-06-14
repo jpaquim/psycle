1fcf171a57736ec77d0ff0ed8891da6483983a4b
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-06-14 10:39:56 +0000

    updated automaker for Linux

--- qpsycle/automaker.sh
+++ qpsycle/automaker.sh
@@ -1,15 +1,13 @@
+#!/bin/bash
 # Created By Graity_0... Released Under GPL Licence
 #Warning Not Complete!!!!!
 echo off
 echo "Welcome to Gravity_0 Automaker for QT - Linux Verion Bet"
 echo "Qt is a registered trademaerk of Trolltech"
+echo " "
 echo "Running QMake"
 qmake-qt4
-echo "Running QMake"
-qmake
 echo "QMake Complete"
 make
 echo "Make Complete"
-echo
 echo "Created By Gravity_0"
-pause
