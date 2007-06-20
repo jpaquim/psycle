8948cb4be5b6d5dd79ab403df40ee8788eeb4505
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-06-20 14:09:18 +0000

    Version 1.15 of automaker.bat

--- trunk/qpsycle/automaker.bat
+++ trunk/qpsycle/automaker.bat
@@ -1,16 +1,33 @@
 @REM Created By Graity_0... Released Under GPL Licence
 @echo off
-title Automaker QT 1.0 - Windows
+title Automaker QT 1.15 - Windows
 COLOR 0%3
 echo Welcome to Gravity_0 Automaker for QT
 echo Qt is a registered trademaerk of Trolltech
 pause
+if "%1" == "help" goto HELP
 echo Running QMake
 qmake
 echo QMake Complete
+if "%1" == "release" goto RELEASE
 if "%1" == "debug" goto DEBUG
-make release
-echo Make Complete
+
+goto HELP
 
 :DEBUG
 make debug
+echo Make Complete
+pause
+exit
+
+:RELEASE
+make release
+echo Make Complete
+pause
+exit
+
+:HELP
+echo [automaker.bat] release : compiles release version.
+echo [automaker.bat] debug   : compiles debug version.
+echo [automaker.bat] help 	 : shows this prompt.
+pause
