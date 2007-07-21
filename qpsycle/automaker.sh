36c530e9aa7ec6576fac3d43b494f3746d27441e
erodix <erodix@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-07-21 19:10:45 +0000

    Rewrote automaker.sh.

--- qpsycle/automaker.sh
+++ qpsycle/automaker.sh
@@ -1,13 +1,72 @@
 #!/bin/bash
-# Created By Graity_0... Released Under GPL Licence
-#Warning Not Complete!!!!!
-echo off
-echo "Welcome to Gravity_0 Automaker for QT - Linux Verion Beta"
-echo "Qt is a registered trademaerk of Trolltech"
-echo " "
-echo "Running QMake"
-qmake-qt4
-echo "QMake Complete"
-make
-echo "Make Complete"
-echo "Created By Gravity_0"
+#
+# automaker.sh
+#
+# Originally Created By Gravity_0
+#
+# Modifications by erodix
+#
+# Released Under GPL Licence
+#
+#
+
+QMAKE='qmake-qt4'
+
+echo "---"
+echo "Welcome to Automaker"
+echo "Qt is a registered trademark of Trolltech."
+echo "---"
+echo ""
+
+QMAKE_BINARY=$( which $QMAKE )
+
+if [ "$QMAKE_BINARY" == '' ]; then
+  echo "FATAL ERROR:  $QMAKE executable not found."
+  echo "Try installing Qt4 development packages."
+  echo "Bailing out."
+  exit 1
+else
+  echo "Using $QMAKE at $QMAKE_BINARY."
+fi
+
+echo "---"
+  $QMAKE_BINARY
+QMAKE_RETVAL="$?"
+echo "---"
+
+if [ $QMAKE_RETVAL == '0' ]; then
+  echo "QMake complete."
+else
+  echo "QMake failed."
+  echo "Bailing out."
+  exit $QMAKE_RETVAL
+fi
+
+MAKE_BINARY=$( which make )
+
+if [ "$MAKE_BINARY" == '' ]; then
+  echo "You seem to be _not_ having make installed. Please install some development tools."
+  echo "Bailing out."
+  exit 0
+else
+  echo "Executing make at $MAKE_BINARY."
+fi
+
+echo "Running make."
+
+echo "---"
+  $MAKE_BINARY
+MAKE_RETVAL="$?"
+echo "---"
+
+
+if [ $MAKE_RETVAL == '0' ]; then
+  echo "Make complete."
+else
+  echo "Make failed."
+fi
+
+echo ""
+
+exit $MAKE_RETVAL
+
