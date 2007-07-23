7ca2c2000bca70afbaa169295252cc3ba8aa9c8c
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-07-23 16:54:54 +0000

    makes it crash

--- processor-exception-handling-tests/msvc.cmd
+++ processor-exception-handling-tests/msvc.cmd
@@ -1,2 +1,1 @@
-call cl main_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -arch:SSE2 
--link -LTCG winmm.lib -out:a.exe
+call cl main_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -arch:SSE2 -link -LTCG winmm.lib -out:a.exe
