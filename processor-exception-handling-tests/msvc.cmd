cdd3d3eaae2a12c227ef9a4690079b1add7b6fd4
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-07-23 16:52:50 +0000

    build command

--- processor-exception-handling-tests/msvc.cmd
+++ processor-exception-handling-tests/msvc.cmd
@@ -1,3 +1,2 @@
-call cl double_to_int_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast              -link -LTCG winmm.lib -out:x87.exe
-call cl double_to_int_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -QIfist      -link -LTCG winmm.lib -out:x87-QIfist.exe
-call cl double_to_int_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -arch:SSE2   -link -LTCG winmm.lib -out:sse2.exe
+call cl main_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -arch:SSE2 
+-link -LTCG winmm.lib -out:a.exe
