bc73df8474d48effc17b4938797003e8cdf17727
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-07-24 02:37:43 +0000

    got only invalids, never denormals

--- branches/bohan/processor-exception-handling-tests/msvc.cmd
+++ branches/bohan/processor-exception-handling-tests/msvc.cmd
@@ -1,1 +1,2 @@
-call cl main_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHsc -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -arch:SSE2 -link -LTCG winmm.lib -out:a.exe
+rem call cl main_winapi.cpp -nologo -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -EHa -MT -GL -GS- -O2 -Ob2 -Oi -Ot -Oy -Op- -fp:fast -arch:SSE2 -link -LTCG winmm.lib -out:a.exe
+call cl main_winapi.cpp -nologo -D_CRT_SECURE_NO_DEPRECATE -EHa -MT -O0 -link -LTCG winmm.lib -out:a.exe 
