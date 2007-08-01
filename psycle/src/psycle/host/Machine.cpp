062f52d5e190b023971184f20eacf16dff77a784
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-01 22:32:37 +0000

    Fixed PSY2 loader problems with wires due to the recent change of InsertInputWireIndex, and improved the loader itself. added code for GCC compiler to allocate aligned memory. (note: the ASM code in dsp might need to change for it) fixed VST's not getting notified of BPM change pattern command.

--- psycle/src/psycle/host/Machine.cpp
+++ psycle/src/psycle/host/Machine.cpp
@@ -154,6 +154,9 @@ namespace psycle
 			///\todo: for gcc: use int posix_memalign(void **memptr, size_t alignment, size_t size);
 			_pSamplesL = static_cast<float*>(_aligned_malloc(STREAM_SIZE*sizeof(float),16));
 			_pSamplesR = static_cast<float*>(_aligned_malloc(STREAM_SIZE*sizeof(float),16));
+		#elif defined DIVERSALIS__PROCESSOR__X86 &&  defined DIVERSALIS__COMPILER__GNU
+			posix_memalign(_pSamplesL,16,STREAM_SIZE*sizeof(float));
+			posix_memalign(_pSamplesR,16,STREAM_SIZE*sizeof(float));
 		#else
 			_pSamplesL = new float[STREAM_SIZE];
 			_pSamplesR = new float[STREAM_SIZE];
@@ -196,10 +199,14 @@ namespace psycle
 		#if defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__MICROSOFT
 			_aligned_free(_pSamplesL);
 			_aligned_free(_pSamplesR);
+		#elif defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__GNU
+			free(_pSamplesL);
+			free(_pSamplesR);
 		#else
 			delete [] _pSamplesL;
 			delete [] _pSamplesR;
 		#endif
+			_pSamplesL = _pSamplesR=0;
 		}
 
 		void Machine::Init()
