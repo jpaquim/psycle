179369e4c2f916be5e7ec38e6855e471551ed79c
gravity0 <gravity0@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-05 14:40:26 +0000

    first try to make a background image for pattern items

--- qpsycle/src/gui/sequencer/sequenceritem.cpp
+++ qpsycle/src/gui/sequencer/sequenceritem.cpp
@@ -55,7 +55,7 @@ void SequencerItem::paint( QPainter * painter, const QStyleOptionGraphicsItem * 
 		painter->setPen( Qt::white ); 
 	}
 	QColor col = QColorFromLongColor( sequenceEntry_->pattern()->category()->color() ); 
-	painter->setBrush( col ); 
+	painter->setBrush(QBrush(col , QPixmap(":/images/def-background.png", 0))); 
 	painter->drawRect( boundingRect() ); // FIXME: need to take border width into account.
 	painter->setPen( Qt::white ); 
 	painter->drawText( boundingRect(), Qt::AlignCenter, QString::fromStdString( sequenceEntry_->pattern()->name() ) );
