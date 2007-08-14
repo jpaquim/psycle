0261b12800c392a3da45dcf89caa5a785a9ce243
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-14 17:45:38 +0000

    fixed build on gcc 3.4

--- trunk/qpsycle/src/gui/global.h
+++ trunk/qpsycle/src/gui/global.h
@@ -17,8 +17,8 @@
 *   Free Software Foundation, Inc.,                                       *
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
-#ifndef PSYCLE__CORE__GLOBAL
-#define PSYCLE__CORE__GLOBAL
+#ifndef PSYCLE__GUI__GLOBAL
+#define PSYCLE__GUI__GLOBAL
 class Configuration;
 class Global
 {
