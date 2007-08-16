c3d25a2ab9c08fbabef828efac3d4af27ab188bd
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-16 20:26:31 +0000

    ?

--- psycle-helpers/src/psycle/helpers/math/clip.hpp
+++ psycle-helpers/src/psycle/helpers/math/clip.hpp
@@ -6,8 +6,8 @@ namespace psycle
 	{
 		namespace math
 		{
-			template<typename Real>
-			Real inline clipped(Real const & minimum, Real const & value, Real const & maximum) UNIVERSALIS__COMPILER__CONST
+			template<typename Real> UNIVERSALIS__COMPILER__CONST
+			Real inline clipped(Real const & minimum, Real const & value, Real const & maximum)
 			{
 				if(minimum > value) return minimum;
 				else if(value > maximum) return maximum;
