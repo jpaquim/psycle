b06e3981391e029fd899303803d0dfd8d3be8318
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-18 17:34:55 +0000

    some test case that shows that lrint depends on the current rounding mode

--- trunk/psycle-helpers/src/unit_tests.cpp
+++ trunk/psycle-helpers/src/unit_tests.cpp
@@ -5,12 +5,35 @@
 //BOOST_AUTO_TEST_SUITE_END
 
 #if 0	
-BOOST_AUTO_TEST_CASE(fail)
-{
-	BOOST_CHECK(1 == 1);
-	BOOST_CHECK(1 == 2);
-	BOOST_CHECK(1 == 3);
-}
+	BOOST_AUTO_TEST_CASE(fail)
+	{
+		BOOST_CHECK(1 == 1);
+		BOOST_CHECK(1 == 2);
+		BOOST_CHECK(1 == 3);
+	}
+#endif
+
+#if 0
+	#include <cmath>
+	#include <fenv.h>
+	BOOST_AUTO_TEST_CASE(lrint_test)
+	{
+		//int const previous_feround(fesetround(FE_DOWNWARD));
+		int const previous_feround(fesetround(FE_TONEAREST));
+		//int const previous_feround(fesetround(FE_TOWARDZERO));
+		//int const previous_feround(fesetround(FE_UPWARD));
+		try {
+			BOOST_CHECK(lrint(+1.6) == +2);
+			BOOST_CHECK(lrint(+1.4) == +1);
+			BOOST_CHECK(lrint(-1.6) == -2);
+			BOOST_CHECK(lrint(-1.4) == -1);
+			BOOST_CHECK(lrint(+2.6) == +3);
+			BOOST_CHECK(lrint(-2.6) == -3);
+		} catch(...) {
+			fesetround(previous_feround);
+			throw;
+		}
+	}
 #endif
 
 #include <psycle/helpers/math/round.hpp>
