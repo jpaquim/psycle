a03dd6a2d8b45c9b41e62609e77416f2bd88ae69
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-18 13:51:53 +0000

    last simple test before setting up the framework

--- trunk/psycle-helpers/src/unit_tests/rounded.cpp
+++ trunk/psycle-helpers/src/unit_tests/rounded.cpp
@@ -1,28 +1,45 @@
+int x_main(int, char*[]);
+#if 0
+	#include <boost/test/included/prg_exec_monitor.hpp> // doesn't need linking, int cpp_main(int, char*[])
+	//#include <boost/test/prg_exec_monitor.hpp> // needs linking , int cpp_main(int, char*[])
+	int cpp_main(int argc, char*argv[]) { return x_main(argc, argv); }
+	#include <stdexcept>
+	#define $(predicate) if(!(predicate)) throw std::runtime_error(#predicate);
+#elif 1
+	#include <boost/test/minimal.hpp> // doesn't need linking, int test_main(int, char*[])
+	int test_main(int argc, char*argv[]) { return x_main(argc, argv); }
+	#define $(predicate) BOOST_CHECK(predicate);
+#else
+	#define BOOST_TEST_MODULE psycle helpers
+	#include <boost/test/unit_test.hpp>
+	#define $(predicate) BOOST_CHECK(predicate);
+#endif
+
 #include <psycle/helpers/math/round.hpp>
 #include <psycle/helpers/math/truncate.hpp>
-#include <boost/test/included/prg_exec_monitor.hpp>
-#include <stdexcept>
-int cpp_main(int, char*[])
+
+int x_main(int, char*[])
 {
-	#define $(call, result) if(call != result) throw std::runtime_error(#call);
+	$(1 == 2);
+	$(1 == 1);
+	$(1 == 3);
 	using psycle::helpers::math::rounded;
-	$(rounded(+1.6), +2)
-	$(rounded(+1.4), +1)
-	$(rounded(-1.6), -2)
-	$(rounded(-1.4), -1)
-	$(rounded(+1.6f), +2)
-	$(rounded(+1.4f), +1)
-	$(rounded(-1.6f), -2)
-	$(rounded(-1.4f), -1)
+	$(rounded(+1.6) == +2);
+	$(rounded(+1.4) == +1);
+	$(rounded(-1.6) == -2);
+	$(rounded(-1.4) == -1);
+	$(rounded(+1.6f) == +2);
+	$(rounded(+1.4f) == +1);
+	$(rounded(-1.6f) == -2);
+	$(rounded(-1.4f) == -1);
 	using psycle::helpers::math::truncated;
-	$(truncated(+1.6), +1)
-	$(truncated(+1.4), +1)
-	$(truncated(-1.6), -2)
-	$(truncated(-1.4), -2)
-	$(truncated(+1.6f), +1)
-	$(truncated(+1.4f), +1)
-	$(truncated(-1.6f), -2)
-	$(truncated(-1.4f), -2)
-	#undef $
+	$(truncated(+1.6) == +1);
+	$(truncated(+1.4) == +1);
+	$(truncated(-1.6) == -2);
+	$(truncated(-1.4) == -2);
+	$(truncated(+1.6f) == +1);
+	$(truncated(+1.4f) == +1);
+	$(truncated(-1.6f) == -2);
+	$(truncated(-1.4f) == -2);
 	return 0;
 }
