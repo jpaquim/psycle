9d48cc4376f7845e919c421b1c9deeaca6e9e8ee
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-18 18:30:07 +0000

    reformating to allow the policy check to pass

--- psycle-helpers/src/psycle/helpers/mersennetwister.cpp
+++ psycle-helpers/src/psycle/helpers/mersennetwister.cpp
@@ -1,5 +1,4 @@
-///\file
-///\brief implementation for psycle::host::dsp::MersenneTwister
+///\implementation psycle::host::dsp::MersenneTwister
 
 /* 
 	A C-program for MT19937, with initialization improved 2002/1/26.
@@ -44,8 +43,7 @@
 	email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
 */
 
-//(c++-ified for psycle by dw aley)
-///todo: inlining and such
+///(c++-ified for psycle by dw aley)
 
 #include <packageneric/pre-compiled.private.hpp>
 #include <packageneric/module.private.hpp>
@@ -58,29 +56,30 @@ namespace psycle
 	{
 		namespace dsp
 		{
-			unsigned long				MersenneTwister::mt[N];								 /* the array for the state vector  */
-			int																MersenneTwister::mti=N+1;				/* mti==N+1 means mt[N] is not initialized */
+			/// the array for the state vector
+			unsigned long MersenneTwister::mt[N];
+			/// mti==N+1 means mt[N] is not initialized
+			int MersenneTwister::mti=N+1;
 
-			/* initializes mt[N] with a seed */
+			/// initializes mt[N] with a seed
 			void MersenneTwister::init_genrand(unsigned long s)
 			{
 				mt[0]= s & 0xffffffffUL;
 				for (mti=1; mti<N; mti++) {
 					mt[mti] = 
 					(1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
-					/* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
-					/* In the previous versions, MSBs of the seed affect   */
-					/* only MSBs of the array mt[].                        */
-					/* 2002/01/09 modified by Makoto Matsumoto             */
+					/// See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier.
+					/// In the previous versions, MSBs of the seed affect
+					/// only MSBs of the array mt[].
+					// 2002/01/09 modified by Makoto Matsumoto
 					mt[mti] &= 0xffffffffUL;
-					/* for >32 bit machines */
+					/// for >32 bit machines
 				}
 			}
 
-			/* initialize by an array with array-length */
-			/* init_key is the array for initializing keys */
-			/* key_length is its length */
-			/* slight change for C++, 2004/2/26 */
+			/// initialize by an array with array-length.
+			/// init_key is the array for initializing keys,
+			/// key_length is its length
 			void MersenneTwister::init_by_array(unsigned long init_key[], int key_length)
 			{
 				int i, j, k;
@@ -89,8 +88,8 @@ namespace psycle
 				k = (N>key_length ? N : key_length);
 				for (; k; k--) {
 					mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL))
-					+ init_key[j] + j; /* non linear */
-					mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
+					+ init_key[j] + j; // non linear
+					mt[i] &= 0xffffffffUL; // for WORDSIZE > 32 machines
 					i++; j++;
 					if (i>=N) { mt[0] = mt[N-1]; i=1; }
 					if (j>=key_length) j=0;
@@ -98,26 +97,26 @@ namespace psycle
 				for (k=N-1; k; k--) {
 					mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL))
 					- i; /* non linear */
-					mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
+					mt[i] &= 0xffffffffUL; // for WORDSIZE > 32 machines
 					i++;
 					if (i>=N) { mt[0] = mt[N-1]; i=1; }
 				}
 
-				mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
+				mt[0] = 0x80000000UL; // MSB is 1; assuring non-zero initial array
 			}
 
-			/* generates a random number on [0,0xffffffff]-interval */
-			unsigned long MersenneTwister::genrand_int32(void)
+			/// generates a random number on [0,0xffffffff]-interval
+			unsigned long MersenneTwister::genrand_int32()
 			{
 				unsigned long y;
 				static unsigned long mag01[2]={0x0UL, MATRIX_A};
-				/* mag01[x] = x * MATRIX_A  for x=0,1 */
+				// mag01[x] = x * MATRIX_A  for x=0,1
 
-				if (mti >= N) { /* generate N words at one time */
+				if (mti >= N) { // generate N words at one time
 					int kk;
 
-					if (mti == N+1)   /* if init_genrand() has not been called, */
-						init_genrand(5489UL); /* a default initial seed is used */
+					if (mti == N+1) // if init_genrand() has not been called,
+						init_genrand(5489UL); // a default initial seed is used
 
 					for (kk=0;kk<N-M;kk++) {
 						y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
@@ -135,7 +134,7 @@ namespace psycle
 				
 				y = mt[mti++];
 
-				/* Tempering */
+				// Tempering
 				y ^= (y >> 11);
 				y ^= (y << 7) & 0x9d2c5680UL;
 				y ^= (y << 15) & 0xefc60000UL;
@@ -144,42 +143,43 @@ namespace psycle
 				return y;
 			}
 
-			/* generates a random number on [0,0x7fffffff]-interval */
-			long MersenneTwister::genrand_int31(void)
+			/// generates a random number on [0,0x7fffffff]-interval
+			long MersenneTwister::genrand_int31()
 			{
 				return (long)(genrand_int32()>>1);
 			}
 
-			/* generates a random number on [0,1]-real-interval */
-			double MersenneTwister::genrand_real1(void)
+			/// generates a random number on [0,1]-real-interval
+			double MersenneTwister::genrand_real1()
 			{
 				return genrand_int32()*(1.0/4294967295.0); 
 				/* divided by 2^32-1 */ 
 			}
 
-			/* generates a random number on [0,1)-real-interval */
-			double MersenneTwister::genrand_real2(void)
+			/// generates a random number on [0,1)-real-interval
+			double MersenneTwister::genrand_real2()
 			{
 				return genrand_int32()*(1.0/4294967296.0); 
 				/* divided by 2^32 */
 			}
 
-			/* generates a random number on (0,1)-real-interval */
-			double MersenneTwister::genrand_real3(void)
+			/// generates a random number on (0,1)-real-interval
+			double MersenneTwister::genrand_real3()
 			{
 				return (((double)genrand_int32()) + 0.5)*(1.0/4294967296.0); 
 				/* divided by 2^32 */
 			}
 
-			/* generates a random number on [0,1) with 53-bit resolution*/
-			double MersenneTwister::genrand_res53(void) 
+			/// generates a random number on [0,1) with 53-bit resolution
+			double MersenneTwister::genrand_res53()
 			{ 
 				unsigned long a=genrand_int32()>>5, b=genrand_int32()>>6; 
 				return(a*67108864.0+b)*(1.0/9007199254740992.0); 
 			} 
-			/* These real versions are due to Isaku Wada, 2002/01/09 added */
+			
+			// These real versions are due to Isaku Wada, 2002/01/09 added
 
-			// gaussian-distribution variation, added by dw
+			/// gaussian-distribution variation, added by dw
 			void MersenneTwister::genrand_gaussian(double& out1, double& out2)
 			{
 				float x1;
