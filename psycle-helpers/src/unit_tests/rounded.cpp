48fb53881a1dcc6643f91d7f89b6652d30f56fc3
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-18 14:44:20 +0000

    now using the framework

--- psycle-helpers/src/unit_tests/rounded.cpp
+++ psycle-helpers/src/unit_tests/rounded.cpp
@@ -1,45 +1,42 @@
-int x_main(int, char*[]);
-#if 0
-	#include <boost/test/included/prg_exec_monitor.hpp> // doesn't need linking, int cpp_main(int, char*[])
-	//#include <boost/test/prg_exec_monitor.hpp> // needs linking , int cpp_main(int, char*[])
-	int cpp_main(int argc, char*argv[]) { return x_main(argc, argv); }
-	#include <stdexcept>
-	#define $(predicate) if(!(predicate)) throw std::runtime_error(#predicate);
-#elif 1
-	#include <boost/test/minimal.hpp> // doesn't need linking, int test_main(int, char*[])
-	int test_main(int argc, char*argv[]) { return x_main(argc, argv); }
-	#define $(predicate) BOOST_CHECK(predicate);
-#else
-	#define BOOST_TEST_MODULE psycle helpers
-	#include <boost/test/unit_test.hpp>
-	#define $(predicate) BOOST_CHECK(predicate);
+#define BOOST_AUTO_TEST_MAIN
+#include <boost/test/auto_unit_test.hpp>
+
+//BOOST_AUTO_TEST_SUITE(psycle)
+//BOOST_AUTO_TEST_SUITE_END
+
+#if 0	
+BOOST_AUTO_TEST_CASE(fail)
+{
+	BOOST_CHECK(1 == 1);
+	BOOST_CHECK(1 == 2);
+	BOOST_CHECK(1 == 3);
+}
 #endif
 
 #include <psycle/helpers/math/round.hpp>
-#include <psycle/helpers/math/truncate.hpp>
-
-int x_main(int, char*[])
+BOOST_AUTO_TEST_CASE(rounded)
 {
-	$(1 == 2);
-	$(1 == 1);
-	$(1 == 3);
 	using psycle::helpers::math::rounded;
-	$(rounded(+1.6) == +2);
-	$(rounded(+1.4) == +1);
-	$(rounded(-1.6) == -2);
-	$(rounded(-1.4) == -1);
-	$(rounded(+1.6f) == +2);
-	$(rounded(+1.4f) == +1);
-	$(rounded(-1.6f) == -2);
-	$(rounded(-1.4f) == -1);
+	BOOST_CHECK(rounded(+1.6) == +2);
+	BOOST_CHECK(rounded(+1.4) == +1);
+	BOOST_CHECK(rounded(-1.6) == -2);
+	BOOST_CHECK(rounded(-1.4) == -1);
+	BOOST_CHECK(rounded(+1.6f) == +2);
+	BOOST_CHECK(rounded(+1.4f) == +1);
+	BOOST_CHECK(rounded(-1.6f) == -2);
+	BOOST_CHECK(rounded(-1.4f) == -1);
+}
+
+#include <psycle/helpers/math/truncate.hpp>
+BOOST_AUTO_TEST_CASE(truncated)
+{
 	using psycle::helpers::math::truncated;
-	$(truncated(+1.6) == +1);
-	$(truncated(+1.4) == +1);
-	$(truncated(-1.6) == -2);
-	$(truncated(-1.4) == -2);
-	$(truncated(+1.6f) == +1);
-	$(truncated(+1.4f) == +1);
-	$(truncated(-1.6f) == -2);
-	$(truncated(-1.4f) == -2);
-	return 0;
+	BOOST_CHECK(truncated(+1.6) == +1);
+	BOOST_CHECK(truncated(+1.4) == +1);
+	BOOST_CHECK(truncated(-1.6) == -2);
+	BOOST_CHECK(truncated(-1.4) == -2);
+	BOOST_CHECK(truncated(+1.6f) == +1);
+	BOOST_CHECK(truncated(+1.4f) == +1);
+	BOOST_CHECK(truncated(-1.6f) == -2);
+	BOOST_CHECK(truncated(-1.4f) == -2);
 }
