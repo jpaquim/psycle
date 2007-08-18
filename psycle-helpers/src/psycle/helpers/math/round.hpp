2f856e8904605b2b85873ce81403238f12c9ee3b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-18 15:27:40 +0000

    added test cases directly after implementation

--- psycle-helpers/src/psycle/helpers/math/round.hpp
+++ psycle-helpers/src/psycle/helpers/math/round.hpp
@@ -1,19 +1,28 @@
 #pragma once
 #include <universalis/compiler.hpp>
 #include <cmath>
-namespace psycle
-{
-	namespace helpers
+namespace psycle { namespace helpers { namespace math {
+
+	/// converts a floating point number to an integer by rounding to the nearest integer.
+	/// note: it is unspecified whether rounding x.5 rounds up or down.
+	template<typename Real> UNIVERSALIS__COMPILER__CONST
+	int inline rounded(Real const & x)
 	{
-		namespace math
-		{
-			/// converts a floating point number to an integer by rounding to the nearest integer.
-			/// note: it is unspecified whether rounding x.5 rounds up or down.
-			template<typename Real> UNIVERSALIS__COMPILER__CONST
-			int inline rounded(Real const & x)
-			{
-				return x > 0 ? std::floor(x + Real(0.5)) : std::ceil(x - Real(0.5));
-			}
-		}
+		return x > 0 ? std::floor(x + Real(0.5)) : std::ceil(x - Real(0.5));
 	}
-}
+}}}
+
+#if defined BOOST_AUTO_TEST_CASE
+	BOOST_AUTO_TEST_CASE(rounded)
+	{
+		using psycle::helpers::math::rounded;
+		BOOST_CHECK(rounded(+1.6) == +2);
+		BOOST_CHECK(rounded(+1.4) == +1);
+		BOOST_CHECK(rounded(-1.6) == -2);
+		BOOST_CHECK(rounded(-1.4) == -1);
+		BOOST_CHECK(rounded(+1.6f) == +2);
+		BOOST_CHECK(rounded(+1.4f) == +1);
+		BOOST_CHECK(rounded(-1.6f) == -2);
+		BOOST_CHECK(rounded(-1.4f) == -1);
+	}
+#endif
--- psycle-helpers/src/psycle/helpers/math/truncate.hpp
+++ psycle-helpers/src/psycle/helpers/math/truncate.hpp
@@ -3,52 +3,61 @@
 #include <universalis/compiler.hpp>
 #include <boost/static_assert.hpp>
 #include <cstdint>
-namespace psycle
-{
-	namespace helpers
-	{
-		namespace math
-		{
-			/// converts a floating point number to an integer by truncating toward -infinity
-			std::int32_t inline UNIVERSALIS__COMPILER__CONST
-			truncated(double d)
-			{
-				BOOST_STATIC_ASSERT((sizeof d == 8));
-				union result_union
-				{
-					double d;
-					std::int32_t i;
-				} result;
-				result.d = d - 0.5 + 6755399441055744.0; // 2^51 + 2^52
-				return result.i;
-			}
+namespace psycle { namespace helpers { namespace math {
 
-			/// converts a floating point number to an integer by truncating toward -infinity
-			std::int32_t inline UNIVERSALIS__COMPILER__CONST
-			truncated(float f)
-			{
-				#if defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__MICROSOFT // also intel's compiler?
-					///\todo not always the fastest when using sse(2)
-					///\todo we can also use C1999's lrint if available
-					///\todo do we really need to write this in custom asm? wouldn't it be better to rely on the compiler?
-					#if 1 // note: this is not as fast as one might expect.
-						///\todo specify the rounding mode.. this is not a truncation!
-						std::int32_t i;
-						double const half(0.5);
-						_asm
-						{ 
-							fld f;
-							fsub half;
-							fistp i;
-						} 
-						return i;
-					#else
-						return truncated(double(f));
-					#endif
-				#else
-					return truncated(double(f));
-				#endif
-			}
-		}
+	/// converts a floating point number to an integer by truncating toward -infinity
+	std::int32_t inline UNIVERSALIS__COMPILER__CONST
+	truncated(double d)
+	{
+		BOOST_STATIC_ASSERT((sizeof d == 8));
+		union result_union
+		{
+			double d;
+			std::int32_t i;
+		} result;
+		result.d = d - 0.5 + 6755399441055744.0; // 2^51 + 2^52
+		return result.i;
 	}
-}
+
+	/// converts a floating point number to an integer by truncating toward -infinity
+	std::int32_t inline UNIVERSALIS__COMPILER__CONST
+	truncated(float f)
+	{
+		#if defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__MICROSOFT // also intel's compiler?
+			///\todo not always the fastest when using sse(2)
+			///\todo we can also use C1999's lrint if available
+			///\todo do we really need to write this in custom asm? wouldn't it be better to rely on the compiler?
+			#if 1 // note: this is not as fast as one might expect.
+				///\todo specify the rounding mode.. this is not a truncation!
+				std::int32_t i;
+				double const half(0.5);
+				_asm
+				{ 
+					fld f;
+					fsub half;
+					fistp i;
+				} 
+				return i;
+			#else
+				return truncated(double(f));
+			#endif
+		#else
+			return truncated(double(f));
+		#endif
+	}
+}}}
+
+#if defined BOOST_AUTO_TEST_CASE
+	BOOST_AUTO_TEST_CASE(truncated)
+	{
+		using psycle::helpers::math::truncated;
+		BOOST_CHECK(truncated(+1.6) == +1);
+		BOOST_CHECK(truncated(+1.4) == +1);
+		BOOST_CHECK(truncated(-1.6) == -2);
+		BOOST_CHECK(truncated(-1.4) == -2);
+		BOOST_CHECK(truncated(+1.6f) == +1);
+		BOOST_CHECK(truncated(+1.4f) == +1);
+		BOOST_CHECK(truncated(-1.6f) == -2);
+		BOOST_CHECK(truncated(-1.4f) == -2);
+	}
+#endif
