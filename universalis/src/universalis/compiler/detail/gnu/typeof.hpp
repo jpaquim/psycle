2c10de9c1146339209187076e07857bd7736cab5
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-20 20:55:01 +0000

    __typeof__

--- universalis/src/universalis/compiler/detail/gnu/typeof.hpp
+++ universalis/src/universalis/compiler/detail/gnu/typeof.hpp
@@ -6,9 +6,10 @@
 #pragma once
 #include <universalis/detail/project.hpp>
 #if defined DIVERSALIS__COMPILER__GNU
-	// typeof is a keyword
+	// typeof is a keyword unless strict iso conformance is enabled, in which case, it is made available with the __typeof__ keyword
+	#define typeof __typeof__
 	#define DIVERSALIS__COMPILER__FEATURE__TYPEOF
 #elif defined DIVERSALIS__COMPILER__MICROSOFT
 	/// it looks like a reserved keyword, but it's not implemented yet as of version 7.1 (only in c#).
-	#define typeof __typeof__
+	#define typeof __typeof
 #endif
