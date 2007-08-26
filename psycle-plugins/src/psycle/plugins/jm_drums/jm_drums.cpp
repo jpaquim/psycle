e8e9d03af47a388820533734a1131d948e6bd1a0
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-26 15:39:39 +0000

    division by zero with some compilers fixed in jmdrum

--- psycle-plugins/src/psycle/plugins/jm_drums/jm_drums.cpp
+++ psycle-plugins/src/psycle/plugins/jm_drums/jm_drums.cpp
@@ -542,7 +542,7 @@ int mi::GetVoice(int channel,bool getnew)
 		while (j<MAX_SIMUL_TRACKS && DTrack[j].Chan != -1) j++;
 		if (j == MAX_SIMUL_TRACKS)
 		{
-			j= MAX_SIMUL_TRACKS * static_cast<unsigned int>(std::rand())/((RAND_MAX+1)*2);
+			j = (unsigned int) (  (double)rand() * MAX_SIMUL_TRACKS /((double)RAND_MAX) + 1.0 ) );
 		}
 	}
 	allocatedvoice[channel]=j;
