b2c49715c6f433ebbc16fef96c32144c41f41883
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-08-31 15:50:42 +0000

    fixed some path handling problems

--- psycle-core/src/psycle/core/file.h
+++ psycle-core/src/psycle/core/file.h
@@ -1,57 +1,60 @@
-/***************************************************************************
-	*   Copyright (C) 2007 Psycledelics Community
-	*   psycle.sf.net
-	*                                                                         *
-	*   This program is free software; you can redistribute it and/or modify  *
-	*   it under the terms of the GNU General Public License as published by  *
-	*   the Free Software Foundation; either version 2 of the License, or     *
-	*   (at your option) any later version.                                   *
-	*                                                                         *
-	*   This program is distributed in the hope that it will be useful,       *
-	*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-	*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-	*   GNU General Public License for more details.                          *
-	*                                                                         *
-	*   You should have received a copy of the GNU General Public License     *
-	*   along with this program; if not, write to the                         *
-	*   Free Software Foundation, Inc.,                                       *
-	*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-	***************************************************************************/
-#ifndef FILE_H
-#define FILE_H
+/******************************************************************************
+*  copyright 2007 members of the psycle project http://psycle.sourceforge.net *
+*                                                                             *
+*  This program is free software; you can redistribute it and/or modify       *
+*  it under the terms of the GNU General Public License as published by       *
+*  the Free Software Foundation; either version 2 of the License, or          *
+*  (at your option) any later version.                                        *
+*                                                                             *
+*  This program is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+*  GNU General Public License for more details.                               *
+*                                                                             *
+*  You should have received a copy of the GNU General Public License          *
+*  along with this program; if not, write to the                              *
+*  Free Software Foundation, Inc.,                                            *
+*  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                  *
+******************************************************************************/
+#pragma once
+#ifndef PSYCLE__CORE__FILE
+#define PSYCLE__CORE__FILE
 #include <vector>
 #include <string>
-
-namespace psy {
-	namespace core {
-
-		const int nFiles = 1;
-		const int nDirs  = 2;
-	
-		class File{
+namespace psy { namespace core {
+	/// misc file operations
+	class File {
 		public:
-			static std::string readFile(const std::string & filename);
-			static std::vector<std::string> fileList( const std::string & path, int fMode = nFiles | nDirs );   
+			/// reads an ascii text file
+			static std::string readFile(std::string const & path);
+			struct list_modes {
+				int const static files = 1;
+				int const static dirs = 2;
+			};
+			/// lists the file in the dir
+			static std::vector<std::string> fileList(std::string const & path, int list_mode = list_modes::files | list_modes::dirs );   
+			/// the current dir
 			static std::string workingDir();
-			static bool fileIsReadable(const std::string & file);
+			/// whether the file can be read
+			static bool fileIsReadable(std::string const & path);
+			/// the parent of the current dir
 			static std::string parentWorkingDir();
-			static void cd( const std::string & path );
+			/// changes the current dir
+			static void cd(std::string const & path);
+			/// changes the current dir to the home dir
 			static void cdHome();
+			/// home dir
 			static std::string home();
-			static std::string replaceTilde(const std::string & path);
-			static void ensurePathTerminated(std::string &path);
-			static std::string env(const std::string & envName);
-#if defined __unix__ || defined __APPLE__
-			static std::string extractFileNameFromPath( const std::string& fileName, bool slash = true );
-#else
-			static std::string extractFileNameFromPath( const std::string& fileName, bool slash = false );
-#endif
-			static std::string slash();
-
-			static std::string replaceIllegalXmlChr( const std::string & text, bool strict = true );
-		};
-
-	}
-}
-
+			/// replaces ~ with home dir
+			static std::string replaceTilde(std::string const & path);
+			/// appends a path separator if needed
+			static void ensurePathTerminated(std::string & path);
+			static std::string env(std::string const & name);
+			/// path separator
+			static std::string const & slash();
+			static std::string replaceIllegalXmlChr(std::string const & text, bool strict = true);
+			/// basename
+			static std::string extractFileNameFromPath(std::string const & path);
+	};
+}}
 #endif
