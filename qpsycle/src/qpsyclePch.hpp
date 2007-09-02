a3df6e5564f4633366b1ceaabf733a81218f5eaa
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-02 23:08:58 +0000

    attempt to fix winapi build problem

--- qpsycle/src/qpsyclePch.hpp
+++ qpsycle/src/qpsyclePch.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#define DIVERSALIS__OPERATING_SYSTEM__VERSION__MAJOR 5
+#define DIVERSALIS__OPERATING_SYSTEM__VERSION__MINOR 0
+#define DIVERSALIS__OPERATING_SYSTEM__VERSION__PATCH 0
+#include <diversalis/operating_system.hpp> // sets winapi version so must be before <windows.h>
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
