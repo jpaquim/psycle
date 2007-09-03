b5717f2fc12c4310baa0d957cab110df532cbaa7
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-03 19:22:39 +0000

    fixed preproc redefinitions no non-mswin platforms

--- psycle-core/src/psycle/core/psycleCorePch.hpp
+++ psycle-core/src/psycle/core/psycleCorePch.hpp
@@ -3,12 +3,11 @@
 
 #pragma once
 
+#ifdef _WIN32
 #define DIVERSALIS__OPERATING_SYSTEM__VERSION__MAJOR 5
 #define DIVERSALIS__OPERATING_SYSTEM__VERSION__MINOR 0
 #define DIVERSALIS__OPERATING_SYSTEM__VERSION__PATCH 0
-#include <diversalis/operating_system.hpp> // sets winapi version so must be before <windows.h>
-
-#ifdef _WIN32
+#include <diversalis/operating_system.hpp> // sets winapi version so must come before <windows.h>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #endif
