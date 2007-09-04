50e4b3f80a7ca16b6fbdf7cde28ad0931ad13f1e
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-04 11:50:34 +0000

    fixed policy

--- psycle-core/src/psycle/core/binread.h
+++ psycle-core/src/psycle/core/binread.h
@@ -1,9 +1,9 @@
 /***************************************************************************
-*   Copyright (C) 2007 Psycledelics					  *
-*   psycle.sf.net							  *
-*
-*   based on binread from sondar					  *
-*   Copyright (C) 2007 Stefan Nattkemper				  *
+*   Copyright (C) 2007 Psycledelics                                       *
+*   psycle.sf.net                                                         *
+*                                                                         *
+*   based on binread from sondar                                          *
+*   Copyright (C) 2007 Stefan Nattkemper                                  *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
