2a25128537683fb26c339ff4e2f2ae97183523e2
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-04 19:10:29 +0000

    added more of the direct C99 mappings

--- psycle-core/src/psycle/core/helpers/math/truncate.hpp
+++ psycle-core/src/psycle/core/helpers/math/truncate.hpp
@@ -3,44 +3,79 @@
 #include <universalis/compiler.hpp>
 #include <boost/static_assert.hpp>
 #include <cstdint>
+#include "fast_unspecified_round_to_integer.hpp"
 namespace psy { namespace common { namespace math {
 
 	/// converts a floating point number to an integer by truncating toward -infinity
-	std::int32_t inline UNIVERSALIS__COMPILER__CONST
-	truncated(double d)
+	template<typename Real> UNIVERSALIS__COMPILER__CONST
+	std::int32_t inline truncated(Real x)
 	{
-		BOOST_STATIC_ASSERT((sizeof d == 8));
-		union result_union
-		{
-			double d;
-			std::int32_t i;
-		} result;
-		result.d = d - 0.5 + 6755399441055744.0; // 2^51 + 2^52
-		return result.i;
+		return x > 0 ? std::floor(x) : std::ceil(x);
 	}
+}}}
 
-	/// converts a floating point number to an integer by truncating toward -infinity
-	std::int32_t inline UNIVERSALIS__COMPILER__CONST
-	truncated(float f)
-	{
-		#if defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__MICROSOFT // also intel's compiler?
-			///\todo not always the fastest when using sse(2)
-			///\todo we can also use C1999's lrint if available
-			///\todo this custom asm is not very fast on some arch, the double "2^51 + 2^52" version might be faster
-			///\todo specify the rounding mode.. is this really a truncation toward -infinity, even with negative numbers?
-			std::int32_t i;
-			double const half(0.5);
-			__asm
-			{ 
-				fld f;
-				fsub half;
-				fistp i;
-			}
-			return i;
-		#else
-			return truncated(double(f));
-		#endif
-	}
+// inline implementation
+namespace psy { namespace common { namespace math {
+
+	#if __STDC__VERSION__ >= 199901 || \
+		(defined DIVERSALIS__COMPILER__GNU && DIVERSALIS__COMPILER__VERSION__MAJOR >= 4)
+		
+		template<> UNIVERSALIS__COMPILER__CONST
+		std::int32_t inline truncated<>(long double ld)
+		{
+			return fast_unspecified_round_to_integer<std::int32_t>(::truncl(ld));
+		}
+
+		template<> UNIVERSALIS__COMPILER__CONST
+		std::int32_t inline truncated<>(double d)
+		{
+			return fast_unspecified_round_to_integer<std::int32_t>(::trunc(d));
+		}
+
+		template<> UNIVERSALIS__COMPILER__CONST
+		std::int32_t inline truncated<>(float f)
+		{
+			return fast_unspecified_round_to_integer<std::int32_t>(::truncf(f));
+		}
+
+	#else
+
+		template<> UNIVERSALIS__COMPILER__CONST
+		std::int32_t inline truncated<>(double d)
+		{
+			BOOST_STATIC_ASSERT((sizeof d == 8));
+			union result_union
+			{
+				double d;
+				std::int32_t i;
+			} result;
+			result.d = d - 0.5 + 6755399441055744.0; // 2^51 + 2^52
+			return result.i;
+		}
+
+		template<> UNIVERSALIS__COMPILER__CONST
+		std::int32_t inline truncated<>(float f)
+		{
+			#if defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__MICROSOFT // also intel's compiler?
+				///\todo not always the fastest when using sse(2)
+				///\todo we can also use C1999's lrint if available
+				///\todo this custom asm is not very fast on some arch, the double "2^51 + 2^52" version might be faster
+				///\todo specify the rounding mode.. is this really a truncation toward -infinity, even with negative numbers?
+				std::int32_t i;
+				double const half(0.5);
+				__asm
+				{ 
+					fld f;
+					fsub half;
+					fistp i;
+				}
+				return i;
+			#else
+				return truncated(double(f));
+			#endif
+		}
+		
+	#endif
 	
 	#if defined BOOST_AUTO_TEST_CASE
 		BOOST_AUTO_TEST_CASE(truncated_test)
