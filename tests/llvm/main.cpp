869f11c24dd0e92cc4dd5747fe57f49d41277f15
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-09 23:34:50 +0000

    llvm test

--- tests/llvm/main.cpp
+++ tests/llvm/main.cpp
@@ -1,9 +1,18 @@
-//#include <iostream>
+#if USE_IOSTREAM
+	#include <iostream>
+#else
+	#include <cstdio>
+#endif
+
 #include <cmath>
 
 int main(int, char**) {
 	float f(1);
 	for(int i(0); i < 10000000; ++i) f += std::log(f);
-	//std::cout << f << "\n";
-	return !f;
+	#if USE_IOSTREAM
+		std::cout << f << "\n";
+	#else
+		std::printf("%f\n", f);
+	#endif
+	return 0;
 }
