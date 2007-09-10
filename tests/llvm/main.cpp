125cbaa9f7bd4dc4583d477ca7f4420196cdc142
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-10 15:17:04 +0000

    wonder why log10(2) returns -inf

--- tests/llvm/main.cpp
+++ tests/llvm/main.cpp
@@ -1,18 +1,9 @@
-#if USE_IOSTREAM
-	#include <iostream>
-#else
-	#include <cstdio>
-#endif
-
+#include <iostream>
 #include <cmath>
-
 int main(int, char**) {
-	float f(1);
-	for(int i(0); i < 10000000; ++i) f += std::log(f);
-	#if USE_IOSTREAM
-		std::cout << f << "\n";
-	#else
-		std::printf("%f\n", f);
-	#endif
+	float f(2);
+	std::cout << std::log(f) << "\n";
+	for(int i(0); i < 10; ++i) f += std::log(f);
+	std::cout << f << "\n";
 	return 0;
 }
