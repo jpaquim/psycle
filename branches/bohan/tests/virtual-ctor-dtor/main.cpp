59bb50566f3df5e6b9f164704ec256da342826ad
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-14 23:31:43 +0000

    generalisation

--- branches/bohan/tests/virtual-ctor-dtor/main.cpp
+++ branches/bohan/tests/virtual-ctor-dtor/main.cpp
@@ -4,8 +4,6 @@
 #include <boost/preprocessor/repetition/enum_params.hpp>
 #include <boost/preprocessor/repetition/enum_trailing_params.hpp>
 #include <boost/preprocessor/repetition/enum_binary_params.hpp>
-#include <boost/preprocessor/repetition/enum_trailing_binary_params.hpp>
-#include <boost/preprocessor/seq/elem.hpp>
 #include <boost/ref.hpp>
 namespace universalis { namespace compiler {
 	#define UNIVERSALIS__COMPILER__TEMPLATE_CONSTRUCTORS__ARITY__MINIMUM  8
@@ -19,25 +17,25 @@ namespace universalis { namespace compiler {
 	class virtual_factory {
 		public:
 			template<typename Type>
-			class stack_alloc {
+			class create_on_stack {
 				private:
 					Type instance;
 				public:
 					#define constructor(_, count, __) \
 						BOOST_PP_EXPR_IF(count, template<) BOOST_PP_ENUM_PARAMS(count, typename Xtra) BOOST_PP_EXPR_IF(count, >) \
-						stack_alloc(BOOST_PP_ENUM_BINARY_PARAMS(count, Xtra, & xtra)) \
+						create_on_stack(BOOST_PP_ENUM_BINARY_PARAMS(count, Xtra, & xtra)) \
 						: instance(BOOST_PP_ENUM_PARAMS(count, xtra)) { instance.after_construction(); }
 						
 						UNIVERSALIS__COMPILER__TEMPLATE_CONSTRUCTORS__LOOP(constructor)
 					#undef constructor
 					operator Type & () { return instance; }
 					operator Type const & () const { return instance; }
-					~stack_alloc()  { instance.before_destruction(); }
+					~create_on_stack()  { instance.before_destruction(); }
 				};
 				
 			#define constructor(_, count, __) \
 				template<typename Type BOOST_PP_ENUM_TRAILING_PARAMS(count, typename Xtra) > \
-				Type static & heap_alloc(BOOST_PP_ENUM_BINARY_PARAMS(count, Xtra, & xtra)) \
+				Type static & create_on_heap(BOOST_PP_ENUM_BINARY_PARAMS(count, Xtra, & xtra)) \
 				{ \
 					Type & instance(*new Type(BOOST_PP_ENUM_PARAMS(count, xtra))); \
 					instance.after_construction(); \
@@ -85,13 +83,13 @@ int main(int, char**) {
 	using namespace test;
 	std::cout << "--------------\n";
 	{
-		node & n(node::heap_alloc<sine>(boost::cref(2)));
+		node & n(node::create_on_heap<sine>(boost::cref(2)));
 		std::cout << n.f_ << "\n";
 		n.heap_free();
 	}
 	std::cout << "--------------\n";
 	{
-		node::stack_alloc<sine> s_(boost::cref(2)); node & n(s_);
+		node::create_on_stack<sine> s_(boost::cref(2)); node & n(s_);
 		std::cout << n.f_ << "\n";
 	}
 	std::cout << "--------------\n";
