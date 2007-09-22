453c8ecf1b09a191179d286e1daa96701dc72c89
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-09-22 11:52:42 +0000

    build fixes

--- trunk/psycle-plugins/src/psycle/plugins/druttis/DspLib/afloat.h
+++ trunk/psycle-plugins/src/psycle/plugins/druttis/DspLib/afloat.h
@@ -1,62 +1,35 @@
 #pragma once
+#include <algorithm>
 
-//============================================================================
-//				Always good to have defines
-//============================================================================
+/// afloat structure (source, target, current) values
+struct afloat {
+	float source;
+	float target;
+	float current;
+};
 
-#define min(x, y) ((x) < (y) ? (x) : (y))
-#define max(x, y) ((x) > (y) ? (x) : (y))
-
-//============================================================================
-//				afloat structure (source, target, current) values
-//============================================================================
-
-typedef struct
-{
-	float				source;
-	float				target;
-	float				current;
-}
-afloat;
-
-//============================================================================
-//				SetAFloat
-//============================================================================
-
-inline void SetAFloat(afloat *afloat, float value)
-{
+/// SetAFloat
+inline void SetAFloat(afloat *afloat, float value) {
 	afloat->source = afloat->current;
 	afloat->target = value;
 }
 
-//============================================================================
-//				AnimateAFloat
-//============================================================================
-
-inline void AnimateAFloat(afloat* p, float fac)
-{
-	if (fac == 0.0f)
-		p->current = p->target;
+/// AnimateAFloat
+inline void AnimateAFloat(afloat* p, float fac) {
+	if (fac == 0.0f) p->current = p->target;
 	float dist = p->target - p->source;
 	float maxdist = dist * fac;
-	if (dist > 0.0f)
-	{
-		if (p->current < p->target)
-		{
-			p->current += min(maxdist, dist);
-			if (p->current > p->target)
-				p->current = p->target;
+	if (dist > 0.0f) {
+		if (p->current < p->target) {
+			p->current += std::min(maxdist, dist);
+			if (p->current > p->target) p->current = p->target;
+		}
+	} else {
+		if (p->current > p->target) {
+			p->current += std::max(maxdist, dist);
+			if (p->current < p->target) p->current = p->target;
 		}
 	}
-	else
-	{
-		if (p->current > p->target)
-		{
-			p->current += max(maxdist, dist);
-			if (p->current < p->target)
-				p->current = p->target;
-		}
-	}
-	if (p->current == p->target)
-		p->source = p->current;
+	if (p->current == p->target) p->source = p->current;
 }
+
