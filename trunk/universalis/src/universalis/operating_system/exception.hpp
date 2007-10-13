b90cb288361061472b052f0fd2156ebb8a68d7ed
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-10-13 13:09:17 +0000

    cosmetic

--- trunk/universalis/src/universalis/operating_system/exception.hpp
+++ trunk/universalis/src/universalis/operating_system/exception.hpp
@@ -8,35 +8,32 @@
 #include <universalis/compiler/compiler.hpp>
 #define UNIVERSALIS__COMPILER__DYNAMIC_LINK UNIVERSALIS__OPERATING_SYSTEM__EXCEPTION
 #include <universalis/compiler/dynamic_link/begin.hpp>
-namespace universalis
-{
-	namespace operating_system
+namespace universalis { namespace operating_system {
+
+/// generic exception thrown by functions of the namespace universalis::operating_system.
+class UNIVERSALIS__COMPILER__DYNAMIC_LINK exception : public universalis::exception {
+	public:
+		inline exception(int const & code, compiler::location const & location) throw() : universalis::exception(location), code_(code) {}
+
+	public:
+		int const code() const throw() { return code_; }
+	private:
+		int const code_;
+
+	public:
+		char const * what() const throw() /*override*/;
+	protected:
+		std::string const mutable * what_;
+};
+
+namespace exceptions {
+	class UNIVERSALIS__COMPILER__DYNAMIC_LINK runtime_error : public universalis::exceptions::runtime_error
 	{
-		/// generic exception thrown by functions of the namespace universalis::operating_system.
-		class UNIVERSALIS__COMPILER__DYNAMIC_LINK exception : public universalis::exception
-		{
-			public:
-				inline exception(int const & code, compiler::location const & location) throw() : universalis::exception(location), code_(code) {}
-
-			public:
-				int const inline & code() const throw() { return code_; }
-			private:
-				int const          code_;
-
-			public:
-				char const * what() const throw() /*override*/;
-			protected:
-				std::string const mutable                            * what_;
-		};
-
-		namespace exceptions
-		{
-			class UNIVERSALIS__COMPILER__DYNAMIC_LINK runtime_error : public universalis::exceptions::runtime_error
-			{
-				public:
-					runtime_error(std::string const & what, compiler::location const &, void const * cause = 0) throw();
-			};
-		}
-	}
+		public:
+			runtime_error(std::string const & what, compiler::location const &, void const * cause = 0) throw();
+	};
 }
+
+}}
 #include <universalis/compiler/dynamic_link/end.hpp>
+
