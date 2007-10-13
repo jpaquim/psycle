9c42660d9ce8d5ba6778184d79e55de6fc4ed0d9
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-10-13 10:01:18 +0000

    note about merging with psycle-helpers

--- freepsycle/src/psycle/engine/sample.hpp
+++ freepsycle/src/psycle/engine/sample.hpp
@@ -1,6 +1,5 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2007 johan boule <bohan@jabber.org>
-// copyright 2004-2007 psycledelics http://psycle.pastnotecut.org
+// copyright 2001-2007 psycledelics http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
 ///\interface psycle::engine::sample
 #pragma once
@@ -13,43 +12,45 @@
 #define UNIVERSALIS__COMPILER__DYNAMIC_LINK  PSYCLE__ENGINE__SAMPLE
 #include <universalis/compiler/dynamic_link/begin.hpp>
 namespace psycle { namespace engine {
-/// mathematical operations on reals
+
+/// mathematical operations on floating point numbers
 namespace math {
 	template<typename Real>
-	Real inline deci_bell_to_linear(Real const & deci_bell) throw()
-	{
+	Real inline deci_bell_to_linear(Real const & deci_bell) throw() {
 		return std::pow(static_cast<unsigned int>(10), deci_bell / 20);
 	}
+	
 	template<typename Real>
-	Real inline linear_to_deci_bell(Real const & linear) throw()
-	{
+	Real inline linear_to_deci_bell(Real const & linear) throw() {
 		return 20 * std::log10(linear);
 	}
+	
 	template<typename Real>
-	Real inline limited(Real const & min, Real const & input, Real const & max) throw()
-	{
+	Real inline limited(Real const & min, Real const & input, Real const & max) throw() {
 		if(input < min) return min;
 		else if(input > max) return max;
 		else return input;
 	}
+	
 	template<typename Real>
-	void inline limit(Real const & min, Real & real, Real const & max) throw()
-	{
+	void inline limit(Real const & min, Real & real, Real const & max) throw() {
 		real = limited<Real>(min, real, max);
 	}
+
+	/// converts a floating point number to an integer.
 	///\post std::numeric_limits<Integral>::min() <= output and output <= std::numeric_limits<Integral>::max()
 	template<typename Real, typename Integral>
-	void inline convert(Real const & input, Integral & output, Real const & amplification = -std::numeric_limits<Integral>::min()) throw()
-	{
+	void inline convert(Real const & input, Integral & output, Real const & amplification = -std::numeric_limits<Integral>::min()) throw() {
 		Real const max(std::numeric_limits<Integral>::max());
 		Real const min(std::numeric_limits<Integral>::min());
 		output = limited<Real>(min, input * amplification, max);
 		assert(min <= output and output <= max);
 	}
+
+	/// converts a floating point number to an unsigned integer.
 	///\post std::numeric_limits<Integral>::min() <= output and output <= std::numeric_limits<Integral>::max()
 	template<typename Real, typename Integral>
-	void inline convert_to_unsigned(Real const & input, Integral & output, Real const & amplification = -std::numeric_limits<Integral>::min()) throw()
-	{
+	void inline convert_to_unsigned(Real const & input, Integral & output, Real const & amplification = -std::numeric_limits<Integral>::min()) throw() {
 		convert<Real, Integral>(input, output, amplification); 
 		Real const max(std::numeric_limits<Integral>::max());
 		Real const min(std::numeric_limits<Integral>::min());
@@ -58,8 +59,8 @@ namespace math {
 	}
 }
 
-	/// double (64-bit ieee-754 format) by default.
-	typedef universalis::compiler::numeric<64>::floating_point real;
+/// double (64-bit ieee-754 format) by default.
+typedef universalis::compiler::numeric<64>::floating_point real;
 	
 namespace math {
 	/// john napier's log(2) aka ln(2) constant 
@@ -78,30 +79,6 @@ namespace math {
 	real const degree = radian * pi / 180;
 	template real inline deci_bell_to_linear<real>(real const &);
 	template real inline linear_to_deci_bell<real>(real const &);
-	namespace vst {
-		universalis::compiler::numeric<32>::floating_point inline limited(const universalis::compiler::numeric<32>::floating_point & f) throw() {
-			return math::limited<universalis::compiler::numeric<32>::floating_point>(0, f, 1); 
-		}
-		void inline limit(universalis::compiler::numeric<32>::floating_point & f) throw() {
-			f = limited(f); 
-		}
-	}
-	/*
-	#include "processor/bits.hpp"
-	template void inline convert_to_unsigned(const sample &, uint8 &, const sample & amplification = std::numeric_limits<uint8>::max() / 2 - 1);
-	template void inline convert(const sample &, int8 &, const sample & amplification = -std::numeric_limits<int8>::min());
-	template void inline convert(const sample &, int16 &, const sample & amplification = -std::numeric_limits<int16>::min());
-	template void inline convert(const sample &, int32 &, const sample & amplification = -std::numeric_limits<int32>::min());
-	#if defined OLDSCHOOL
-		inline int16 convert(sample sample)
-		{
-			sample *= 32767;
-			if(sample < -32678) return = -32768;
-			if(sample > +32767) return = +32767;
-			return sample;
-		}
-	#endif
-	*/
 
 	template<int i>
 	struct compile_time_factorial {
@@ -113,6 +90,17 @@ namespace math {
 	struct compile_time_factorial<0> {
 		int const static value = 1;
 	};
-}}}
+}
+
+namespace vst {
+	float inline limited(float const & f) throw() {
+		return math::limited<float>(0, f, 1); 
+	}
+	void inline limit(float & f) throw() {
+		f = limited(f); 
+	}
+}
+
+}}
 #include <universalis/compiler/dynamic_link/end.hpp>
 
