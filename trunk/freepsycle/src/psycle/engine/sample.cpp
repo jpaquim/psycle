a98f25a09f7c09df3aebb9d2ef5572c1cde2311f
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-10-13 10:01:18 +0000

    note about merging with psycle-helpers

--- trunk/freepsycle/src/psycle/engine/sample.cpp
+++ trunk/freepsycle/src/psycle/engine/sample.cpp
@@ -1,19 +1,12 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2007 johan boule <bohan@jabber.org>
-// copyright 2004-2007 psycledelics http://psycle.pastnotecut.org
+// copyright 2001-2007 psycledelics http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
 ///\implementation psycle::engine::sample
 #include <packageneric/pre-compiled.private.hpp>
 #include <packageneric/module.private.hpp>
 #include <psycle/detail/project.private.hpp>
 #include "sample.hpp"
-namespace psycle
-{
-	namespace engine
-	{
-		namespace sample
-		{
-			// everything is inlined
-		}
-	}
-}
+namespace psycle { namespace engine { namespace sample {
+	// everything is inlined
+}}}
+
