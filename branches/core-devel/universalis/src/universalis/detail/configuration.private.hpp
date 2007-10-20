aaa26eea05ecbc6a079b001eb5af362be1d7bd61
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-10-20 09:56:30 +0000

    better use svn:externals

--- branches/core-devel/universalis/src/universalis/detail/configuration.private.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-// This is just an empty file to avoid build failures for people having their own build system.
-// The sources are generally able to build without specifying anything in this file,
-// but this file exist as a placeholder for possible platform-specific tweaks.
