5521fbb98184b166d51542812383312b332e7dbb
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-10-21 00:01:14 +0000

    msvc

--- trunk/universalis/src/universalis/detail/configuration.hpp
+++ trunk/universalis/src/universalis/detail/configuration.hpp
@@ -1,1 +1,4 @@
 #pragma once
+// This is just an empty file to avoid build failures for people having their own build system.
+// The sources are generally able to build without specifying anything in this file,
+// but this file exist as a placeholder for possible platform-specific tweaks.
