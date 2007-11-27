06a613feb279125694aad27ff825e4532fa0f0b8
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-11-27 20:56:42 +0000

    commiting too psycle-player

--- trunk/psycle-player/src/psycle/player/main.cpp
+++ trunk/psycle-player/src/psycle/player/main.cpp
@@ -5,23 +5,56 @@
 #include <string>
 #include <sstream>
 
+
+#if defined _WIN32
+#include <windows.h> // for Sleep(ms)
+#else
+#include <unistd.h> // for sleep(s)
+#endif
+
+void usage() {
+		std::cerr <<
+			"Usage: psycle-player [options] [--input-file] <song file name>\n"
+			"Plays a Psycle song file.\n"
+			"\n"
+			" -odrv, --output-driver <name>   name of the output driver to use.\n"
+			"                                 examples: silent, alsa, jack, esd, dsound, mmewaveout\n"
+			"\n"
+			" -odev, --output-device <name>   name of the output device the driver should use.\n"
+			"                                 The default device will be used if this option is not specified.\n"
+			"                                 examples for alsa: default, hw:0, plughw:0, pulse.\n"
+			//"                                 examples for gstreamer: autoaudiosink, gconfaudiosink.\n"
+			//"                                 examples for pulseaudio: hostname:port\n"
+			//"                                 examples for esound: hostname:port\n"
+			"\n"
+			" -of,   --output-file <riff wave file name>\n"
+			"                                 name of the output file to render to in riff-wave format.\n"
+			"\n"
+			" -if,   --input-file <song file name>\n"
+			"                                 name of the song file to play.\n"
+			"\n"
+			"        --help                   display this help and exit.\n"
+			"        --version                output version information and exit.\n"
+			"\n"
+			"Report bugs to the bug tracker at http://sourceforge.net/projects/psycle\n"
+			;
+}
+
 int main(int argument_count, char * arguments[]) {
 	if(argument_count < 2) {
-		std::cerr << "psycle: player: usage: psycle-player [options] <song file name>" << std::endl;
-		std::cerr << "Options:" << std::endl;
-		std::cerr << "\t-od|--output-device drivername\tUse the specified output driver." << std::endl;
-		std::cerr << "\t-of|--output-file filename\tOutputs the audio to a file. .wav format." << std::endl;
-		std::cerr << "\t-if|--input-file\tOptional way to specify the input filename." << std::endl;
+		usage();
 		return 1;
 	}
 	
 	std::string input_file_name;
-	std::string output_device_name("auto");
+	std::string output_driver_name;
+	std::string output_device_name;
 	std::string output_file_name;
 	
 	struct tokens { enum type {
 		none,
 		input_file_name,
+		output_driver_name,
 		output_device_name,
 		output_file_name
 	};};
@@ -36,6 +69,10 @@ int main(int argument_count, char * arguments[]) {
 				input_file_name = s;
 				token = tokens::none;
 				break;
+			case tokens::output_driver_name:
+				output_driver_name = s;
+				token = tokens::none;
+				break;
 			case tokens::output_device_name:
 				output_device_name = s;
 				token = tokens::none;
@@ -45,39 +82,70 @@ int main(int argument_count, char * arguments[]) {
 				token = tokens::none;
 				break;
 			default:
-				if(s == "-od" || s == "--output-device") token = tokens::output_device_name;
+				if(s == "-odrv" || s == "--output-driver") token = tokens::output_driver_name;
+				else if(s == "-odev" || s == "--output-device") token = tokens::output_device_name;
 				else if(s == "-of" || s == "--output-file") token = tokens::output_file_name;
 				else if(s == "-if" || s == "--input-file") token = tokens::input_file_name;
-				else {
+				else if(s == "--help") {
+					usage();
+					return 0;
+				} else if(s == "--version") {
+					std::cout << "psycle-player devel (built on " __DATE__ " " __TIME__ ")\n"; ///\todo need a real version
+					return 0;
+				} else if(s.length() && s[0] == '-') { // unrecognised option
+					std::cerr << "error: unknown option: " << s << '\n';
+					usage();
+					return 1;
+				} else {
 					input_file_name = s;
+					
 					token = tokens::none;
 				}
 		}
 	}
 
-	std::cout << output_device_name << " " << output_file_name << "\n";
-	
-	psy::core::Player player;
+	psy::core::Player &player = *psy::core::Player::Instance();
+	///\todo: player is defined as a machine_callback. The correct design would utilize "timeInfo",
+	// or similar instead of passing all player.
+	psy::core::CoreSong song(&player);
+	//Song is passed to player, since in a good scenario, we would have one player, and several songs (MDI interface)
+	player.song(&song);
 
-	
+
 	if(output_file_name.length()) {
 		std::cout << "psycle: player: setting output file name to: " << output_file_name << "\n";
 		player.setFileName(output_file_name);
 	}
 
 	Configuration configuration;
-	std::cout << "psycle: player: plugins are looked for in: " << configuration.pluginPath() << "\n";
 
-	if(output_device_name.length()) {
-		std::cout << "psycle: player: setting output device name to: " << output_device_name << "\n";
-		configuration.setDriverByName(output_device_name);
+	if(!configuration.pluginPath().length())
+		std::cerr << "psycle: player: native plugin path not configured. You can set the PSYCLE_PATH environment variable.\n";
+	else
+		std::cout << "psycle: player: native plugins are looked for in: " << configuration.pluginPath() << "\n";
+
+	if(!configuration.ladspaPath().length())
+		std::cerr << "psycle: player: ladspa plugin path not configured. You can set the LADSPA_PATH environment variable.\n";
+	else
+		std::cout << "psycle: player: ladspa plugins are looked for in: " << configuration.ladspaPath() << "\n";
+
+	if(output_driver_name.length()) {
+		std::cout << "psycle: player: setting output driver name to: " << output_driver_name << "\n";
+		configuration.setDriverByName(output_driver_name);
 	}
 
-	player.setDriver(*configuration._pOutputDriver); ///\todo needs a getter
-	
-	psy::core::Song song(&player);
-	
-	if(output_file_name.length()) player.startRecording();
+	psy::core::AudioDriver & output_driver(*configuration._pOutputDriver); ///\todo needs a getter
+
+	if(output_device_name.length()) {
+		std::cout << "psycle: player: setting output driver device name to: " << output_device_name << "\n";
+		psy::core::AudioDriverSettings settings(output_driver.settings()); ///\todo why do we do a copy?
+		settings.setDeviceName(output_device_name);
+		output_driver.setSettings(settings); ///\todo why do we copy?
+	}
+
+	player.setDriver(output_driver);
+	// since driver is cloned, we cannot use output_driver!!!!
+	player.driver().Enable(false);
 	
 	if(input_file_name.length()) {
 		std::cout << "psycle: player: loading song file: " << input_file_name << "\n";
@@ -86,25 +154,26 @@ int main(int argument_count, char * arguments[]) {
 			return 2;
 		}
 
-		///\todo this is not intuitive... we pass the player when creating the song, and now we have to pass the song to the player?
-		player.song(&song);
+		int itmp=256;
+		player.Work(&player,itmp);
 
+		if(output_file_name.length()) player.startRecording();
+		// since driver is cloned, we cannot use output_driver!!!!
+		player.driver().Enable(true);
 		player.start(0);
-		std::cout << "psycle: player: playing...\n";
+		std::cout << "psycle: player: playing..." << std::endl;
+		std::cout << "psycle: player: press any letter and enter to stop" << std::endl;
 
-		std::cout << "psycle: player: (press Ctrl+C to end)\n";
-	
-		while (1)
-		{
-			printf("Beat: %.02f\n",player.timeInfo().playBeatPos());
-			sleep(1);
-		}
+		std::string s; std::cin >> s;
+		
+		std::cout << std::endl << "psycle: player: stopping at position " << player.playPos() << "." << std::endl;
 		player.stop();
-		configuration.setDriverByName("silent");
-		player.setDriver(*configuration._pOutputDriver);
+		if(output_file_name.length()) player.stopRecording();
 	}
-
-	if(output_file_name.length()) player.stopRecording();
+	// since driver is cloned, we cannot use output_driver!!!!
+	player.driver().Enable(false);
+	//configuration.setDriverByName("silent");
+	//player.setDriver(*configuration._pOutputDriver);
 	
 	return 0;
 }
