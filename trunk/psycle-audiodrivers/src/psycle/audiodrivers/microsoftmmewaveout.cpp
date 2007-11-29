3bb18ba4e509e55e483660732b53798122284712
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-11-29 19:04:22 +0000

    homogenified some of the file headers

--- trunk/psycle-audiodrivers/src/psycle/audiodrivers/microsoftmmewaveout.cpp
+++ trunk/psycle-audiodrivers/src/psycle/audiodrivers/microsoftmmewaveout.cpp
@@ -1,22 +1,21 @@
-/***************************************************************************
-*   Copyright (C) 2007 Psycledelics   and others  *
-*   psycle.sf.net   *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-*   This program is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU General Public License for more details.                          *
-*                                                                         *
-*   You should have received a copy of the GNU General Public License     *
-*   along with this program; if not, write to the                         *
-*   Free Software Foundation, Inc.,                                       *
-*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-***************************************************************************/
+/******************************************************************************
+*  copyright 2007 members of the psycle project http://psycle.sourceforge.net *
+*                                                                             *
+*  This program is free software; you can redistribute it and/or modify       *
+*  it under the terms of the GNU General Public License as published by       *
+*  the Free Software Foundation; either version 2 of the License, or          *
+*  (at your option) any later version.                                        *
+*                                                                             *
+*  This program is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+*  GNU General Public License for more details.                               *
+*                                                                             *
+*  You should have received a copy of the GNU General Public License          *
+*  along with this program; if not, write to the                              *
+*  Free Software Foundation, Inc.,                                            *
+*  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                  *
+******************************************************************************/
 #if defined PSYCLE__MICROSOFT_MME_AVAILABLE
 #include "microsoftmmewaveout.h"
 namespace psy
