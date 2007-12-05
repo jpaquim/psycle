226b83fa3927ebb79809ce2177143b3344690433
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-12-05 22:59:30 +0000

    trying to fix build on the various platforms

--- universalis/src/unit_tests.cpp
+++ universalis/src/unit_tests.cpp
@@ -1,7 +1,9 @@
 #include <packageneric/pre-compiled.private.hpp>
 
 #define BOOST_LIB_NAME boost_unit_test_framework
-#define BOOST_DYN_LINK
+#if BOOST_VERSION >= 103400
+	#define BOOST_DYN_LINK
+#endif
 #include <boost/config/auto_link.hpp>
 
 #define BOOST_TEST_DYN_LINK
