938114574c32ec07b8bfe7ce73a64f598238414e
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-12-14 22:56:20 +0000

    std <date_time> nearly complete

--- universalis/src/universalis/standard_library/date_time.hpp
+++ universalis/src/universalis/standard_library/date_time.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <ctime> // for std::time_t in std::utc_time constructor
+#include <ctime> // for std::time_t in std::utc_time
 
 namespace std {
 	/*
@@ -22,106 +22,99 @@ namespace std {
 	*/
 
 	/*******************************************************************/
-	// duration types
+	// time duration types
 		
-	class nanoseconds;
-	class microseconds;
-	class milliseconds;
-	class seconds;
-	class minutes;
-	class hours;
-	//class day; // difficult to implement and not useful for specifying pause durations
-	//class month; // difficult to implement and not useful for specifying pause durations
-	//class year; // difficult to implement and not useful for specifying pause durations
-	
-	class nanoseconds {
+	template<typename Final, typename Tick, Tick Ticks_Per_Seconds, Tick Seconds_Per_Tick>
+	class basic_time_duration {
 		public:
-			typedef long long int tick_type;
-			nanoseconds(tick_type ns = 0) : ns_(ns) {}
-			tick_type get_count() const { return ns_; }
-			static bool is_subsecond() { return true; }
+			typedef Tick tick_type;
+			basic_time_duration(tick_type tick = 0) : tick_(tick) {}
+			tick_type get_count() const { return tick_; }
+			tick_type static ticks_per_second() { return Ticks_Per_Seconds; }
+			tick_type static seconds_per_tick() { return Seconds_Per_Tick; }
+			bool static is_subsecond() { return ticks_per_second() > seconds_per_tick(); }
+			bool operator==(Final const & that) const { return this->tick_ == that.tick_; }
+			bool operator!=(Final const & that) const { return this->tick_ != that.tick_; }
+			bool operator<=(Final const & that) const { return this->tick_ <= that.tick_; }
+			bool operator>=(Final const & that) const { return this->tick_ >= that.tick_; }
+			bool operator< (Final const & that) const { return this->tick_ < that.tick_; }
+			bool operator> (Final const & that) const { return this->tick_ > that.tick_; }
+			Final operator-() const { Final f(-this->tick_); return f; }
+			Final operator+(Final const & that) const { Final f(this->tick_ + that.tick_); return f; }
+			Final operator-(Final const & that) const { Final f(this->tick_ - that.tick_); return f; }
+			Final operator/(int d) const { Final f(tick_ / d); return f; }
+			Final operator*(int m) const { Final f(tick_ * m); return f; }
+			Final & operator+=(Final const & that) { this->tick_ += that.tick_; return static_cast<Final>(*this); }
+			Final & operator-=(Final const & that) { this->tick_ -= that.tick_; return static_cast<Final>(*this); }
+			Final & operator/=(int d) { tick_ /= d; return static_cast<Final>(*this); }
+			Final & operator*=(int m) { tick_ *= m; return static_cast<Final>(*this); }
+		protected:
+			typedef basic_time_duration<Final, Tick, Ticks_Per_Seconds, Seconds_Per_Tick> basic_time_duration_type;
 		private:
-			tick_type ns_;
+			tick_type tick_;
 	};
 
-	class microseconds {
+	class nanoseconds : public basic_time_duration<nanoseconds, long long int, 1000000000, 0> {
 		public:
-			typedef long long int tick_type;
-			microseconds(tick_type us = 0) : us_(us) {}
-			tick_type get_count() const { return us_; }
-			static bool is_subsecond() { return true; }
-			operator nanoseconds() const { nanoseconds ns(us_ * 1000); return ns; }
-		private:
-			tick_type us_;
+			nanoseconds(tick_type ns = 0) : basic_time_duration_type(ns) {}
 	};
 
-	class milliseconds {
+	class microseconds : public basic_time_duration<microseconds, long long int, 1000000, 0> {
 		public:
-			typedef long long int tick_type;
-			milliseconds(tick_type ms = 0) : ms_(ms) {}
-			tick_type get_count() const { return ms_; }
-			static bool is_subsecond() { return true; }
-			operator nanoseconds() const { nanoseconds ns(ms_ * 1000000); return ns; }
-			operator microseconds() const { microseconds us(ms_ * 1000); return us; }
-		private:
-			tick_type ms_;
+			microseconds(tick_type us = 0) : basic_time_duration_type(us) {}
+			operator nanoseconds() const { nanoseconds ns(get_count() * nanoseconds::ticks_per_second() / ticks_per_second()); return ns; }
 	};
 
-	class seconds {
+	class milliseconds : public basic_time_duration<milliseconds, long long int, 1000, 0> {
 		public:
-			typedef long long int tick_type;
-			seconds(tick_type s = 0) : s_(s) {}
-			tick_type get_count() const { return s_; }
-			static bool is_subsecond() { return false; }
-			operator nanoseconds() const { nanoseconds ns(s_ * 1000000000); return ns; }
-			operator microseconds() const { microseconds us(s_ * 1000000); return us; }
-			operator milliseconds() const { milliseconds ms(s_ * 1000); return ms; }
-		private:
-			tick_type s_;
+			milliseconds(tick_type ms = 0) : basic_time_duration_type(ms) {}
+			operator nanoseconds() const { nanoseconds ns(get_count() * nanoseconds::ticks_per_second() / ticks_per_second()); return ns; }
+			operator microseconds() const { microseconds us(get_count() * microseconds::ticks_per_second() / ticks_per_second()); return us; }
 	};
 
-	class minutes {
+	class seconds : public basic_time_duration<seconds, long long int, 1, 1> {
 		public:
-			typedef long long int tick_type;
-			minutes(tick_type m = 0) : m_(m) {}
-			tick_type get_count() const { return m_; }
-			static bool is_subsecond() { return false; }
-			operator nanoseconds() const { nanoseconds ns(m_ * 60000000000LL); return ns; }
-			operator microseconds() const { microseconds us(m_ * 60000000); return us; }
-			operator milliseconds() const { milliseconds ms(m_ * 60000); return ms; }
-			operator seconds() const { seconds s(m_ * 60); return s; }
-		private:
-			tick_type m_;
+			seconds(tick_type s = 0) : basic_time_duration_type(s) {}
+			operator nanoseconds() const { nanoseconds ns(get_count() * nanoseconds::ticks_per_second() / ticks_per_second()); return ns; }
+			operator microseconds() const { microseconds us(get_count() * microseconds::ticks_per_second() / ticks_per_second()); return us; }
+			operator milliseconds() const { milliseconds ms(get_count() * milliseconds::ticks_per_second() / ticks_per_second()); return ms; }
 	};
 
-	class hours {
+	class minutes : public basic_time_duration<minutes, long long int, 0, 60> {
 		public:
-			typedef long long int tick_type;
-			hours(tick_type h = 0) : h_(h) {}
-			tick_type get_count() const { return h_; }
-			static bool is_subsecond() { return false; }
-			operator nanoseconds() const { nanoseconds ns(h_ * 3600000000000LL); return ns; }
-			operator microseconds() const { microseconds us(h_ * 3600000000LL); return us; }
-			operator milliseconds() const { milliseconds ms(h_ * 3600000); return ms; }
-			operator seconds() const { seconds s(h_ * 3600); return s; }
-			operator minutes() const { minutes m(h_ * 60); return m; }
-		private:
-			tick_type h_;
+			minutes(tick_type m = 0) : basic_time_duration_type(m) {}
+			operator nanoseconds() const { nanoseconds ns(get_count() * nanoseconds::ticks_per_second() / ticks_per_second()); return ns; }
+			operator microseconds() const { microseconds us(get_count() * microseconds::ticks_per_second() / ticks_per_second()); return us; }
+			operator milliseconds() const { milliseconds ms(get_count() * milliseconds::ticks_per_second() / ticks_per_second()); return ms; }
+			operator seconds() const { seconds s(get_count() * seconds_per_tick()); return s; }
+	};
+
+	class hours : public basic_time_duration<hours, long long int, 0, 60 * 60> {
+		public:
+			hours(tick_type h = 0) : basic_time_duration_type(h) {}
+			operator nanoseconds() const { nanoseconds ns(get_count() * nanoseconds::ticks_per_second() / ticks_per_second()); return ns; }
+			operator microseconds() const { microseconds us(get_count() * microseconds::ticks_per_second() / ticks_per_second()); return us; }
+			operator milliseconds() const { milliseconds ms(get_count() * milliseconds::ticks_per_second() / ticks_per_second()); return ms; }
+			operator seconds() const { seconds s(get_count() * seconds_per_tick()); return s; }
+			operator minutes() const { minutes m(get_count() * seconds_per_tick() / minutes::seconds_per_tick()); return m; }
 	};
 	
+	class days : public basic_time_duration<days, long long int, 0, 60 * 60 * 24> {
+		public:
+			days(tick_type d = 0) : basic_time_duration_type(d) {}
+			operator nanoseconds() const { nanoseconds ns(get_count() * nanoseconds::ticks_per_second() / ticks_per_second()); return ns; }
+			operator microseconds() const { microseconds us(get_count() * microseconds::ticks_per_second() / ticks_per_second()); return us; }
+			operator milliseconds() const { milliseconds ms(get_count() * milliseconds::ticks_per_second() / ticks_per_second()); return ms; }
+			operator seconds() const { seconds s(get_count() * seconds_per_tick()); return s; }
+			operator minutes() const { minutes m(get_count() * seconds_per_tick() / minutes::seconds_per_tick()); return m; }
+			operator hours() const { hours h(get_count() * 24); return h; }
+	};
+
+	//class months; // difficult to implement and not useful for specifying pause durations
+	//class years; // difficult to implement and not useful for specifying pause durations
+
 	/*******************************************************************/
-	// timepoint
-	
-	class utc_time {
-		private:
-			nanoseconds ns_;
-		public:
-			/// epoch
-			utc_time() : ns_() {} ///\ŧodo
-			utc_time(time_t t, nanoseconds ns) : ns_(ns) {} ///\ŧodo
-			
-			static bool is_subsecond() { return true; }
-	};
+	// time clock
 	
 	template<typename time_type>
 	class hiresolution_clock {
@@ -133,7 +126,79 @@ namespace std {
 			}
 	};
 
+	/*******************************************************************/
+	// time point
+	
+	class utc_time {
+		public:
+			/// storage type for number of nanoseconds since epoch
+			typedef nanoseconds::tick_type tick_type;
 
+			/// number of ticks per second
+			tick_type static ticks_per_second() { return nanoseconds::ticks_per_second(); }
+
+			/// number of seconds per tick
+			tick_type static seconds_per_tick() { return nanoseconds::seconds_per_tick(); }
+
+			/// true if resolution is less than 1 second
+			bool static is_subsecond() { return nanoseconds::is_subsecond(); }
+
+			/// zero time (i.e. epoch)
+			utc_time() : ns_() {}
+
+			#if 0 ///\todo
+			/// constructs a time point from the given separate seconds and nanoseconds since epoch
+			utc_time(time_t t, nanoseconds ns);
+			#endif
+
+			/// number of seconds since epoch
+			std::time_t seconds_since_epoch() const { return ns_ / ticks_per_second(); }
+
+			/// number of nanoseconds since epoch
+			nanoseconds nanoseconds_since_epoch() const { return ns_; }
+
+			/// returns true if this == that
+			bool operator==(utc_time const & that) const { return this->ns_ == that.ns_; }
+
+			/// returns true if this != that
+			bool operator!=(utc_time const & that) const { return this->ns_ != that.ns_; }
+
+			/// returns true if this < that
+			bool operator<(utc_time const & that) const { return this->ns_ < that.ns_; }
+
+			/// returns true if this > that
+			bool operator>(utc_time const & that) const { return this->ns_ > that.ns_; }
+
+			/// returns true if this <= that
+			bool operator<=(utc_time const & that) const { return this->ns_ <= that.ns_; }
+
+			/// returns true if this >= that
+			bool operator>=(utc_time const & that) const { return this->ns_ >= that.ns_; }
+
+			/// returns duration between this and that
+			nanoseconds operator-(utc_time const & that) const { return this->ns_ - that.ns_; }
+
+			/// returns this time point shifted by the given duration
+			template<typename Duration>
+			utc_time operator+(Duration const & d) const { nanoseconds ns(d); return utc_time(ns_ + d); }
+
+			/// returns this time point shifted by the given duration
+			template<typename Duration>
+			utc_time operator-(Duration const & d) const { nanoseconds ns(d); return utc_time(ns_ - d); }
+
+			/// returns shifts this time point by the given duration
+			template<typename Duration>
+			utc_time operator+=(Duration const & d) const { nanoseconds ns(d); this->ns_ += ns.get_count(); return *this; }
+
+			/// returns shifts this time point by the given duration
+			template<typename Duration>
+			utc_time operator-=(Duration const & d) const { nanoseconds ns(d); this->ns_ -= ns.get_count(); return *this; }
+
+		private:
+			tick_type ns_;
+			utc_time(tick_type t) : ns_(t) {} friend class hiresolution_clock<tick_type>;
+	};
+	
 	#if defined BOOST_AUTO_TEST_CASE
 		BOOST_AUTO_TEST_CASE(std_date_time_test)
 		{
