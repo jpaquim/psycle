952625a2d1fc3a4393d44fe92e801c115b6649ff
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-12-16 03:00:17 +0000

    test cases to measure clock resolutions

--- universalis/src/universalis/standard_library/condition.hpp
+++ universalis/src/universalis/standard_library/condition.hpp
@@ -2,6 +2,6 @@
 #include <universalis/operating_system/threads/condition.hpp>
 namespace std {
 	template <class Lock> class condition;
-	typedef condition<mutex>              condition_mtx;
-	typedef condition<unique_lock<mutex>> condition_ulm;
+	typedef condition<mutex> condition_mtx;
+	typedef condition< unique_lock<mutex> > condition_ulm;
 }
