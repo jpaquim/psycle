8e381df7904b6a929fad1ffbb8ae5c8735bce7d9
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-12-17 19:15:18 +0000

    msvc didn't see that bug

--- universalis/src/universalis/standard_library/date_time.hpp
+++ universalis/src/universalis/standard_library/date_time.hpp
@@ -28,7 +28,7 @@ namespace std {
 	class hiresolution_clock {
 		public:
 			Time_Point static universal_time() {
-				std::nanoseconds const ns(operating_system::clocks::utc_since_epoch::current());
+				std::nanoseconds const ns(universalis::operating_system::clocks::utc_since_epoch::current());
 				Time_Point t(ns);
 				return t;
 			}
