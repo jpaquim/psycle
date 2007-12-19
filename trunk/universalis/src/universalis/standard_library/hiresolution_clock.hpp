634f442fc255119a8ca4d35992ca467eec750b37
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-12-19 00:24:45 +0000

    workaround for the lack of high-resolution utc time clock on microsoft's platform

--- trunk/universalis/src/universalis/standard_library/hiresolution_clock.hpp
+++ trunk/universalis/src/universalis/standard_library/hiresolution_clock.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "duration.hpp"
+#include "utc_time.hpp"
 #include <universalis/operating_system/clocks.hpp>
 namespace std {
 	template<typename Time_Point>
@@ -8,6 +9,15 @@ namespace std {
 			Time_Point static universal_time() {
 				std::nanoseconds const ns(universalis::operating_system::clocks::utc_since_epoch::current());
 				Time_Point t(ns);
+				return t;
+			}
+	};
+	template<>
+	class hiresolution_clock<std::utc_time> {
+		public:
+			std::utc_time static universal_time() {
+				std::nanoseconds const ns(universalis::operating_system::clocks::utc_since_epoch::current());
+				std::utc_time t(ns.get_count());
 				return t;
 			}
 	};
@@ -20,9 +30,9 @@ namespace std {
 	BOOST_AUTO_TEST_CASE(std_hiresolution_clock_and_sleep_test)
 	{
 		std::nanoseconds const sleep_nanoseconds(std::milliseconds(250));
-		std::nanoseconds const t0(std::hiresolution_clock<std::nanoseconds>::universal_time());
+		std::utc_time const t0(std::hiresolution_clock<std::utc_time>::universal_time());
 		universalis::operating_system::threads::sleep(sleep_nanoseconds);
-		double const ratio(double((std::hiresolution_clock<std::nanoseconds>::universal_time() - t0).get_count()) / sleep_nanoseconds.get_count());
+		double const ratio(double((std::hiresolution_clock<std::utc_time>::universal_time() - t0).get_count()) / sleep_nanoseconds.get_count());
 		{
 			std::ostringstream s; s << ratio;
 			BOOST_MESSAGE(s.str());
