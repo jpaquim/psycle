2789a76f7c97566b3dcadc09858eca345d60a57f
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2007-12-30 22:45:14 +0000

    cosmetics

--- freepsycle/src/psycle/plugins/outputs/asio.hpp
+++ freepsycle/src/psycle/plugins/outputs/asio.hpp
@@ -5,7 +5,7 @@
 #pragma once
 #include <psycle/detail/project.hpp>
 
-#if !defined OPERATING_SYSTEM__MICROSOFT
+#if !defined DIVERSALIS__OPERATING_SYSTEM__MICROSOFT
 	#error "this plugin is specific to microsoft's operating system"
 #endif
 
