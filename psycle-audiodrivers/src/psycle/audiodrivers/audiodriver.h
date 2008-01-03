59737995b25d22465167c7ed45e0650f8911369c
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-01-03 15:56:30 +0000

    plugin interface fix (don't move the destructor!) several changes copied from core-devel.

--- psycle-audiodrivers/src/psycle/audiodrivers/audiodriver.h
+++ psycle-audiodrivers/src/psycle/audiodrivers/audiodriver.h
@@ -36,109 +36,92 @@ namespace psy
 		/// provides some text info about the driver
 		class AudioDriverInfo
 		{
-			public:
-				AudioDriverInfo(const std::string & name, const std::string & header, const std::string & description, bool show);
-				/// the driver name, used e.g in the configuration for the driver map.
-				/// As convention, please use only lower case names here.
-				const std::string& name() const { return name_; }
-				/// short description
-				const std::string& header() const { return header_; }
-				/// deeper explanation
-				const std::string& description() const { return description_; }
-				/// hint, if audiodialog should list the driver
-				bool show() const { return show_; }
-			private:
-				std::string name_;
-				std::string header_;
-				std::string description_;
-				bool show_;
+		public:
+			AudioDriverInfo(const std::string & name, const std::string & header, const std::string & description, bool show);
+			/// the driver name, used e.g in the configuration for the driver map.
+			/// As convention, please use only lower case names here.
+			const std::string& name() const { return name_; }
+			/// short description
+			const std::string& header() const { return header_; }
+			/// deeper explanation
+			const std::string& description() const { return description_; }
+			/// hint, if audiodialog should list the driver
+			bool show() const { return show_; }
+		private:
+			std::string name_;
+			std::string header_;
+			std::string description_;
+			bool show_;
 		};
 
 		/// holds the info about sample rate, bit depth, etc
 		class AudioDriverSettings
 		{
-			public :
-				AudioDriverSettings();
+		public:
+			AudioDriverSettings();
 
-			///\name device name
+			///\name getter/setter for device name  ( hw:0 and others )
 			///\{
-				public:
-					/// some drivers require a device name (alsa, for example)
-					const std::string& deviceName() const { return deviceName_; }
-					/// some drivers require a device name (alsa, for example)
-					void setDeviceName(const std::string & name);
-				private:
-					std::string deviceName_;
+		public:
+			const std::string& deviceName() const { return deviceName_; }
+			void setDeviceName(const std::string & name);
+		private:
+			std::string deviceName_;
 			///\}
-
-			///\name sample rate
+			///\name getter/setter for sample rate
 			///\{
-				public:
-					/// not all values possible maybe we should add a enum type here ..
-					int samplesPerSec() const { return samplesPerSec_; }
-					/// not all values possible maybe we should add a enum type here ..
-					void setSamplesPerSec( int samples );
-				private:
-					int samplesPerSec_;
+		public:
+			int samplesPerSec() const { return samplesPerSec_; }
+			void setSamplesPerSec( int samples );
+		private:
+			int samplesPerSec_;
 			///\}
-
-			///\name bit depth
+			///\name getter/setter for bit depth ( 8, 16, 24. what about ASIO that offers more modes? 
 			///\{
-				public:
-					/// bit depth values 8 16 24 
-					int bitDepth() const { return bitDepth_; }
-					/// bit depth values 8 16 24 
-					void setBitDepth( int depth );
-				private:
-					int bitDepth_;
+		public:
+			int bitDepth() const { return bitDepth_; }
+			void setBitDepth( int depth );
+		private:
+			int bitDepth_;
 			///\}
-
-			///\name channels (and their mode)
+			///\name getter/setter for channel mode (mode 3 == stereo, 1 == mono left, 2 == mono right, 0 = mono both channels)
 			///\{
-				public:
-					/// channel mode 3 == stereo, 1 == mono left, 2 == mono right, 0 = mono both channels
-					int channelMode() const { return channelMode_; }
-					/// channel mode 3 == stereo, 1 == mono left, 2 == mono right, 0 = mono both channels
-					void setChannelMode( int mode );
-				private:
-					int channelMode_;
+		public:
+			int channelMode() const { return channelMode_; }
+			void setChannelMode( int mode );
+		private:
+			int channelMode_;
 			///\}
+			/// getter for number of bits per sample
+		public:
+			int sampleSize() const;
 
-			///\todo doc
-			public:
-				int sampleSize() const;
-
-			///\name buffer size
+			///\todo: Unused????
+			///\name getter/setter for buffer size
 			///\{
-				public:
-					/// buffer size
-					int bufferSize() const { return bufferSize_; }
-					/// buffer size
-					void setBufferSize( int size );
-				private:
-					int bufferSize_;
-			///\]
-
-			///\name block size
-			///\{
-				public:
-					///\todo doc
-					int blockSize() const { return blockSize_; }
-					///\todo doc
-					void setBlockSize( int size );
-				private:
-					int blockSize_;
+		public:
+			int bufferSize() const { return bufferSize_; }
+			void setBufferSize( int size );
+		private:
+			int bufferSize_;
 			///\}
-
-			///\name block count
+			///\todo: Only used by mmewaveout???
+			///\name getter/setter for the audio block size 
 			///\{
-				public:
-					///\todo doc
-					int blockCount() const { return blockCount_; }
-					///\todo doc
-					void setBlockCount( int count );
-				private:
-					int blockCount_;
+		public:
+			int blockSize() const { return blockSize_; }
+			void setBlockSize( int size );
+		private:
+			int blockSize_;
+			///\}
+			///\todo: Only used by mmewaveout???
+			///\name getter/setter for number of blocks.
+			///\{
+		public:
+			int blockCount() const { return blockCount_; }
+			void setBlockCount( int count );
+		private:
+			int blockCount_;
 			///\}
 		};
 
@@ -168,19 +151,19 @@ namespace psy
 				static void Quantize16AndDeinterlace(float const *pin, std::int16_t *pileft, int strideleft, std::int16_t *piright, int strideright, int c);
 				static void DeQuantize16AndDeinterlace(int const *pin, float *poutleft,float *poutright,int c);
 
-			///\name settings
-			///\{
-				public:
-					/// here you can set the settings of the driver, like samplerate depth etc
-					virtual void setSettings( const AudioDriverSettings& settings );
+		///\name settings
+		///\{
+			public:
+				/// here you can set the settings of the driver, like samplerate depth etc
+				virtual void setSettings( const AudioDriverSettings& settings );
 
-					/// here you get the special audio driver settings.
-					/// In case of some drivers like  e.g jack you must prepare, that a driver can set itself.
-					const AudioDriverSettings& settings() const { return settings_; }
-				private:
-					/// holds the sample rate, bit depth, etc
-					AudioDriverSettings settings_; 
-			///\}
+				/// here you get the special audio driver settings.
+				/// In case of some drivers like  e.g jack you must prepare, that a driver can set itself.
+				const AudioDriverSettings& settings() const { return settings_; }
+			private:
+				/// holds the sample rate, bit depth, etc
+				AudioDriverSettings settings_; 
+		///\}
 		};
 	}
 }
