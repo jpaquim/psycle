635a420d427c76eb41d44c4c0ef72b2ae83a8e8f
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-01-06 12:53:02 +0000

    event scheduler now working (weird demo tune)

--- freepsycle/src/psycle/plugins/pulse.cpp
+++ freepsycle/src/psycle/plugins/pulse.cpp
@@ -20,21 +20,32 @@ pulse::pulse(engine::plugin_library_reference & plugin_library_reference, engine
 }
 
 void pulse::do_process() throw(engine::exception) {
+//std::clog << "pulse " << name() << '\n';
 	if(!*output_ports()[0]) return;
 	engine::buffer & out(output_ports()[0]->buffer());
 	if(!out.events()) return; // ?
 	real const samples_per_beat = output_ports()[0]->events_per_second() / beats_per_second_;
 	std::uint64_t const initial_sample(static_cast<std::size_t>(beat_ * samples_per_beat));
 	real const last_beat(beat_ + out.events() / samples_per_beat);
-std::clog << samples_per_beat << ' ' << out.events() << ' ' << initial_sample << ' ' << i << ' ' << last_beat << '\n';
+//std::clog << samples_per_beat << ' ' << out.events() << ' ' << initial_sample << ' ' << i << ' ' << last_beat << '\n';
 	std::size_t const channels(output_ports()[0]->channels());
-	for(std::size_t c(0); c < channels; ++c) out[c][0].index(out.events());
+	std::size_t last_index(0);
 	for(; i < events_.size() && events_[i].beat() < last_beat; ++i) {
 		event & e(events_[i]);
-		std::size_t const index(static_cast<std::size_t>(e.beat() * samples_per_beat - initial_sample));
-		if(index < out.size()) for(std::size_t c(0); c < channels; ++c) out[c][index](index, e.sample());
+		std::size_t const i(static_cast<std::size_t>(e.beat() * samples_per_beat - initial_sample));
+		if(i < out.events()) {
+//std::clog << "event " << e.beat() << ' ' << e.sample() << ' ' << i << '\n';
+			for(std::size_t c(0); c < channels; ++c) out[c][last_index](i, e.sample());
+			++last_index;
+		}
+		else {
+			// event lost!
+std::clog << "event lost " << e.beat() << ' ' << e.sample() << ' ' << i << '\n';
+		}
 	}
+	if(last_index < out.events()) for(std::size_t c(0); c < channels; ++c) out[c][last_index].index(out.events());
 	beat_ = last_beat;
+//std::clog << last_index << '\n';
 }
 
 }}
--- freepsycle/src/psycle/plugins/sine.cpp
+++ freepsycle/src/psycle/plugins/sine.cpp
@@ -44,11 +44,13 @@ void sine::seconds_per_event_change_notification_from_port(engine::port const & 
 }
 
 void sine::do_process() throw(engine::exception) {
+//std::clog << "sine " << name() << '\n';
 	if(!have_out()) return;
 	if(!have_frequency()) goto const_frequency;
 	if(!have_phase()) goto const_phase;
 	
 	//none_const:
+//std::clog << "none_const\n";
 		for(std::size_t frequency_event(0), phase_event(0), out_event(0); out_event < out_channel().size(); ++out_event) {
 			if(frequency_event < frequency_channel().size() && frequency_channel()[frequency_event].index() == out_event)
 				this->frequency(frequency_channel()[frequency_event++].sample());
@@ -61,6 +63,7 @@ void sine::do_process() throw(engine::exception) {
 	
 	const_frequency:
 		if(!have_phase()) goto all_const;
+//std::clog << "const_frequency\n";
 
 		for(std::size_t phase_event(0), out_event(0); out_event < out_channel().size(); ++out_event) {
 			if(phase_event < phase_channel().size() && phase_channel()[phase_event].index() == out_event)
@@ -71,12 +74,11 @@ void sine::do_process() throw(engine::exception) {
 	goto modulo;
 	
 	const_phase: {
-std::clog << "const_phase\n";
+//std::clog << "const_phase\n";
 		for(std::size_t frequency_event(0), out_event(0); out_event < out_channel().size(); ++out_event) {
 			if(frequency_event < frequency_channel().size() && frequency_channel()[frequency_event].index() == out_event) {
-std::clog << frequency_event << ' ' << frequency_channel()[frequency_event].sample() << '\n';
+//std::clog << frequency_event << ' ' << frequency_channel()[frequency_event].sample() << '\n';
 				this->frequency(frequency_channel()[frequency_event++].sample());
-std::clog << this->frequency() << '\n';
 			}
 			out_channel()[out_event](out_event, 0.3 * std::sin(phase_)); // \todo optimize with a cordic algorithm
 			phase_ += step_;
@@ -85,6 +87,7 @@ std::clog << this->frequency() << '\n';
 	goto modulo;
 		
 	all_const:
+//std::clog << "all_const\n";
 		for(std::size_t out_event(0) ; out_event < out_channel().size() ; ++out_event) {
 			out_channel()[out_event](out_event, 0.3 * std::sin(phase_)); // \todo optimize with a cordic algorithm
 			phase_ += step_;
