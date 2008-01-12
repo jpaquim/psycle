2e7132dc8b4a1f1bf538d1b00e60cfa7531ddeb1
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-01-12 01:39:24 +0000

    build fix for ms

--- trunk/universalis/src/universalis/standard_library/thread.hpp
+++ trunk/universalis/src/universalis/standard_library/thread.hpp
@@ -8,6 +8,9 @@
 #include "detail/boost_xtime.hpp"
 #include <boost/thread.hpp>
 #include <boost/operators.hpp>
+#elif defined DIVERSALIS__OPERATING_SYSTEM__MICROSOFT
+	#include <windows.h>
+#endif
 namespace std {
 
 	class thread {
