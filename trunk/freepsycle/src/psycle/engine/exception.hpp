156f4c35e732360f2d39133e582f32470736db2e
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-01-17 20:22:10 +0000

    removed empty translation unit

--- trunk/freepsycle/src/psycle/engine/exception.hpp
+++ trunk/freepsycle/src/psycle/engine/exception.hpp
@@ -1,6 +1,5 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2007 johan boule <bohan@jabber.org>
-// copyright 2004-2007 psycledelics http://psycle.pastnotecut.org
+// copyright 1999-2007 psycledelics http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
 ///\interface psycle::engine::exception
 #pragma once
@@ -9,25 +8,27 @@
 #include <string>
 #define UNIVERSALIS__COMPILER__DYNAMIC_LINK  PSYCLE__ENGINE__EXCEPTION
 #include <universalis/compiler/dynamic_link/begin.hpp>
-namespace psycle
-{
-	namespace engine
-	{
-		/// generic exception thrown by functions of the namespace psycle::engine.
-		class UNIVERSALIS__COMPILER__DYNAMIC_LINK exception : public universalis::exception
-		{
-			public:
-				inline exception(universalis::compiler::location const & location, void const * cause = 0) throw() : universalis::exception(location, cause) {}
-		};
+namespace psycle { namespace engine {
 
-		namespace exceptions
-		{
-			class UNIVERSALIS__COMPILER__DYNAMIC_LINK runtime_error : public universalis::exceptions::runtime_error
-			{
-				public:
-					runtime_error(std::string const & what, universalis::compiler::location const & location, void const * cause = 0) throw();
-			};
-		}
-	}
-}
+/// generic exception thrown by functions of the namespace psycle::engine.
+class exception : public universalis::exception {
+	public:
+		inline exception(
+			universalis::compiler::location const & location,
+			void const * cause = 0
+		) throw() : universalis::exception(location, cause) {}
+};
+
+namespace exceptions {
+	/// runtime errors thrown by functions of the namespace psycle::engine.
+	class runtime_error : public universalis::exceptions::runtime_error {
+		public:
+			runtime_error(
+				std::string const & what,
+				universalis::compiler::location const & location,
+				void const * cause = 0
+			) throw() : universalis::exceptions::runtime_error(what, location, cause) {}
+	};
+}}}
+
 #include <universalis/compiler/dynamic_link/end.hpp>
