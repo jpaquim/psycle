7ba88601ea4574b5c47fb97f8027cd3d7e5fa8c7
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-01-19 17:33:34 +0000

    changed underlying container type from vector to map

--- freepsycle/src/psycle/plugins/pulse.cpp
+++ freepsycle/src/psycle/plugins/pulse.cpp
@@ -1,5 +1,5 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2007-2007 psycledelics http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
+// copyright 2007-2008 psycledelics http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
 ///\implementation psycle::plugins::pulse
 #include <psycle/detail/project.private.hpp>
@@ -22,28 +22,18 @@ pulse::pulse(engine::plugin_library_reference & plugin_library_reference, engine
 
 void pulse::beat(real beat) {
 	this->beat_ = beat;
-	i_ = events_.begin();
-	while(i_ != events_.end() && i_->beat() < beat) ++i_;
+	i_ = events_.lower_bound(beat);
 }
 
 void pulse::insert_event(real beat, real sample) {
-	if(events_.size() && events_.back().beat() < beat) events_.push_back(event(beat, sample));
-	else {
-		events_type::iterator i(events_.begin());
-		while(i != events_.end() && i->beat() < beat) ++i;
-		events_.insert(i, event(beat, sample));
-	}
+	events_[beat] = sample;
 	this->beat(this->beat()); // recompute the iterator
 }
 
 void pulse::erase_events(real begin_beat, real end_beat) {
-	events_type::iterator first(events_.begin());
-	while(first != events_.end() && first->beat() < begin_beat) ++first;
-	if(first == events_.end()) return;
-	events_type::iterator last(first);
-	while(last != events_.end() && last->beat() > end_beat) ++last;
-	if(last != events_.end() && first != last && last->beat() == end_beat) --last; // exclude end_beat from range
-	events_.erase(first, last);
+	events_type::iterator last(events_.lower_bound(end_beat));
+	if(last != events_.end()) --last;
+	events_.erase(events_.lower_bound(begin_beat), last);
 	beat(beat()); // recompute the iterator
 }
 
@@ -55,16 +45,16 @@ void pulse::do_process() throw(engine::exception) {
 	real last_beat(beat_ + (out.events() - 1) / samples_per_beat);
 	std::size_t const channels(output_ports()[0]->channels());
 	std::size_t last_index(0);
-	for(; i_ != events_.end() && i_->beat() < last_beat; ++i_) {
-		event const & e(*i_);
-		std::size_t const i(static_cast<std::size_t>(e.beat() * samples_per_beat - initial_sample));
+	for(; i_ != events_.end() && i_->first < last_beat; ++i_) {
+		real const b(i_->first), s(i_->second);
+		std::size_t const i(static_cast<std::size_t>(b * samples_per_beat - initial_sample));
 		if(i < out.events()) {
-			for(std::size_t c(0); c < channels; ++c) out[c][last_index](i, e.sample());
+			for(std::size_t c(0); c < channels; ++c) out[c][last_index](i, s);
 			++last_index;
 		} else { // event lost! should never happen.
 			if(loggers::warning()) {
 				std::ostringstream s;
-				s << "event lost: " << e.beat() << ' ' << e.sample();
+				s << "event lost: " << b << ' ' << s;
 				loggers::warning()(s.str(), UNIVERSALIS__COMPILER__LOCATION);
 			}
 		}
