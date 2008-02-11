e18f8b9347f2a29c7053356e500ea3e74d829332
zealmange <zealmange@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-02-11 22:12:17 +0000

    removed a stray character, changed constants used in helpers/math/erase_denormals.hpp

--- trunk/psycle-helpers/src/psycle/helpers/math/erase_denormals.hpp
+++ trunk/psycle-helpers/src/psycle/helpers/math/erase_denormals.hpp
@@ -6,14 +6,14 @@ namespace psycle
 		namespace math
 		{
 			inline float erase_denormals(float x) {
-				x += 2.350988701644575e-38f; // 2^-125
-				x -= 2.350988701644575e-38f; // 2^-125
+				x += 1.0e-30f;
+				x -= 1.0e-30f;
 				return x;
 			}
 			
 			inline double erase_denormals(double x) {
-				x += 4.450147717014403e-308; // 2^-1021
-				x -= 4.450147717014403e-308; // 2^-1021
+				x += 1.0e-291;
+				x -= 1.0e-291;
 				return x;
 			}
 
@@ -26,19 +26,19 @@ namespace psycle
 			}
 
 			inline float fast_erase_denormals(float x) {
-				return x + 2.350988701644575e-38f; // 2^-125
+				return x + 1.0e-30f;
 			}
 			
 			inline double fast_erase_denormals(double x) {
-				return x + 4.450147717014403e-308; // 2^-1021
+				return x + 1.0e-291;
 			}
 
 			inline float fast_erase_denormals_inplace(float& x) {
-				return x += 2.350988701644575e-38f; // 2^-125
+				return x += 1.0e-30f;
 			}
 
 			inline double fast_erase_denormals_inplace(double& x) {
-				return x += 4.450147717014403e-308; // 2^-1021
+				return x += 1.0e-291;
 			}
 		}
 	}
