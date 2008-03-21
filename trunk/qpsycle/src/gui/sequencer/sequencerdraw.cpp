04fdf8ccf7a61835e7c76d4228e26a33c1dbe21b
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-03-21 16:30:24 +0000

    Policy.

--- trunk/qpsycle/src/gui/sequencer/sequencerdraw.cpp
+++ trunk/qpsycle/src/gui/sequencer/sequencerdraw.cpp
@@ -46,8 +46,8 @@ namespace qpsycle {
 
 	SequencerDraw::SequencerDraw( SequencerView *seqView )
 		: seqView_(seqView),
-		  beatPxLength_(5),
-		  lineHeight_(30)
+		beatPxLength_(5),
+		lineHeight_(30)
 	{
 		setAlignment ( Qt::AlignLeft | Qt::AlignTop );
 
@@ -75,14 +75,14 @@ namespace qpsycle {
 			patternSequence->newLineInserted.connect
 				(boost::bind(&SequencerDraw::onNewLineInserted,this,_1,_2));
 			/*
-			  Does not work because of a boost bug in boost 1.33
-			  as of Mar 28 2007.
-			  This bug is fixed in boost SVN. I put a workaround in
-			  makeSequencerLine instead.
-			  / Magnus
+			Does not work because of a boost bug in boost 1.33
+			as of Mar 28 2007.
+			This bug is fixed in boost SVN. I put a workaround in
+			makeSequencerLine instead.
+			/ Magnus
 
-			  patternSequence->lineRemoved.connect
-			  (boost::bind(&SequencerDraw::onLineRemoved,this,_1));
+			patternSequence->lineRemoved.connect
+			(boost::bind(&SequencerDraw::onLineRemoved,this,_1));
 			*/
 			patternSequence->linesSwapped.connect
 				(boost::bind(&SequencerDraw::onLinesSwapped,this,_1,_2));
