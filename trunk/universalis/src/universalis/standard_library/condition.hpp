53ec1a43f463449d937d7c4c2d69e1b090d4e0d5
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-03-25 19:05:08 +0000

    shows a race condition with more than two threads

--- trunk/universalis/src/universalis/standard_library/condition.hpp
+++ trunk/universalis/src/universalis/standard_library/condition.hpp
@@ -152,5 +152,98 @@ namespace std {
 			condition_test_class test;
 			test.test();
 		}
+		
+		class condition_speed_test_class {
+			private:
+				nanoseconds static cpu_time_clock() {
+					#if 0
+						return hiresolution_clock<utc_time>::universal_time().nanoseconds_since_epoch();
+					#elif 0
+						return universalis::operating_system::clocks::thread_cpu_time::current();
+					#elif 0
+						return universalis::operating_system::clocks::process_cpu_time::current();
+					#else
+						return universalis::operating_system::clocks::monotonic::current();
+					#endif
+				}
+				
+				class tls {
+					public:
+						tls(int i) : t(), count(), i(i) {}
+						nanoseconds t;
+						unsigned int count;
+						int i;
+				};
+				
+				void thread_function(tls * const tls_pointer) {
+					tls & tls(*tls_pointer);
+					{ scoped_lock_type l(m);
+						--shared_start;
+						while(shared_start) c.wait(l);
+					}
+					c.notify_all();
+					while(tls.count < end) {
+						nanoseconds const t0(cpu_time_clock());
+						for(unsigned int i(0); i < inner_loop; ++i) {
+							{ scoped_lock_type l(m);
+								if(shared == -1) return;
+								shared = tls.i;
+							}
+							c.notify_all();
+							{ scoped_lock_type l(m);
+								while(shared == tls.i) c.wait(l);
+							}
+						}
+						std::nanoseconds const t1(cpu_time_clock());
+						if(++tls.count > start) tls.t += t1 - t0;
+					}
+					{ scoped_lock_type l(m);
+						shared = -1;
+					}
+					c.notify_all();
+				}
+				
+				typedef scoped_lock<mutex> scoped_lock_type;
+				mutex m;
+				condition<scoped_lock_type> c;
+
+				unsigned int inner_loop, start, end;
+				
+				int shared, shared_start;
+
+			public:
+				void test(unsigned int threads) {
+					inner_loop = 1000;
+					start = 100;
+					end = start + 100;
+					shared_start = threads;
+					shared = 0;
+
+					tls ** const tls_(new tls*[threads]);
+					for(unsigned int i(0); i < threads; ++i) tls_[i] = new tls(i);
+					
+					thread ** const threads_(new thread*[threads]);
+					for(unsigned int i(0); i < threads; ++i)
+						threads_[i] = new thread(boost::bind(&condition_speed_test_class::thread_function, this, tls_[i]));
+					
+					for(unsigned int i(0); i < threads; ++i) threads_[i]->join();
+					
+					for(unsigned int i(0); i < threads; ++i) {
+						delete threads_[i];
+						{
+							std::ostringstream s;
+							s << threads << " threads: " << i << ": " << tls_[i]->t.get_count() * 1e-9 / (tls_[i]->count - start) << 's';
+							BOOST_MESSAGE(s.str());
+						}
+						delete tls_[i];
+					}
+				}
+		};
+
+		BOOST_AUTO_TEST_CASE(std_condition_speed_test) {
+			condition_speed_test_class test;
+			test.test(2);
+			test.test(4); // shows a race condition
+		}		
 	}}}}
 #endif
