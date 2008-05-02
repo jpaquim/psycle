cc952bb9555bdfcc816b100414e1d291b0866553
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-05-02 18:22:00 +0000

    Add block set machine command.

--- qpsycle/src/gui/configuration.cpp
+++ qpsycle/src/gui/configuration.cpp
@@ -476,6 +476,7 @@ namespace qpsycle {
 		inputHandler_.changeKeyCode( commands::transpose_block_dec12, Key( Qt::ControlModifier | Qt::ShiftModifier, Qt::Key_F11 ) );
 
 		inputHandler_.changeKeyCode( commands::block_set_instrument, Key( Qt::ControlModifier, Qt::Key_T ) );
+		inputHandler_.changeKeyCode( commands::block_set_machine, Key( Qt::ControlModifier, Qt::Key_G ) );
 
 		inputHandler_.changeKeyCode( commands::row_clear, Key(Qt::NoModifier, Qt::Key_Delete) );
 	}
