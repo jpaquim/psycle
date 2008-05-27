3566ffa33a58e4a9b8db821f74088a7b38313f87
nmather <nmather@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-05-27 18:29:48 +0000

    Switch behaviour page dialog to use the ui file.

--- qpsycle/src/gui/configdlg/behaviourpage.cpp
+++ qpsycle/src/gui/configdlg/behaviourpage.cpp
@@ -42,85 +42,32 @@ namespace qpsycle {
 	BehaviourPage::BehaviourPage( QWidget *parent )
 		: QWidget( parent )
 	{
-		setWindowTitle("General Settings");
+		setupUi( this );
+		// see ui_behaviourpage.h, or load behaviourpage.ui into Qt Designer,
+		// to see available widgets.
 
-		QLabel *knobComboLabel = new QLabel("Knob behaviour: ");
-
-
-		/*** Machine View ***/
-		knobBehaviourCombo_ = new QComboBox( this );
 		knobBehaviourCombo_->addItem("Rotary (QDial)");
 		knobBehaviourCombo_->addItem("Angular (QSynth)");
 		knobBehaviourCombo_->addItem("Linear");
 		knobBehaviourCombo_->addItem("Fixed Linear");
+
 		QSettings settings;
 		knobBehaviourCombo_->setCurrentIndex( settings.value( "behaviour/machineview/knob" ).toInt() );
 		connect( knobBehaviourCombo_, SIGNAL( currentIndexChanged( int ) ), this, SLOT( onSettingsChanged() ) );
 
-		QVBoxLayout *mainLay = new QVBoxLayout();
-		QHBoxLayout *buttonsLay = new QHBoxLayout();
-		QHBoxLayout *knobLay = new QHBoxLayout();
-		
-		QGroupBox *settingsGroup = new QGroupBox( "Machine View", this );
-		QWidget *buttonsGroup = new QWidget( this );
-
-		saveBtn_ = new QPushButton( "Save" );
 		saveBtn_->setEnabled( false );
-		closeBtn_ = new QPushButton( "Close" );
-
-		knobLay->addWidget( knobComboLabel );
-		knobLay->setAlignment( Qt::AlignRight );
-		knobLay->addWidget( knobBehaviourCombo_ );
 
 		connect( saveBtn_, SIGNAL( clicked() ), this, SLOT( onSaveButtonClicked() ) );
 
-		settingsGroup->setLayout( knobLay );
+		homeEndChk_->setChecked( settings.value( "behaviour/patternview/ft2HomeEnd" ).toBool() );
+		shiftChk_->setChecked( settings.value( "behaviour/patternview/shiftArrowForSelect" ).toBool() );
+		wrapChk_->setChecked( settings.value( "behaviour/patternview/wrapAround" ).toBool() );
+		centerCursorChk_->setChecked( settings.value( "behaviour/patternview/centerCursor" ).toBool() );
 
-		buttonsLay->addWidget( saveBtn_ );
-		buttonsGroup->setLayout( buttonsLay );
-
-
-		/*** Pattern View Settings ***/
-		QGroupBox *patternView = new QGroupBox( "Pattern View", this );
-		QGridLayout *patViewLay = new QGridLayout();
-		patternView->setLayout( patViewLay );
-		QLabel *homeEndLabel = new QLabel( "FT2 Home/End Behaviour" );
-		QLabel *shiftLabel = new QLabel( "Shift Keys For Select" );
-		QLabel *wrapLabel = new QLabel( "Wrap Around" );
-		QLabel *centerCursorLabel = new QLabel( "Center Cursor" );
-	
-		homeEndChk = new QCheckBox();
-		shiftChk = new QCheckBox();
-		wrapChk = new QCheckBox();
-		centerCursorChk = new QCheckBox();
-
-		homeEndChk->setChecked( settings.value( "behaviour/patternview/ft2HomeEnd" ).toBool() );
-		shiftChk->setChecked( settings.value( "behaviour/patternview/shiftArrowForSelect" ).toBool() );
-		wrapChk->setChecked( settings.value( "behaviour/patternview/wrapAround" ).toBool() );
-		centerCursorChk->setChecked( settings.value( "behaviour/patternview/centerCursor" ).toBool() );
-
-		connect( homeEndChk, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
-
-		connect( shiftChk, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
-		connect( wrapChk, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
-		connect( centerCursorChk, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
-
-		patViewLay->addWidget( homeEndLabel );
-		patViewLay->addWidget( homeEndChk );
-		patViewLay->addWidget( shiftLabel );
-		patViewLay->addWidget( shiftChk );
-		patViewLay->addWidget( wrapLabel );
-		patViewLay->addWidget( wrapChk );
-		patViewLay->addWidget( centerCursorLabel );
-		patViewLay->addWidget( centerCursorChk );
-
-
-		mainLay->addWidget( settingsGroup );
-		mainLay->addWidget( patternView );
-
-		mainLay->addWidget( buttonsGroup );
-
-		setLayout( mainLay );
+		connect( homeEndChk_, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
+		connect( shiftChk_, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
+		connect( wrapChk_, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
+		connect( centerCursorChk_, SIGNAL( stateChanged( int ) ), SLOT( onSettingsChanged() ) );
 	}
 
 	void BehaviourPage::onSettingsChanged()
@@ -133,10 +80,10 @@ namespace qpsycle {
 		QSettings settings;
 		
 		settings.setValue( "behaviour/machineview/knob", knobBehaviourCombo_->currentIndex() );
-		settings.setValue( "behaviour/patternview/ft2HomeEnd", homeEndChk->checkState() == Qt::Checked? true: false );
-		settings.setValue( "behaviour/patternview/shiftArrowForSelect", shiftChk->checkState() == Qt::Checked? true: false );
-		settings.setValue( "behaviour/patternview/wrapAround", wrapChk->checkState() == Qt::Checked? true: false );
-		settings.setValue( "behaviour/patternview/centerCursor", centerCursorChk->checkState() == Qt::Checked? true: false );
+		settings.setValue( "behaviour/patternview/ft2HomeEnd", homeEndChk_->checkState() == Qt::Checked? true: false );
+		settings.setValue( "behaviour/patternview/shiftArrowForSelect", shiftChk_->checkState() == Qt::Checked? true: false );
+		settings.setValue( "behaviour/patternview/wrapAround", wrapChk_->checkState() == Qt::Checked? true: false );
+		settings.setValue( "behaviour/patternview/centerCursor", centerCursorChk_->checkState() == Qt::Checked? true: false );
 
 		saveBtn_->setEnabled( false );
 	}
