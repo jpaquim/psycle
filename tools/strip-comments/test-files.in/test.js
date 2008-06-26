c86fbc3f5a02789eaf69ebcec1557042c0c6ddba
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-06-26 00:06:42 +0000

    comment strip tool

--- tools/strip-comments/test-files.in/test.js
+++ tools/strip-comments/test-files.in/test.js
@@ -1,4 +1,4 @@
-var a = 0;
+var a = 1. / 3 * 2;
 // single line comment from start 'not a string ... still inside comment ... /* still the same comment
 // single line comment from start "not a string ... still inside comment ... /* still the same comment
 var b = 0; // single line comment at end of line
@@ -10,5 +10,5 @@ var b = 0; // single line comment at end of line
 	no escape in comments \*/
 var c /* multi-line
 	comment in the middle of a statement */ = 0;
-var d = 'inside string "still inside same string ... escaped \' \" \n \t \a \r \\ still inside string /* not a comment // not a comment ... still inside string';
-var e = "inside string 'still inside same string ... escaped \" \" \n \t \a \r \\ still inside string /* not a comment // not a comment ... still inside string";
+var d = 'inside string "still inside same string ... escaped \\ \' \" \0 \a \n \r \t still inside string /* not a comment // not a comment ... still inside string';
+var e = "inside string 'still inside same string ... escaped \\ \" \" \0 \a \n \r \t still inside string /* not a comment // not a comment ... still inside string";
