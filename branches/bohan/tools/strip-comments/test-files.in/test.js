3af6e3b76df0eac92c1405f2ecdfcf0cee659919
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-06-26 03:23:13 +0000

    comment strip tool

--- branches/bohan/tools/strip-comments/test-files.in/test.js
+++ branches/bohan/tools/strip-comments/test-files.in/test.js
@@ -1,18 +1,24 @@
 var a = 1. / 3 * 2;
+
 // single line comment from start 'not a string ... still inside comment ... /* still the same comment
 // single line comment from start "not a string ... still inside comment ... /* still the same comment
+
 var b = 0; // single line comment at end of line
+
 /* multi-line comment from start
 	'not a string ... still inside comment
 	"not a string ... still inside comment
 	// still the same comment
 	nested /* should be ignored
 	no escape in comments \*/
+
 var c /* multi-line
 	comment in the middle of a statement */ = 0;
+
 var d /* multi-line
 	comment followed by new line */
 	= 0;
+
 var e = 'inside string "still inside same string ... escaped \\ \' \" \0 \a \n \r \t still inside string /* not a comment // not a comment ... still inside string';
 var f = "inside string 'still inside same string ... escaped \\ \" \" \0 \a \n \r \t still inside string /* not a comment // not a comment ... still inside string";
 
@@ -20,7 +26,7 @@ var f = "inside string 'still inside same string ... escaped \\ \" \" \0 \a \n \
 
 
 		
-	    
+	  var    s    =   '.  .    .     .'  ;
 
 if(cond) { // indented comment
 	// indented comment
