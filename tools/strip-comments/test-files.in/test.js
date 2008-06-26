ba72b305ba30d0b4080ba244282f696ef619af0d
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-06-26 03:10:32 +0000

    comment strip tool

--- tools/strip-comments/test-files.in/test.js
+++ tools/strip-comments/test-files.in/test.js
@@ -12,3 +12,19 @@ var c /* multi-line
 	comment in the middle of a statement */ = 0;
 var d = 'inside string "still inside same string ... escaped \\ \' \" \0 \a \n \r \t still inside string /* not a comment // not a comment ... still inside string';
 var e = "inside string 'still inside same string ... escaped \\ \" \" \0 \a \n \r \t still inside string /* not a comment // not a comment ... still inside string";
+
+// blanks follows
+
+
+		
+	    
+
+if(cond) { // indented comment
+	// indented comment
+	var f = 0;
+	// indented comment, followed by blank lines
+	
+
+	if(cond2)
+		/* idented comment */ var g = 0;
+}
