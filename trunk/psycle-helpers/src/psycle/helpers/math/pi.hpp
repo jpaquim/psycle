95955564839ff259cd8c83b3ae66fe39a15b76f2
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-07-13 15:55:22 +0000

    misc

--- trunk/psycle-helpers/src/psycle/helpers/math/pi.hpp
+++ trunk/psycle-helpers/src/psycle/helpers/math/pi.hpp
@@ -1,24 +1,21 @@
 // -*- mode:c++; indent-tabs-mode:t -*-
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2004-2008 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
 #pragma once
 #include <cmath>
-namespace psycle
-{
-	namespace helpers
-	{
-		namespace math
-		{
-			/// the pi constant as a 64-bit floating point number
-			double const pi
-			(
-				#if defined M_PI
-					M_PI
-				#else
-					3.14159265358979323846264338327950288
-				#endif
-			);
+namespace psycle { namespace helpers { namespace math {
 
-			/// the pi constant as a 32-bit floating point number
-			float const pi_f = float(pi);
-		}
-	}
-}
+/// the pi constant as a 64-bit floating point number
+double const pi(
+	#if defined M_PI
+		M_PI
+	#else
+		3.14159265358979323846264338327950288
+	#endif
+);
+
+/// the pi constant as a 32-bit floating point number
+float const pi_f(float(pi));
+
+}}}
