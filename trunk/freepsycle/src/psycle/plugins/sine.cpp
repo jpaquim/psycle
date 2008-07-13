81583a4dd3614549339d91369a23ef37a9c9765f
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-07-13 17:07:14 +0000

    hmmwell...

--- trunk/freepsycle/src/psycle/plugins/sine.cpp
+++ trunk/freepsycle/src/psycle/plugins/sine.cpp
@@ -80,11 +80,11 @@ void sine::do_process_template() throw(engine::exception) {
 			case channel::flags::empty: default: /* nothing */ ;
 		}
 
+		phase_ = std::fmod(phase_ + engine::math::pi, 2 * engine::math::pi) - engine::math::pi;
 		out_channel()[out_event](out_event, amplitude_ * helpers::math::fast_sin<2>(phase_));
 		phase_ += step_;
 	}
 	out_channel().flag(channel::flags::continuous);
-	phase_ = std::fmod(phase_, 2 * engine::math::pi);
 }
 
 }}
