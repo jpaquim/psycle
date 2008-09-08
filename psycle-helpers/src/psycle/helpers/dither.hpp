3616f99e84e108cf4f6656563d2264be057964ec
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-09-08 23:09:50 +0000

    misc 64-bit issues

--- psycle-helpers/src/psycle/helpers/dither.hpp
+++ psycle-helpers/src/psycle/helpers/dither.hpp
@@ -2,35 +2,35 @@
 ///\interface psycle::helpers::dsp::Dither
 #include <psycle/helpers/mersennetwister.hpp>
 namespace psycle { namespace helpers { namespace dsp {
-	class Dither
-	{
-		public:
-			Dither();
-			virtual ~Dither() {}
 
-			void Process(float* inSamps, int length);
+class Dither {
+	public:
+		Dither();
+		virtual ~Dither() {}
 
-			enum Pdf
-			{
-				triangular = 0,
-				rectangular,
-				gaussian
-			};
-			enum NoiseShape
-			{
-				none = 0,
-				highpass
-			};
+		void Process(float * inSamps, unsigned int length);
 
-			void SetBitDepth(int newdepth) { bitdepth = newdepth; }
-			void SetPdf(Pdf newpdf) { pdf = newpdf; }
-			void SetNoiseShaping(NoiseShape newns) { noiseshape = newns; }
+		enum Pdf {
+			triangular = 0,
+			rectangular,
+			gaussian
+		};
+		
+		enum NoiseShape {
+			none = 0,
+			highpass
+		};
 
-		private:
-			int bitdepth;
-			Pdf pdf;
-			NoiseShape noiseshape;
+		void SetBitDepth(unsigned int newdepth) { bitdepth = newdepth; }
+		void SetPdf(Pdf newpdf) { pdf = newpdf; }
+		void SetNoiseShaping(NoiseShape newns) { noiseshape = newns; }
 
-			MersenneTwister mt;
-	};
+	private:
+		unsigned int bitdepth;
+		Pdf pdf;
+		NoiseShape noiseshape;
+
+		MersenneTwister mt;
+};
+
 }}}
