d7ab364ded9283f4b925d7ab793633f06a8abf11
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-09-09 20:18:21 +0000

    routine code review

--- universalis/src/universalis/standard_library/detail/allocators.hpp
+++ universalis/src/universalis/standard_library/detail/allocators.hpp
@@ -1,6 +1,6 @@
 // -*- mode:c++; indent-tabs-mode:t -*-
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2004-2007 psycle development team http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+// copyright 2004-2008 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
 ///\file
 #pragma once
