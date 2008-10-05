e1e31b939308f822d067617dd3fb18b0377de75a
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-05 11:09:32 +0000

    misc

--- qpsycle/src/gui/configuration.hpp
+++ qpsycle/src/gui/configuration.hpp
@@ -16,8 +16,7 @@ using psy::core::AudioDriver;
 
 namespace qpsycle {
 
-	enum KnobMode
-	{
+	enum KnobMode {
 		QDialMode, ///< Built-in QDial behaviour
 		QSynthAngularMode, ///< Knob moves angularly as the mouse around the widget center
 		PsycleLinearMode,
@@ -26,53 +25,55 @@ namespace qpsycle {
 
 
 
-/// configuration for the user interface
+	/// configuration for the user interface
 	class Configuration {
-	public:
-		Configuration();
+		public:
+			Configuration();
 	
 		///\name configuration file
 		///\{
-	public:
-		void loadConfig();
-		void loadConfig( const std::string & path );
+			public:
+				void loadConfig();
+				void loadConfig( const std::string & path );
+		///\}
 	
 		///\name input handler
 		///\{
-	public:
-		InputHandler const & inputHandler() const { return inputHandler_; }
-	private:
-		InputHandler inputHandler_;
-		void configureKeyBindings();
+			public:
+				InputHandler const & inputHandler() const { return inputHandler_; }
+			private:
+				InputHandler inputHandler_;
+				void configureKeyBindings();
 		///\}
 
 		///\name paths
-	private:
-		std::string configFilePath_;
+		///\{
+			private:
+				std::string configFilePath_;
+		///\}
 
 		///\name audio drivers
 		///\{
-	public:
-		void setDriverByName( const std::string & driverName );        
-		std::map<std::string, AudioDriver*> & driverMap() { return driverMap_; }
-		bool enableSound() const { return enableSound_; }
-	public://private: ///\ todo private access
-		AudioDriver* _pOutputDriver;
-		AudioDriver* _pSilentDriver;
-	private:
-		std::map<std::string, AudioDriver*> driverMap_;
-		bool enableSound_;
-		bool doEnableSound;
-		void addAudioDriver(AudioDriver* driver);
+			public:
+				void setDriverByName( const std::string & driverName );        
+				std::map<std::string, AudioDriver*> & driverMap() { return driverMap_; }
+				bool enableSound() const { return enableSound_; }
+			public://private: ///\ todo private access
+				AudioDriver* _pOutputDriver;
+				AudioDriver* _pSilentDriver;
+			private:
+				std::map<std::string, AudioDriver*> driverMap_;
+				bool enableSound_;
+				bool doEnableSound;
+				void addAudioDriver(AudioDriver* driver);
 		///\}
 
-	public://private: ///\ todo private access
-		///\ todo put this in player ..
-		bool _RecordTweaks;
-		///\ todo put this in player ..
-		bool _RecordUnarmed;
+		public://private: ///\ todo private access
+			///\ todo put this in player ..
+			bool _RecordTweaks;
+			///\ todo put this in player ..
+			bool _RecordUnarmed;
 	};
-
 }
 
 #endif
