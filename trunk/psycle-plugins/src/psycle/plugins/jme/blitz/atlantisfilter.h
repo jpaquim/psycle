7dce2fbfcbb4202c2dabd61a94870fc0b7bf5629
jm-edelmann <jm-edelmann@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-11 22:33:20 +0000

    Added GPL Header

--- trunk/psycle-plugins/src/psycle/plugins/jme/blitz/atlantisfilter.h
+++ trunk/psycle-plugins/src/psycle/plugins/jme/blitz/atlantisfilter.h
@@ -1,5 +1,19 @@
-// CSIDFilter by Jeremy Evers - ripped from Atlantis which is available at 
-// http:://jeremyevers.com
+/*		CSIDFilter (C)2008 Jeremy Evers, http:://jeremyevers.com
+
+		This program is free software; you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation; either version 2 of the License, or
+		(at your option) any later version.\n"\
+
+		This plugin is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program; if not, write to the Free Software
+		Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #define PIf	3.1415926535897932384626433832795f;
 
