8f1f3a3e0fa5c63ffb1711f2f9d41228f00e7ef3
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-12 18:07:28 +0000

    testing email notification

--- freepsycle/src/psycle/generic/typenames.hpp
+++ freepsycle/src/psycle/generic/typenames.hpp
@@ -1,3 +1,4 @@
+xxxxxxxxxxxxxxxxxxxxxxx
 // -*- mode:c++; indent-tabs-mode:t -*-
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 1999-2008 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
