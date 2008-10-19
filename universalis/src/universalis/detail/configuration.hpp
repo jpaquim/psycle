e9caef03329bcde688e36baad8f2339a4887b372
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-19 12:50:06 +0000

    let's try that

--- universalis/src/universalis/detail/configuration.hpp
+++ universalis/src/universalis/detail/configuration.hpp
@@ -1,4 +1,7 @@
 // -*- mode:c++; indent-tabs-mode:t -*-
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 1999-2008 psycledelics http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
+
 #pragma once
 // This is just an empty file to avoid build failures for people having their own build system.
 // The sources are generally able to build without specifying anything in this file,
