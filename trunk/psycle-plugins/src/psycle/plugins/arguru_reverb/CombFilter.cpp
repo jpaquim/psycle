018b6d469ac27e2678d8c9f264cf34c700eb1633
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-19 18:16:30 +0000

    buildfix

--- trunk/psycle-plugins/src/psycle/plugins/arguru_reverb/CombFilter.cpp
+++ trunk/psycle-plugins/src/psycle/plugins/arguru_reverb/CombFilter.cpp
@@ -11,16 +11,16 @@
 
 CCombFilter::CCombFilter()
 {
-#if defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__MICROSOFT
+#if defined DIVERSALIS__PROCESSOR__X86 &&  defined DIVERSALIS__OPERATING_SYSTEM__POSIX
+	posix_memalign(reinterpret_cast<void**>(&leftBuffer),16,MAX_COMB_DELAY*sizeof(float));
+	posix_memalign(reinterpret_cast<void**>(&rightBuffer),16,MAX_COMB_DELAY*sizeof(float));
+#elif defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__MICROSOFT
 	leftBuffer = static_cast<float*>(_aligned_malloc(MAX_COMB_DELAY*sizeof(float),16));
 	rightBuffer = static_cast<float*>(_aligned_malloc(MAX_COMB_DELAY*sizeof(float),16));
 #elif defined DIVERSALIS__PROCESSOR__X86 && defined DIVERSALIS__COMPILER__GNU
 	///\todo mingw
 	leftBuffer= new float[MAX_COMB_DELAY];
 	rightBuffer = new float[MAX_COMB_DELAY];
-#elif defined DIVERSALIS__PROCESSOR__X86 &&  defined DIVERSALIS__OPERATING_SYSTEM__POSIX
-	posix_memalign(reinterpret_cast<void**>(&leftBuffer),16,MAX_COMB_DELAY*sizeof(float));
-	posix_memalign(reinterpret_cast<void**>(&rightBuffer),16,MAX_COMB_DELAY*sizeof(float));
 #else
 	leftBuffer= new float[MAX_COMB_DELAY];
 	rightBuffer = new float[MAX_COMB_DELAY];
