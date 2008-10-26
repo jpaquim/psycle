4931e48c23209cc0906dfac58525e37adcf11ca2
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-26 13:45:53 +0000

    cas measurement

--- tests/atomic/compare_and_swap_test.cpp
+++ tests/atomic/compare_and_swap_test.cpp
@@ -1,48 +1,62 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2007 johan boule <bohan@jabber.org>
-// copyright 2004-2007 psycledelics http://psycle.pastnotecut.org
+// copyright 2006-2008 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
-// This is a unit test for universalis::processor::atomic::compare_and_swap
-#if defined UNIVERSALIS__PROCESSOR__ATOMIC__COMPARE_AND_SWAP__UNIT_TEST
-	#include <pthread.h>
-	#include <sched.h>
-	//#include "compare_and_swap.hpp"
-	template<typename Value>
-	bool inline compare_and_swap(Value * const address, Value const old_value, Value const new_value)
-	{
-		return __sync_bool_compare_and_swap(address, old_value, new_value);
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <iostream>
+
+template<typename Value>
+bool inline compare_and_swap(Value * const address, Value const old_value, Value const new_value) {
+	return __sync_bool_compare_and_swap(address, old_value, new_value);
+}
+
+
+int const threads = 4;
+int const iterations = 10000000;
+int results[threads][iterations];
+int shared_counter(0);
+
+void loop(int thread, int flip, int flop) {
+	{ // dummy loop to make sure all threads have had time to start
+		int i(300000000);
+		while(--i);
+		results[threads][0] = i;
 	}
-	#include <boost/thread.hpp>
-	#include <iostream>
-	int shared_counter(0);
-	int * shared_counter_pointer(&shared_counter);
-	void f1()
-	{
-		int local_counter(0);
-		for(int i(0); i < 10000; ++i)
-		{
-			retry:
-			bool b(compare_and_swap(shared_counter_pointer, 0, 1));
-			if(!b) { sched_yield(); goto retry; }
-			std::cout << "thread id: " << pthread_self() << ", b: " << b << ", local counter: " << local_counter << "\n";
+	for(int i(0); i < iterations; ++i) {
+		retry:
+		bool b(compare_and_swap(&shared_counter, flip, flop));
+		//if(!b) goto retry;
+		results[thread][i] = b;//shared_counter;
+	}
+}
+
+int main() {
+	boost::thread thread1(boost::bind(&loop, 0, 0, 1));
+	boost::thread thread2(boost::bind(&loop, 1, 1, 2));
+	boost::thread thread3(boost::bind(&loop, 2, 2, 3));
+	boost::thread thread4(boost::bind(&loop, 3, 3, 0));
+	thread1.join();
+	thread2.join();
+	thread3.join();
+	thread4.join();
+	for(int t(0); t < threads; ++t) {
+		int prev_r(~results[t][0]), prev_i(0), diffs(0), diff_tot(0), max_diff(0);
+		for(int i(0); i < iterations; ++i) {
+			int const r(results[t][i]);
+			if(prev_r != r) {
+				++diffs;
+				int const diff = i - prev_i;
+				diff_tot += diff;
+				prev_i = i;
+				prev_r = r;
+				if(diff > max_diff) max_diff = diff;
+			}
 		}
+		std::cout
+			<< "thread " << t
+			<< "\tdiffs " << diffs
+			<< "\tmax diff " << max_diff
+			<< "\tdiff avg " << double(diff_tot) / diffs
+			<< '\n';
 	}
-	void f2()
-	{
-		int local_counter(0);
-		for(int i(0); i < 10000; ++i)
-		{
-			retry:
-			bool b(compare_and_swap(shared_counter_pointer, 1, 0));
-			if(!b) { /*sched_yield();*/ goto retry; }
-			std::cout << "thread id: " << pthread_self() << ", b: " << b << ", local counter: " << local_counter << "\n";
-		}
-	}
-	int main()
-	{
-		boost::thread thread1(&f1);
-		boost::thread thread2(&f2);
-		thread1.join();
-		thread2.join();
-	}
-#endif
+}
