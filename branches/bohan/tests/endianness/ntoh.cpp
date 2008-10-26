e7dce1692f59b244649d3d5072d3ae6a9cd803fb
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-26 22:46:45 +0000

    endianness

--- branches/bohan/tests/endianness/ntoh.cpp
+++ branches/bohan/tests/endianness/ntoh.cpp
@@ -3,32 +3,31 @@
 #include <iostream>
 
 int main() {
+	std::cout << std::hex;
 	{
 		uint32_t i(0x11223344);
+		std::cout << "logical value: " << i;
+
 		uint8_t c[4];
 		std::memcpy(c, &i, sizeof c);
 	
-		std::cout << std::hex;
 
-		std::cout << "physical layout: ";
+		std::cout << ", physical layout: ";
 		for(int i = 0; i < sizeof c; ++i) std::cout << (unsigned int)(c[i]);
-		std::cout << ", ";
 	
 		// reverse the byte order if it's a little endian machine
 		// this actually does nothing on a big endian machine.
 		i = c[3] << 24 | c[2] << 16 | c[1] << 8 | c[0];
-		std::cout << "ntoh: " << i << '\n';
+		std::cout << ", hton: " << i << '\n';
 	}
 	{
 		uint64_t i(0x1122334455667788);
+		std::cout << "logical value: " << i;
+
 		uint8_t c[8];
 		std::memcpy(c, &i, sizeof c);
-	
-		std::cout << std::hex;
-
-		std::cout << "physical layout: ";
+		std::cout << ", physical layout: ";
 		for(int i = 0; i < sizeof c; ++i) std::cout << (unsigned int)(c[i]);
-		std::cout << ", ";
 	
 		// reverse the byte order if it's a little endian machine
 		// this actually does nothing on a big endian machine.
@@ -41,6 +40,6 @@ int main() {
 			c[2] << 16 |
 			c[1] << 8 |
 			c[0];
-		std::cout << "ntoh: " << i << '\n';
+		std::cout << ", hton: " << i << '\n';
 	}
 }
