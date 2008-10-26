1dc864e7ef25d91db10d1690f4f10caca693ff15
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-26 19:23:17 +0000

    portable cas

--- trunk/universalis/src/universalis/processor/atomic/compare_and_swap.hpp
+++ trunk/universalis/src/universalis/processor/atomic/compare_and_swap.hpp
@@ -4,31 +4,32 @@
 
 ///\interface universalis::processor::atomic::compare_and_swap
 #pragma once
-#include <universalis/operating_system/exception.hpp>
 #include <diversalis/compiler.hpp>
 #if defined DIVERSALIS__COMPILER__GNU && DIVERSALIS__COMPILER__VERSION >= 40100 // 4.1.0
-	// gcc built-in functions documented at http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
+	// gcc's __sync_* built-in functions documented at http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
 #else
 	#include <glib/gatomic.h>
 #endif
 namespace universalis { namespace processor { namespace atomic {
 
-// see http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2006/n2047.html
-// see http://tech.groups.yahoo.com/group/liblf-dev
-// see http://www.audiomulch.com/~rossb/code/lockfree/liblfds
-// see http://portaudio.com/trac/browser/portaudio/trunk/src/common/pa_memorybarrier.h
-// see http://webcvs.freedesktop.org/gstreamer/gst-plugins-base/gst-libs/gst/audio/gstringbuffer.c?view=markup
-// see http://svn.gnome.org/viewvc/glib/trunk/glib/gatomic.c?view=markup
+// see c++ standard http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2006/n2047.html
+// see glibc */bits/atomic.h at http://sources.redhat.com/cgi-bin/cvsweb.cgi/libc/sysdeps/?cvsroot=glibc
+// see glib atomic http://svn.gnome.org/viewvc/glib/trunk/glib/gatomic.c?view=markup
+// see glib/gstreamer ring buffer http://webcvs.freedesktop.org/gstreamer/gst-plugins-base/gst-libs/gst/audio/gstringbuffer.c?view=markup
+// see portaudio memory barrier http://portaudio.com/trac/browser/portaudio/trunk/src/common/pa_memorybarrier.h
+// see portaudio ring buffer    http://portaudio.com/trac/browser/portaudio/trunk/src/common/pa_ringbuffer.c
+// see liblf data structures http://www.audiomulch.com/~rossb/code/lockfree/liblfds
+// see liblf data structures http://tech.groups.yahoo.com/group/liblf-dev
+// see microsoft/intel _Interlocked* intrinsics
 
-/// atomic compare and swap.
-/// with full memory barrier.
+/// atomic compare and swap with full memory barrier.
 /// does not fail spuriously.
 template<typename Value>
 bool inline compare_and_swap(Value * const address, Value const old_value, Value const new_value) {
 	#if defined DIVERSALIS__COMPILER__GNU && DIVERSALIS__COMPILER__VERSION >= 40100 // 4.1.0
 		return __sync_bool_compare_and_swap(address, old_value, new_value);
 	#else
-		// no return statement, on purpose, to use the sfinae rule
+		// no return statement to error on purpose -- see template specialisations below
 	#endif
 }
 
@@ -42,30 +43,6 @@ bool inline compare_and_swap(Value * const address, Value const old_value, Value
 	bool inline compare_and_swap< ::gint>(::gint * const address, ::gint const old_value, ::gint const new_value) {
 		return ::g_atomic_int_compare_and_exchange(address, old_value, new_value);
 	}
-#endif
-
-#if 0
-	// other operations
-	
-	// gcc
-	// "..." optional list of values to protect (currently unused)
-	
-	type __sync_val_compare_and_swap (type * p, type oldval, type newval, ...);
-	// returns the content of *ptr before the operation
-	
-	void __sync_synchronize();
-	// full memory barrier
-	
-	with op in (add, sub, or, and, xor, nand):
-		type __sync_fetch_and_<op>(type * p, type value, ...);
-		type __sync_<op>_and_fetch(type * p, type value, ...);
-	
-	type __sync_lock_test_and_set (type * p, type value, ...);
-	// not a full memory barrier
-	
-	void __sync_lock_release(type * p, ...);
-	// not a full memory barrier
-		
 #endif
 
 }}}
