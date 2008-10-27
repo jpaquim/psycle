69e66a4425da44cad88b2194f6983bfdd6d690e0
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-10-27 00:26:05 +0000

    endianness

--- branches/bohan/tests/endianness/hton.cpp
+++ branches/bohan/tests/endianness/hton.cpp
@@ -7,49 +7,51 @@ int main() {
 	{
 		std::cout << "32-bit\n";
 		
-		uint32_t i(0x44332211);
-		std::cout << "\t       logical value: 0x" << i;
+		union u {
+			uint32_t i;
+			uint8_t c[sizeof(uint32_t)];
+		} x;
+		x.i = 0x44332211;
+		std::cout << "\t       logical value: 0x" << x.i;
 
-		uint8_t c[4];
-		std::memcpy(c, &i, sizeof c);
 		std::cout << "\n\t     physical layout: 0x";
-		for(int i = 0; i < sizeof c; ++i) std::cout << (unsigned int)(c[i]);
+		for(int i = 0; i < sizeof x.c; ++i) std::cout << (unsigned int)(x.c[i]);
 	
 		// reverse the byte order if it's a little endian machine
 		// this actually does nothing on a big endian machine.
-		i = c[0] << 24 | c[1] << 16 | c[2] << 8 | c[3];
+		x.i = x.c[0] << 24 | x.c[1] << 16 | x.c[2] << 8 | x.c[3];
 
 		std::cout << "\n\thton physical layout: 0x";
-		std::memcpy(c, &i, sizeof c);
-		for(int i = 0; i < sizeof c; ++i) std::cout << (unsigned int)(c[i]);
+		for(int i = 0; i < sizeof x.c; ++i) std::cout << (unsigned int)(x.c[i]);
 		std::cout << '\n';
 	}
 	{
 		std::cout << "64-bit\n";
 
-		uint64_t i(0x8877665544332211ULL);
-		std::cout << "\t       logical value: 0x" << i;
+		union u {
+			uint64_t i;
+			uint8_t c[sizeof(uint64_t)];
+		} x;
+		x.i = 0x8877665544332211ULL;
+		std::cout << "\t       logical value: 0x" << x.i;
 
-		uint8_t c[8];
-		std::memcpy(c, &i, sizeof c);
 		std::cout << "\n\t     physical layout: 0x";
-		for(int i = 0; i < sizeof c; ++i) std::cout << (unsigned int)(c[i]);
+		for(int i = 0; i < sizeof x.c; ++i) std::cout << (unsigned int)(x.c[i]);
 	
 		// reverse the byte order if it's a little endian machine
 		// this actually does nothing on a big endian machine.
-		i =
-			uint64_t(c[0]) << 56 |
-			uint64_t(c[1]) << 48 |
-			uint64_t(c[2]) << 40 |
-			uint64_t(c[3]) << 32 |
-			c[4] << 24 |
-			c[5] << 16 |
-			c[6] << 8 |
-			c[7];
+		x.i =
+			uint64_t(x.c[0]) << 56 |
+			uint64_t(x.c[1]) << 48 |
+			uint64_t(x.c[2]) << 40 |
+			uint64_t(x.c[3]) << 32 |
+			x.c[4] << 24 |
+			x.c[5] << 16 |
+			x.c[6] << 8 |
+			x.c[7];
 
 		std::cout << "\n\thton physical layout: 0x";
-		std::memcpy(c, &i, sizeof c);
-		for(int i = 0; i < sizeof c; ++i) std::cout << (unsigned int)(c[i]);
+		for(int i = 0; i < sizeof x.c; ++i) std::cout << (unsigned int)(x.c[i]);
 		std::cout << '\n';
 	}
 }
