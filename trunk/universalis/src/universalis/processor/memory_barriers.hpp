25a6a7ba5c917cb2a927704881f082667fefc34b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-11-02 18:25:42 +0000

    use compare_and_swap as a fallback in case we don't have explicit memory barriers

--- trunk/universalis/src/universalis/processor/memory_barriers.hpp
+++ trunk/universalis/src/universalis/processor/memory_barriers.hpp
@@ -4,7 +4,8 @@
 // copyright 2008-2008 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 //
 // This source code is based on the pa_memorybarrier.h header of PortAudio (Portable Audio I/O Library)
-// For more information on PortAudio, see: http://www.portaudio.com
+// See portaudio memory barrier http://portaudio.com/trac/browser/portaudio/trunk/src/common/pa_memorybarrier.h
+// For more information on PortAudio, see http://www.portaudio.com
 //
 // The PortAudio community makes the following non-binding requests:
 // Any person wishing to distribute modifications to the Software
@@ -17,65 +18,77 @@
 /// In addition to providing memory barriers, these functions should ensure that data cached in registers
 /// is written out to cache where it can be snooped by other CPUs. (ie, the volatile keyword should not be required).
 ///
-/// the primitives that defined here are:
-///
+/// The primitives defined here are:
 /// universalis::processor::memory_barriers::full()
 /// universalis::processor::memory_barriers::read()
 /// universalis::processor::memory_barriers::write()
 
 #include <diversalis/diversalis.hpp>
+
 #if defined DIVERSALIS__OPERATING_SYSTEM__APPLE
 	#include <libkern/OSAtomic.h>
-#elif defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION >= 1400
-	#include <intrin.h>
-#endif
-
-namespace universalis { namespace processor { namespace memory_barriers {
-	#if defined DIVERSALIS__OPERATING_SYSTEM__APPLE
-		// Mac OS X only provides full memory barriers, so the three types of barriers are the same,
-		// However, these barriers are superior to compiler-based ones.
+	// Mac OS X only provides full memory barriers, so the three types of barriers are the same,
+	// However, these barriers are superior to compiler-based ones.
+	namespace universalis { namespace processor { namespace memory_barriers {
 		void inline  full() { OSMemoryBarrier(); }
 		void inline  read() { full(); }
 		void inline write() { full(); }
-	#elif defined DIVERSALIS__COMPILER__GNU
-		#if DIVERSALIS__COMPILER__VERSION >= 40100 // 4.1.0
-			// GCC >= 4.1 has built-in intrinsics. We'll use those.
+	}}}
+#elif defined DIVERSALIS__COMPILER__GNU
+	#if DIVERSALIS__COMPILER__VERSION >= 40100 // 4.1.0
+		// GCC >= 4.1 has built-in intrinsics. We'll use those.
+		namespace universalis { namespace processor { namespace memory_barriers {
 			void inline  full() { __sync_synchronize(); }
 			void inline  read() { full(); }
 			void inline write() { full(); }
-		#else
-			// As a fallback, GCC understands volatile asm and "memory"
-			// to mean it should not reorder memory read/writes.
-			#if defined DIVERSALIS__PROCESSOR__POWER_PC
+		}}}
+	#else
+		// As a fallback, GCC understands volatile asm and "memory"
+		// to mean it should not reorder memory read/writes.
+		#if defined DIVERSALIS__PROCESSOR__POWER_PC
+			namespace universalis { namespace processor { namespace memory_barriers {
 				void inline  full() { asm volatile("sync":::"memory"); }
 				void inline  read() { full(); }
 				void inline write() { full(); }
-			#elif defined DIVERSALIS__PROCESSOR__X86 // defined __i386__ || defined __i486__ || defined __i586__ || defined __i686__ || defined __x86_64__
+			}}}
+		#elif defined DIVERSALIS__PROCESSOR__X86
+			namespace universalis { namespace processor { namespace memory_barriers {
 				void inline  full() { asm volatile("mfence":::"memory"); }
 				void inline  read() { asm volatile("lfence":::"memory"); }
 				void inline write() { asm volatile("sfence":::"memory"); }
-			#else
-				#warning Memory barriers are not defined on this system. SMP safety will not be guaranteed.
-				void inline  full() {}
-				void inline  read() {}
-				void inline write() {}
-			#endif
+			}}}
+		#else
+			#warning Memory barriers are not defined on this system. Will use compare_and_swap to emulate.
+			#include <universalis/processor/atomic/compare_and_swap.hpp>
+			namespace universalis { namespace processor { namespace memory_barriers {
+				void inline  full() { static int dummy_; compare_and_swap(&dummy_, 0, 1); }
+				void inline  read() { full(); }
+				void inline write() { full(); }
+			}}}
 		#endif
-	#elif defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION >= 1400
-		#pragma intrinsic(_ReadWriteBarrier)
-		#pragma intrinsic(_ReadBarrier)
-		#pragma intrinsic(_WriteBarrier)
+	#endif
+#elif defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION >= 1400
+	#include <intrin.h>
+	#pragma intrinsic(_ReadWriteBarrier)
+	#pragma intrinsic(_ReadBarrier)
+	#pragma intrinsic(_WriteBarrier)
+	namespace universalis { namespace processor { namespace memory_barriers {
 		void inline  full() { _ReadWriteBarrier(); }
 		void inline  read() { _ReadBarrier(); }
 		void inline write() { _WriteBarrier(); }
-	#elif defined DIVERSALIS__COMPILER__MICROSOFT || defined DIVERSALIS__COMPILER__BORLAND
+	}}}
+#elif defined DIVERSALIS__COMPILER__MICROSOFT || defined DIVERSALIS__COMPILER__BORLAND
+	namespace universalis { namespace processor { namespace memory_barriers {
 		void inline  full() { _asm { lock add [esp], 0}; }
 		void inline  read() { full(); }
 		void inline write() { full(); }
-	#else
-		#warning Memory barriers are not defined on this system. SMP safety will not be guaranteed.
-		void inline  full() {}
+	}}}
+#else
+	#warning Memory barriers are not defined on this system. Will use compare_and_swap to emulate.
+	#include <universalis/processor/atomic/compare_and_swap.hpp>
+	namespace universalis { namespace processor { namespace memory_barriers {
+		void inline  full() { static int dummy_; compare_and_swap(&dummy_, 0, 1); }
 		void inline  read() {}
 		void inline write() {}
-	#endif
-}}}
+	}}}
+#endif
