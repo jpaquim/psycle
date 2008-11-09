ad644329bb0bdbbe20e2cf90253f9ca3b40e7487
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-11-09 23:25:11 +0000

    fixed a couple of bugs

--- trunk/psycle-plugins/src/psycle/plugins/alk_muter/alk_muter.cpp
+++ trunk/psycle-plugins/src/psycle/plugins/alk_muter/alk_muter.cpp
@@ -26,7 +26,7 @@ CMachineInfo const static machine_info = {
 	sizeof parameters / sizeof *parameters, // number of parameters
 	parameters, // pointer to parameters
 
-	"Alk Muter" // name
+	"Alk Muter 1.1" // name
 	#ifndef NDEBUG
 		" (debug build)"
 	#endif
@@ -48,11 +48,14 @@ class machine : public CMachineInterface {
 		bool DescribeValue(char* text, int const parameter, int const value);
 		void Command();
 		void ParameterTweak(int parameter, int value);
+private:
+	float volume;
+	bool change;
 };
 
 PSYCLE__PLUGIN__INSTANTIATOR(machine, machine_info)
 
-machine::machine() {
+machine::machine():volume(1.0f), change(false) {
 	Vals = new int[sizeof parameters];
 }
 
@@ -69,18 +72,56 @@ void machine::SequencerTick() {
 
 void machine::Command() {
 	// called when user presses editor button
-	char text[] = "Made by Alkz0r\0";
+	char text[] = "Made by alk\nThis machine is an alternative way to mute the audio of a path.\0";
 	char caption[] = "Alk's Muter\0";
 	pCB->MessBox(text, caption, 0);
 }
 
 void machine::ParameterTweak(int parameter, int value) {
-	Vals[parameter] = value;
+	switch(parameter) {
+		case 0:
+			if (value != Vals[0]) {
+				change=true;
+			}
+			Vals[0] = value;
+			break;
+		default:
+			break;
+	}
 }
 
 void machine::Work(float * left_samples, float * right_samples, int sample_count, int /*tracks*/) {
-	if(!Vals[0]) return;
-	while(sample_count--) *left_samples++ = *right_samples++ = 0;
+	try {
+	if (!change) {
+		if(!Vals[0]) return;
+		if (sample_count < 0 ) {
+			int i = 1;
+		}
+		while(sample_count--) *left_samples++ = *right_samples++ = 0;
+	} else if (!Vals[0]){ // mute disabled
+		while(volume<0.99f && sample_count--) {
+			(*left_samples)*=volume; (*right_samples)*=volume;
+			left_samples++; right_samples++;
+			volume+=0.01f;
+		}
+		if (volume>=0.99f) {
+			change=false;
+		}
+	} else if (Vals[0] == 1){ // mute enabled
+		while(volume>0.01f && sample_count--) {
+			(*left_samples)*=volume; (*right_samples)*=volume;
+			left_samples++; right_samples++;
+			volume-=0.01f;
+		}
+		sample_count++;
+		while(sample_count--) *left_samples++ = *right_samples++ = 0;
+		if (volume<=0.01f) {
+			change=false;
+		}
+	}
+	}catch(std::exception e) {
+		int i =1;
+	}
 }
 
 bool machine::DescribeValue(char * text, int const parameter, int const value) {
