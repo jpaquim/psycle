4b4f76bc74eedfdc8b807cb0535b43910762fbb5
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2008-11-10 01:35:49 +0000

    #include

--- psycle-helpers/src/psycle/helpers/riff.cpp
+++ psycle-helpers/src/psycle/helpers/riff.cpp
@@ -7,7 +7,7 @@
 	You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **********************************************************************************************/
 
-#include "riff.h"
+#include "riff.hpp"
 namespace psycle { namespace host {
 
 std::uint32_t FourCC(const char * ChunkName) {
