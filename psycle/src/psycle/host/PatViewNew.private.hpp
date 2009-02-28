594748e6cb9388ac42c8f16f05aba855af74c6da
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-02-28 14:20:52 +0000

    del PatViewNew.private.hpp

--- psycle/src/psycle/host/PatViewNew.private.hpp
+++
@@ -1,8 +0,0 @@
-///\file
-///\brief pattern view graphic operations for psycle::host::CChildView, private header
-PSYCLE__MFC__NAMESPACE__BEGIN(psycle)
-	PSYCLE__MFC__NAMESPACE__BEGIN(host)
-
-
-	PSYCLE__MFC__NAMESPACE__END
-PSYCLE__MFC__NAMESPACE__END
