67709fbf50bd4c91f706f08f6e5f84021dd1e1ec
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-03-15 22:28:21 +0000

    misc

--- universalis/src/universalis/standard_library/condition.hpp
+++ universalis/src/universalis/standard_library/condition.hpp
@@ -1,15 +1,11 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2007-2008 members of the psycle project http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
+// copyright 2007-2009 members of the psycle project http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
 ///\file \brief condition standard header
 /// This file implements the C++ standards proposal at http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2007/n2320.html
 #pragma once
 #include "detail/boost_xtime.hpp"
-#if BOOST_VERSION >= 103500
-	#include <boost/thread/condition_variable.hpp>
-#else
-	#include <boost/thread/condition.hpp>
-#endif
+#include <boost/thread/condition.hpp> // or since version 1.35 <boost/thread/condition_variable.hpp>
 #include <date_time>
 namespace std {
 	typedef boost::lock_error lock_error;
@@ -253,3 +249,4 @@ namespace std {
 		}
 	}}}}
 #endif
+
