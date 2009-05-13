4db520f01119c724215e9b4a2b0ae7475201826c
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-05-13 18:43:36 +0000

    policy

--- trunk/universalis/src/universalis/compiler/detail/pragmatic/demangle.cpp
+++ trunk/universalis/src/universalis/compiler/detail/pragmatic/demangle.cpp
@@ -9,26 +9,20 @@
 #include "demangle.hpp"
 #if defined DIVERSALIS__COMPILER__GNU
 	#include <cxxabi.h>
-    #include <cstdlib>
+	#include <cstdlib>
 #endif
-namespace universalis
-{
-	namespace compiler
-	{
-		namespace detail
-		{
-			std::string demangled(std::string const & mangled_symbol)
-			{
-				#if defined DIVERSALIS__COMPILER__GNU
-					int status;
-					char * c(abi::__cxa_demangle(mangled_symbol.c_str(), 0, 0, &status));
-					std::string s(c);
-					std::free(c);
-					return s;
-				#else
-					return mangled_symbol;
-				#endif
-			}
-		}
-	}
+namespace universalis { namespace compiler { namespace detail {
+
+std::string demangled(std::string const & mangled_symbol) {
+	#if defined DIVERSALIS__COMPILER__GNU
+		int status;
+		char * c(abi::__cxa_demangle(mangled_symbol.c_str(), 0, 0, &status));
+		std::string s(c);
+		std::free(c);
+		return s;
+	#else
+		return mangled_symbol;
+	#endif
 }
+
+}}}
