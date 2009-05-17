10d61804aa7c9c368bd352e7977c65f017006712
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-05-17 15:06:21 +0000

    big reorg

--- universalis/src/universalis/compiler/location.hpp
+++ universalis/src/universalis/compiler/location.hpp
@@ -20,7 +20,7 @@
 	#include <boost/current_function.hpp>
 #endif
 #if defined PACKAGENERIC
-	#include <packageneric-module.private.hpp>
+	#include <packageneric.private.hpp>
 #endif
 
 namespace universalis { namespace compiler {
