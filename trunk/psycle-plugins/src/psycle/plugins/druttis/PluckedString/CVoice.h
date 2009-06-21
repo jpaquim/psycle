fecae62112aa54a0c2dfc1f846f5d3bfac612878
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-06-21 22:21:39 +0000

    cleaning up druttis libs

--- trunk/psycle-plugins/src/psycle/plugins/druttis/PluckedString/CVoice.h
+++ trunk/psycle-plugins/src/psycle/plugins/druttis/PluckedString/CVoice.h
@@ -6,10 +6,10 @@
 //
 //============================================================================
 #pragma once
-#include "../Lib/AMath.h"
-#include "../Lib/BiQuad.h"
-#include "../Lib/DLineN.h"
-#include "../Lib/CEnvelope.h"
+#include "AMath.h"
+#include "CEnvelope.h"
+#include "DLineN.h"
+#include "BiQuad.h"
 
 #define EXCITATION_FILTER_LENGTH 3
 
@@ -74,8 +74,6 @@ public:
 	//				Methods
 	//------------------------------------------------------------------------
 public:
-	CVoice();
-	~CVoice();
 	void Init();
 	void Clear();
 	void SetFreq(float frequency);
