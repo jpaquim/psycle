fadf100ad057f25b12493dcde665b8f72a32d74b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-07-01 23:25:32 +0000

    syntax error

--- diversalis/src/diversalis/compiler.hpp
+++ diversalis/src/diversalis/compiler.hpp
@@ -1,3 +1,4 @@
+xxxxxx
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 1999-2008 members of the psycle project http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
