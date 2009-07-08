d75e2392ced85bee11420782d820a4d0bca97055
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-07-08 03:13:36 +0000

    ray

--- tests/raytrace/src/raytrace/cpu/color.hpp
+++ tests/raytrace/src/raytrace/cpu/color.hpp
@@ -1,18 +1,19 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 2009-2009 psycledelics http://psycle.pastnotecut.org : johan boule
 
-#ifndef RAYTRACE__SCENE
-#define RAYTRACE__SCENE
+#ifndef RAYTRACE__COLOR
+#define RAYTRACE__COLOR
 #pragma once
 
-#include "color.hpp"
-#include "vertex.hpp"
+#include "real.hpp"
 
 namespace raytrace {
 
-class scene {
+class color {
 	public:
-		color virtual trace(vertex const & from, vertex const & to) = 0;
+		color() {}
+		color(real r, real g, real b) : r(r), g(g), b(b) {}
+		real r, g, b;
 };
 
 }
--- tests/raytrace/src/raytrace/cpu/vertex.hpp
+++ tests/raytrace/src/raytrace/cpu/vertex.hpp
@@ -1,18 +1,19 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 2009-2009 psycledelics http://psycle.pastnotecut.org : johan boule
 
-#ifndef RAYTRACE__SCENE
-#define RAYTRACE__SCENE
+#ifndef RAYTRACE__VERTEX
+#define RAYTRACE__VERTEX
 #pragma once
 
-#include "color.hpp"
-#include "vertex.hpp"
+#include "real.hpp"
 
 namespace raytrace {
 
-class scene {
+class vertex {
 	public:
-		color virtual trace(vertex const & from, vertex const & to) = 0;
+		vertex() {}
+		vertex(real x, real y, real z) : x(x), y(y), z(z) {}
+		real x, y, z;
 };
 
 }
