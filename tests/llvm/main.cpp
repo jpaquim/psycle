d84739b2c7a33fa4645165bda5773aa3f72131d3
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-11-14 14:49:15 +0000

    removed old stuff

--- tests/llvm/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-int main(int, char**) {
-	float f(2);
-	std::cout << std::log(f) << "\n";
-	for(int i(0); i < 10; ++i) f += std::log(f);
-	std::cout << f << "\n";
-	return 0;
-}
