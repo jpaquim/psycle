ce63bd1225d25500fec115c673867b42a212809b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-12-05 17:10:41 +0000

    fix for the sine_cosine function

--- trunk/psycle-helpers/src/psycle/helpers/math/sine_cosine.hpp
+++ trunk/psycle-helpers/src/psycle/helpers/math/sine_cosine.hpp
@@ -5,10 +5,15 @@
 #include <diversalis/compiler.hpp>
 #include <universalis/compiler.hpp>
 #include <cmath>
+#if defined BOOST_AUTO_TEST_CASE
+	#include "pi.hpp"
+	#include <universalis/compiler/typenameof.hpp>
+	#include <sstream>
+#endif
 namespace psycle { namespace helpers { namespace math {
 
 	/// computes both the sine and the cosine at the same time
-	template<typename Real> UNIVERSALIS__COMPILER__CONST
+	template<typename Real>
 	void inline sin_cos(Real x, Real & restrict sine, Real & restrict cosine) {
 		// some compilers are able to optimise those two calls into one.
 		sine = std::sin(x);
@@ -20,28 +25,49 @@ namespace psycle { namespace helpers { namespace math {
 			// we need to do cosine = -cosine for some values of x
 		#endif
 	}
-}}}
 
-// inline implementation
-namespace psycle { namespace helpers { namespace math {
-
-	#if __STDC__VERSION__ >= 199901 || \
-		(defined DIVERSALIS__COMPILER__GNU && DIVERSALIS__COMPILER__VERSION__MAJOR >= 4)
+	#if __STDC__VERSION__ >= 199901 || (defined DIVERSALIS__COMPILER__GNU && DIVERSALIS__COMPILER__VERSION__MAJOR >= 4)
 		
-		template<> UNIVERSALIS__COMPILER__CONST
+		template<>
 		void inline sin_cos<>(long double const x, long double & restrict sine, long double & restrict cosine) {
 			::sincosl(x, &sine, &cosine);
 		}
 
-		template<> UNIVERSALIS__COMPILER__CONST
+		template<>
 		void inline sin_cos<>(double const x, double & restrict sine, double & restrict cosine) {
 			::sincos(x, &sine, &cosine);
 		}
 
-		template<> UNIVERSALIS__COMPILER__CONST
+		template<>
 		void inline sin_cos<>(float const x, float & restrict sine, float & restrict cosine) {
 			::sincosf(x, &sine, &cosine);
 		}
 
 	#endif
+	
+	#if defined BOOST_AUTO_TEST_CASE && !defined PSYCLE__HELPERS__MATH__FAST_LOG2__SKIP_TEST_CASE
+		template<typename Real>
+		void sin_cos_test_template() {
+			Real const tolerance = 1e-20;
+			Real const input_values[] = { 0, pi / 4, pi / 3, pi };
+			for(unsigned int i(0); i < sizeof input_values / sizeof *input_values; ++i) {
+				Real const x(input_values[i]);
+				Real s, c;
+				sin_cos(x, s, c);
+				std::ostringstream oss;
+				oss << "sin_cos<" << universalis::compiler::typenameof(x) << ">(" << x << "): sin = " << s << ", cos = " << c;
+				BOOST_MESSAGE(oss.str());
+				BOOST_CHECK(std::abs(s - std::sin(x)) < tolerance);
+				BOOST_CHECK(std::abs(c - std::cos(x)) < tolerance);
+			}
+		}
+		BOOST_AUTO_TEST_CASE(sin_cos_test) {
+			sin_cos_test_template<float>();
+			sin_cos_test_template<double>();
+			#if __STDC__VERSION__ >= 199901 || (defined DIVERSALIS__COMPILER__GNU && DIVERSALIS__COMPILER__VERSION__MAJOR >= 4)
+				sin_cos_test_template<long double>();
+			#endif
+		}
+	#endif
+	
 }}}
