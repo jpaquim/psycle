2baec5f0eedc5f711746d183bb4d3f81780a05c1
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-12-06 20:44:54 +0000

    misc

--- branches/bohan/tests/clutter/t.cpp
+++ branches/bohan/tests/clutter/t.cpp
@@ -1,81 +1,90 @@
+#include <gtk/gtk.h>
 #include <clutter/clutter.h>
+#include <clutter-gtk/gtk-clutter-embed.h>
 #include <stdlib.h>
 
-ClutterActor *rect = NULL;
+ClutterActor *stage = NULL;
 
-/* This must return a value between 0 and CLUTTER_ALPHA_MAX_ALPHA,
- * where 0 means the start of the path, 
- * and CLUTTER_ALPHA_MAX_ALPHA is the end of the path.
- *
- * This will be called as many times per seconds as specified in our call to clutter_timeline_new().
- *
- * See also, for instance CLUTTER_ALPHA_SINE_HALF for a useful built-in callback.
- */
-guint32
-on_alpha (ClutterAlpha *alpha, gpointer data)
+static gboolean
+on_button_clicked (GtkButton *button, gpointer user_data)
 {
-  /* Get the position in the timeline, 
-   *  so we can base our value upon it:
-   */
-  ClutterTimeline *timeline = clutter_alpha_get_timeline (alpha);
-  const int current_frame_num = clutter_timeline_get_current_frame (timeline);
-  const int n_frames = clutter_timeline_get_n_frames (timeline);
+  static gboolean already_changed = FALSE;
+  if(already_changed)
+  {
+    ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff }; /* Black */
+    clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
+  }
+  else
+  {
+    ClutterColor stage_color = { 0x20, 0x20, 0xA0, 0xff };
+    clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
+  }
 
-  /* Return a value that is simply proportional to the frame position: */
-  return CLUTTER_ALPHA_MAX_ALPHA * current_frame_num / n_frames;
+  already_changed = !already_changed;
+
+  return TRUE; /* Stop further handling of this event. */
 }
 
+static gboolean
+on_stage_button_press (ClutterStage *stage, ClutterEvent *event, gpointer user_data)
+{
+  gint x = 0;
+  gint y = 0;
+  clutter_event_get_coords (event, &x, &y);
+
+  g_print ("Stage clicked at (%d, %d)\n", x, y);
+
+  return TRUE; /* Stop further handling of this event. */
+}
 
 int main(int argc, char *argv[])
 {
-  ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff };
-  ClutterColor rect_color = { 0xff, 0xff, 0xff, 0x99 };
+  ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff }; /* Black */
 
   clutter_init (&argc, &argv);
+  gtk_init (&argc, &argv);
+
+  /* Create the window and some child widgets: */
+  GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  GtkWidget *vbox = gtk_vbox_new (FALSE, 6);
+  gtk_container_add (GTK_CONTAINER (window), vbox);
+  gtk_widget_show (vbox);
+  GtkWidget *button = gtk_button_new_with_label ("Change Color");
+  gtk_box_pack_end (GTK_BOX (vbox), button, FALSE, FALSE, 0);
+  gtk_widget_show (button);
+  g_signal_connect (button, "clicked",
+    G_CALLBACK (on_button_clicked), NULL);
+
+  /* Stop the application when the window is closed: */
+  g_signal_connect (window, "hide",
+                    G_CALLBACK (gtk_main_quit), NULL);
+
+  /* Create the clutter widget: */
+  GtkWidget *clutter_widget = gtk_clutter_embed_new ();
+  gtk_box_pack_start (GTK_BOX (vbox), clutter_widget, TRUE, TRUE, 0);
+  gtk_widget_show (clutter_widget);
+
+  /* Set the size of the widget, 
+   * because we should not set the size of its stage when using GtkClutterEmbed.
+   */ 
+  gtk_widget_set_size_request (clutter_widget, 200, 200);
 
   /* Get the stage and set its size and color: */
-  ClutterActor *stage = clutter_stage_get_default ();
-  clutter_actor_set_size (stage, 200, 200);
+  stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (clutter_widget));
   clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
-
-  /* Add a rectangle to the stage: */
-  rect = clutter_rectangle_new_with_color (&rect_color);
-  clutter_actor_set_size (rect, 40, 40);
-  clutter_actor_set_position (rect, 10, 10);
-  clutter_container_add_actor (CLUTTER_CONTAINER (stage), rect);
-  clutter_actor_show (rect);
 
   /* Show the stage: */
   clutter_actor_show (stage);
 
-  ClutterTimeline *timeline = clutter_timeline_new(100 /* frames */, 85 /* frames per second. */);
-  clutter_timeline_set_loop(timeline, TRUE); 
-  clutter_timeline_start(timeline);
+  /* Connect a signal handler to handle mouse clicks and key presses on the stage: */ 
+  g_signal_connect (stage, "button-press-event",
+    G_CALLBACK (on_stage_button_press), NULL);
 
-  /* Instead of our custom callback, 
-   * we could use a standard callback. For instance, CLUTTER_ALPHA_SINE_INC. 
-   */
-  ClutterEffectTemplate *effect_template = clutter_effect_template_new (timeline, &on_alpha);
-
-  ClutterKnot knot[2];
-  knot[0].x = 10;
-  knot[0].y = 10;
-  knot[1].x= 150;
-  knot[1].y= 150;
- 
-  // Move the actor along the path:
-  clutter_effect_path (effect_template, rect, knot, sizeof(knot) / sizeof(ClutterKnot), NULL, NULL);
-
-  // Also change the actor's opacity while moving it along the path:
-  // (You would probably want to use a different ClutterEffectTemplate, 
-  // so you could use a different alpha callback for this.)
-  clutter_effect_fade (effect_template, rect, 50, NULL, NULL);
-
-  g_object_unref (effect_template);
-  g_object_unref (timeline);
+  /* Show the window: */
+  gtk_widget_show (GTK_WIDGET (window));
 
   /* Start the main loop, so we can respond to events: */
-  clutter_main ();
+  gtk_main ();
 
   return EXIT_SUCCESS;
 
