4df126ea76f9ed63d7f0272e1188d58502000897
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-12-12 20:02:06 +0000

    bugfixes and rework of arguru synth. (still missing a couple of samplerate related issues)

--- trunk/psycle-plugins/src/psycle/plugins/arguru_synth_2_final/SynthTrack.cpp
+++ trunk/psycle-plugins/src/psycle/plugins/arguru_synth_2_final/SynthTrack.cpp
@@ -3,17 +3,27 @@
 ///\file arguru_synth_2_final/SynthTrack.cpp
 ///\brief implementation of the CSynthTrack class.
 
+const signed char CSynthTrack::ArpNote[9][16] = {
+	{0,  3,  7, 12, 15, 19, 24, 27, 31, 36, 39, 43, 48, 51, 55, 60},
+	{0,  4,  7, 12, 16, 19, 24, 28, 31, 36, 40, 43, 48, 52, 55, 60},
+	{0,  3,  7, 10, 12, 15, 19, 22, 24, 27, 31, 34, 36, 39, 43, 46},
+	{0,  4,  7, 10, 12, 16, 19, 22, 24, 28, 31, 34, 36, 40, 43, 46},
+	{0, 12,  0,-12, 12,  0, 12,  0,  0, 12,-12,  0, 12,  0, 12,-12},
+	{0, 12, 24,  0, 12, 24, 12,  0, 24, 12,  0,  0, 12,  0,  0, 24},
+	{0, 12, 19,  0,  0,  7,  0,  7,  0, 12, 19,  0, 12, 19,  0,-12},
+	{0,  3,  7, 12, 15, 19, 24, 27, 31, 27, 24, 19, 15, 12,  7,  3},
+	{0,  4,  7, 12, 16, 19, 24, 28, 31, 28, 24, 19, 16, 12,  7,  4},
+};
+
+
 CSynthTrack::CSynthTrack()
 {
 	syntp=0;
-	output=0;
 	NoteCutTime=0;
-	NoteCut=false;
 
 	timetocompute=FILTER_CALC_TIME;
 
 	lfo_phase=0;
-	synthglide = 256;
 
 	VibratoSpeed=0;
 	VibratoDepth=0;
@@ -43,77 +53,88 @@ CSynthTrack::CSynthTrack()
 	VcfEnvStage=0;
 	VcfEnvValue=0.0f;
 	Stage5AmpVal=0.0f;
-	Stage5VcfVal=0.0f;
 	vibrato=false;
 
-	InitArpeggio();
-	m_filter.init(44100);
+//	InitArpeggio();
+//	setSampleRate(44100,2048,1.0f);
 }
 
 CSynthTrack::~CSynthTrack()
 {
 
 }
+void CSynthTrack::setSampleRate(int currentSR_, int wavetableSize_, float srCorrection_) {
+	///\todo: modify the note-on and initlfo calls to use srCorrection
+	/// also modify the doglide and performFX for srCorrection (oscglide should remain unmodified)
+	m_filter.init(currentSR_);
+	waveTableSize = wavetableSize_;
+	srCorrection = srCorrection_;
+}
 
-void CSynthTrack::NoteOn(int note, SYNPAR *tspar,int spd)
+void CSynthTrack::setGlobalPar(SYNPAR* globalPar) {
+	syntp=globalPar;
+}
+void CSynthTrack::NoteOn(int note,int spd)
 {
-	bool initenvelopes=false;
-	syntp=tspar;
+	bool forceNew=false;
 
 	InitLfo(syntp->vcf_lfo_speed,syntp->vcf_lfo_amplitude);
 
 	float nnote=(float)note+
-		(float)tspar->globalfinetune*0.0038962f+
-		(float)tspar->globaldetune;
+		(float)syntp->globalfinetune*0.0038962f+
+		(float)syntp->globaldetune;
 	OSC1Speed=(float)pow(2.0, (float)nnote/12.0);
 
 	float note2=nnote+
-		(float)tspar->osc2finetune*0.0038962f+
-		(float)tspar->osc2detune;
+		(float)syntp->osc2finetune*0.0038962f+
+		(float)syntp->osc2detune;
 	OSC2Speed=(float)pow(2.0, (float)note2/12.0);
 
-	if (oscglide == 0.0f || AmpEnvStage == 0)
+	if (oscglide == 0.0f)
 	{
-		ROSC1Speed = OSC1Speed;
-		ROSC2Speed = OSC2Speed;
 		if (AmpEnvStage == 0)
 		{
+			ROSC1Speed = OSC1Speed;
+			ROSC2Speed = OSC2Speed;
 			if (syntp->osc2sync) OSC2Position = 0;
 			else
 			{
 				OSC2Position-= OSC1Position;
-				if (OSC2Position<0) OSC2Position+=2048.0f;
+				if (OSC2Position<0) OSC2Position+=waveTableSize;
 			}
 			OSC1Position = 0;
 		}
-		initenvelopes =true;
+		forceNew =true;
 	}
-	else if ( oscglide > 0.0f && AmpEnvStage > 3) initenvelopes = true;
+	else if ( AmpEnvStage == 0 || AmpEnvStage > 3) forceNew = true;
 
 	OSC2Vol=(float)syntp->osc_mix*0.0039062f;
 	OSC1Vol=1.0f-OSC2Vol;
 
-
 	float spdcoef;
 	
-	if(spd<65)				spdcoef=(float)spd*0.015625f;
-	else								spdcoef=1.0f;
+	if(spd<65) spdcoef=(float)spd*0.015625f;
+	else       spdcoef=1.0f;
 
 	OSC1Vol*=(float)syntp->out_vol*0.0039062f*spdcoef;
 	OSC2Vol*=(float)syntp->out_vol*0.0039062f*spdcoef;
 
-	ArpLimit=tspar->arp_cnt;
+	if (sp_cmd != 0x0E) {
+		NoteCutTime = 0;
+	}
+
+	//ArpLimit=tspar->arp_cnt;
 	ArpMode=syntp->arp_mod;
 	Arp_tickcounter=0;
 	Arp_basenote=nnote;
 	Arp_samplespertick=2646000/(syntp->arp_bpm*4);
 	ArpCounter=1;
 
-	InitEnvelopes(initenvelopes);
+	InitEnvelopes(forceNew);
 
 }
 
-void CSynthTrack::InitEnvelopes(bool force)
+void CSynthTrack::InitEnvelopes(bool forceNew)
 {
 	VcfEnvMod=(float)syntp->vcf_envmod;
 	VcfCutoff=(float)syntp->vcf_cutoff;
@@ -121,30 +142,22 @@ void CSynthTrack::InitEnvelopes(bool force)
 
 	// Init Amplitude Envelope
 	AmpEnvSustainLevel=(float)syntp->amp_env_sustain*0.0039062f;
-	if(AmpEnvStage < 2 || force)
+	VcfEnvSustainLevel=(float)syntp->vcf_env_sustain*0.0039062f;
+
+	if(AmpEnvStage == 0)
 	{
 		AmpEnvStage=1;
 		AmpEnvCoef=1.0f/(float)syntp->amp_env_attack;
-	}
-	else if (AmpEnvStage < 4)
-	{
-		AmpEnvStage=5;
-		AmpEnvCoef=1.0f/(float)syntp->amp_env_attack;
-		Stage5AmpVal=0.0f;
-	}
-	
-	// Init Filter Envelope
-	VcfEnvSustainLevel=(float)syntp->vcf_env_sustain*0.0039062f;
-	if (VcfEnvStage < 2 || force)
-	{
 		VcfEnvStage=1;
 		VcfEnvCoef=1.0f/(float)syntp->vcf_env_attack;
 	}
-	else if (VcfEnvStage < 4)
+	else if (forceNew)
 	{
-		VcfEnvStage=5;
-		VcfEnvCoef=1.0f/(float)syntp->vcf_env_attack;
-		Stage5VcfVal=0.0f;
+		AmpEnvStage=5;
+		AmpEnvCoef=AmpEnvValue/(float)syntp->amp_env_release;
+		Stage5AmpVal=0.0f;
+		VcfEnvStage=4;
+		VcfEnvCoef=VcfEnvValue/(float)syntp->vcf_env_release;
 	}
 }
 
@@ -162,8 +175,6 @@ void CSynthTrack::NoteOff(bool fast)
 
 		if(AmpEnvCoef<unde)AmpEnvCoef=unde;
 		if(VcfEnvCoef<unde)VcfEnvCoef=unde;
-
-//								if(!syntp->amp_env_sustain) AmpEnvStage=0;
 	}
 }
 
@@ -181,173 +192,19 @@ void CSynthTrack::Vibrate()
 
 void CSynthTrack::ActiveVibrato(int speed,int depth)
 {
-	VibratoSpeed=(float)depth/16.0f;
-	VibratoDepth=(float)speed/16.0f;
-	vibrato=true;
+	if (depth != 0 ) {
+		VibratoSpeed=(float)depth/16.0f;
+		VibratoDepth=(float)speed/16.0f;
+	}
+	if (VibratoDepth > 0.0f ) {
+		vibrato=true;
+	}
 }
 
 void CSynthTrack::DisableVibrato()
 {
 	vibrato=false;
 }
-
-void CSynthTrack::InitArpeggio()
-{
-	ArpNote[0][0]				=0;
-	ArpNote[0][1]				=3;
-	ArpNote[0][2]				=7;
-	ArpNote[0][3]				=12;
-	ArpNote[0][4]				=15;
-	ArpNote[0][5]				=19;
-	ArpNote[0][6]				=24;
-	ArpNote[0][7]				=27;
-	ArpNote[0][8]				=31;
-	ArpNote[0][9]				=36;
-	ArpNote[0][10]				=39;
-	ArpNote[0][11]				=43;
-	ArpNote[0][12]				=48;
-	ArpNote[0][13]				=51;
-	ArpNote[0][14]				=55;
-	ArpNote[0][15]				=60;
-
-	ArpNote[1][0]				=0;
-	ArpNote[1][1]				=4;
-	ArpNote[1][2]				=7;
-	ArpNote[1][3]				=12;
-	ArpNote[1][4]				=16;
-	ArpNote[1][5]				=19;
-	ArpNote[1][6]				=24;
-	ArpNote[1][7]				=28;
-	ArpNote[1][8]				=31;
-	ArpNote[1][9]				=36;
-	ArpNote[1][10]				=40;
-	ArpNote[1][11]				=43;
-	ArpNote[1][12]				=48;
-	ArpNote[1][13]				=52;
-	ArpNote[1][14]				=55;
-	ArpNote[1][15]				=60;
-
-	ArpNote[2][0]				=0;
-	ArpNote[2][1]				=3;
-	ArpNote[2][2]				=7;
-	ArpNote[2][3]				=10;
-	ArpNote[2][4]				=12;
-	ArpNote[2][5]				=15;
-	ArpNote[2][6]				=19;
-	ArpNote[2][7]				=22;
-	ArpNote[2][8]				=24;
-	ArpNote[2][9]				=27;
-	ArpNote[2][10]				=31;
-	ArpNote[2][11]				=34;
-	ArpNote[2][12]				=36;
-	ArpNote[2][13]				=39;
-	ArpNote[2][14]				=43;
-	ArpNote[2][15]				=46;
-
-	ArpNote[3][0]				=0;
-	ArpNote[3][1]				=4;
-	ArpNote[3][2]				=7;
-	ArpNote[3][3]				=10;
-	ArpNote[3][4]				=12;
-	ArpNote[3][5]				=16;
-	ArpNote[3][6]				=19;
-	ArpNote[3][7]				=22;
-	ArpNote[3][8]				=24;
-	ArpNote[3][9]				=28;
-	ArpNote[3][10]				=31;
-	ArpNote[3][11]				=34;
-	ArpNote[3][12]				=36;
-	ArpNote[3][13]				=40;
-	ArpNote[3][14]				=43;
-	ArpNote[3][15]				=46;
-
-	ArpNote[4][0]				=0;
-	ArpNote[4][1]				=12;
-	ArpNote[4][2]				=0;
-	ArpNote[4][3]				=-12;
-	ArpNote[4][4]				=12;
-	ArpNote[4][5]				=0;
-	ArpNote[4][6]				=12;
-	ArpNote[4][7]				=0;
-	ArpNote[4][8]				=0;
-	ArpNote[4][9]				=12;
-	ArpNote[4][10]				=-12;
-	ArpNote[4][11]				=0;
-	ArpNote[4][12]				=12;
-	ArpNote[4][13]				=0;
-	ArpNote[4][14]				=12;
-	ArpNote[4][15]				=-12;
-
-	ArpNote[5][0]				=0;
-	ArpNote[5][1]				=12;
-	ArpNote[5][2]				=24;
-	ArpNote[5][3]				=0;
-	ArpNote[5][4]				=12;
-	ArpNote[5][5]				=24;
-	ArpNote[5][6]				=12;
-	ArpNote[5][7]				=0;
-	ArpNote[5][8]				=24;
-	ArpNote[5][9]				=12;
-	ArpNote[5][10]				=0;
-	ArpNote[5][11]				=0;
-	ArpNote[5][12]				=12;
-	ArpNote[5][13]				=0;
-	ArpNote[5][14]				=0;
-	ArpNote[5][15]				=24;
-
-	ArpNote[6][0]				=0;
-	ArpNote[6][1]				=12;
-	ArpNote[6][2]				=19;
-	ArpNote[6][3]				=0;
-	ArpNote[6][4]				=0;
-	ArpNote[6][5]				=7;
-	ArpNote[6][6]				=0;
-	ArpNote[6][7]				=7;
-	ArpNote[6][8]				=0;
-	ArpNote[6][9]				=12;
-	ArpNote[6][10]				=19;
-	ArpNote[6][11]				=0;
-	ArpNote[6][12]				=12;
-	ArpNote[6][13]				=19;
-	ArpNote[6][14]				=0;
-	ArpNote[6][15]				=-12;
-
-	ArpNote[7][0]				=0;
-	ArpNote[7][1]				=3;
-	ArpNote[7][2]				=7;
-	ArpNote[7][3]				=12;
-	ArpNote[7][4]				=15;
-	ArpNote[7][5]				=19;
-	ArpNote[7][6]				=24;
-	ArpNote[7][7]				=27;
-	ArpNote[7][8]				=31;
-	ArpNote[7][9]				=27;
-	ArpNote[7][10]				=24;
-	ArpNote[7][11]				=19;
-	ArpNote[7][12]				=15;
-	ArpNote[7][13]				=12;
-	ArpNote[7][14]				=7;
-	ArpNote[7][15]				=3;
-
-	ArpNote[8][0]				=0;
-	ArpNote[8][1]				=4;
-	ArpNote[8][2]				=7;
-	ArpNote[8][3]				=12;
-	ArpNote[8][4]				=16;
-	ArpNote[8][5]				=19;
-	ArpNote[8][6]				=24;
-	ArpNote[8][7]				=28;
-	ArpNote[8][8]				=31;
-	ArpNote[8][9]				=28;
-	ArpNote[8][10]				=24;
-	ArpNote[8][11]				=19;
-	ArpNote[8][12]				=16;
-	ArpNote[8][13]				=12;
-	ArpNote[8][14]				=7;
-	ArpNote[8][15]				=4;
-
-}
-
 void CSynthTrack::DoGlide()
 {
 	// Glide Handler
@@ -357,7 +214,7 @@ void CSynthTrack::DoGlide()
 
 		if(ROSC1Speed>OSC1Speed) ROSC1Speed=OSC1Speed;
 	}
-	else
+	else if (ROSC1Speed>OSC1Speed)
 	{
 		ROSC1Speed-=oscglide;
 
@@ -370,7 +227,7 @@ void CSynthTrack::DoGlide()
 
 		if(ROSC2Speed>OSC2Speed) ROSC2Speed=OSC2Speed;
 	}
-	else
+	else if(ROSC2Speed>OSC2Speed)
 	{
 		ROSC2Speed-=oscglide;
 
@@ -384,7 +241,6 @@ void CSynthTrack::PerformFx()
 	if(NoteCutTime<0)
 	{
 		NoteCutTime=0;
-		NoteCut=false;
 		NoteOff();
 	}
 
@@ -396,19 +252,18 @@ void CSynthTrack::PerformFx()
 	{
 		/* 0x01 : Pitch Up */
 		case 0x01:
-			//FIXME: THESE COMMANDS ARE BROKEN, because oscglide is by default zero. shift should modify osglide too.
 			shift=(float)sp_val*0.001f;
-			OSC1Speed+=shift;
-			OSC2Speed+=shift;
+			ROSC1Speed+=shift;
+			ROSC2Speed+=shift;
 			break;
 
 		/* 0x02 : Pitch Down */
 		case 0x02:
 			shift=(float)sp_val*0.001f;
-			OSC1Speed-=shift;
-			OSC2Speed-=shift;
-			if(OSC1Speed<0)OSC1Speed=0;
-			if(OSC2Speed<0)OSC2Speed=0;
+			ROSC1Speed-=shift;
+			ROSC2Speed-=shift;
+			if(ROSC1Speed<0.0f)ROSC1Speed=0.0f;
+			if(ROSC2Speed<0.0f)ROSC2Speed=0.0f;
 			break;
 
 		/* 0x11 : CutOff Up */
@@ -433,28 +288,23 @@ void CSynthTrack::InitEffect(int cmd, int val)
 	sp_val=val;
 
 	// Init glide
-	if (cmd==3) { if ( val != 0 ) oscglide= (float)(val*val)*0.001f; }
+	if (cmd==3) { if ( val != 0 ) oscglide= (float)val*0.001f; }
 	else 
 	{
-		if ( syntp )
-		{
-			synthglide = 256-syntp->synthglide;
-		}
+		const float synthglide = 256-syntp->synthglide;
 		if (synthglide < 256.0f) oscglide = (synthglide*synthglide)*0.0000625f;
 		else oscglide= 0.0f;
 	}
 
 	// Init vibrato
-	if (cmd==4)				ActiveVibrato(val>>4,val&0xf);
-	else								DisableVibrato();
+	if (cmd==4) ActiveVibrato(val>>4,val&0xf);
+	else        DisableVibrato();
 
 	// Note CUT
 	if (cmd==0x0E && val>0)
 	{
 		NoteCutTime=val*32;
-		NoteCut=true;
 	}
-	else NoteCut=false;
 
 }
 
--- trunk/psycle-plugins/src/psycle/plugins/arguru_synth_2_final/arguru_synth_2_final.cpp
+++ trunk/psycle-plugins/src/psycle/plugins/arguru_synth_2_final/arguru_synth_2_final.cpp
@@ -1,11 +1,15 @@
 #include <psycle/plugin_interface.hpp>
+#include <psycle/helpers/math.hpp>
+#include <psycle/helpers/math/pi.hpp>
 #include "SynthTrack.hpp"
 #include <cstdlib>
 
 using namespace psycle::plugin_interface;
+using namespace psycle::helpers;
 
 int const MAX_ENV_TIME = 250000;
 int const NUMPARAMETERS = 28;
+#define USE_NEW_WAVETABLES 1
 
 CMachineParameter const paraOSC1wave = {
 	"OSC1 Wave",
@@ -240,15 +244,20 @@ CMachineParameter const paraGlobalDetune = {
 	MPF_STATE,
 	1 
 };
-// Why is the default tuning +1 +60? answer:
-// the wavetable contains one period of 2048 samples,
-// at 44100Hz (which Asynth assumes), this is a ~21.5Hz signal.
-// A standard tune is A-5 (i.e, note 69) to be 440Hz, which is
-// ~ 20.4335 times bigger. log2 of this value is ~ 4.3528.
-// Multiply this value by 12 (notes/octave) to get 52.2344,
-// which stands for note 52 and finetune 0.2344.
-// The value of note in SeqTick() is note-18 ( 69-18 = 51 ),
-// So we add one to Tune, and fine is 0.2344 * 256 ~ 60.01
+// Why is the default tuning +1 +60?
+// Answer:
+// The original implementation generated a wavetable of 2048 samples.
+// At 44100Hz (which Asynth assumed), this is a ~21.5Hz signal.
+// A standard tune is for A-5 (i.e, note 69) to be 440Hz, which is
+// ~ 20.4335 times the sampled signal.
+// log2 of this value is ~ 4.3528.
+// Multiply this amount by 12 (notes/octave) to get 52.2344, which stands
+// for note 52 and finetune 0.2344.
+// The original compensation was using note-18 in the SeqTick() call ( 69-18 = 51 )
+// So to correctly compensate, 1 seminote and fine of 60 is added (0.2344 * 256 ~ 60.01)
+//
+// With the new implementation where the wavetable is generated depending
+// on the sampling rate, this correction is maintained for compatibility.
 
 CMachineParameter const paraGlobalFinetune = {
 	"Gbl. Finetune",
@@ -327,8 +336,6 @@ CMachineInfo const MacInfo (
 
 class mi : public CMachineInterface {
 	public:
-		void InitWaveTable();
-
 		mi();
 		virtual ~mi();
 		virtual void Init();
@@ -341,59 +348,94 @@ class mi : public CMachineInterface {
 		virtual void Stop();
 
 	private:
+		void InitWaveTableOrig();
+		void InitWaveTableSR(bool delArray=false);
+#if USE_NEW_WAVETABLES
+		signed short *WaveTable[5];
+#else
 		signed short WaveTable[5][2050];
+#endif
+		std::uint32_t waveTableSize;
+		float srCorrection;
 		CSynthTrack track[MAX_TRACKS];
 	
 		SYNPAR globalpar;
 		bool reinitChannel[MAX_TRACKS];
+		std::uint32_t currentSR;
 };
 
 PSYCLE__PLUGIN__INSTANCIATOR(mi, MacInfo)
 
 mi::mi() {
 	Vals = new int[NUMPARAMETERS];
-	InitWaveTable();
-	for(int i = 0; i < MAX_TRACKS; ++i) reinitChannel[i] = false;
+	//Initialize here only those things that don't depend on
+	//external values (like sampling rate)
+#if !USE_NEW_WAVETABLES
+	InitWaveTableOrig();
+#endif
+	for(int i = 0; i < MAX_TRACKS; ++i) {
+		track[i].setGlobalPar(&globalpar);
+		reinitChannel[i] = false;
+	}
 }
 
 mi::~mi() {
 	delete[] Vals;
 	// Destroy dinamically allocated objects/memory here
+#if USE_NEW_WAVETABLES
+	for (int i=0;i < 5; i++) {
+		delete[] WaveTable[i];
+	}
+#endif
 }
 
 void mi::Init() {
-	// Initialize your stuff here
+	currentSR=pCB->GetSamplingRate();
+	// Initialize your stuff here (you can use pCB here without worries)
+#if USE_NEW_WAVETABLES
+	InitWaveTableSR();
+#endif
+	for (int i = 0; i < MAX_TRACKS; ++i) {
+		track[i].setSampleRate(currentSR, waveTableSize, srCorrection);
+	}
 }
 
 void mi::Stop() {
 	for(int c=0;c<MAX_TRACKS;c++)
-	track[c].NoteOff(true);
+		track[c].NoteOff(true);
 }
 
 void mi::SequencerTick() {
+	if (currentSR != pCB->GetSamplingRate()) {
+		Init();
+		//force an update of all the parameters.
+		ParameterTweak(-1,-1);
+		Stop();
+	}
 	for (int i = 0; i < MAX_TRACKS; ++i) reinitChannel[i] = true;
 }
 
 void mi::ParameterTweak(int par, int val) {
 	// Called when a parameter is changed by the host app / user gui
-	Vals[par]=val;
+	if (par >= 0 ) Vals[par]=val;
+	float multiplier = currentSR/44100.0f;
 
-	globalpar.pWave=WaveTable[Vals[0]];
-	globalpar.pWave2=WaveTable[Vals[1]];
+	if (par == 0) globalpar.pWave=WaveTable[Vals[0]];
+	if (par == 1) globalpar.pWave2=WaveTable[Vals[1]];
 	globalpar.osc2detune=Vals[2];
 	globalpar.osc2finetune=Vals[3];
 	globalpar.osc2sync=Vals[4];
 	
-	globalpar.amp_env_attack=Vals[5];
-	globalpar.amp_env_decay=Vals[6];
+	if (par == 5) globalpar.amp_env_attack=Vals[5]*multiplier;
+	if (par == 6) globalpar.amp_env_decay=Vals[6]*multiplier;
 	globalpar.amp_env_sustain=Vals[7];
-	globalpar.amp_env_release=Vals[8];
+	if (par == 8) globalpar.amp_env_release=Vals[8]*multiplier;
 
-	globalpar.vcf_env_attack=Vals[9];
-	globalpar.vcf_env_decay=Vals[10];
+	if (par == 9) globalpar.vcf_env_attack=Vals[9]*multiplier;
+	if (par == 10) globalpar.vcf_env_decay=Vals[10]*multiplier;
 	globalpar.vcf_env_sustain=Vals[11];
-	globalpar.vcf_env_release=Vals[12];
-	globalpar.vcf_lfo_speed=Vals[13];
+	if (par == 12) globalpar.vcf_env_release=Vals[12]*multiplier;
+	if (par == 13) globalpar.vcf_lfo_speed=Vals[13]*multiplier;
 	globalpar.vcf_lfo_amplitude=Vals[14];
 
 	globalpar.vcf_cutoff=Vals[15];
@@ -406,7 +448,14 @@ void mi::ParameterTweak(int par, int val) {
 	globalpar.arp_bpm=Vals[22];
 	globalpar.arp_cnt=Vals[23];
 	globalpar.globaldetune=Vals[24];
+#if USE_NEW_WAVETABLES
+	globalpar.globalfinetune=Vals[25]-60;
+#else
 	globalpar.globalfinetune=Vals[25];
+#endif
+	if (par == 26 && val > paraGlide.MaxValue) {
+		Vals[26] = paraGlide.MaxValue;
+	}
 	globalpar.synthglide=Vals[26];
 	globalpar.interpolate=Vals[27];
 }
@@ -417,7 +466,7 @@ void mi::Command() {
 	// or an about button
 	char buffer[2048];
 
-	sprintf(
+	std::sprintf(
 			buffer,"%s%s%s%s%s%s%s%s%s%s%s",
 			"Pattern commands\n",
 			"\n01xx : Pitch slide-up",
@@ -450,8 +499,10 @@ void mi::Work(float *psamplesleft, float *psamplesright , int numsamples,int tra
 			int xnumsamples=numsamples;
 		
 			CSynthTrack *ptrack=&track[c];
-			if(reinitChannel[c]) ptrack->InitEffect(0,0);
-			reinitChannel[c]=false;
+			if(reinitChannel[c]) {
+				ptrack->InitEffect(0,0);
+				reinitChannel[c]=false;
+			}
 
 			if(ptrack->NoteCutTime >0) ptrack->NoteCutTime-=numsamples;
 		
@@ -485,88 +536,94 @@ void mi::Work(float *psamplesleft, float *psamplesright , int numsamples,int tra
 // Function that describes value on client's displaying
 bool mi::DescribeValue(char* txt,int const param, int const value) {
 	// Oscillators waveform descriptions
-	if(param==0 || param==1) {
-		switch(value) {
-			case 0:sprintf(txt,"Sine");return true;break;
-			case 1:sprintf(txt,"Sawtooth");return true;break;
-			case 2:sprintf(txt,"Square");return true;break;
-			case 3:sprintf(txt,"Triangle");return true;break;
-			case 4:sprintf(txt,"Random");return true;break;
-		}
-	}
-
-	if(param==4) {
-		switch(value) {
-			case 0:sprintf(txt,"Off");return true;break;
-			case 1:sprintf(txt,"On");return true;break;
-		}
-	}
-
-	if(param==17) {
-		switch(value) {
-			case 0:sprintf(txt,"Lowpass A");return true;break;
-			case 1:sprintf(txt,"Hipass A");return true;break;
-			case 2:sprintf(txt,"Bandpass A");return true;break;
-			case 3:sprintf(txt,"Bandreject A");return true;break;
-			case 4:sprintf(txt,"ParaEQ1 A");return true;break;
-			case 5:sprintf(txt,"InvParaEQ1 A");return true;break;
-			case 6:sprintf(txt,"ParaEQ2 A");return true;break;
-			case 7:sprintf(txt,"InvParaEQ2 A");return true;break;
-			case 8:sprintf(txt,"ParaEQ3 A");return true;break;
-			case 9:sprintf(txt,"InvParaEQ3 A");return true;break;
-			case 10:sprintf(txt,"Lowpass B");return true;break;
-			case 11:sprintf(txt,"Hipass B");return true;break;
-			case 12:sprintf(txt,"Bandpass B");return true;break;
-			case 13:sprintf(txt,"Bandreject B");return true;break;
-			case 14:sprintf(txt,"ParaEQ1 B");return true;break;
-			case 15:sprintf(txt,"InvParaEQ1 B");return true;break;
-			case 16:sprintf(txt,"ParaEQ2 B");return true;break;
-			case 17:sprintf(txt,"InvParaEQ2 B");return true;break;
-			case 18:sprintf(txt,"ParaEQ3 B");return true;break;
-			case 19:sprintf(txt,"InvParaEQ3 B");return true;break;
-		}
-	}
-
-	if(param==19) {
-		float fv=(float)value*0.390625f;
-
-		if ( value == 0 ) sprintf(txt,"Osc1");
-		else if ( value == 256 ) sprintf(txt,"Osc2");
-		else sprintf(txt,"%.1f%% : %.1f%%",100-fv,fv);
-
+	switch(param) {
+	case 0: //fallthrough
+	case 1:
+			switch(value) {
+				case 0:std::strcpy(txt,"Sine");return true;
+				case 1:std::strcpy(txt,"Sawtooth");return true;
+				case 2:std::strcpy(txt,"Square");return true;
+				case 3:std::strcpy(txt,"Triangle");return true;
+				case 4:std::strcpy(txt,"Random");return true;
+			}
+			break;
+	case 3: //fallthrough
+	case 25:
+			std::sprintf(txt,"%.03f cts.",Vals[param]*0.390625f);
+			return true;
+	case 4:	//fallthrough
+	case 27:
+			std::strcpy(txt,(value==0)?"Off":"On");
+			return true;
+	case 5: //fallthrough
+	case 6: //fallthrough
+	case 8: //fallthrough
+	case 9: //fallthrough
+	case 10: //fallthrough
+	case 12:
+			std::sprintf(txt,"%.03f ms",Vals[param]*1000.0f/currentSR);
+			return true;
+	case 7:
+	case 11:
+	case 20:
+		std::sprintf(txt,"%.1f%%",(float)value*0.390625f);
 		return true;
-	}
-
-	if(param==20) {
-		sprintf(txt,"%.1f%%",(float)value*0.390625f);
+	case 13:
+		std::sprintf(txt,"%.03f Hz",(value*0.000005f)/(2.0f*math::pi_f)*(currentSR/64.0f));
 		return true;
-	}
+	case 17:
+			switch(value) {
+			case 0:std::strcpy(txt,"Lowpass A");return true;
+			case 1:std::strcpy(txt,"Hipass A");return true;
+			case 2:std::strcpy(txt,"Bandpass A");return true;
+			case 3:std::strcpy(txt,"Bandreject A");return true;
+			case 4:std::strcpy(txt,"ParaEQ1 A");return true;
+			case 5:std::strcpy(txt,"InvParaEQ1 A");return true;
+			case 6:std::strcpy(txt,"ParaEQ2 A");return true;
+			case 7:std::strcpy(txt,"InvParaEQ2 A");return true;
+			case 8:std::strcpy(txt,"ParaEQ3 A");return true;
+			case 9:std::strcpy(txt,"InvParaEQ3 A");return true;
+			case 10:std::strcpy(txt,"Lowpass B");return true;
+			case 11:std::strcpy(txt,"Hipass B");return true;
+			case 12:std::strcpy(txt,"Bandpass B");return true;
+			case 13:std::strcpy(txt,"Bandreject B");return true;
+			case 14:std::strcpy(txt,"ParaEQ1 B");return true;
+			case 15:std::strcpy(txt,"InvParaEQ1 B");return true;
+			case 16:std::strcpy(txt,"ParaEQ2 B");return true;
+			case 17:std::strcpy(txt,"InvParaEQ2 B");return true;
+			case 18:std::strcpy(txt,"ParaEQ3 B");return true;
+			case 19:std::strcpy(txt,"InvParaEQ3 B");return true;
+			}
+			break;
+	case 19:
+			{
+				float fv=(float)value*0.390625f;
 
-	if(param==21) {
-		switch(value) {
-			case 0:sprintf(txt,"Off");return true;break;
-			case 1:sprintf(txt,"Minor1");return true;break;
-			case 2:sprintf(txt,"Major1");return true;break;
-			case 3:sprintf(txt,"Minor2");return true;break;
-			case 4:sprintf(txt,"Major2");return true;break;
-			case 5:sprintf(txt,"Riffer1");return true;break;
-			case 6:sprintf(txt,"Riffer2");return true;break;
-			case 7:sprintf(txt,"Riffer3");return true;break;
-			case 8:sprintf(txt,"Minor Bounce");return true;break;
-			case 9:sprintf(txt,"Major Bounce");return true;break;
-		}
+				if ( value == 0 ) std::strcpy(txt,"Osc1");
+				else if ( value == 256 ) std::strcpy(txt,"Osc2");
+				else std::sprintf(txt,"%.1f%% : %.1f%%",100-fv,fv);
+			}
+			return true;
+	case 21:
+			switch(value) {
+			case 0:std::strcpy(txt,"Off");return true;
+			case 1:std::strcpy(txt,"Minor1");return true;
+			case 2:std::strcpy(txt,"Major1");return true;
+			case 3:std::strcpy(txt,"Minor2");return true;
+			case 4:std::strcpy(txt,"Major2");return true;
+			case 5:std::strcpy(txt,"Riffer1");return true;
+			case 6:std::strcpy(txt,"Riffer2");return true;
+			case 7:std::strcpy(txt,"Riffer3");return true;
+			case 8:std::strcpy(txt,"Minor Bounce");return true;
+			case 9:std::strcpy(txt,"Major Bounce");return true;
+			}
+			break;
+	case 26:
+			if (value == 0) {std::strcpy(txt, "Off"); return true;}
+			break;
+	default:
+			break;
 	}
-
-	if(param==26 && value == 0) {
-		sprintf(txt,"Off");
-		return true;
-	}
-	
-	if(param==27) {
-		sprintf(txt,value?"On":"Off");
-		return true;
-	}
-	
 	return false;
 }
 
@@ -587,11 +644,11 @@ void mi::SeqTick(int channel, int note, int ins, int cmd, int val) {
 	break;
 	
 	case 8: // Change cutoff
-		globalpar.vcf_cutoff=val/2;
+		globalpar.vcf_cutoff=val*0.5f;
 	break;
 	
 	case 9: // Change reso
-		globalpar.vcf_resonance=val/2;
+		globalpar.vcf_resonance=val*0.5f;
 	break;
 	}
 
@@ -599,14 +656,14 @@ void mi::SeqTick(int channel, int note, int ins, int cmd, int val) {
 	// Empty Note Row == 255
 	// Less than note off value??? == NoteON!
 	if(note<120)
-		track[channel].NoteOn(note-18,&globalpar,(cmd == 0x0C)?(val>>2)&0x3F:64);
+		track[channel].NoteOn(note-18,(cmd == 0x0C)?(val>>2)&0x3F:64);
 
 	// Note off
 	else if(note==120)
 		track[channel].NoteOff();
 }
 
-void mi::InitWaveTable() {
+void mi::InitWaveTableOrig() {
 	for(int c=0;c<2050;c++) {
 		double sval=(double)c*0.00306796157577128245943617517898389;
 		WaveTable[0][c]=int(sin(sval)*16384.0f);
@@ -621,6 +678,62 @@ void mi::InitWaveTable() {
 		else if(c < 2048) WaveTable[3][c]=16384-((c-1024)*32);
 		else WaveTable[3][c]=((c-2048)*32)-16384;
 
+		//This assumes MAX_RAND is 0x7fff
 		WaveTable[4][c]= std::rand() - 16384;
 	}
+
+	waveTableSize = 2048;
+	srCorrection = 1.0f;
+}
+
+//New method to generate the wavetables:
+//
+//Generate a signal of aproximately 22Hz for the current sampling rate.
+//since it will not be an integer amount of samples, store the difference
+//as a factor in srCorrection, so that it can be applied when calculating
+//the OSC speed.
+void mi::InitWaveTableSR(bool delArray) {
+	//Ensure the value is even, we need two halfs.
+	const std::uint32_t amount = math::rounded((float)currentSR/22.0f) &0xFFFFFFF8;
+	const std::uint32_t half = amount >> 1;
+
+	const double sinFraction = 2.0*math::pi/(double)amount;
+	const float increase = 32768.0f/(float)amount;
+	const float increase2 = 65536.0f/(float)amount;
+
+	for (std::uint32_t i=0;i < 5; i++) {
+		if (delArray) {
+			delete WaveTable[i];
+		}
+		//Two more shorts allocated for the interpolation routine.
+		WaveTable[i]=new signed short[amount+2];
+	}
+	for(std::uint32_t c=0;c<half;c++) {
+		double sval=(double)c*sinFraction;
+		WaveTable[0][c]=short int(sin(sval)*16384.0);
+		WaveTable[1][c]=(c*increase)-16384;
+		WaveTable[2][c]=-16384;
+		WaveTable[3][c]=(c*increase2)-16384;
+		//This assumes MAX_RAND is 0x7fff
+		WaveTable[4][c]= std::rand() - 16384;
+	}
+	for(std::uint32_t c=half;c<amount;c++) {
+		double sval=(double)c*sinFraction;
+		WaveTable[0][c]=short int(sin(sval)*16384.0);
+		WaveTable[1][c]=(c*increase)-16384;
+		WaveTable[2][c]=16384;
+		WaveTable[3][c]=16384-((c-half)*increase2);
+		//This assumes MAX_RAND is 0x7fff
+		WaveTable[4][c]= std::rand() - 16384;
+	}
+	//Two more shorts allocated for the interpolation routine.
+	for (std::uint32_t i=0;i < 5; i++) {
+		WaveTable[i][amount]=WaveTable[i][0];
+		WaveTable[i][amount+1]=WaveTable[i][1];
+	}
+
+
+	waveTableSize = amount;
+	srCorrection = (float)amount*22.0f / (float)currentSR;
+
 }
