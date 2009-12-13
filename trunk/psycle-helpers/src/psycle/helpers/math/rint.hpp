1ea217a666f94051eac378cb5733034e374b9563
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2009-12-13 23:58:11 +0000

    svn properties

--- trunk/psycle-helpers/src/psycle/helpers/math/rint.hpp
+++ trunk/psycle-helpers/src/psycle/helpers/math/rint.hpp
@@ -1,8 +1,8 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 2004-2009 members of the psycle project http://psycle.sourceforge.net
 
-#ifndef PSYCLE__HELPERS__MATH__FAST_UNSPECIFIED_ROUND_TO_INTEGER__INCLUDED
-#define PSYCLE__HELPERS__MATH__FAST_UNSPECIFIED_ROUND_TO_INTEGER__INCLUDED
+#ifndef PSYCLE__HELPERS__MATH__LRINT__INCLUDED
+#define PSYCLE__HELPERS__MATH__LRINT__INCLUDED
 #pragma once
 
 #include <diversalis/stdlib.hpp>
