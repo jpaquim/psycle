bd74e6a7d7f3d3ddf0c391fa5c6929604b1a88e2
zealmange <zealmange@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-01-01 21:13:13 +0000

    Changes to make machineview.cpp compile.

--- trunk/qpsycle/src/gui/mainwindow.cpp
+++ trunk/qpsycle/src/gui/mainwindow.cpp
@@ -18,7 +18,6 @@
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
 #include <psycle/audiodrivers/audiodriver.h>
-#include <psycle/core/patternpool.h>
 #include <psycle/core/patternsequence.h>
 #include <psycle/core/player.h>
 #include <psycle/core/singlepattern.h>
