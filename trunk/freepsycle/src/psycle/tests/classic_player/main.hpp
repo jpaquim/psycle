d3c02443dce62b016b70222179b3950cb5e44120
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-01-02 11:57:11 +0000

    i don't remember why we put that in the subversion repo

--- trunk/freepsycle/src/psycle/tests/classic_player/main.hpp
+++ trunk/freepsycle/src/psycle/tests/classic_player/main.hpp
@@ -1,9 +1,9 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 1999-2009 members of the psycle project http://psycle.pastnotecut.org : johan boule <bohan@jabber.org>
 
-///\interface psycle::front_ends::text::main
+///\interface psycle::test::random_notes::main
 #pragma once
 #include <psycle/detail/project.hpp>
-namespace psycle { namespace tests { namespace random_notes {
+namespace psycle { namespace tests { namespace classic_player {
 	int main(int /*const*/ argument_count, char /*const*/ * /*const*/ arguments[]);
 }}}
