db9312243ba3100801d0242251828aeec43ce4d7
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-01-07 01:45:53 +0000

    some work on the compressor

--- psycle-plugins/src/psycle/plugins/arguru_compressor/arguru_compressor.cpp
+++ psycle-plugins/src/psycle/plugins/arguru_compressor/arguru_compressor.cpp
@@ -1,20 +1,22 @@
 ///\file
 ///\brief Arguru compressor plugin for PSYCLE
 #include <psycle/plugin_interface.hpp>
+#include <psycle/helpers/math.hpp>
 #include <cstdio> // for std::sprintf
 
 using namespace psycle::plugin_interface;
+using namespace psycle::helpers::math;
 
-CMachineParameter const paraGain = {"Input Gain","Input Gain",0,128,MPF_STATE, 0};
-CMachineParameter const paraThreshold = {"Threshold","Threshold",0,128,MPF_STATE,64};
-CMachineParameter const paraRatio = {"Ratio","Ratio",0,16,MPF_STATE,16};
-CMachineParameter const paraAttack = {"Attack","Attack",0,128,MPF_STATE,6};
-CMachineParameter const paraRelease = {"Release","Release",0,128,MPF_STATE,0x2C};
-CMachineParameter const paraClip = {"Soft clip","Soft clip",0,1,MPF_STATE,0};
+CMachineParameter const paraGain = {"Input Gain", "Input Gain", 0, 128, MPF_STATE, 0};
+CMachineParameter const paraThreshold = {"Threshold", "Threshold", 0, 128,MPF_STATE, 64};
+CMachineParameter const paraRatio = {"Ratio", "Ratio", 0, 16, MPF_STATE, 16};
+CMachineParameter const paraAttack = {"Attack", "Attack", 0, 128, MPF_STATE, 6};
+CMachineParameter const paraRelease = {"Release", "Release", 0, 128, MPF_STATE, 0x2C};
+CMachineParameter const paraClip = {"Soft clip", "Soft clip", 0, 1,MPF_STATE, 0};
+
 enum { paramGain = 0, paramThreshold, paramRatio, paramAttack, paramRelease, paramClip };
 
-CMachineParameter const *pParameters[] = 
-{ 
+CMachineParameter const *pParameters[] =  { 
 	&paraGain,
 	&paraThreshold,
 	&paraRatio,
@@ -29,166 +31,158 @@ CMachineInfo const MacInfo (
 	sizeof pParameters / sizeof *pParameters,
 	pParameters,
 	"Arguru Compressor 1.2"
-#ifndef NDEBUG
-	" (debug build)"
-#endif
+	#ifndef NDEBUG
+		" (debug build)"
+	#endif
 	,
 	"Compressor",
 	"J. Arguelles & psycledelics",
 	"About",
 	1
 );
-typedef enum { none=0, actAttack, actRelease}  action;
 
-
-class mi : public CMachineInterface
-{
-public:
-	mi();
-	virtual ~mi();
-
-	virtual void Init();
-	virtual void SequencerTick();
-	virtual void Work(float *psamplesleft, float *psamplesright , int numsamples, int tracks);
-	virtual bool DescribeValue(char* txt,int const param, int const value);
-	virtual void Command();
-	virtual void ParameterTweak(int par, int val);
-private:
-	float currentGain;
-	float gainShift;
-	action currentAction;
-
+class mi : public CMachineInterface {
+	public:
+		mi();
+		~mi();
+		/*override*/ void Work(float *psamplesleft, float *psamplesright , int numsamples, int tracks);
+		/*override*/ bool DescribeValue(char* txt,int const param, int const value);
+		/*override*/ void Command();
+		/*override*/ void ParameterTweak(int par, int val);
+	private:
+		//float avgInputLevel;
+		float currentGain;
+		float gainStep;
+		enum action { none, actAttack, actRelease} currentAction;
 };
 
 PSYCLE__PLUGIN__INSTANTIATOR(mi, MacInfo)
 
-mi::mi(): currentGain(1.0f), gainShift(0.0f), currentAction(none)
+mi::mi()
+:
+	//avgInputLevel(0),
+	currentGain(1),
+	gainStep(0),
+	currentAction(none)
 {
 	Vals = new int[MacInfo.numParameters];
 }
 
-mi::~mi()
-{
+mi::~mi() {
 	delete[] Vals;
 }
 
-void mi::Init()
-{
+void mi::Command() {
+	pCB->MessBox("By Arguru <arguru@smartelectronix.com>", "Arguru compressor", 0);
 }
 
-void mi::SequencerTick()
-{
-}
-
-void mi::Command()
-{
-	pCB->MessBox("By Arguru <arguru@smartelectronix.com>","Arguru compressor",0);
-}
-
-void mi::ParameterTweak(int par, int val)
-{
-	Vals[par]=val;
+void mi::ParameterTweak(int par, int val) {
+	Vals[par] = val;
 }
 
 
-void mi::Work(float *psamplesleft, float *psamplesright , int numsamples, int tracks)
-{
-	float const gain = 1.0f + Vals[paramGain]*0.015625f;
-	float const threshold = Vals[paramThreshold]*0.0078125f;
-	float const attack = Vals[paramAttack]*pCB->GetSamplingRate()*0.001f;
-	float const release = Vals[paramRelease]*pCB->GetSamplingRate()*0.001f;
-
-	if (Vals[paramRatio] == 0) {
+void mi::Work(float *psamplesleft, float *psamplesright , int numsamples, int tracks) {
+	float const makeUpGain = 1 + Vals[paramGain] * 0.015625f;
+	if(true || Vals[paramRatio] == 0) {
 		// No compression. 
-		do
-		{
+		do {
+			*psamplesleft *= makeUpGain;
+			*psamplesright *= makeUpGain;
 			++psamplesleft;
 			++psamplesright;
 		} while(--numsamples);
-	}
-	else if (Vals[paramRatio] >= 16){
-		// HardLimiter
-		do
-		{
-			float analyzedValue = (*psamplesleft + *psamplesright) * 0.5f;
+	} else {
+		float const static gainStepSpeed = deci_bell_to_linear(10.0f);
+		float const threshold = Vals[paramThreshold] * 0.0078125f;
+		float const attack = Vals[paramAttack] * pCB->GetSamplingRate() * 0.001f;
+		float const release = Vals[paramRelease] * pCB->GetSamplingRate() * 0.001f;
 
-			++psamplesleft;
-			++psamplesright;
-		} while(--numsamples);
-	}
-	else {
-		//target gain is incorrect, and currentGain is not constant. it depends on the analyzed value.
-		float const targetGain = 1.0f/(1.0f+Vals[paramRatio]);
-		// Compressor
-		do
-		{
-			float analyzedValue = fabs(*psamplesleft) + fabs(*psamplesright) * 0.5f * 0.000030517578125f;
-			if (analyzedValue > threshold) {
-				if (currentAction != actAttack) {
-					gainShift = (1.0f-targetGain)/attack;
-					currentAction = actAttack;
-				}
-				if (currentGain > targetGain) {
-					currentGain-=gainShift;
-				}
-			}
-			else if (currentGain < 1.0f) {
-				if (currentAction != actRelease) {
-					gainShift = (1.0f-targetGain)/release;
-					currentAction = actRelease;
-				}
-				currentGain+=gainShift;
-			}
-			*psamplesleft *= currentGain*gain;
-			*psamplesright *= currentGain*gain;
+		// TODO use peak or RMS?
+		float inputLevel = (std::fabs(*psamplesleft) + std::fabs(*psamplesright)) * 0.5f * 0.000030517578125f;
+		//float const avgSamples = pCB->GetSamplingRate() / 20; // window for average (should lead to a constant level for 20Hz)
+		//inputLevel = (avgInputLevel + inputLevel) / (avgSamples + 1); // average ponderation
+		
+		/// TODO soft clip
 
-			++psamplesleft;
-			++psamplesright;
-		} while(--numsamples);
+		if(Vals[paramRatio] >= 16) {
+			// Hard limiter
+			do {
+				if(inputLevel > threshold) {
+					currentGain = threshold / inputLevel;
+				} else if(currentGain < 1) {
+					if(currentAction != actRelease) {
+						currentAction = actRelease;
+						gainStep = gainStepSpeed / (release * pCB->GetSamplingRate());
+					}
+					currentGain += gainStep;
+				} else {
+					//currentAction = none;
+					currentGain = 1;
+				}
+				*psamplesleft *= makeUpGain * currentGain;
+				*psamplesright *= makeUpGain * currentGain;
+				++psamplesleft;
+				++psamplesright;
+			} while(--numsamples);
+		} else {
+			// Compressor
+			do {
+				if(inputLevel > threshold) {
+					if(currentAction != actAttack) {
+						currentAction = actAttack;
+						currentGain = 1;
+						gainStep = gainStepSpeed / (attack * pCB->GetSamplingRate());
+					} else {
+						// Note: Vals[paramRatio] is unusual: it does not express a dB ratio, but a linear one!
+						float const targetGain = (threshold + (inputLevel - threshold) / (1 + Vals[paramRatio])) / inputLevel;
+						if(currentGain > targetGain)
+							currentGain  = std::max(currentGain - gainStep, targetGain);
+					}
+				} else if(currentGain < 1) {
+					if(currentAction != actRelease) {
+						currentAction = actRelease;
+						gainStep = gainStepSpeed / (release * pCB->GetSamplingRate());
+					}
+					currentGain = std::min(currentGain + gainStep, 1.0f);
+				} else {
+					//currentAction = none;
+					currentGain = 1;
+				}
+				*psamplesleft *= makeUpGain * currentGain;
+				*psamplesright *= makeUpGain * currentGain;
+				++psamplesleft;
+				++psamplesright;
+			} while(--numsamples);
+		}
 	}
 }
 
-bool mi::DescribeValue(char* txt,int const param, int const value)
-{
+bool mi::DescribeValue(char* txt,int const param, int const value) {
 	switch(param) {
-	case paramGain:
-	{
-		std::sprintf(txt,"+%.1f dB",20.0f * log10(1.0f+value*0.015625f));
-		return true;
-	}
-	case paramThreshold:
-	{
-		if(value == 0)
-			std::sprintf(txt,"-inf. dB");
-		else
-			std::sprintf(txt,"%.1f dB",20.0f * log10(value*0.0078125f));
-		return true;
-	}
-	case paramRatio:
-	{
-		if (value >= 16) {
-			std::sprintf(txt,"Limiter");
-		}
-		else if (value == 0) {
-			std::sprintf(txt,"1:1 (Bypass)");
-		}
-		else {
-			std::sprintf(txt,"1:%d", value+1);
-		}
-		return true;
-	}
-	case paramAttack: //fallthrough
-	case paramRelease:
-	{
-		std::sprintf(txt,"%d ms.", value);
-		return true;
-	}
-	case paramClip:
-		{
-			if (value == 0) std::sprintf(txt,"Off");
-			else std::sprintf(txt,"On");
+		case paramGain:
+			std::sprintf(txt,"+%.1f dB", linear_to_deci_bell(1 + value * 0.015625f));
 			return true;
-		}
-	default: return false;
+		case paramThreshold:
+			if(value == 0)
+				std::sprintf(txt,"-inf. dB");
+			else
+				std::sprintf(txt,"%.1f dB", linear_to_deci_bell(value * 0.0078125f));
+			return true;
+		case paramRatio:
+			if(value >= 16)
+				std::sprintf(txt,"inf:1 (Limiter)");
+			else if(value == 0)
+				std::sprintf(txt,"1:1 (Bypass)");
+			else
+				std::sprintf(txt,"%d:1", value + 1);
+			return true;
+		case paramAttack: //fallthrough
+		case paramRelease:
+			std::sprintf(txt,"%d ms.", value);
+			return true;
+		case paramClip:
+			std::sprintf(txt, value == 0 ? "Off" : "On");
+			return true;
+		default: return false;
 	}
 }
