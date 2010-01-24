e99ee76420b20a85507db2ea39bf24e51b91b3f7
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-01-24 19:14:47 +0000

    the bug is still there, more beautiful than ever

--- trunk/psycle-plugins/src/psycle/plugins/gverb/gverb.h
+++ trunk/psycle-plugins/src/psycle/plugins/gverb/gverb.h
@@ -17,21 +17,19 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	*/
+*/
 
-#ifndef GVERB_H
-#define GVERB_H
+#ifndef PSYCLE__PLUGINS__GVERB__GVERB__INCLUDED
+#define PSYCLE__PLUGINS__GVERB__GVERB__INCLUDED
 
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
 #include "gverbdsp.h"
-//#include "gverb.h"
-//#include "ladspa-util.h"
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
 
 #define FDNORDER 4
 
-typedef struct {
+struct ty_gverb {
 	int rate;
 	float inputbandwidth;
 	float taillevel;
@@ -56,8 +54,7 @@ typedef struct {
 	float *u;
 	float *f;
 	double alpha;
-} ty_gverb;
-
+};
 
 ty_gverb *gverb_new(int, float, float, float, float, float, float, float, float);
 void gverb_free(ty_gverb *);
@@ -93,150 +90,123 @@ static void gverb_set_taillevel(ty_gverb *, float);
 	* be 0.5, say.
 	*/
 
-static inline void gverb_fdnmatrix(float *a, float *b)
-{
+inline void gverb_fdnmatrix(float *a, float *b) {
 	const float dl0 = a[0], dl1 = a[1], dl2 = a[2], dl3 = a[3];
 
-	b[0] = 0.5f*(+dl0 + dl1 - dl2 - dl3);
-	b[1] = 0.5f*(+dl0 - dl1 - dl2 + dl3);
-	b[2] = 0.5f*(-dl0 + dl1 - dl2 + dl3);
-	b[3] = 0.5f*(+dl0 + dl1 + dl2 + dl3);
+	b[0] = 0.5f * (+dl0 + dl1 - dl2 - dl3);
+	b[1] = 0.5f * (+dl0 - dl1 - dl2 + dl3);
+	b[2] = 0.5f * (-dl0 + dl1 - dl2 + dl3);
+	b[3] = 0.5f * (+dl0 + dl1 + dl2 + dl3);
 }
 
-static inline void gverb_do(ty_gverb *p, float x, float *yl, float *yr)
-{
-	float z;
-	unsigned int i;
-	float lsum,rsum,sum,sign;
-
-	if (/*isnan(x) ||*/ fabsf(x) > 100000.0f) {
-	x = 0.0f;
+inline void gverb_do(ty_gverb *p, float x, float *yl, float *yr) {
+	if (/*isnan(x) ||*/ std::abs(x) > 100000.0f) {
+		x = 0.0f;
 	}
 
-	z = damper_do(p->inputdamper, x);
+	float z = damper_do(p->inputdamper, x);
 
-	z = diffuser_do(p->ldifs[0],z);
+	z = diffuser_do(p->ldifs[0], z);
 
-	for(i = 0; i < FDNORDER; i++) {
-	p->u[i] = p->tapgains[i]*fixeddelay_read(p->tapdelay,p->taps[i]);
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		p->u[i] = p->tapgains[i] * fixeddelay_read(p->tapdelay, p->taps[i]);
 	}
 	fixeddelay_write(p->tapdelay,z);
 
-	for(i = 0; i < FDNORDER; i++) {
-	p->d[i] = damper_do(p->fdndamps[i],
-			p->fdngains[i]*fixeddelay_read(p->fdndels[i],
-								p->fdnlens[i]));
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		p->d[i] = damper_do(
+			p->fdndamps[i],
+			p->fdngains[i] * fixeddelay_read(p->fdndels[i],
+			p->fdnlens[i])
+		);
 	}
 
-	sum = 0.0f;
-	sign = 1.0f;
-	for(i = 0; i < FDNORDER; i++) {
-	sum += sign*(p->taillevel*p->d[i] + p->earlylevel*p->u[i]);
-	sign = -sign;
+	float sum = 0.0f;
+	float sign = 1.0f;
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		sum += sign * (p->taillevel*p->d[i] + p->earlylevel*p->u[i]);
+		sign = -sign;
 	}
-	sum += x*p->earlylevel;
-	lsum = sum;
-	rsum = sum;
+	sum += x * p->earlylevel;
+	float lsum = sum;
+	float rsum = sum;
 
-	gverb_fdnmatrix(p->d,p->f);
+	gverb_fdnmatrix(p->d, p->f);
 
-	for(i = 0; i < FDNORDER; i++) {
-	fixeddelay_write(p->fdndels[i],p->u[i]+p->f[i]);
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		fixeddelay_write(p->fdndels[i], p->u[i] + p->f[i]);
 	}
 
-	lsum = diffuser_do(p->ldifs[1],lsum);
-	lsum = diffuser_do(p->ldifs[2],lsum);
-	lsum = diffuser_do(p->ldifs[3],lsum);
-	rsum = diffuser_do(p->rdifs[1],rsum);
-	rsum = diffuser_do(p->rdifs[2],rsum);
-	rsum = diffuser_do(p->rdifs[3],rsum);
+	lsum = diffuser_do(p->ldifs[1], lsum);
+	lsum = diffuser_do(p->ldifs[2], lsum);
+	lsum = diffuser_do(p->ldifs[3], lsum);
+	rsum = diffuser_do(p->rdifs[1], rsum);
+	rsum = diffuser_do(p->rdifs[2], rsum);
+	rsum = diffuser_do(p->rdifs[3], rsum);
 
 	*yl = lsum;
 	*yr = rsum;
 }
 
-static inline void gverb_set_roomsize(ty_gverb *p, const float a)
-{
-	unsigned int i;
-
+inline void gverb_set_roomsize(ty_gverb *p, const float a) {
 	if (a <= 1.0 /*|| isnan(a)*/) {
-	p->roomsize = 1.0;
+		p->roomsize = 1.0;
 	} else {
-	p->roomsize = a;
+		p->roomsize = a;
 	}
 	p->largestdelay = p->rate * p->roomsize * 0.00294f;
 
-	//p->fdnlens[0] = f_round(1.000000f*p->largestdelay);
-	//p->fdnlens[1] = f_round(0.816490f*p->largestdelay);
-	//p->fdnlens[2] = f_round(0.707100f*p->largestdelay);
-	//p->fdnlens[3] = f_round(0.632450f*p->largestdelay);
-	p->fdnlens[0] = floorf(1.000000f*p->largestdelay);
-	p->fdnlens[1] = floorf(0.816490f*p->largestdelay);
-	p->fdnlens[2] = floorf(0.707100f*p->largestdelay);
-	p->fdnlens[3] = floorf(0.632450f*p->largestdelay);
+	p->fdnlens[0] = std::floor(1.000000f * p->largestdelay);
+	p->fdnlens[1] = std::floor(0.816490f * p->largestdelay);
+	p->fdnlens[2] = std::floor(0.707100f * p->largestdelay);
+	p->fdnlens[3] = std::floor(0.632450f * p->largestdelay);
 
-	for(i = 0; i < FDNORDER; i++) {
-	p->fdngains[i] = -powf((float)p->alpha, p->fdnlens[i]);
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		p->fdngains[i] = -std::pow((float)p->alpha, p->fdnlens[i]);
 	}
 
-	//p->taps[0] = 5+f_round(0.410f*p->largestdelay);
-	//p->taps[1] = 5+f_round(0.300f*p->largestdelay);
-	//p->taps[2] = 5+f_round(0.155f*p->largestdelay);
-	//p->taps[3] = 5+f_round(0.000f*p->largestdelay);
-	p->taps[0] = 5+floorf(0.410f*p->largestdelay);
-	p->taps[1] = 5+floorf(0.300f*p->largestdelay);
-	p->taps[2] = 5+floorf(0.155f*p->largestdelay);
-	p->taps[3] = 5+floorf(0.000f*p->largestdelay);
+	p->taps[0] = 5+std::floor(0.410f*p->largestdelay);
+	p->taps[1] = 5+std::floor(0.300f*p->largestdelay);
+	p->taps[2] = 5+std::floor(0.155f*p->largestdelay);
+	p->taps[3] = 5+std::floor(0.000f*p->largestdelay);
 
-	for(i = 0; i < FDNORDER; i++) {
-	p->tapgains[i] = powf((float)p->alpha, p->taps[i]);
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		p->tapgains[i] = std::pow((float)p->alpha, p->taps[i]);
 	}
-
 }
 
-static inline void gverb_set_revtime(ty_gverb *p,float a)
-{
-	float ga,gt;
-	double n;
-	unsigned int i;
-
+inline void gverb_set_revtime(ty_gverb *p, float a) {
 	p->revtime = a;
 
-	ga = 60.0;
-	gt = p->revtime;
-	ga = powf(10.0f,-ga/20.0f);
-	n = p->rate*gt;
-	p->alpha = (double)powf(ga,1.0f/n);
+	float ga = 60.0f;
+	float gt = p->revtime;
 
-	for(i = 0; i < FDNORDER; i++) {
-	p->fdngains[i] = -powf((float)p->alpha, p->fdnlens[i]);
+	ga = std::pow(10.0f, -ga / 20.0f);
+	double n = p->rate * gt;
+	p->alpha = std::pow((double)ga, 1.0 / n);
+
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		p->fdngains[i] = -std::pow((float)p->alpha, p->fdnlens[i]);
 	}
-
 }
 
-static inline void gverb_set_damping(ty_gverb *p,float a)
-{
-	unsigned int i;
-
+inline void gverb_set_damping(ty_gverb *p,float a) {
 	p->fdndamping = a;
-	for(i = 0; i < FDNORDER; i++) {
-	damper_set(p->fdndamps[i],p->fdndamping);
+	for(unsigned int i = 0; i < FDNORDER; ++i) {
+		damper_set(p->fdndamps[i], p->fdndamping);
 	}
 }
 
-static inline void gverb_set_inputbandwidth(ty_gverb *p,float a)
-{
+inline void gverb_set_inputbandwidth(ty_gverb *p, float a) {
 	p->inputbandwidth = a;
-	damper_set(p->inputdamper,1.0 - p->inputbandwidth);
+	damper_set(p->inputdamper, 1.0 - p->inputbandwidth);
 }
 
-static inline void gverb_set_earlylevel(ty_gverb *p,float a)
-{
+inline void gverb_set_earlylevel(ty_gverb *p, float a) {
 	p->earlylevel = a;
 }
 
-static inline void gverb_set_taillevel(ty_gverb *p,float a)
-{
+inline void gverb_set_taillevel(ty_gverb *p, float a) {
 	p->taillevel = a;
 }
 
--- trunk/psycle-plugins/src/psycle/plugins/gverb/gverbdsp.h
+++ trunk/psycle-plugins/src/psycle/plugins/gverb/gverbdsp.h
@@ -1,96 +1,75 @@
+#ifndef PSYCLE__PLUGINS__GVERB__GVERB_DSP__INCLUDED
+#define PSYCLE__PLUGINS__GVERB__GVERB_DSP__INCLUDED
+#pragma once
 
-#ifndef GVERBDSP_H
-#define GVERBDSP_H
-
-//#include "ladspa-util.h"
 #include <psycle/helpers/math/erase_all_nans_infinities_and_denormals.hpp>
 
-typedef struct {
-	int size;
-	int idx;
-	float *buf;
-} ty_fixeddelay;
+/**********************************************************/
+// diffuser
 
-typedef struct {
+struct ty_diffuser {
 	int size;
 	float coeff;
 	int idx;
 	float *buf;
-} ty_diffuser;
-
-typedef struct {
-	float damping;
-	float delay;
-} ty_damper;
-
-ty_diffuser *diffuser_make(int, float);
+};
+ty_diffuser * diffuser_make(int, float);
 void diffuser_free(ty_diffuser *);
 void diffuser_flush(ty_diffuser *);
-//float diffuser_do(ty_diffuser *, float);
-
-ty_damper *damper_make(float);
-void damper_free(ty_damper *);
-void damper_flush(ty_damper *);
-//void damper_set(ty_damper *, float);
-//float damper_do(ty_damper *, float);
-
-ty_fixeddelay *fixeddelay_make(int);
-void fixeddelay_free(ty_fixeddelay *);
-void fixeddelay_flush(ty_fixeddelay *);
-//float fixeddelay_read(ty_fixeddelay *, int);
-//void fixeddelay_write(ty_fixeddelay *, float);
-
-int isprime(int);
-int nearest_prime(int, float);
-
-#if 0
-	#define flush_to_zero(fv) (((*(unsigned int*)&(fv))&0x7f800000)==0)?0.0f:(fv)
-#endif
-
-static inline float diffuser_do(ty_diffuser *p, float x)
-{
+inline float diffuser_do(ty_diffuser *p, float x) {
 	float y,w;
 	float* buf = &p->buf[p->idx];
 	w = x - (*buf)*p->coeff;
-	#if 0
-		w = flush_to_zero(w);
-	#else
-		psycle::helpers::math::erase_all_nans_infinities_and_denormals(w);
-	#endif
+	psycle::helpers::math::erase_all_nans_infinities_and_denormals(w);
 	y = (*buf) + w*p->coeff;
 	*buf = w;
 	p->idx = (p->idx + 1) % p->size;
 	///\todo: denormal check on y too?
-	return(y);
+	return y;
 }
 
-static inline float fixeddelay_read(ty_fixeddelay *p, int n)
-{
-	int i;
+/**********************************************************/
+// damper
 
-	i = (p->idx - n + p->size) % p->size;
-	return(p->buf[i]);
-}
-
-static inline void fixeddelay_write(ty_fixeddelay *p, float x)
-{
-	p->buf[p->idx] = x;
-	p->idx = (p->idx + 1) % p->size;
-}
-
-static inline void damper_set(ty_damper *p, float damping)
-{ 
+struct ty_damper {
+	float damping;
+	float delay;
+};
+ty_damper * damper_make(float);
+void damper_free(ty_damper *);
+void damper_flush(ty_damper *);
+inline void damper_set(ty_damper *p, float damping) {
 	p->damping = damping;
-} 
-	
-static inline float damper_do(ty_damper *p, float x)
-{ 
-	float y;
-	
-	y = x*(1.0-p->damping) + p->delay*p->damping;
+}
+inline float damper_do(ty_damper *p, float x) {
+	float y = x*(1.0-p->damping) + p->delay*p->damping;
 	psycle::helpers::math::erase_all_nans_infinities_and_denormals(y);
 	p->delay = y;
 	return(y);
 }
 
+/**********************************************************/
+// fixeddelay
+
+struct ty_fixeddelay {
+	int size;
+	int idx;
+	float *buf;
+};
+ty_fixeddelay * fixeddelay_make(int);
+void fixeddelay_free(ty_fixeddelay *);
+void fixeddelay_flush(ty_fixeddelay *);
+inline float fixeddelay_read(ty_fixeddelay *p, int n) {
+	int i = (p->idx - n + p->size) % p->size;
+	return(p->buf[i]);
+}
+inline void fixeddelay_write(ty_fixeddelay *p, float x) {
+	p->buf[p->idx] = x;
+	p->idx = (p->idx + 1) % p->size;
+}
+
+/**********************************************************/
+//int isprime(int);
+//int nearest_prime(int, float);
+	
 #endif
