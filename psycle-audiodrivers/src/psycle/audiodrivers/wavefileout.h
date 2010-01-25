94236ef9a2d24a1e6c496f9d5c2ff276638be655
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-01-25 16:42:51 +0000

    forgotton updates

--- psycle-audiodrivers/src/psycle/audiodrivers/wavefileout.h
+++ psycle-audiodrivers/src/psycle/audiodrivers/wavefileout.h
@@ -9,6 +9,8 @@
 #include <universalis/stdlib/thread.hpp>
 #include <universalis/stdlib/mutex.hpp>
 #include <universalis/stdlib/condition.hpp>
+#include <psycle/helpers/dither.hpp>
+#include <psycle/helpers/riff.hpp>
 
 namespace psycle { namespace audiodrivers {
 
@@ -20,11 +22,23 @@ class WaveFileOut : public AudioDriver {
 		WaveFileOut();
 		~WaveFileOut() throw();
 
+		void Write(float* data, unsigned int nframes);
+
+		void set_pdf(psycle::helpers::dsp::Dither::Pdf::type pdf) {
+			dither_.SetPdf(pdf);
+		}
+
+		void set_noiseshaping(psycle::helpers::dsp::Dither::NoiseShape::type shape) {
+			dither_.SetNoiseShaping(shape);
+		}
+
+		void set_dither_enabled(bool on) { dither_enabled_ = on; }
+
 	protected:
-		/*override*/ void do_open() throw(std::exception) {}
+		/*override*/ void do_open() throw(std::exception);
 		/*override*/ void do_start() throw(std::exception);
 		/*override*/ void do_stop() throw(std::exception);
-		/*override*/ void do_close() throw(std::exception) {}
+		/*override*/ void do_close() throw(std::exception);
 
 	private:
 		void thread_function();
@@ -33,6 +47,12 @@ class WaveFileOut : public AudioDriver {
 		typedef class scoped_lock<mutex> scoped_lock;
 		mutex mutable mutex_;
 		bool stop_requested_;
+
+		float left[65335];
+		float right[65335];
+		psycle::helpers::WaveFile wav_file_;
+		psycle::helpers::dsp::Dither dither_;
+		bool dither_enabled_;
 };
 
 }}
