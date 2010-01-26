107fbd0c68eac2302e723db11d53d2d4838f5b3e
natti__ <natti__@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-01-26 12:17:53 +0000

    savewavedlg: save tracks reactivated

--- trunk/psycle/src/psycle/host/PatHelperCommand.hpp
+++ trunk/psycle/src/psycle/host/PatHelperCommand.hpp
@@ -1,10 +1,10 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2007-2010 members of the psycle project http://psycle.sourceforge.net
 #pragma once
-#include "Command.hpp"
-#include "configuration_options.hpp"
 
-#if PSYCLE__CONFIGURATION__USE_PSYCORE
+#include "Command.hpp"
+
 #include <psycle/core/pattern.h>
-#endif
 
 // This class offers help to prepare undo/redo storage for patternview commands
 // It prepares the undo doing a copy of the pattern
@@ -23,17 +23,14 @@ namespace psycle {
 			virtual void Redo();
 
 		protected:
-
 			void PrepareUndoStorage();
 			void PrepareRedoStorage();
 			PatternView* pat_view() { return pat_view_; }
 
 		private:
 			PatternView* pat_view_;
-#if PSYCLE__CONFIGURATION__USE_PSYCORE
 			psycle::core::Pattern prev_pattern_;
 			psycle::core::Pattern next_pattern_;
-#endif
 		};
 
 	}	// namespace host
