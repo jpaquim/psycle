87aa080cffe276dd31b53c9e502790ad779a8cb6
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-02-08 00:09:26 +0000

    set thread cpu affinity

--- trunk/universalis/src/universalis/os/sched.hpp
+++ trunk/universalis/src/universalis/os/sched.hpp
@@ -24,6 +24,7 @@
 
 namespace universalis { namespace os { namespace sched {
 
+/// cpu affinity mask
 class UNIVERSALIS__COMPILER__DYNAMIC_LINK affinity_mask {
 	public:
 		/// initialises a zeroed-out mask
@@ -56,6 +57,7 @@ class UNIVERSALIS__COMPILER__DYNAMIC_LINK affinity_mask {
 		friend class process;
 };
 
+/// process
 class UNIVERSALIS__COMPILER__DYNAMIC_LINK process {
 	public:
 		/// native handle type
@@ -72,59 +74,73 @@ class UNIVERSALIS__COMPILER__DYNAMIC_LINK process {
 		native_handle_type native_handle_;
 		
 	public:
-		/// initialises an instance that represents the current process
-		process();
-		/// initialises an instance that represents the given process
-		process(native_handle_type native_handle) : native_handle_(native_handle) {}
+		///\name ctors
+		///\{
+			/// initialises an instance that represents the current process
+			process();
+			/// initialises an instance that represents the given process
+			process(native_handle_type native_handle) : native_handle_(native_handle) {}
+		///\}
 
-		/// gets the affinity mask against the set of cpu available to the process.
-		class affinity_mask affinity_mask() const throw(exception);
-		/// sets the affinity mask against the set of cpu available to the process.
-			#if defined DIVERSALIS__OS__MICROSOFT
-				/// special case for windows, this must be done inline!
-				/// The doc says:
-				/// "Do not call SetProcessAffinityMask in a DLL that may be called by processes other than your own."
-				inline
-			#endif
-		void affinity_mask(class affinity_mask const &) throw(exception);
+		///\name cpu affinity
+		///\{
+			/// affinity mask type
+			typedef class affinity_mask affinity_mask_type;
+			/// gets the affinity mask against the set of cpu available to the process.
+			affinity_mask_type affinity_mask() const throw(exception);
+			/// sets the affinity mask against the set of cpu available to the process.
+				#if defined DIVERSALIS__OS__MICROSOFT
+					/// special case for windows, this must be done inline!
+					/// The doc says:
+					/// "Do not call SetProcessAffinityMask in a DLL that may be called by processes other than your own."
+					inline
+				#endif
+			void affinity_mask(affinity_mask_type const &) throw(exception);
+		///\}
 
-		typedef
-			#if defined DIVERSALIS__OS__POSIX
-				int
-			#elif defined DIVERSALIS__OS__MICROSOFT
-				DWORD
-			#else
-				#error unsupported operating system
-			#endif
-			priority_type;
+		///\name priority
+		///\{
+			/// process priority type
+			typedef
+				#if defined DIVERSALIS__OS__POSIX
+					int
+				#elif defined DIVERSALIS__OS__MICROSOFT
+					DWORD
+				#else
+					#error unsupported operating system
+				#endif
+				priority_type;
 
-		/// gets the process priority level
-		priority_type priority() throw(exception);
-		/// sets the process priority level
-		void priority(priority_type priority) throw(exception);
+			/// gets the process priority level
+			priority_type priority() throw(exception);
+			/// sets the process priority level
+			void priority(priority_type priority) throw(exception);
 
-		struct priorities {
-			#if defined DIVERSALIS__OS__MICROSOFT
-				static priority_type const idle     = IDLE_PRIORITY_CLASS;
-				static priority_type const lowest   = IDLE_PRIORITY_CLASS;
-				static priority_type const low      = BELOW_NORMAL_PRIORITY_CLASS;
-				static priority_type const normal   = NORMAL_PRIORITY_CLASS;
-				static priority_type const high     = ABOVE_NORMAL_PRIORITY_CLASS;
-				static priority_type const highest  = HIGH_PRIORITY_CLASS;
-				static priority_type const realtime = REALTIME_PRIORITY_CLASS;
-			#else
-				// Note: These are nice values. Some systems may accept +20.
-				static priority_type const idle     = -20;
-				static priority_type const lowest   = -10;
-				static priority_type const low      = -5;
-				static priority_type const normal   =  0;
-				static priority_type const high     = +5;
-				static priority_type const highest  = +10;
-				static priority_type const realtime = +19;
-			#endif
-		};
+			/// process priority values
+			struct priorities {
+				#if defined DIVERSALIS__OS__MICROSOFT
+					static priority_type const idle     = IDLE_PRIORITY_CLASS;
+					static priority_type const lowest   = IDLE_PRIORITY_CLASS;
+					static priority_type const low      = BELOW_NORMAL_PRIORITY_CLASS;
+					static priority_type const normal   = NORMAL_PRIORITY_CLASS;
+					static priority_type const high     = ABOVE_NORMAL_PRIORITY_CLASS;
+					static priority_type const highest  = HIGH_PRIORITY_CLASS;
+					static priority_type const realtime = REALTIME_PRIORITY_CLASS;
+				#else
+					// Note: These are nice values. Some systems may accept +20.
+					static priority_type const idle     = -20;
+					static priority_type const lowest   = -10;
+					static priority_type const low      = -5;
+					static priority_type const normal   =  0;
+					static priority_type const high     = +5;
+					static priority_type const highest  = +10;
+					static priority_type const realtime = +19;
+				#endif
+			};
+		///\}
 };
 
+/// thread
 class UNIVERSALIS__COMPILER__DYNAMIC_LINK thread {
 	public:
 		/// native handle type
@@ -141,52 +157,65 @@ class UNIVERSALIS__COMPILER__DYNAMIC_LINK thread {
 		native_handle_type native_handle_;
 
 	public:
-		/// initialises an instance that represents the current thread
-		thread();
-		/// initialises an instance that represents the given thread
-		thread(native_handle_type native_handle) : native_handle_(native_handle) {}
+		///\name ctors
+		///\{
+			/// initialises an instance that represents the current thread
+			thread();
+			/// initialises an instance that represents the given thread
+			thread(native_handle_type native_handle) : native_handle_(native_handle) {}
+		///\}
 
-		/// gets the affinity mask against the set of cpu available to the process.
-		class affinity_mask affinity_mask() const throw(exception);
-		/// sets the affinity mask against the set of cpu available to the process.
-		void affinity_mask(class affinity_mask const &) throw(exception);
+		///\name cpu affinity
+		///\{
+			/// affinity mask type
+			typedef class affinity_mask affinity_mask_type;
+			/// gets the affinity mask against the set of cpu available to the process.
+			class affinity_mask affinity_mask() const throw(exception);
+			/// sets the affinity mask against the set of cpu available to the process.
+			void affinity_mask(class affinity_mask const &) throw(exception);
+		///\}
 
-		typedef int priority_type;
+		///\name priority
+		///\{
+			/// thread priority type
+			typedef int priority_type;
 
-		/// gets the thread priority level
-		priority_type priority() throw(exception);
-		/// sets the thread priority level
-		void priority(priority_type priority) throw(exception);
+			/// gets the thread priority level
+			priority_type priority() throw(exception);
+			/// sets the thread priority level
+			void priority(priority_type priority) throw(exception);
 
-		struct priorities {
-			#if defined DIVERSALIS__OS__MICROSOFT
-				static priority_type const idle     = THREAD_PRIORITY_IDLE;
-				static priority_type const lowest   = THREAD_PRIORITY_LOWEST;
-				static priority_type const low      = THREAD_PRIORITY_BELOW_NORMAL;
-				static priority_type const normal   = THREAD_PRIORITY_NORMAL;
-				static priority_type const high     = THREAD_PRIORITY_ABOVE_NORMAL;
-				static priority_type const highest  = THREAD_PRIORITY_HIGHEST;
-				static priority_type const realtime = THREAD_PRIORITY_TIME_CRITICAL;
-				// Note: If the thread (actually, the process) has the REALTIME_PRIORITY_CLASS base class,
-				//       then this can also be -7, -6, -5, -4, -3, 3, 4, 5, or 6.
-			#else
-				// Note: These are not the actual native values but an arbitrary scale.
-				//       The native values depends on the scheduling policy,
-				//       and the sched_get_priority_min(policy) and sched_get_priority_max(policy) functions
-				//       are used to rescale to native values.
-				//       The policy is chosen this way:
-				//           - value >  normal: if min == max, the policy is set to SCHED_RR,
-				//           - value == normal: SCHED_OTHER (the default policy of the os),
-				//           - value <  normal: SCHED_BATCH if defined, otherwise SCHED_OTHER.
-				static priority_type const idle     = -300; // min
-				static priority_type const lowest   = -200; // (max + min) * 1 / 6
-				static priority_type const low      = -100; // (max + min) * 2 / 6
-				static priority_type const normal   =    0; // (max + min) / 2
-				static priority_type const high     = +100; // (max + min) * 4 / 6
-				static priority_type const highest  = +200; // (max + min) * 5 / 6
-				static priority_type const realtime = +300; // max
-			#endif
-		};
+			/// thread priority values
+			struct priorities {
+				#if defined DIVERSALIS__OS__MICROSOFT
+					static priority_type const idle     = THREAD_PRIORITY_IDLE;
+					static priority_type const lowest   = THREAD_PRIORITY_LOWEST;
+					static priority_type const low      = THREAD_PRIORITY_BELOW_NORMAL;
+					static priority_type const normal   = THREAD_PRIORITY_NORMAL;
+					static priority_type const high     = THREAD_PRIORITY_ABOVE_NORMAL;
+					static priority_type const highest  = THREAD_PRIORITY_HIGHEST;
+					static priority_type const realtime = THREAD_PRIORITY_TIME_CRITICAL;
+					// Note: If the thread (actually, the process) has the REALTIME_PRIORITY_CLASS base class,
+					//       then this can also be -7, -6, -5, -4, -3, 3, 4, 5, or 6.
+				#else
+					// Note: These are not the actual native values but an arbitrary scale.
+					//       The native values depends on the scheduling policy,
+					//       and the sched_get_priority_min(policy) and sched_get_priority_max(policy) functions
+					//       are used to rescale to native values.
+					//       The policy is chosen this way:
+					//           - value >  normal: if min == max, the policy is set to SCHED_RR,
+					//           - value == normal: SCHED_OTHER (the default policy of the os),
+					//           - value <  normal: SCHED_BATCH if defined, otherwise SCHED_OTHER.
+					static priority_type const idle     = -300; // min
+					static priority_type const lowest   = -200; // (max + min) * 1 / 6
+					static priority_type const low      = -100; // (max + min) * 2 / 6
+					static priority_type const normal   =    0; // (max + min) / 2
+					static priority_type const high     = +100; // (max + min) * 4 / 6
+					static priority_type const highest  = +200; // (max + min) * 5 / 6
+					static priority_type const realtime = +300; // max
+				#endif
+			};
+		///\}
 };
 
 /****************************************************/
