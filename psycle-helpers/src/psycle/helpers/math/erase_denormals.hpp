9998a41c22e9695af999bcfd2715d5b6b222dc91
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-02-13 17:57:59 +0000

    bugfixed for gcc optimisations

--- psycle-helpers/src/psycle/helpers/math/erase_denormals.hpp
+++ psycle-helpers/src/psycle/helpers/math/erase_denormals.hpp
@@ -5,41 +5,43 @@
 #define PSYCLE__HELPERS__MATH__ERASE_DENORMALS__INCLUDED
 #pragma once
 
+#include <universalis/compiler.hpp>
+
 namespace psycle { namespace helpers { namespace math {
 
-inline float erase_denormals(float x) {
+float inline UNIVERSALIS__COMPILER__CONST erase_denormals(float x) {
 	x += 1.0e-30f;
 	x -= 1.0e-30f;
 	return x;
 }
 
-inline double erase_denormals(double x) {
+double inline UNIVERSALIS__COMPILER__CONST erase_denormals(double x) {
 	x += 1.0e-291;
 	x -= 1.0e-291;
 	return x;
 }
 
-inline float erase_denormals_inplace(float & x) {
+float inline erase_denormals_inplace(float & x) {
 	return x = erase_denormals(x);
 }
 
-inline double erase_denormals_inplace(double & x) {
+double inline erase_denormals_inplace(double & x) {
 	return x = erase_denormals(x);
 }
 
-inline float fast_erase_denormals(float x) {
+float inline UNIVERSALIS__COMPILER__CONST fast_erase_denormals(float x) {
 	return x + 1.0e-30f;
 }
 
-inline double fast_erase_denormals(double x) {
+double inline UNIVERSALIS__COMPILER__CONST fast_erase_denormals(double x) {
 	return x + 1.0e-291;
 }
 
-inline float fast_erase_denormals_inplace(float & x) {
+float inline fast_erase_denormals_inplace(float & x) {
 	return x += 1.0e-30f;
 }
 
-inline double fast_erase_denormals_inplace(double & x) {
+double inline fast_erase_denormals_inplace(double & x) {
 	return x += 1.0e-291;
 }
 
