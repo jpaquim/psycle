39c4eedec44d976710bedccb2e304cace817b81b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-04-05 18:20:50 +0000

    misc

--- tests/raytrace/src/raytrace/cpu/pixels.cpp
+++ tests/raytrace/src/raytrace/cpu/pixels.cpp
@@ -1,17 +1,28 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2009-2009 psycledelics http://psycle.pastnotecut.org : johan boule
+// copyright 2009-2010 psycledelics http://psycle.pastnotecut.org : johan boule
 
 #include "pixels.hpp"
-#include <cassert>
 
 namespace raytrace {
 
 pixels::pixels(unsigned int width, unsigned int height) :
-	width_(width),
-	height_(height),
-	row_stride_(width * sizeof *data_.words)
+	width_(), height_()
 {
+	data_.bytes = 0;
+	resize(width, height);
+}
+
+void pixels::resize(unsigned int width, unsigned int height) {
+	if(width_ == width && height_ == height) return;
+	width_ = width;
+	height_ = height;
+	row_stride_ = width * sizeof *data_.words;
+	delete[] data_.bytes;
 	data_.bytes = new char[width * height * sizeof *data_.words];
+}
+
+pixels::~pixels() {
+	delete[] data_.bytes;
 }
 
 void pixels::fill(color c) {
--- tests/raytrace/src/raytrace/cpu/render.hpp
+++ tests/raytrace/src/raytrace/cpu/render.hpp
@@ -1,5 +1,5 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2009-2009 psycledelics http://psycle.pastnotecut.org : johan boule
+// copyright 2009-2010 psycledelics http://psycle.pastnotecut.org : johan boule
 
 #ifndef RAYTRACE__RENDER
 #define RAYTRACE__RENDER
@@ -40,6 +40,7 @@ class render {
 
 	public:
 		class pixels & pixels() { return pixels_; }
+		void resize(unsigned int width, unsigned int height);
 	private:
 		class pixels & pixels_;
 
