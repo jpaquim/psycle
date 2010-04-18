8ed69732a132113cfa333703c5404335ecb2401a
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-04-18 11:53:02 +0000

    build fix for gcc + other resampler typenames changes

--- trunk/psycle-helpers/src/psycle/helpers/dsp.hpp
+++ trunk/psycle-helpers/src/psycle/helpers/dsp.hpp
@@ -379,125 +379,114 @@ namespace psycle { namespace helpers { /** various signal processing utility fun
 
 	/****************************************************************************/
 
-	/// sample interpolator kinds.
-	///\todo typdef should be inside the Resampler or Cubic class itself.
-	enum ResamplerQuality
-	{
-		R_NONE  = 0,
-		R_LINEAR,
-		R_SPLINE,
-		R_BANDLIM
-	};
-
-	/// interpolator work function.
-	///\todo typdef should be inside the Resampler class itself.
-	typedef float (*PRESAMPLERFN)(int16_t const * pData, uint64_t offset, uint32_t res, uint64_t length);
-	typedef float (*PRESAMPLERFNFLOAT)(float const * pData, float offset, uint64_t length);
-
 	/// sample interpolator.
-	class Resampler
-	{
+	class resampler {
 		public:
+			/// interpolator work function type
+			typedef float (*work_func_type)(int16_t const * data, uint64_t offset, uint32_t res, uint64_t length);
+			typedef float (*work_float_func_type)(float const * data, float offset, uint64_t length);
+			
+			/// sample interpolator kinds.
+			struct quality { enum type {
+				none,
+				linear,
+				spline,
+				band_limited
+			};};
+			
 			/// constructor
-			Resampler() : _pWorkFn(None), _pWorkFnFloat(None), _quality(R_NONE) {}
+			resampler() : work(none), work_float(none), quality_(quality::none) {}
+			
 			/// work function corresponding to the selected kind.
-			PRESAMPLERFN _pWorkFn;
-			PRESAMPLERFNFLOAT _pWorkFnFloat;
+			work_func_type work;
+			work_float_func_type work_float;
+			
 			/// sets the kind of interpolation.
-			virtual void SetQuality(ResamplerQuality quality) = 0;
-			virtual ResamplerQuality GetQuality() const = 0;
+			virtual void quality(quality::type) = 0;
+			virtual quality::type quality() const = 0;
+			
 		protected:
 			/// kind of interpolation.
-			ResamplerQuality _quality;
+			quality::type quality_;
+			
 			/// interpolation work function which does nothing.
-			static float None(int16_t const * pData, uint64_t offset, uint32_t res, uint64_t length)
-			{
-				return *pData;
+			static float none(int16_t const * data, uint64_t /*offset*/, uint32_t /*res*/, uint64_t /*length*/) {
+				return *data;
 			}
-			static float None(float const * pData, float offset, uint64_t length)
-			{
-				return *pData;
+			static float none(float const * data, float /*offset*/, uint64_t /*length*/) {
+				return *data;
 			}
 	};
 
 	/// cubic sample interpolator.
-	class Cubic : public Resampler
-	{
+	class cubic_resampler : public resampler {
 		///
 		#define CUBIC_RESOLUTION 2048
 		public:
 			/// constructor.
-			Cubic();
+			cubic_resampler();
 
-			virtual ResamplerQuality GetQuality() const { return _quality; }
+			/*override*/ quality::type quality() const { return quality_; }
 
-			/// refefinition.
-			virtual void SetQuality(ResamplerQuality quality)
-			{
-				_quality = quality;
-				switch (quality)
-				{
-					case R_NONE:
-					_pWorkFn = None;
+			/*override*/ void quality(quality::type quality) {
+				quality_ = quality;
+				switch(quality) {
+					case quality::none:
+					work = none;
 					break;
-				case R_LINEAR:
-					_pWorkFn = Linear;
+				case quality::linear:
+					work = linear;
 					break;
-				case R_SPLINE:
-					_pWorkFn = Spline;
-					_pWorkFnFloat = SplineFloat;
+				case quality::spline:
+					work = spline;
+					work_float = spline_float;
 					break;
-				case R_BANDLIM:
-					_pWorkFn = Bandlimit;
+				case quality::band_limited:
+					work = band_limited;
 					break;
 				}
 			}
 
 		protected:
 			/// interpolation work function which does linear interpolation.
-			static float Linear(int16_t const * pData, uint64_t offset, uint32_t res, uint64_t length)
-			{
-				float y0,y1;
-				y0 = *pData;
-				y1 =(offset+1 == length)?0:*(pData+1);
-				return (y0+(y1-y0)*_lTable[res>>21]);
+			static float linear(int16_t const * data, uint64_t offset, uint32_t res, uint64_t length) {
+				float y0 = *data;
+				float y1 = offset + 1 == length ? 0 : *(data + 1);
+				return y0 + (y1 - y0) * l_table_[res >> 21];
 			}
-			/// interpolation work function which does spline interpolation.
-			static float Spline(const short *pData, uint64_t offset, uint32_t res, uint64_t length)
-			{
-				float yo, y0,y1, y2;
-				res = res >> 21;
 			
-				yo=(offset==0)?0:*(pData-1);
-				y0=*(pData);
-				y1=(offset+1 == length)?0:*(pData+1);
-				y2=(offset+2 == length)?0:*(pData+2);
-				return (_aTable[res]*yo+_bTable[res]*y0+_cTable[res]*y1+_dTable[res]*y2);
-			}
 			/// interpolation work function which does spline interpolation.
-			static float SplineFloat(float const * pData, float offset, uint64_t length)
-			{
-				int iOsc=floor(offset);
-				uint32_t fractpart=(offset-iOsc)*CUBIC_RESOLUTION;
+			static float spline(int16_t const * data, uint64_t offset, uint32_t res, uint64_t length) {
+				res = res >> 21;
+				float yo = offset == 0 ? 0 : *(data - 1);
+				float y0 = *data;
+				float y1 = offset + 1 == length ? 0 : *(data + 1);
+				float y2 = offset + 2 == length ? 0 : *(data + 2);
+				return a_table_[res] * yo + b_table_[res] * y0 + c_table_[res] * y1 + d_table_[res] * y2;
+			}
+			
+			/// interpolation work function which does spline interpolation.
+			static float spline_float(float const * data, float offset, uint64_t length) {
+				int iOsc = std::floor(offset);
+				uint32_t fractpart = (offset - iOsc) * CUBIC_RESOLUTION;
 
 				float d0;
-				float d1=pData[iOsc];
-				float d2=pData[iOsc+1];
-				float d3=pData[iOsc+2];
-				if (iOsc == 0) {
-					d0=pData[length - 1];
-				}
-				else {
-					d0=pData[iOsc-1];
-				}
-				return (_aTable[fractpart]*d0+_bTable[fractpart]*d1+_cTable[fractpart]*d2+_dTable[fractpart]*d3);
+				float d1 = data[iOsc];
+				float d2 = data[iOsc + 1];
+				float d3 = data[iOsc + 2];
+				if(iOsc == 0)
+					d0 = data[length - 1];
+				else
+					d0 = data[iOsc - 1];
+				return a_table_[fractpart] * d0 + b_table_[fractpart] * d1 + c_table_[fractpart] * d2 + d_table_[fractpart] * d3;
 			}			
+			
 			// yo = y[-1] [sample at x-1]
 			// y0 = y[0]  [sample at x (input)]
 			// y1 = y[1]  [sample at x+1]
 			// y2 = y[2]  [sample at x+2]
 			
-			// res= distance between two neighboughing sample points [y0 and y1], so [0...1.0].
+			// res = distance between two neighboughing sample points [y0 and y1], so [0...1.0].
 			// You have to multiply this distance * RESOLUTION used
 			// on the spline conversion table. [2048 by default]
 			// If you are using 2048 is asumed you are using 12 bit decimal
@@ -519,26 +508,24 @@ namespace psycle { namespace helpers { /** various signal processing utility fun
 			#define SINC_TABLESIZE SINC_RESOLUTION * SINC_ZEROS
 
 			/// interpolation work function which does band-limited interpolation.
-			static float Bandlimit(int16_t const * pData, uint64_t offset, uint32_t res, uint64_t length)
-			{
-				res = res>>23; //!!!assumes SINC_RESOLUTION == 512!!!
+			static float band_limited(int16_t const * data, uint64_t offset, uint32_t res, uint64_t length) {
+				res = res >> 23; // TODO assumes SINC_RESOLUTION == 512
 				int leftExtent(SINC_ZEROS), rightExtent(SINC_ZEROS);
-				if(offset<SINC_ZEROS) leftExtent=offset;
-				if(length-offset<SINC_ZEROS) rightExtent=length-offset;
+				if(offset < SINC_ZEROS) leftExtent = offset;
+				if(length - offset < SINC_ZEROS) rightExtent = length - offset;
 				
 				const int sincInc(SINC_RESOLUTION);
-				float newval(0.0);
 
-				newval += sincTable[res] * *(pData);
-				float sincIndex(sincInc+res);
-				float weight(sincIndex - floor(sincIndex));
-				for(int i(1); i < leftExtent; ++i, sincIndex+=sincInc)
-					newval+= (sincTable[(int)sincIndex] + sincDelta[(int)sincIndex]*weight ) * *(pData-i);
+				float newval = sinc_table_[res] * *data;
+				float sincIndex(sincInc + res);
+				float weight(sincIndex - std::floor(sincIndex));
+				for(int i(1); i < leftExtent; ++i, sincIndex += sincInc)
+					newval += (sinc_table_[int(sincIndex)] + sinc_delta_[int(sincIndex)] * weight) * *(data - i);
 
-				sincIndex = sincInc-res;
-				weight = sincIndex - floor(sincIndex);
-				for(int i(1); i < rightExtent; ++i, sincIndex+=sincInc)
-					newval += ( sincTable[(int)sincIndex] + sincDelta[(int)sincIndex]*weight ) * *(pData+i);
+				sincIndex = sincInc - res;
+				weight = sincIndex - std::floor(sincIndex);
+				for(int i(1); i < rightExtent; ++i, sincIndex += sincInc)
+					newval += (sinc_table_[int(sincIndex)] + sinc_delta_[int(sincIndex)] * weight) * *(data + i);
 
 				return newval;
 			}
@@ -603,19 +590,19 @@ namespace psycle { namespace helpers { /** various signal processing utility fun
 
 		private:
 			/// Currently is 2048
-			static int _resolution;
+			static int resolution_;
 
-			static float _aTable[CUBIC_RESOLUTION];
-			static float _bTable[CUBIC_RESOLUTION];
-			static float _cTable[CUBIC_RESOLUTION];
-			static float _dTable[CUBIC_RESOLUTION];
-			static float _lTable[CUBIC_RESOLUTION];
+			static float a_table_[CUBIC_RESOLUTION];
+			static float b_table_[CUBIC_RESOLUTION];
+			static float c_table_[CUBIC_RESOLUTION];
+			static float d_table_[CUBIC_RESOLUTION];
+			static float l_table_[CUBIC_RESOLUTION];
 
 			/// sinc function table
-			static float sincTable[SINC_TABLESIZE];
+			static float sinc_table_[SINC_TABLESIZE];
 
 			/// table of deltas between sincTable indices.. sincDelta[i] = sincTable[i+1] - sincTable[i]
 			/// used to speed up linear interpolation of sinc table-- this idea stolen from libresampler
-			static float sincDelta[SINC_TABLESIZE];
+			static float sinc_delta_[SINC_TABLESIZE];
 	};
 }}}
