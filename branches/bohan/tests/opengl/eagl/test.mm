d3f64c811eaa93f4f14edd2d37bb454a74bc276c
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-07-04 03:45:15 +0000

    eagl test

--- branches/bohan/tests/opengl/eagl/test.mm
+++ branches/bohan/tests/opengl/eagl/test.mm
@@ -1,5 +1,5 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2009-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+// copyright 2010-2010 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
 #import <OpenGLES/EAGL.h>
 #import <OpenGLES/ES2/gl.h>
