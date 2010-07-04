f7e87568fe52509782803f9df19edf5c1b5a881b
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-07-04 02:29:54 +0000

    eagl test

--- tests/opengl/eagl/test.mm
+++ tests/opengl/eagl/test.mm
@@ -2,8 +2,16 @@
 // copyright 2009-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
 #import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES2/gl.h>
 
 int main() {
+	EAGLContext* myContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+	[EAGLContext setCurrentContext: myContext];
+
+	GLuint framebuffer;
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
 	return 0;
 }
 
