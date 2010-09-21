37e21072a62b8edb2e285986b5a2ccb56752fe87
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-09-21 20:37:05 +0000

    aligned allocator

--- universalis/src/universalis/os/aligned_memory_alloc.hpp
+++ universalis/src/universalis/os/aligned_memory_alloc.hpp
@@ -41,6 +41,7 @@ void aligned_memory_alloc(std::size_t alignment, X *& x, std::size_t count) {
 		size; // unused
 		x = new X[count];
 		// note: free with delete[]
+		#error TODO alloc some extra mem and store orig pointer or alignment to the x[-1]
 	#endif
 }
 
@@ -54,8 +55,79 @@ void aligned_memory_dealloc(X *& address) {
 		_aligned_free(address);
 	#else
 		delete[] address;
+		#error TODO alloc some extra mem and store orig pointer or alignment to the x[-1]
 	#endif
 }
+
+template<typename T, std::size_t Alignment>
+class aligned_alloc {
+	public:
+		// type definitions
+		typedef T value_type;
+		typedef T* pointer;
+		typedef const T* const_pointer;
+		typedef T& reference;
+		typedef const T& const_reference;
+		typedef std::size_t size_type;
+		typedef std::ptrdiff_t difference_type;
+
+		// rebind allocator to type U
+		template<typename T2>
+		class rebind {
+			public: typedef aligned_alloc<T2, Alignment> other;
+		};
+
+		// return address of values
+		pointer address(reference value) const { return &value; }
+		const_pointer address(const_reference value) const { return &value; }
+
+		// constructors and destructor. nothing to do because the allocator has no state
+		aligned_alloc() throw() {}
+		aligned_alloc(const aligned_alloc&) throw() {}
+		template<typename T2, std::size_t Alignment2> aligned_alloc(const aligned_alloc<T2, Alignment2>&) throw() {}
+		~aligned_alloc() throw() {}
+
+		// return maximum number of elements that can be allocated
+		size_type max_size() const throw() { return std::numeric_limits<std::size_t>::max() / sizeof(T); }
+
+		// allocate but don't initialize num elements of type T
+		pointer allocate(size_type num, const void* = 0) {
+			// print message and allocate memory with global new
+			std::cerr << "allocate " << num << " element(s)" << " of size " << sizeof(T) << std::endl;
+			//pointer ret = static_cast<pointer>(::operator new(num * sizeof(T)));
+			pointer ret;
+			aligned_memory_alloc(Alignment, ret, num);
+			std::cerr << " allocated at: " << ret << std::endl;
+			return ret;
+		}
+
+		// initialize elements of allocated storage p with value value
+		void construct(pointer p, const T& value) {
+			// initialize memory with placement new
+			new(p) T(value);
+		}
+
+		// destroy elements of initialized storage p
+		void destroy(pointer p) {
+			// destroy objects by calling their destructor
+			p->~T();
+		}
+
+		// deallocate storage p of deleted elements
+		void deallocate(pointer p, size_type num) {
+			// print message and deallocate memory with global delete
+			std::cerr << "deallocate " << num << " element(s)" << " of size " << sizeof(T) << " at: " << p << std::endl;
+			//::operator delete(p);
+			aligned_memory_dealloc(p);
+		}
+};
+
+// return that all specializations of this allocator are interchangeable
+template<typename T1, std::size_t Alignment1, typename T2, std::size_t Alignment2>
+bool operator==(const aligned_alloc<T1, Alignment1>&, const aligned_alloc<T2, Alignment2>&) throw() { return true; }
+
+template<typename T1, std::size_t Alignment1, typename T2, std::size_t Alignment2>
+bool operator!=(const aligned_alloc<T1, Alignment1>&, const aligned_alloc<T2, Alignment2>&) throw() { return false; }
 
 }}
 
