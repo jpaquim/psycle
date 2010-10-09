742853e5bb90ff7407baa303980ffc4b519d87bb
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-10-09 14:50:00 +0000

    wtf

--- tests/freepsycle.old/freepsycle/src/psycle/host/scheduler.cpp
+++
@@ -1,9 +0,0 @@
-// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2009 members of the psycle project http://psycle.pastnotecut.org : johan boule <bohan@jabber.org>
-
-///\implementation psycle::host::scheduler
-#include <psycle/detail/project.private.hpp>
-#include "scheduler.hpp"
-namespace psycle { namespace host {
-	// everything is inlined
-}}
