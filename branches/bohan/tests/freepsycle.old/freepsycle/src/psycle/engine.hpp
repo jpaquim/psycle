a867c1bbea8d2e02866fa390dc61840942ffb9ab
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-10-09 14:50:00 +0000

    wtf

--- branches/bohan/tests/freepsycle.old/freepsycle/src/psycle/engine.hpp
+++
@@ -1,7 +0,0 @@
-// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2009 members of the psycle project http://psycle.pastnotecut.org : johan boule <bohan@jabber.org>
-
-///\file
-///\brief meta header: all the psycle::engine interface in one header file inclusion.
-#pragma once
-#include "engine/engine.hpp"
