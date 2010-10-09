9d7174d10e9ed1a90c8de67f40c821c7a1d8a8ef
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-10-09 14:48:49 +0000

    old vs new

---
+++ branches/bohan/tests/freepsycle.old/freepsycle/src/psycle/plugins/bipolar_filter.cpp
@@ -0,0 +1,54 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 1999-2009 members of the psycle project http://psycle.pastnotecut.org : johan boule <bohan@jabber.org>
+
+///\\implementation psycle::plugins::bipolar_filter
+#include <psycle/detail/project.private.hpp>
+#include "bipolar_filter.hpp"
+namespace psycle { namespace plugins {
+
+bipolar_filter::bipolar_filter(engine::plugin_library_reference & plugin_library_reference, engine::graph & graph, std::string const & name, real const & logical_zero)
+:
+	node(plugin_library_reference, graph, name),
+	logical_zero_(logical_zero)
+{
+	engine::ports::inputs::multiple::create_on_heap(*this, "in", /*single_connection_is_identity_transform*/ boost::cref(true));
+	engine::ports::output::create_on_heap(*this, "out");
+}
+
+void bipolar_filter::channel_change_notification_from_port(engine::port const & port) throw(engine::exception) {
+	if(&port == output_ports()[0]) multiple_input_port()->propagate_channels(port.channels());
+	else if(&port == multiple_input_port()) output_ports()[0]->propagate_channels(port.channels());
+	assert(multiple_input_port()->channels() == output_ports()[0]->channels());
+}
+
+void bipolar_filter::seconds_per_event_change_notification_from_port(engine::port const & port) {
+	if(&port == output_ports()[0]) multiple_input_port()->propagate_seconds_per_event(port.seconds_per_event());
+	else if(&port == multiple_input_port()) output_ports()[0]->propagate_seconds_per_event(port.seconds_per_event());
+	assert(engine::math::roughly_equals(multiple_input_port()->seconds_per_event(), output_ports()[0]->seconds_per_event()));
+}
+
+void bipolar_filter::do_process_first() throw(engine::exception) {
+	if(!*output_ports()[0]) return;
+	if(!*multiple_input_port()) return;
+	assert(&multiple_input_port()->buffer());
+	assert(&output_ports()[0]->buffer());
+	engine::buffer & in(multiple_input_port()->buffer());
+	engine::buffer & out(output_ports()[0]->buffer());
+	assert(out.channels() == in.channels());
+	for(std::size_t channel(0); channel < in.channels(); ++channel)
+		for(std::size_t event(0); event < in.events() && in[channel][event].index() < in.events(); ++event) {
+			// not needed because in and out have the same buffer: out[channel][event].index(event);
+			out[channel][event].sample() = logical_zero_;
+		}
+	do_process();
+}
+
+void bipolar_filter::do_process() throw(engine::exception) {
+	if(!*output_ports()[0]) return;
+	if(!*multiple_input_port()) return;
+	engine::buffer & in(multiple_input_port()->buffer());
+	engine::buffer & out(output_ports()[0]->buffer());
+	for(std::size_t channel(0); channel < in.channels(); ++channel) out[channel].flag(in[channel].flag());
+}
+
+}}
---
+++ branches/bohan/tests/freepsycle.old/freepsycle/src/psycle/tests/random_notes/score2.cpp
@@ -0,0 +1,60 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2007-2010 members of the psycle project http://psycle.pastnotecut.org : johan boule <bohan@jabber.org>
+
+///\implementation psycle::plugins::sequence
+#include <psycle/detail/project.private.hpp>
+#include "score2.hpp"
+namespace psycle { namespace tests { namespace random_notes {
+
+score2::score2(host::plugin_resolver & resolver, engine::graph & graph)
+:
+	freq_("freq"),
+	resolver_(resolver),
+	graph_(graph),
+	sine_(static_cast<plugins::sine&>(resolver("sine", graph, "sine").node())),
+	freq_plug_(static_cast<plugins::sequence&>(resolver("sequence", graph, freq_.name()).node()))
+{}
+
+void score2::connect(engine::node & out) {
+	freq_plug_.sequence_iterator() = new engine::sequence_iterator(freq_);
+	
+	sine_.output_port("out")->connect(*out.input_port("in"));
+	sine_.input_port("frequency")->connect(*freq_plug_.output_port("out"));
+}
+
+void score2::generate() {
+	sine_.amplitude(1);
+	//sine_.frequency(440);
+
+	engine::real const beats_per_second(1);
+	unsigned int const notes(100000);
+	engine::real beat(0);
+	engine::real duration(0.00001 / beats_per_second);
+	float slowdown(0.001);
+	float f1(100), f2(400), f3(1000);
+	float ratio(1.1);
+	for(unsigned int note(0); note < notes; ++note) {
+		//std::clog << beat << ' ' << f1 << ' ' << f2 << ' ' << f3 << '\n';
+
+		engine::real const b1(beat), b2(beat * 1.1), b3(beat * 1.2);
+
+		freq_.insert_event(b1, f1);
+
+		f1 *= ratio;
+		if(f1 > 8000) { f1 /= 150; ratio *= 1.05; }
+		f2 *= ratio * ratio;
+		if(f2 > 8000) f2 /= 150;
+		f3 *= ratio * ratio * ratio;
+		if(f3 > 8000) f3 /= 150;
+		if(ratio > 1.5) ratio -= 0.5;
+		if(ratio < 1.01) ratio += 0.01;
+		beat += duration;
+		duration += duration * slowdown;
+		if(
+			duration > 0.1  / beats_per_second ||
+			duration < 0.00001 / beats_per_second
+		) slowdown = -slowdown;
+	}
+}
+
+}}}
