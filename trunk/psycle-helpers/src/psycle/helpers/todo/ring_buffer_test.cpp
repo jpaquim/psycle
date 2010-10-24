f2ad90170b52d29b390a0c32ac4a82ee46b69bfd
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2010-10-24 02:49:10 +0000

    tests

--- trunk/psycle-helpers/src/psycle/helpers/todo/ring_buffer_test.cpp
+++ trunk/psycle-helpers/src/psycle/helpers/todo/ring_buffer_test.cpp
@@ -1,3 +1,5 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2008-2010 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
 // compile with g++ --std=c++0x -pthread -Wall -O3
 
