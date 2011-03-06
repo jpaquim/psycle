162ae6e8456e4a78b06774918b0a2aa0a63beb4f
jaz001 <jaz001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2011-03-06 17:05:19 +0000

    source improvements on stk.reverbs

--- psycle-plugins/src/psycle/plugins/stk/stk.reverbs.cpp
+++ psycle-plugins/src/psycle/plugins/stk/stk.reverbs.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////
-// Dmitry "Sartorius" Kulikov stk Plucked plugin for PSYCLE
-// v0.3
+// Dmitry "Sartorius" Kulikov stk Reverbs plugin for PSYCLE
+// v1.1
 //
 // Based on The Synthesis ToolKit in C++ (STK)
 // By Perry R. Cook and Gary P. Scavone, 1995-2004.
@@ -8,7 +8,6 @@
 
 #include <psycle/plugin_interface.hpp>
 #include <stk/Stk.h>
-#include <stk/Effect.h>
 #include <stk/JCRev.h>
 #include <stk/NRev.h>
 #include <stk/PRCRev.h>
@@ -18,10 +17,10 @@ using namespace psycle::plugin_interface;
 // Stk recently got a namespace. We (re)declare it for backward compatibility with older stk versions.
 namespace stk {} using namespace stk;
 
-CMachineParameter const paraRev = {"Reverb", "Reverb", 0, 2, MPF_STATE, 0};
-CMachineParameter const paraTime = {"Time", "Time", 1, 32767, MPF_STATE, 32};
-CMachineParameter const paraDryWet = {"Dry/Wet", "Dry/Wet", 0,100, MPF_STATE, 50};
-CMachineParameter const paraMixing = {"Mixing", "Mixing", 0, 1, MPF_STATE, 0};
+CMachineParameter const paraRev = {"Reverb", "Reverb", 0, 2, MPF_STATE, 1};
+CMachineParameter const paraTime = {"Time", "Time", 1, 1024, MPF_STATE, 80};
+CMachineParameter const paraDryWet = {"Dry/Wet", "Dry/Wet", 0,100, MPF_STATE, 25};
+CMachineParameter const paraMixing = {"Mixing", "Mixing", 0, 1, MPF_STATE, 1};
 
 CMachineParameter const *pParameters[] = 
 { 
@@ -44,7 +43,7 @@ CMachineInfo const MacInfo(
 			#endif
 			,
 		"stk Reverbs",
-		"Sartorius and STK 4.2.1 developers",
+		"Sartorius and STK developers",
 		"Help",
 		1
 );
@@ -65,7 +64,6 @@ class mi : public CMachineInterface {
 			JCRev   jcrev[2];
 			NRev    nrev[2];
 			PRCRev  pcrrev[2];
-			Effect  *rev_l, *rev_r;
 			StkFloat samplerate;
 };
 
@@ -73,8 +71,6 @@ PSYCLE__PLUGIN__INSTANTIATOR(mi, MacInfo)
 
 mi::mi() {
 	Vals = new int[MacInfo.numParameters];
-		rev_l = &jcrev[0];
-		rev_r = &jcrev[1];
 }
 
 mi::~mi() {
@@ -107,102 +103,97 @@ void mi::ParameterTweak(int par, int val) {
 	Vals[par] = val;
 	switch(par) {
 		case 0:
+		{
 			switch(val) {
-				case 0: rev_l = &jcrev[0]; rev_r = &jcrev[1]; break;
-				case 1: rev_l = &nrev[0]; rev_r = &nrev[1]; break;
-				case 2: rev_l = &pcrrev[0]; rev_r = &pcrrev[1]; break;
+				case 0: jcrev[0].clear(); jcrev[1].clear(); break;
+				case 1: nrev[0].clear(); nrev[1].clear(); break;
+				case 2: pcrrev[0].clear(); pcrrev[1].clear(); break;
 			}
-			rev_l->clear();
-			rev_r->clear();
-		break;
+			break;
+		}
 		case 1:
+		{
 			for(unsigned int i = 0; i < 2; ++i) {
 				jcrev[i].setT60(StkFloat(val) * 0.03125);
 				nrev[i].setT60(StkFloat(val) * 0.03125);
 				pcrrev[i].setT60(StkFloat(val) * 0.03125);
 			}
+			break;
+		}
+		case 2:
+		{
+			StkFloat const drywet = StkFloat(val)*.01;
+			jcrev[0].setEffectMix(drywet); jcrev[1].setEffectMix(drywet);
+			nrev[0].setEffectMix(drywet); nrev[1].setEffectMix(drywet);
+			pcrrev[0].setEffectMix(drywet); pcrrev[1].setEffectMix(drywet);
+			break;
+		}
 	}
 }
 
 void mi::Work(float *psamplesleft, float *psamplesright , int numsamples,int tracks) {
-	if(Vals[2]) {
-		StkFloat const drywet = StkFloat(Vals[2])*.01;
-		rev_l->setEffectMix(drywet);
-		rev_r->setEffectMix(drywet);
-		if(Vals[3] == 0) {
-			#if STK_VERSION != -1
-				switch(Vals[0]) {
-					case 0:
-						do {
-							jcrev[0].tick(StkFloat(*psamplesleft / 65536)) * 65536.f;
-							jcrev[1].tick(StkFloat(*psamplesright / 65536)) * 65536.f;
-							++psamplesleft;
-							++psamplesright;
-						} while(--numsamples);
-					case 1:
-						do {
-							nrev[0].tick(StkFloat(*psamplesleft / 65536)) * 65536.f;
-							nrev[1].tick(StkFloat(*psamplesright / 65536)) * 65536.f;
-							++psamplesleft;
-							++psamplesright;
-						} while(--numsamples);
-					case 2:
-						do {
-							pcrrev[0].tick(StkFloat(*psamplesleft / 65536)) * 65536.f;
-							pcrrev[1].tick(StkFloat(*psamplesright / 65536)) * 65536.f;
-							++psamplesleft;
-							++psamplesright;
-						} while(--numsamples);
-				}
-			#else
+	if(Vals[2] == 0) 
+		return;
+
+	if(Vals[3] == 0) {
+		switch(Vals[0]) {
+			case 0:
 				do {
-					*psamplesleft=rev_l->tick(StkFloat(*psamplesleft / 65536)) * 65536.f;
-					*psamplesright=rev_r->tick(StkFloat(*psamplesright / 65536)) * 65536.f;
+					*psamplesleft = jcrev[0].tick(StkFloat(*psamplesleft),0);
+					*psamplesright = jcrev[1].tick(StkFloat(*psamplesright),1);
 					++psamplesleft;
 					++psamplesright;
 				} while(--numsamples);
-			#endif
-		} else {
-			#if STK_VERSION != -1
-				switch(Vals[0]) {
-					case 0:
-						do {
-							jcrev[0].tick(StkFloat(*psamplesleft / 65536));
-							jcrev[1].tick(StkFloat(*psamplesright / 65536));
-							*psamplesleft = float(jcrev[0].lastOut(0) + 0.3 * jcrev[0].lastOut(1)) * 65536.f;
-							*psamplesright = float(jcrev[1].lastOut(1) + 0.3 * jcrev[1].lastOut(0)) * 65536.f;
-							++psamplesleft;
-							++psamplesright;
-						} while(--numsamples);
-					case 1:
-						do {
-							nrev[0].tick(StkFloat(*psamplesleft / 65536));
-							nrev[1].tick(StkFloat(*psamplesright / 65536));
-							*psamplesleft = float(nrev[0].lastOut(0) + 0.3 * nrev[0].lastOut(1)) * 65536.f;
-							*psamplesright = float(nrev[1].lastOut(1) + 0.3 * nrev[1].lastOut(0)) * 65536.f;
-							++psamplesleft;
-							++psamplesright;
-						} while(--numsamples);
-					case 2:
-						do {
-							pcrrev[0].tick(StkFloat(*psamplesleft / 65536));
-							pcrrev[1].tick(StkFloat(*psamplesright / 65536));
-							*psamplesleft = float(pcrrev[0].lastOut(0) + 0.3 * pcrrev[0].lastOut(1)) * 65536.f;
-							*psamplesright = float(pcrrev[1].lastOut(1) + 0.3 * pcrrev[1].lastOut(0)) * 65536.f;
-							++psamplesleft;
-							++psamplesright;
-						} while(--numsamples);
-				}
-			#else
+				break;
+			case 1:
 				do {
-					rev_l->tick(StkFloat(*psamplesleft / 65536));
-					rev_r->tick(StkFloat(*psamplesright / 65536));
-					*psamplesleft = float(rev_l->lastOutLeft() + 0.3 * rev_r->lastOutRight()) * 65536.f;
-					*psamplesright = float(rev_l->lastOutRight() + 0.3 * rev_r->lastOutLeft()) * 65536.f;
+					*psamplesleft = nrev[0].tick(StkFloat(*psamplesleft),0);
+					*psamplesright = nrev[1].tick(StkFloat(*psamplesright),1);
 					++psamplesleft;
 					++psamplesright;
 				} while(--numsamples);
-			#endif
+				break;
+			case 2:
+				do {
+					*psamplesleft = pcrrev[0].tick(StkFloat(*psamplesleft),0);
+					*psamplesright = pcrrev[1].tick(StkFloat(*psamplesright),1);
+					++psamplesleft;
+					++psamplesright;
+				} while(--numsamples);
+				break;
+		}
+	} else {
+		switch(Vals[0]) {
+			case 0:
+				do {
+					jcrev[0].tick(StkFloat(*psamplesleft));
+					jcrev[1].tick(StkFloat(*psamplesright));
+					*psamplesleft = float(jcrev[0].lastOut(0) + 0.3 * jcrev[1].lastOut(0));
+					*psamplesright = float(jcrev[1].lastOut(1) + 0.3 * jcrev[0].lastOut(1));
+					++psamplesleft;
+					++psamplesright;
+				} while(--numsamples);
+				break;
+			case 1:
+				do {
+					nrev[0].tick(StkFloat(*psamplesleft));
+					nrev[1].tick(StkFloat(*psamplesright));
+					*psamplesleft = float(nrev[0].lastOut(0) + 0.3 * nrev[1].lastOut(0));
+					*psamplesright = float(nrev[1].lastOut(1) + 0.3 * nrev[0].lastOut(1));
+					++psamplesleft;
+					++psamplesright;
+				} while(--numsamples);
+				break;
+			case 2:
+				do {
+					pcrrev[0].tick(StkFloat(*psamplesleft));
+					pcrrev[1].tick(StkFloat(*psamplesright));
+					*psamplesleft = float(pcrrev[0].lastOut(0) + 0.3 * pcrrev[1].lastOut(0));
+					*psamplesright = float(pcrrev[1].lastOut(1) + 0.3 * pcrrev[0].lastOut(1));
+					++psamplesleft;
+					++psamplesright;
+				} while(--numsamples);
+				break;
 		}
 	}
 }
@@ -213,13 +204,13 @@ bool mi::DescribeValue(char* txt, int const param, int const value) {
 		case 0:
 			switch(value) {
 				case 0:
-					std::sprintf(txt, "JCRev");
+					std::sprintf(txt, "Church (JCRev)");
 					return true;
 				case 1:
-					std::sprintf(txt, "NRev");
+					std::sprintf(txt, "Open (NRev)");
 					return true;
 				case 2:
-					std::sprintf(txt, "PCRRev");
+					std::sprintf(txt, "Room (PCRRev)");
 					return true;
 			}
 		break;
@@ -235,7 +226,7 @@ bool mi::DescribeValue(char* txt, int const param, int const value) {
 			return true;
 		break;
 		case 3:
-			std::sprintf(txt, value == 0 ? "as is" : "mix channels");
+			std::sprintf(txt, value == 0 ? "independent" : "mix channels");
 			return true;
 	}
 	return false;
