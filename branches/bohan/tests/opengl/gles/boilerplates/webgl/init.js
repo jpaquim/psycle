f32cabf279f4fd5f857de04f0b282377a40b9f31
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2011-04-02 16:05:09 +0000

    misc

--- branches/bohan/tests/opengl/gles/boilerplates/webgl/init.js
+++ branches/bohan/tests/opengl/gles/boilerplates/webgl/init.js
@@ -1,8 +1,20 @@
 
+function log(obj) { console.log(obj) }
+
 gl = {};
 
 gl.getCtx = function(canvas, attr) {
+	log('gl.getCtx: for canvas id: ' + canvas.id);
 	function getCtx(name) { return attr === undefined ? canvas.getContext(name) : canvas.getContext(name, attr); }
-	return getCtx('webgl') || getCtx('experimental-webgl');
+	var ctx = getCtx('webgl') || getCtx('experimental-webgl');
+	if(!ctx) throw 'Your browser does not support webgl';
+	return ctx;
 }
 
+gl.resizeCanvas = function(canvas, width, height) {
+	if(width === undefined) width = canvas.scrollWidth;
+	if(height === undefined) height = canvas.scrollHeight;
+	log('gl.resizeCanvas: ' + width + ', ' + height);
+	canvas.width = width;
+	canvas.height = height;
+}
