1b68bb9bea8404fc672ade6be3f90ec5801c31d7
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2011-06-11 21:05:06 +0000

    fp

--- branches/bohan/tests/freepsycle.new/src/psycle/detail/config.hpp
+++
@@ -1,8 +0,0 @@
-// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2009-2010 members of the psycle project http://psycle.sourceforge.net
-
-#ifndef PSYCLE__DETAIL__CONFIG__INCLUDED
-#define PSYCLE__DETAIL__CONFIG__INCLUDED
-#pragma once
-
-#endif
