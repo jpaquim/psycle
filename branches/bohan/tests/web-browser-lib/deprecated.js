051e03bf684696d033215fe9ea513f7d57689550
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2011-07-15 23:55:37 +0000

    web tests

---
+++ branches/bohan/tests/web-browser-lib/deprecated.js
@@ -0,0 +1,18 @@
+var bohan = bohan || {};
+
+bohan.log = function(/*arguments*/) {
+	console.log(arguments);
+};
+	
+bohan.deprecationEnabled = true;
+	
+bohan.deprecated = function(f, replacement) {
+	if(!bohan.deprecationEnabled) return f;
+	return function(/*arguments*/) {
+		var msg = 'warning: deprecated function \'' + f.toString().split('{')[0].replace(/ $/, '') + '\' called.';
+		if(replacement) msg += ' Please use \'' + replacement + '\' instead.';
+		bohan.log(msg);
+		f.apply(this, arguments);
+	};
+}
+
