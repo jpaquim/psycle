9fdd015c8d751515673df8d0b577ad8d32f38a15
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2011-11-07 19:07:13 +0000

    warning

--- tests/c++0x/e.cpp
+++ tests/c++0x/e.cpp
@@ -15,6 +15,7 @@ struct d : public b {
 
 template<typename T>
 struct v : public std::vector<T*> {
+	// warning: http://stackoverflow.com/questions/4807643/container-covariance-in-c
 	template<typename U>
 	operator v<U> const & () const {
 		static_assert(boost::is_base_and_derived<U, T>::value, "target element type is base of this element type");
