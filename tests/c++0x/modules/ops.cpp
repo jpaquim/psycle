78ef23de78f519b56e37853e2e43fae35daba43c
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2011-12-12 02:55:37 +0000

    module impl test

---
+++ tests/c++0x/modules/ops.cpp
@@ -0,0 +1,9 @@
+
+#include "ops.hpp"
+
+module_begin(ops)
+	number add_impl(number a, number b) {
+		return a + b;
+	}
+module_end
+
---
+++ tests/c++0x/modules/ops.hpp
@@ -0,0 +1,17 @@
+#ifndef OPS__INCLUDED
+#define OPS__INCLUDED
+#pragma once
+
+#include <module.hpp>
+
+module_begin(ops)
+	float typedef number;
+	number add_impl(number, number);
+	number inline add(number a, number b) { return private_::add_impl(a, b); }
+	
+	module_export(number)
+	module_export(add)
+module_end
+
+#endif
+
