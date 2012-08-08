0ba2739a7e7b6510023e73625e558c5a7b4fbfaf
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2012-08-08 22:55:46 +0000

    attempt to fix build failures on older systems

--- universalis/src/universalis/compiler/thread_local_storage.hpp
+++ universalis/src/universalis/compiler/thread_local_storage.hpp
@@ -7,27 +7,24 @@
 #pragma once
 #include <universalis/detail/project.hpp>
 
-#if defined DIVERSALIS__COMPILER__FEATURE__CXX0X && !defined DIVERSALIS__STDLIB__CXX0X__BROKEN__THREAD && !defined DIVERSALIS__COMPILER__GNU
+#if __cplusplus >= 201103L
 	// thread_local is a keyword
-#else
+#elif \
+	defined DIVERSALIS__COMPILER__GNU && ( \
+		/* TLS support added on windows only in version 4.3.0 */ \
+		!defined DIVERSALIS__OS__MICROSOFT || \
+		DIVERSALIS__COMPILER__VERSION >= 40300 \
+	)
+
+	#define thread_local __thread
+#elif defined DIVERSALIS__COMPILER__MICROSOFT
 	#include "attribute.hpp"
+	#define thread_local UNIVERSALIS__COMPILER__ATTRIBUTE(thread)
+#else
+	/// variable stored in a per thread local storage.
+	#define thread_local
 
-	#if \
-		defined DIVERSALIS__COMPILER__GNU && ( \
-			/* TLS support added on windows only in version 4.3.0 */ \
-			!defined DIVERSALIS__OS__MICROSOFT || \
-			DIVERSALIS__COMPILER__VERSION >= 40300 \
-		)
-
-		#define thread_local __thread
-	#elif defined DIVERSALIS__COMPILER__MICROSOFT
-		#define thread_local UNIVERSALIS__COMPILER__ATTRIBUTE(thread)
-	#else
-		/// variable stored in a per thread local storage.
-		#define thread_local
-
-		#if !defined DIVERSALIS__COMPILER__FEATURE__NOT_CONCRETE && defined DIVERSALIS__COMPILER__FEATURE__WARNING
-			#warning "Cannot generate thread-safe code for this compiler ; please add support for thread-local storage for your compiler in the file where this warning is triggered."
-		#endif
+	#if !defined DIVERSALIS__COMPILER__FEATURE__NOT_CONCRETE && defined DIVERSALIS__COMPILER__FEATURE__WARNING
+		#warning "Cannot generate thread-safe code for this compiler ; please add support for thread-local storage for your compiler in the file where this warning is triggered."
 	#endif
 #endif
