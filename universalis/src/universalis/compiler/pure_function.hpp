9bda15388b911becba1129ec110a22dc3560e1fb
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2013-06-16 17:11:38 +0000

    missing #pragma once

--- universalis/src/universalis/compiler/pure_function.hpp
+++ universalis/src/universalis/compiler/pure_function.hpp
@@ -1,6 +1,7 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 2013-2013 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
+#pragma once
 #include "attribute.hpp"
 
 #if defined DIVERSALIS__COMPILER__GNU
