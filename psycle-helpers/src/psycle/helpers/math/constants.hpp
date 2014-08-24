a778a23393cb7d8ce4e70ab2a433511b2cd19df0
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2014-08-24 17:58:26 +0000

    added a stdlib/cmath.hpp header to universalis for math stuffs that are part of the standard, or ought to be soon, and made psycle_helpers::math point to those in universalis (with using directives) so that the transition is transparent.

--- psycle-helpers/src/psycle/helpers/math/constants.hpp
+++ psycle-helpers/src/psycle/helpers/math/constants.hpp
@@ -4,21 +4,16 @@
 #ifndef PSYCLE__HELPERS__MATH__CONSTANTS__INCLUDED
 #define PSYCLE__HELPERS__MATH__CONSTANTS__INCLUDED
 #pragma once
+#include <universalis/stdlib/cmath.hpp>
 
 namespace psycle { namespace helpers { namespace math {
 
-/// john napier's log(2) aka ln(2) constant
-double const log2 = /* std::log(2) */ .69314718055994530941723212145817656807550013436025/*...*/;
-/// apery's zeta(3) constant
-double const zeta3 = 1.20205690315959428539973816151144999076498629234049/*...*/;
-/// leibniz's and euler's e constant
-double const e = /* std::exp(1) */ 2.71828182845904523536028747135266249775724709369995/*...*/;
-/// pythagoreans' nu2 constant
-double const nu2 = 1.41421356237309504880168872420969807856967187537694/*...*/;
-/// leonhard euler's gamma constant
-double const gamma = .57721566490153286060651209008240243104215933593992/*...*/;
-/// archimedes' pi constant
-double const pi = /* std::acos(-1) */ 3.1415926535897932384626433832795028841971693993751/*...*/;
+using universalis::stdlib::log2;
+using universalis::stdlib::zeta3;
+using universalis::stdlib::e;
+using universalis::stdlib::nu2;
+using universalis::stdlib::gamma;
+using universalis::stdlib::pi;
 float const pi_f = float(pi);
 double const radians_per_degree = pi / 180;
 
