a477ce08264e812444196b0abf6b43f7604d1122
johan-boule <johan-boule@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2015-06-23 21:06:43 +0000

    boggus

--- tests/modules/prog.cpp
+++ tests/modules/prog.cpp
@@ -6,12 +6,6 @@ int main() {
 	std::cout << foo.compute(3) << std::endl;
 }
 
-#if 0 // boggus
-	#ifdef FOO
-		#error foo was not imported as a module
-	#endif
-#endif
-
 #ifdef BAR
 	#error bar was not imported as a module
 #endif
