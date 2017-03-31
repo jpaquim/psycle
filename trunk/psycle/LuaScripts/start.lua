2dff054db4ecd05e3dcb687f8d576387cb0a4b92
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2017-03-31 14:41:03 +0000

    childview resize repaint

--- trunk/psycle/LuaScripts/start.lua
+++ trunk/psycle/LuaScripts/start.lua
@@ -24,3 +24,39 @@ local link = {
 }
 psycle.addmenu("view", link)  
 
+
+-- @designer.lua
+  link = {
+    label = "designer",
+    plugin = "designer.lua",
+    viewport = psycle.CHILDVIEWPORT,
+    userinterface = psycle.SDI
+  }
+  psycle.addmenu("view", link)
+
+-- @gamecontrollertest.lua
+  link = {
+    label = "gamecontrollertest",
+    plugin = "gamecontrollertest.lua",
+    viewport = psycle.CHILDVIEWPORT,
+    userinterface = psycle.SDI
+  }
+  psycle.addmenu("view", link)
+
+-- @pianoroll.lua
+  link = {
+    label = "pianoroll",
+    plugin = "pianoroll.lua",
+    viewport = psycle.CHILDVIEWPORT,
+    userinterface = psycle.SDI
+  }
+  psycle.addmenu("view", link)
+
+-- @test.lua
+  link = {
+    label = "test",
+    plugin = "test.lua",
+    viewport = psycle.CHILDVIEWPORT,
+    userinterface = psycle.SDI
+  }
+  psycle.addmenu("view", link)
