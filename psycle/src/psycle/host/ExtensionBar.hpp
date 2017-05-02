adee4c2a8165e0f1ee7bc6275ed11d5090bff8f3
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2017-05-02 10:35:09 +0000

    enabled events pattern/machineview

--- psycle/src/psycle/host/ExtensionBar.hpp
+++ psycle/src/psycle/host/ExtensionBar.hpp
@@ -5,35 +5,50 @@
 #include "Ui.hpp"
 
 namespace psycle { namespace host {
-	class CMainFrame;
-	class CChildView;	
+		
+	class LuaPlugin;
 
+	class ExtensionWindow : public ui::Group {
+		public:
+			ExtensionWindow();
+			~ExtensionWindow() {}
+
+			void Push(LuaPlugin& plugin);			   
+			
+			void Push();
+			void Pop();
+			void DisplayTop();
+			bool HasExtensions() const { return !extensions_.empty(); }
+			void RemoveExtensions();
+			void UpdateMenu(class MenuHandle& menu_handle);
+
+		protected:
+			virtual void OnSize(const ui::Dimension& dimension) { UpdateAlign(); }
+
+		private:
+			typedef std::stack<boost::weak_ptr<LuaPlugin> > Extensions;
+			Extensions extensions_;
+	};
+	
 	class ExtensionBar : public CDialogBar
-	{
-		friend class CMainFrame;
+	{		
 		DECLARE_DYNAMIC(ExtensionBar)
 	public:
 		ExtensionBar();   // standard constructor
 		virtual ~ExtensionBar();
-		
-		void InitializeValues(CMainFrame* frame, CChildView* view);
-		void set_minimum_dimension(const ui::Dimension& dimension);
-		boost::shared_ptr<CWnd> m_luaWndView;
-		void Resize();
+				
+		void Add(LuaPlugin& plugin);
+		boost::shared_ptr<ExtensionWindow> m_luaWndView;
 
 	protected:
 		virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 			
 		DECLARE_MESSAGE_MAP()
 		afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
-		afx_msg LRESULT OnInitDialog ( WPARAM , LPARAM );									
-		void ResizeFloating();			
-		CMainFrame* m_pParentMain;
-		CChildView*  m_pWndView;		
-		CSize CalcDynamicLayout(int length, DWORD dwMode);	
+		afx_msg LRESULT OnInitDialog ( WPARAM , LPARAM );													
+		CSize CalcDynamicLayout(int length, DWORD dwMode);			
 
 	private:
-		ui::Dimension minimum_dimension_;
 		CSize m_sizeFloating;
 		bool docked(DWORD mode) const { return (mode & LM_VERTDOCK) || (mode & LM_HORZDOCK); }
 	};
