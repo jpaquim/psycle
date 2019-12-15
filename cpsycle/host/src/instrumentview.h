42e9d0489e935d22b03d623c823b526e2be572fa
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2019-12-15 10:29:14 +0000

    -unneeded files

--- cpsycle/host/src/instrumentview.h
+++ cpsycle/host/src/instrumentview.h
@@ -1,40 +1,70 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 2000-2019 members of the psycle project http://psycle.sourceforge.net
 
-#if !defined(SAMPLERINSTRUMENTVIEW_H)
-#define SAMPLERINSTRUMENTVIEW_H
+#if !defined(INSTRUMENTVIEW_H)
+#define INSTRUMENTVIEW_H
 
-#include "tabbar.h"
 #include "envelopeview.h"
+#include "tabbar.h"
+#include "instrumentsbox.h"
+#include "samplesbox.h"
+#include "workspace.h"
+
 #include <uinotebook.h>
 #include <uilabel.h>
 #include <uiedit.h>
 #include <uibutton.h>
 #include <uislider.h>
-#include "instrumentsbox.h"
 #include <uigroupbox.h>
-#include "workspace.h"
 
 typedef struct {
 	ui_component component;	
 	ui_label namelabel;
 	ui_edit nameedit;	
-	ui_button previnstrumentbutton;
-	ui_button nextinstrumentbutton;
-	ui_button deleteinstrumentbutton;
+	ui_button prevbutton;
+	ui_button nextbutton;	
 	Instrument* instrument;
 	Instruments* instruments;
-} SamplerInstrumentHeaderView;
+} InstrumentHeaderView;
 
 typedef struct {
-	ui_component component;		
+	ui_component component;	
+	int dy;
+} InstrumentKeyboardView;
+
+void instrumentkeyboardview_init(InstrumentKeyboardView*,
+	ui_component* parent);
+
+typedef struct {
+	ui_component component;	
+	int dy;
+	Instrument* instrument;
+} InstrumentEntryView;
+
+void instrumententryview_init(InstrumentEntryView*,
+	ui_component* parent);
+
+typedef struct {
+	ui_component component;
+	ui_button load;
+	ui_button save;
+	ui_button duplicate;
+	ui_button del;
+} InstrumentViewButtons;
+
+typedef struct {
+	ui_component component;
+	ui_component nna;
 	ui_label nnaheaderlabel;
 	ui_button nnacutbutton;
 	ui_button nnareleasebutton;
 	ui_button nnanonebutton;
+	SamplesBox samplesbox;
+	InstrumentKeyboardView keyboard;
+	InstrumentEntryView entryview;
 	Instrument* instrument;
 	Instruments* instruments;
-} SamplerInstrumentGeneralView;
+} InstrumentGeneralView;
 
 typedef struct {
 	ui_component component;	
@@ -47,13 +77,13 @@ typedef struct {
 	Player* player;
 	Instrument* instrument;
 	Instruments* instruments;
-} SamplerInstrumentVolumeView;
+} InstrumentVolumeView;
 
 typedef struct {
 	ui_component component;		
 	Instrument* instrument;
 	Instruments* instruments;
-} SamplerInstrumentPanView;
+} InstrumentPanView;
 
 typedef struct {
 	ui_component component;	
@@ -69,28 +99,32 @@ typedef struct {
 	Player* player;
 	Instrument* instrument;
 	Instruments* instruments;
-} SamplerInstrumentFilterView;
+} InstrumentFilterView;
 
 typedef struct {
 	ui_component component;		
 	Instrument* instrument;
 	Instruments* instruments;
-} SamplerInstrumentPitchView;
+} InstrumentPitchView;
 
 typedef struct {
-	ui_component component;	
+	ui_component component;
 	ui_notebook notebook;
 	TabBar tabbar;
-	InstrumentsBox instrumentsbox;	
-	SamplerInstrumentHeaderView header;
-	SamplerInstrumentGeneralView general;
-	SamplerInstrumentVolumeView volume;
-	SamplerInstrumentPanView pan;
-	SamplerInstrumentFilterView filter;
-	SamplerInstrumentPitchView pitch;
+	ui_component left;
+	ui_label label;
+	InstrumentViewButtons buttons;
+	InstrumentsBox instrumentsbox;
+	ui_component client;
+	InstrumentHeaderView header;
+	InstrumentGeneralView general;
+	InstrumentVolumeView volume;
+	InstrumentPanView pan;
+	InstrumentFilterView filter;
+	InstrumentPitchView pitch;	
 	Player* player;
-} SamplerInstrumentView;
+} InstrumentView;
 
-void InitSamplerInstrumentView(SamplerInstrumentView*, ui_component* parent, Workspace*);
+void instrumentview_init(InstrumentView*, ui_component* parent, Workspace*);
 
 #endif
