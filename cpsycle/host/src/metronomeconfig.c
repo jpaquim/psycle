f3ea3e984994c6c2818178f5a851ec49059b6e2d
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2021-02-13 14:30:21 +0000

    config

---
+++ cpsycle/host/src/metronomeconfig.c
@@ -0,0 +1,79 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2000-2021 members of the psycle project http://psycle.sourceforge.net
+
+#include "../../detail/prefix.h"
+
+#include "metronomeconfig.h"
+
+static void metronomeconfig_make(MetronomeConfig*, psy_Property*);
+
+void metronomeconfig_init(MetronomeConfig* self, psy_Property* parent)
+{
+	assert(self && parent);
+
+	self->parent = parent;
+	metronomeconfig_make(self, parent);
+	psy_signal_init(&self->signal_changed);
+}
+
+void metronomeconfig_dispose(MetronomeConfig* self)
+{
+	assert(self);
+
+	psy_signal_dispose(&self->signal_changed);
+}
+
+void metronomeconfig_make(MetronomeConfig* self, psy_Property* parent)
+{
+	assert(self);
+
+	self->metronome = psy_property_setid(psy_property_settext(
+		psy_property_append_section(parent, "metronome"),
+		"settingsview.metronome"), PROPERTY_ID_METRONOME);
+	psy_property_setid(psy_property_settext(
+		psy_property_append_bool(self->metronome, "showmetronome", FALSE),
+		"settingsview.metronome-show"), PROPERTY_ID_SHOWMETRONOME);
+	psy_property_settext(
+		psy_property_append_int(self->metronome, "machine", 0x3F, 0, 0x40),
+		"settingsview.metronome-machine");
+	psy_property_settext(
+		psy_property_append_int(self->metronome, "note", 48, 0, 119),
+		"settingsview.metronome-note");
+}
+
+// Properties
+uint8_t metronomeconfig_note(const MetronomeConfig* self)
+{
+	assert(self);
+
+	return (uint8_t)(psy_property_at_int(self->metronome, "note", 48));
+}
+
+uintptr_t metronomeconfig_machine(const MetronomeConfig* self)
+{
+	assert(self);
+
+	return (psy_property_at_int(self->metronome, "machine", 0x3F));		
+}
+
+bool metronomeconfig_showmetronomebar(const MetronomeConfig* self)
+{
+	assert(self);
+
+	return (psy_property_at_bool(self->metronome, "showmetronome", FALSE));
+}
+
+// events
+bool metronomeconfig_onchanged(MetronomeConfig* self, psy_Property*
+	property)
+{
+	psy_signal_emit(&self->signal_changed, self, 1, property);
+	return TRUE;
+}
+
+bool metronomeconfig_hasproperty(const MetronomeConfig* self, psy_Property* property)
+{
+	assert(self && self->metronome);
+
+	return psy_property_insection(property, self->metronome);
+}
---
+++ cpsycle/host/src/metronomeconfig.h
@@ -0,0 +1,44 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2000-2021 members of the psycle project http://psycle.sourceforge.net
+
+#if !defined(METRONOMECONFIG_H)
+#define METRONOMECONFIG_H
+
+// audio
+#include <machinefactory.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// MetronomeConfig
+
+enum {
+	PROPERTY_ID_METRONOME = 12000,
+	PROPERTY_ID_SHOWMETRONOME
+};
+
+typedef struct MetronomeConfig {
+	// signals
+	psy_Signal signal_changed;
+	psy_Property* metronome;
+	// references
+	psy_Property* parent;	
+} MetronomeConfig;
+
+void metronomeconfig_init(MetronomeConfig*, psy_Property* parent);
+void metronomeconfig_dispose(MetronomeConfig*);
+
+bool metronomeconfig_showmetronomebar(const MetronomeConfig*);
+uint8_t metronomeconfig_note(const MetronomeConfig* self);
+uintptr_t metronomeconfig_machine(const MetronomeConfig*);
+
+bool metronomeconfig_onchanged(MetronomeConfig*, psy_Property*);
+bool metronomeconfig_hasproperty(const MetronomeConfig*, psy_Property*);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* METRONOMECONFIG_H */
