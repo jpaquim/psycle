a283e9c4f0a6e28376618ef2166a1e5d623f64fe
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2021-03-08 07:36:20 +0000

    stackview

---
+++ trunk/cpsycle/host/src/switchui.c
@@ -0,0 +1,157 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2000-2021 members of the psycle project http://psycle.sourceforge.net
+
+#include "../../detail/prefix.h"
+
+#include "switchui.h"
+// host
+#include "skingraphics.h"
+#include "paramview.h"
+#include "machineui.h"
+#include "wireview.h"
+// audio
+#include <exclusivelock.h>
+// std
+#include <math.h>
+// platform
+#include "../../detail/portable.h"
+#include "../../detail/trace.h"
+
+// SwitchUi
+// prototypes
+static void switchui_dispose(SwitchUi*);
+static void switchui_ondraw(SwitchUi*, psy_ui_Graphics*);
+static void switchui_invalidate(SwitchUi*);
+static void switchui_onpreferredsize(SwitchUi*, const psy_ui_Size* limit,
+	psy_ui_Size* rv);
+static void switchui_onmousedown(SwitchUi*, psy_ui_MouseEvent*);
+static void switchui_onmouseup(SwitchUi*, psy_ui_MouseEvent*);
+static void switchui_onmousemove(SwitchUi*, psy_ui_MouseEvent*);
+
+// vtable
+static psy_ui_ComponentVtable switchui_vtable;
+static psy_ui_ComponentVtable switchui_super_vtable;
+static bool switchui_vtable_initialized = FALSE;
+
+static psy_ui_ComponentVtable* switchui_vtable_init(SwitchUi* self)
+{
+	assert(self);
+
+	if (!switchui_vtable_initialized) {
+		switchui_vtable = *(self->component.vtable);
+		switchui_super_vtable = switchui_vtable;
+		switchui_vtable.dispose = (psy_ui_fp_component_dispose)switchui_dispose;
+		switchui_vtable.ondraw = (psy_ui_fp_component_ondraw)switchui_ondraw;		
+		switchui_vtable.invalidate = (psy_ui_fp_component_invalidate)
+			switchui_invalidate;
+		switchui_vtable.onpreferredsize = (psy_ui_fp_component_onpreferredsize)
+			switchui_onpreferredsize;
+		switchui_vtable.onmousedown = (psy_ui_fp_component_onmouseevent)switchui_onmousedown;
+		switchui_vtable.onmouseup = (psy_ui_fp_component_onmouseevent)switchui_onmouseup;
+		switchui_vtable.onmousemove = (psy_ui_fp_component_onmouseevent)switchui_onmousemove;
+		switchui_vtable_initialized = TRUE;
+	}
+	return &switchui_vtable;
+}
+// implementation
+void switchui_init(SwitchUi* self, psy_ui_Component* parent,
+	psy_ui_Component* view, psy_audio_MachineParam* param,
+	ParamSkin* paramskin)
+{
+	assert(self);	
+	assert(paramskin);	
+	assert(view);
+
+	psy_ui_component_init(&self->component, parent, view);
+	switchui_vtable_init(self);
+	self->component.vtable = &switchui_vtable;		
+	self->view = view;	
+	self->skin = paramskin;
+	self->param = param;
+	paramtweak_init(&self->paramtweak);
+}
+
+void switchui_dispose(SwitchUi* self)
+{
+	assert(self);
+	
+	switchui_super_vtable.dispose(&self->component);
+}
+
+void switchui_ondraw(SwitchUi* self, psy_ui_Graphics* g)
+{	
+	psy_ui_RealSize size;				
+	psy_ui_RealRectangle r;
+	char label[128];
+	
+	size = psy_ui_component_sizepx(&self->component);
+	psy_ui_setrectangle(&r, 0, 0, size.width, size.height);
+	psy_ui_drawsolidrectangle(g, r, self->skin->bottomcolour);
+	
+	if (!self->param || psy_audio_machineparam_normvalue(self->param) == 0.f) {
+		skin_blitcoord(g, &self->skin->mixerbitmap,
+			psy_ui_realpoint_zero(), &self->skin->switchoff);
+	} 	else {
+		skin_blitcoord(g, &self->skin->mixerbitmap,
+			psy_ui_realpoint_zero(), &self->skin->switchon);
+	}	
+	psy_snprintf(label, 128, "%s", "");
+	if (self->param && !psy_audio_machineparam_name(self->param, label)) {
+		if (!psy_audio_machineparam_label(self->param, label)) {
+			psy_snprintf(label, 128, "%s", "");
+		}
+	}
+	psy_ui_setbackgroundcolour(g, self->skin->topcolour);
+	psy_ui_settextcolour(g, self->skin->fonttopcolour);
+	psy_ui_setrectangle(&r,
+		psy_ui_realrectangle_width(&self->skin->switchon.dest), 0,
+		size.width - psy_ui_realrectangle_width(&self->skin->switchon.dest),
+		size.height / 2);
+	psy_ui_textoutrectangle(g,
+		psy_ui_realpoint_make(
+			psy_ui_realrectangle_width(&self->skin->switchon.dest), 0.0),
+		psy_ui_ETO_OPAQUE | psy_ui_ETO_CLIPPED,
+		r, label, strlen(label));	
+}
+
+void switchui_invalidate(SwitchUi* self)
+{
+	if (!machineui_vuupdate()) {
+		switchui_super_vtable.invalidate(&self->component);
+	}
+}
+
+void switchui_onpreferredsize(SwitchUi* self, const psy_ui_Size* limit,
+	psy_ui_Size* rv)
+{	
+	psy_ui_RealSize size;
+
+	size = mpfsize(self->skin, psy_ui_component_textmetric(self->view),
+		MPF_SWITCH, FALSE);
+	*rv = psy_ui_size_makepx(100.0, size.height);
+}
+
+void switchui_onmousedown(SwitchUi* self, psy_ui_MouseEvent* ev)
+{
+	if (ev->button == 1 && self->param != NULL) {		
+		paramtweak_begin(&self->paramtweak, NULL, psy_INDEX_INVALID);
+		self->paramtweak.param = self->param;
+		paramtweak_onmousedown(&self->paramtweak, ev);		
+		psy_ui_component_capture(&self->component);		
+	}
+}
+
+void switchui_onmousemove(SwitchUi* self, psy_ui_MouseEvent* ev)
+{
+	if (self->paramtweak.param != NULL) {
+		paramtweak_onmousemove(&self->paramtweak, ev);
+		psy_ui_component_invalidate(&self->component);
+	}
+}
+
+void switchui_onmouseup(SwitchUi* self, psy_ui_MouseEvent* ev)
+{
+	paramtweak_end(&self->paramtweak);
+	psy_ui_component_releasecapture(&self->component);
+	psy_ui_component_invalidate(&self->component);
+}
---
+++ trunk/cpsycle/host/src/switchui.h
@@ -0,0 +1,40 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2000-2021 members of the psycle project http://psycle.sourceforge.net
+
+#if !defined(SWITCHUI_H)
+#define SWITCHUI_H
+
+// host
+#include "machineeditorview.h" // vst view
+#include "machineframe.h"
+#include "machineviewskin.h"
+#include "workspace.h"
+// ui
+#include <uiedit.h>
+#include <uinotebook.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// SwitchUi
+typedef struct SwitchUi {
+	// inherits
+	psy_ui_Component component;
+	// internal
+	ParamTweak paramtweak;
+	// references
+	ParamSkin* skin;
+	Workspace* workspace;
+	psy_ui_Component* view;
+	psy_audio_MachineParam* param;
+} SwitchUi;
+
+void switchui_init(SwitchUi*, psy_ui_Component* parent,
+	psy_ui_Component* view, psy_audio_MachineParam*, ParamSkin*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SLIDERUI_H */
