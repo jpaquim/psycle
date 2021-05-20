067baa13bbcf34040f253d1f6448b680e5b3a8f8
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2021-05-20 13:58:28 +0000

    plugins

---
+++ trunk/cpsycle/host/src/searchfield.h
@@ -0,0 +1,45 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 2000-2021 members of the psycle project http://psycle.sourceforge.net
+
+#if !defined(SEARCHFIELD_H)
+#define SEARCHFIELD_H
+
+// ui
+#include <uiedit.h>
+#include <uiimage.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// NewMachine
+		
+struct NewMachine;
+
+typedef struct SearchField {
+	// inherits
+	psy_ui_Component component;
+	// signals
+	psy_Signal signal_changed;
+	// intern
+	psy_ui_Image image;
+	psy_ui_Edit edit;
+	bool hasdefaulttext;
+	char* defaulttext;	
+} SearchField;
+
+void searchfield_init(SearchField*, psy_ui_Component* parent);
+
+void searchfield_setdefaulttext(SearchField*, const char* text);
+const char* searchfield_text(const SearchField*);
+
+INLINE psy_ui_Component* searchfield_base(SearchField* self)
+{
+	return &self->component;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SEARCHFIELD_H */
--- trunk/cpsycle/ui/src/uiedit.h
+++ trunk/cpsycle/ui/src/uiedit.h
@@ -57,7 +57,7 @@ void psy_ui_edit_multiline_init(psy_ui_Edit*, psy_ui_Component* parent);
 
 void psy_ui_edit_enableinputfield(psy_ui_Edit*);
 void psy_ui_edit_settext(psy_ui_Edit*, const char* text);
-const char* psy_ui_edit_text(psy_ui_Edit* self);
+const char* psy_ui_edit_text(const psy_ui_Edit* self);
 void psy_ui_edit_setcharnumber(psy_ui_Edit*, int number);
 void psy_ui_edit_setlinenumber(psy_ui_Edit*, int number);
 void psy_ui_edit_enableedit(psy_ui_Edit*);
