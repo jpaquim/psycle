38e3cbe898735b8ebc13804b90aa35d0f051f48a
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2021-06-10 19:46:03 +0000

    seqeditor

---
+++ cpsycle/host/src/seqeditorentry.h
@@ -0,0 +1,133 @@
+/*
+** This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+**  copyright 2000-2021 members of the psycle project http://psycle.sourceforge.net
+*/
+
+#if !defined(SEQEDITORENTRY_H)
+#define SEQEDITORENTRY_H
+
+/* host */
+#include "intedit.h"
+#include "wavebox.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct SeqEditState;
+
+/* SeqEditEntry */
+typedef struct SeqEditEntry {
+	/* inherits */
+	psy_ui_Component component;
+	/* internal */
+	psy_audio_OrderIndex seqpos;
+	bool preventresize;
+	/* references */
+	struct SeqEditState* state;
+	psy_audio_SequenceEntry* seqentry;
+} SeqEditEntry;
+
+void seqeditentry_init(SeqEditEntry*,
+	psy_ui_Component* parent, psy_ui_Component* view, psy_audio_SequenceEntry*,
+	psy_audio_OrderIndex seqpos, struct SeqEditState*);
+
+void seqeditentry_startdrag(SeqEditEntry*, psy_ui_MouseEvent*);
+
+INLINE psy_audio_OrderIndex seqeditentry_seqpos(const SeqEditEntry* self)
+{
+	return self->seqpos;
+}
+
+/* SeqEditPatternEntry */
+typedef struct SeqEditPatternEntry {
+	/* inherits */
+	SeqEditEntry seqeditorentry;
+	/* internal */	
+	/* references */	
+	psy_audio_SequencePatternEntry* sequenceentry;	
+} SeqEditPatternEntry;
+
+void seqeditpatternentry_init(SeqEditPatternEntry*,
+	psy_ui_Component* parent, psy_ui_Component* view,
+	psy_audio_SequencePatternEntry*, psy_audio_OrderIndex seqpos,
+	struct SeqEditState*);
+
+SeqEditPatternEntry* seqeditpatternentry_alloc(void);
+SeqEditPatternEntry* seqeditpatternentry_allocinit(
+	psy_ui_Component* parent, psy_ui_Component* view,
+	psy_audio_SequencePatternEntry* entry, psy_audio_OrderIndex seqpos,
+	struct SeqEditState*);
+
+INLINE psy_ui_Component* seqeditpatternentry_base(SeqEditPatternEntry* self)
+{
+	assert(self);
+
+	return &self->seqeditorentry.component;
+}
+
+/* SeqEditMarkerEntry */
+typedef struct SeqEditMarkerEntry {
+	/* inherits */
+	SeqEditEntry seqeditorentry;
+	/* internal */
+	/* references */	
+	psy_audio_SequenceMarkerEntry* sequenceentry;	
+	bool preventedit;
+} SeqEditMarkerEntry;
+
+void seqeditmarkerentry_init(SeqEditMarkerEntry*,
+	psy_ui_Component* parent, psy_ui_Component* view,
+	psy_audio_SequenceMarkerEntry*, psy_audio_OrderIndex seqpos,
+	struct SeqEditState*);
+
+SeqEditMarkerEntry* seqeditmarkerentry_alloc(void);
+SeqEditMarkerEntry* seqeditmarkerentry_allocinit(
+	psy_ui_Component* parent, psy_ui_Component* view,
+	psy_audio_SequenceMarkerEntry* entry, psy_audio_OrderIndex seqpos,
+	struct SeqEditState*);
+
+INLINE psy_ui_Component* seqeditmarkerentry_base(SeqEditMarkerEntry* self)
+{
+	assert(self);
+
+	return &self->seqeditorentry.component;
+}
+
+/* SeqEditSampleEntry */
+typedef struct SeqEditSampleEntry {
+	/* inherits */
+	SeqEditEntry seqeditorentry;
+	/* internal */
+	WaveBox wavebox;
+	psy_ui_Label label;
+	/* references */		
+	psy_audio_SequenceSampleEntry* sequenceentry;	
+	bool preventedit;
+} SeqEditSampleEntry;
+
+void seqeditsampleentry_init(SeqEditSampleEntry*,
+	psy_ui_Component* parent, psy_ui_Component* view,
+	psy_audio_SequenceSampleEntry*, psy_audio_OrderIndex seqpos,
+	struct SeqEditState*);
+
+SeqEditSampleEntry* seqeditsampleentry_alloc(void);
+SeqEditSampleEntry* seqeditsampleentry_allocinit(
+	psy_ui_Component* parent, psy_ui_Component* view,
+	psy_audio_SequenceSampleEntry* entry, psy_audio_OrderIndex seqpos,
+	struct SeqEditState*);
+
+void seqeditsampleentry_updatesample(SeqEditSampleEntry*);
+
+INLINE psy_ui_Component* seqeditsampleentry_base(SeqEditSampleEntry* self)
+{
+	assert(self);
+
+	return &self->seqeditorentry.component;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SEQEDITORENTRY_H */
---
+++ cpsycle/host/src/seqeditorstate.c
@@ -0,0 +1,63 @@
+/*
+** This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+**  copyright 2000-2021 members of the psycle project http://psycle.sourceforge.net
+*/
+
+#include "../../detail/prefix.h"
+
+
+#include "seqeditorstate.h"
+/* audio */
+#include <exclusivelock.h>
+#include <sequencecmds.h>
+/* platform */
+#include "../../detail/portable.h"
+
+#define DEFAULT_PXPERBEAT 10.0
+
+void seqeditstate_init(SeqEditState* self, SequenceCmds* cmds,
+	psy_ui_Edit* edit)
+{	
+	self->workspace = cmds->workspace;
+	self->cmds = cmds;
+	self->edit = edit;
+	self->inserttype = psy_audio_SEQUENCEENTRY_PATTERN;
+	psy_signal_init(&self->signal_cursorchanged);
+	self->pxperbeat = DEFAULT_PXPERBEAT;
+	self->defaultlineheight = psy_ui_value_make_eh(2.0);	
+	self->lineheight = self->defaultlineheight;	
+	self->cursorposition = 0.0;
+	self->cursoractive = FALSE;
+	self->drawcursor = TRUE;
+	self->drawpatternevents = TRUE;		
+	self->dragposition = 0.0;	
+	self->dragseqpos = psy_audio_orderindex_zero();
+	self->dragtype = SEQEDIT_DRAGTYPE_MOVE;
+	self->dragstatus = SEQEDIT_DRAG_NONE;
+	self->updatecursorposition = TRUE;
+	self->seqentry = NULL;
+	self->cmd = SEQEDTCMD_NONE;	
+	self->showpatternnames = FALSE;	
+}
+
+void seqeditstate_dispose(SeqEditState* self)
+{
+	psy_signal_dispose(&self->signal_cursorchanged);
+}
+
+psy_audio_Sequence* seqeditstate_sequence(SeqEditState* self)
+{
+	if (workspace_song(self->workspace)) {
+		return &workspace_song(self->workspace)->sequence;
+	}
+	return NULL;
+}
+
+psy_dsp_big_beat_t seqeditstate_quantize(const SeqEditState* self,
+	psy_dsp_big_beat_t position)
+{
+	uintptr_t lpb;
+
+	lpb = psy_audio_player_lpb(workspace_player(self->workspace));
+	return floor(position * lpb) / (psy_dsp_big_beat_t)lpb;
+}
