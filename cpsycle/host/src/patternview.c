f9c1035ec6991513c21112a27100877a4b75f559
stefan001 <stefan001@f55022e6-6845-49fe-a7ed-519222bdd1fd>
2021-09-01 05:00:05 +0000

    build

--- cpsycle/host/src/patternview.c
+++ cpsycle/host/src/patternview.c
@@ -743,7 +743,7 @@ void patternview_readfont(PatternView* self)
 			psy_ui_app_zoomrate(psy_ui_app());
 #if PSYCLE_USE_TK == PSYCLE_TK_X11
 		/* todo */
-		self->linestate.lineheight = psy_ui_value_make_eh(1.0);
+		self->gridstate.lineheight = psy_ui_value_make_eh(1.0);		
 		fontinfo.lfHeight = 18;
 		self->baselfheight = 18;
 #else			
